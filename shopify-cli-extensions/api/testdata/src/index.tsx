6885c0d1f793e355c6eb90c430ffc00121cb8490
Trish Ta <trish.ta@shopify.com>
2021-08-30 11:22:05 -0400

    Fix tests

--- shopify-cli-extensions/api/testdata/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { render, Text } from "@shopify/checkout-ui-extensions-react";
-
-render("Checkout::Feature::Render", ({ extensionPoint }) => (
-  <App extensionPoint={extensionPoint} />
-));
-
-function App({ extensionPoint }) {
-  return <Text>Welcome to the {extensionPoint} extension!</Text>;
-}
