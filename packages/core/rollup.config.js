064ea2d0de1082a7ef4ad110f171ba7dc1bde3a2
Pedro Piñera <pedro@craftweg.com>
2022-01-11 17:51:20 +0100

    Implement the export of types

---
+++ packages/core/rollup.config.js
@@ -0,0 +1,50 @@
+import esbuild from 'rollup-plugin-esbuild';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import pkg from './package.json';
+import dts from 'rollup-plugin-dts'
+
+const entry = ['src/index.ts'];
+
+const external = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies),
+];
+
+export default ({ watch }) => [
+  {
+    input: entry,
+    output: {
+      dir: 'dist',
+      format: 'esm',
+      sourcemap: 'inline',
+    },
+    external,
+    plugins: [
+      resolve({
+        preferBuiltins: true,
+      }),
+      json(),
+      commonjs(),
+      esbuild({
+        target: 'node12',
+      }),
+    ],
+    onwarn(message) {
+      if (/Circular dependencies/.test(message)) return;
+      console.error(message);
+    },
+  },
+  {
+    input: entry,
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'esm',
+    },
+    external,
+    plugins: [
+      dts(),
+    ],
+  }
+];
---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+export interface User {
+    name: string
+}
+
+export default function (): User {
+ return {name: "Pedro"}
+}
