a44daeba9458d8d0164dc7dbead2ad62d48b21d6
Pedro Piñera <pedro@craftweg.com>
2022-01-11 17:43:50 +0100

    Set up the CLI package

---
+++ packages/cli/rollup.config.js
@@ -0,0 +1,38 @@
+import esbuild from 'rollup-plugin-esbuild';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import pkg from './package.json';
+
+const entries = ['src/index.ts'];
+
+const external = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies),
+];
+
+export default ({ watch }) => [
+  {
+    input: entries,
+    output: {
+      dir: 'dist',
+      format: 'esm',
+      sourcemap: 'inline',
+    },
+    external,
+    plugins: [
+      resolve({
+        preferBuiltins: true,
+      }),
+      json(),
+      commonjs(),
+      esbuild({
+        target: 'node12',
+      }),
+    ],
+    onwarn(message) {
+      if (/Circular dependencies/.test(message)) return;
+      console.error(message);
+    },
+  },
+];
---
+++ packages/cli/src/index.test.ts
@@ -0,0 +1,5 @@
+import { assert, expect, it, suite, test } from 'vitest';
+
+test('it works', () => {
+  expect(true).toBeTruthy();
+});
