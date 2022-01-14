a9e691d761c6cf3a4ec7c3f138a841d6feff121f
Pedro Piñera <pedro@craftweg.com>
2022-01-14 15:51:58 +0100

    Add prerelease

---
+++ bin/prerelease.mjs
@@ -0,0 +1,15 @@
+
+
+#!/usr/bin/env node
+
+import path from 'path';
+import {execSync} from 'child_process';
+import {fileURLToPath} from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const ROOT_PATH = path.resolve(__dirname, '..');
+
+console.log('🔁 Checking out latest commits and setting up environment');
+execSync(`git checkout main`, {stdio: 'inherit', cwd: ROOT_PATH});
+execSync(`git pull --tags`, {stdio: 'inherit', cwd: ROOT_PATH});
+execSync(`/opt/dev/bin/dev up`, {stdio: 'inherit', cwd: ROOT_PATH});
