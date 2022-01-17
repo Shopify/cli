d1762e26de5a5fc5cf6eba056b4d5850389aa297
Pedro Piñera <pedro@craftweg.com>
2022-01-17 18:32:18 +0100

    Fix the executable and add a template

--- packages/cli-app/src/commands/app/init.ts
+++
@@ -1,7 +0,0 @@
-import {Command} from '@oclif/core';
-
-export default class Scaffold extends Command {
-  static description = 'Scaffold a new block inside the app';
-
-  async run(): Promise<void> {}
-}
