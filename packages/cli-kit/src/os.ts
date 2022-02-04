d7332ca16a9921ac3df5a57279379ff3d1bb5824
Pedro <pedro@craftweg.com>
2022-02-04 15:27:07 +0100

    Vendor username

--- packages/cli-kit/src/os.ts
+++ packages/cli-kit/src/os.ts
@@ -1,3 +1,61 @@
-export const username = async () => {
-  return '';
+import process from 'node:process';
+import os from 'node:os';
+
+import {execa} from 'execa';
+
+const getEnvironmentVariable = () => {
+  const {env} = process;
+
+  return (
+    env.SUDO_USER ||
+    env.C9_USER ||
+    env.LOGNAME ||
+    env.USER ||
+    env.LNAME ||
+    env.USERNAME
+  );
+};
+
+const getUsernameFromOsUserInfo = (): string | null => {
+  try {
+    return os.userInfo().username;
+  } catch {
+    return null;
+  }
+};
+
+const cleanWindowsCommand = (value: string) => value.replace(/^.*\\/, '');
+
+const makeUsernameFromId = (userId: string) => `no-username-${userId}`;
+
+export const username = async (): Promise<string | null> => {
+  const environmentVariable = getEnvironmentVariable();
+  if (environmentVariable) {
+    return environmentVariable;
+  }
+
+  const userInfoUsername = getUsernameFromOsUserInfo();
+  if (userInfoUsername) {
+    return userInfoUsername;
+  }
+
+  /**
+	First we try to get the ID of the user and then the actual username. We do this because in `docker run --user <uid>:<gid>` context, we don't have "username" available. Therefore, we have a fallback to `makeUsernameFromId` for such scenario. Applies also to the `sync()` method below.
+	*/
+  try {
+    if (process.platform === 'win32') {
+      const {stdout} = await execa('whoami');
+      return cleanWindowsCommand(stdout);
+    }
+
+    const {stdout: userId} = await execa('id', ['-u']);
+    try {
+      const {stdout} = await execa('id', ['-un', userId]);
+      return stdout;
+      // eslint-disable-next-line no-empty
+    } catch {}
+    return makeUsernameFromId(userId);
+  } catch {
+    return null;
+  }
 };
