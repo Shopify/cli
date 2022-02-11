9c0b8ef4fd621a4eac7e20ebe99ef4c2c128ef14
Pedro <pedro@craftweg.com>
2022-02-11 10:23:12 +0100

    Fix issue with Node 12

--- packages/cli/rollup.config.js
+++ packages/cli/rollup.config.js
@@ -31,7 +31,7 @@ const configuration = () => [
             // Preserves the commands/... path
             return `commands/${chunkInfo.facadeModuleId
               .split('src/commands')
-              .at(-1)
+              .slice(-1)[0]
               .replace('ts', 'js')}`;
           } else {
             return '[name].js';
