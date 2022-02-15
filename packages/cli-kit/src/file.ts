be80ae6c19c5bd69756add30420a2265f59cb9f0
Pedro <pedro@craftweg.com>
2022-02-15 16:52:37 +0100

    Use fs-extra

--- packages/cli-kit/src/file.ts
+++ packages/cli-kit/src/file.ts
@@ -1,30 +1,30 @@
-import fs from 'fs';
+import fs from 'fs-extra';
 
-export async function read(path: fs.PathLike): Promise<string> {
-  return fs.promises.readFile(path, {encoding: 'utf-8'});
+export async function read(path: string): Promise<string> {
+  return fs.readFile(path, {encoding: 'utf-8'});
 }
 
-export async function write(path: fs.PathLike, data: string): Promise<void> {
-  return fs.promises.writeFile(path, data);
+export async function write(path: string, data: string): Promise<void> {
+  return fs.writeFile(path, data);
 }
 
-export async function mkdir(path: fs.PathLike): Promise<string | undefined> {
-  return fs.promises.mkdir(path, {recursive: true});
+export async function mkdir(path: string): Promise<void> {
+  return fs.mkdirp(path);
 }
 
-export async function rmdir(path: fs.PathLike): Promise<void> {
-  return fs.promises.rm(path, {recursive: true});
+export async function rmdir(path: string): Promise<void> {
+  return fs.rm(path, {recursive: true});
 }
 
 export async function mkTmpDir(): Promise<string> {
-  return fs.promises.mkdtemp('tmp-');
+  return fs.mkdtemp('tmp-');
 }
 
-export async function isDirectory(path: fs.PathLike): Promise<boolean> {
+export async function isDirectory(path: string): Promise<boolean> {
   return (await fs.promises.lstat(path)).isDirectory();
 }
 
-export async function exists(path: fs.PathLike): Promise<boolean> {
+export async function exists(path: string): Promise<boolean> {
   try {
     await fs.promises.access(path);
     return true;
