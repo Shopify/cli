48d5a8480cce92a5b18cf5f28a60fb3e67dc58b0
Ariel Caplan <ariel.caplan@shopify.com>
2022-02-22 23:11:38 +0200

    Linter fix

--- packages/app/src/services/extensionInit.test.ts
+++ packages/app/src/services/extensionInit.test.ts
@@ -1,88 +1,94 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import {describe, it, expect, vi, beforeEach, afterEach} from 'vitest';
-import {file, path} from '@shopify/cli-kit';
+import {describe, it, expect, vi, beforeEach, afterEach} from 'vitest'
+import {file, path} from '@shopify/cli-kit'
 
-import {App, load as loadApp} from '../cli/app/app';
-import extensionInit from './extensionInit';
-import {configurationFileNames, ExtensionTypes} from '../cli/constants';
+import {App, load as loadApp} from '../cli/app/app'
+import {configurationFileNames, ExtensionTypes} from '../cli/constants'
+
+import extensionInit from './extensionInit'
 
 class MockLog {
-  contents: string;
+  contents: string
 
   constructor() {
-    this.contents = "";
+    this.contents = ''
   }
 
   log(message: string) {
-    this.contents += `${message}\n`;
+    this.contents += `${message}\n`
   }
 
   clear() {
-    this.contents = "";
+    this.contents = ''
   }
 
   toFunc() {
-    return this.log.bind(this);
+    return this.log.bind(this)
   }
 }
 
-describe('initialize an extension', async () => {
-  let tmpDir: string;
-  let log: MockLog;
+describe('initialize an extension', () => {
+  let tmpDir: string
+  let log: MockLog
 
   beforeEach(async () => {
-    tmpDir = await file.mkTmpDir();
-    const appConfigurationPath = path.join(tmpDir, configurationFileNames.app);
+    tmpDir = await file.mkTmpDir()
+    const appConfigurationPath = path.join(tmpDir, configurationFileNames.app)
     const appConfiguration = `
       name = "my_app"
-      `;
-    await file.write(appConfigurationPath, appConfiguration);
-    log = new MockLog();
-  });
+      `
+    await file.write(appConfigurationPath, appConfiguration)
+    log = new MockLog()
+  })
   afterEach(async () => {
     if (tmpDir) {
-      await file.rmdir(tmpDir);
+      await file.rmdir(tmpDir)
     }
-  });
+  })
 
   interface CreateFromTemplateOptions {
-    name: string;
-    extensionType: ExtensionTypes;
+    name: string
+    extensionType: ExtensionTypes
   }
-  const createFromTemplate = async ({name, extensionType}: CreateFromTemplateOptions) => {
+  const createFromTemplate = async ({
+    name,
+    extensionType,
+  }: CreateFromTemplateOptions) => {
     await extensionInit({
       name,
       extensionType,
       parentApp: await loadApp(tmpDir),
       log: log.toFunc(),
-    });
+    })
   }
 
   it('successfully scaffolds the extension when no other extensions exist', async () => {
-    const name = "my-ext-1";
-    const extensionType = "checkout-post-purchase";
-    await createFromTemplate({name, extensionType});
-    expect(log.contents).toContain("Generating .shopify.ui-extension.toml");
-    expect(log.contents).toContain("Generating index.jsx");
-    const scaffoldedExtension = (await loadApp(tmpDir)).uiExtensions[0];
-    expect(scaffoldedExtension.configuration.name).toBe(name);
-  });
+    const name = 'my-ext-1'
+    const extensionType = 'checkout-post-purchase'
+    await createFromTemplate({name, extensionType})
+    expect(log.contents).toContain('Generating .shopify.ui-extension.toml')
+    expect(log.contents).toContain('Generating index.jsx')
+    const scaffoldedExtension = (await loadApp(tmpDir)).uiExtensions[0]
+    expect(scaffoldedExtension.configuration.name).toBe(name)
+  })
 
   it('successfully scaffolds the extension when another extension exists', async () => {
-    const name1 = "my-ext-1";
-    const name2 = "my-ext-2";
-    const extensionType = "checkout-post-purchase";
-    await createFromTemplate({name: name1, extensionType});
-    await createFromTemplate({name: name2, extensionType});
-    const scaffoldedExtension2 = (await loadApp(tmpDir)).uiExtensions[1];
-    expect(scaffoldedExtension2.configuration.name).toBe(name2);
-  });
+    const name1 = 'my-ext-1'
+    const name2 = 'my-ext-2'
+    const extensionType = 'checkout-post-purchase'
+    await createFromTemplate({name: name1, extensionType})
+    await createFromTemplate({name: name2, extensionType})
+    const scaffoldedExtension2 = (await loadApp(tmpDir)).uiExtensions[1]
+    expect(scaffoldedExtension2.configuration.name).toBe(name2)
+  })
 
   it('errors when trying to re-scaffold an existing extension', async () => {
-    const name = "my-ext-1";
-    const extensionType = "checkout-post-purchase";
-    await createFromTemplate({name, extensionType});
-    await expect(createFromTemplate({name, extensionType})).rejects.toThrow(`Extension ${name} already exists!`);
-  });
-});
+    const name = 'my-ext-1'
+    const extensionType = 'checkout-post-purchase'
+    await createFromTemplate({name, extensionType})
+    await expect(createFromTemplate({name, extensionType})).rejects.toThrow(
+      `Extension ${name} already exists!`,
+    )
+  })
+})
