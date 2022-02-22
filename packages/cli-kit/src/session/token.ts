b0460a8b0a786e9d2399088451038123f49fa484
Pedro <pedro@craftweg.com>
2022-02-22 12:13:12 +0100

    Add token class

---
+++ packages/cli-kit/src/session/token.ts
@@ -0,0 +1,55 @@
+/**
+ * It represents a temporary token that can be
+ * used to send authenticated HTTP requests.
+ */
+class Token {
+  /**
+   * A fully-qualified domain name of the service
+   * this token is for.
+   */
+  fqdn: string;
+
+  /**
+   * Access token
+   */
+  accessToken: string;
+
+  /**
+   * Token to refresh the access token if it has expired.
+   */
+  refreshToken?: string;
+
+  /**
+   * The expiration date of the session
+   */
+  expiresAt: Date;
+
+  /**
+   * The list of scopes the token has access to.
+   */
+  scopes: string[];
+
+  constructor(options: {
+    fqdn: string;
+    accessToken: string;
+    refreshToken?: string;
+    expiresAt: Date;
+    scopes: string[];
+  }) {
+    this.fqdn = options.fqdn;
+    this.accessToken = options.accessToken;
+    this.refreshToken = options.refreshToken;
+    this.expiresAt = options.expiresAt;
+    this.scopes = options.scopes;
+  }
+
+  /**
+   * Returns true if the session is expired.
+   * @returns {boolean} True if the session is expired.
+   */
+  get isExpired(): boolean {
+    return new Date() > this.expiresAt;
+  }
+}
+
+export default Token;
