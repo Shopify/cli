018f0184bbc998136a01ad7cca7934408dbd966e
Pedro <pedro@craftweg.com>
2022-03-01 11:50:48 +0100

    Expose the versions from cli-kit

--- packages/cli-kit/src/constants.ts
+++ packages/cli-kit/src/constants.ts
@@ -1,3 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import {version as cliKitVersion} from '../package.json'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import {version as cliVersion} from '../../cli/package.json'
+
 const constants = {
   environmentVariables: {
     debug: 'DEBUG',
@@ -9,6 +16,13 @@ const constants = {
     spinWorkspace: 'SPIN_WORKSPACE',
     spinNamespace: 'SPIN_NAMESPACE',
     spinHost: 'SPIN_HOST',
+  },
+  /**
+   * Versions are resolved at build time by Rollup's JSON plugin.
+   */
+  versions: {
+    cliKit: cliKitVersion,
+    cli: cliVersion,
   },
   keychain: {
     service: 'shopify-cli',
