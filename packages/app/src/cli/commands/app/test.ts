25e94352a891d3ff8dedcdf12c180208893edcaf
Isaac Roldán <isaac.roldan@shopify.com>
2022-03-01 14:51:06 +0100

    Remove manual testing code

--- packages/app/src/cli/commands/app/test.ts
+++ packages/app/src/cli/commands/app/test.ts
@@ -1,7 +1,4 @@
 import {Command} from '@oclif/core'
-import {api} from '@shopify/cli-kit'
-// import {fetchApiVersion} from '@shopify/cli-kit/src/api/admin-api'
-import {gql} from 'graphql-request'
 
 export default class Test extends Command {
   static description = 'Run the tests for a given block or app'
