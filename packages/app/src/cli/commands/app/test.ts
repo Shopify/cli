c95ba5789f5dbe685e6fd81f81f6b4c14e25d17d
Isaac Roldán <isaac.roldan@shopify.com>
2022-03-01 14:53:49 +0100

    Restore unwanted change

--- packages/app/src/cli/commands/app/test.ts
+++ packages/app/src/cli/commands/app/test.ts
@@ -2,5 +2,6 @@ import {Command} from '@oclif/core'
 
 export default class Test extends Command {
   static description = 'Run the tests for a given block or app'
+
   async run(): Promise<void> {}
 }
