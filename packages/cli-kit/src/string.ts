a4222cea3d602f129453c5bbdd4e3d12ae100a2b
Isaac Roldán <isaac.roldan@shopify.com>
2022-03-03 10:47:40 +0100

    Extract uppercase outside of the loop

--- packages/cli-kit/src/string.ts
+++ packages/cli-kit/src/string.ts
@@ -6,9 +6,7 @@ export function randomHex(size: number): string {
   let _size = size || 16
   let result = ''
   while (_size--) {
-    result += Math.floor(Math.random() * 16)
-      .toString(16)
-      .toUpperCase()
+    result += Math.floor(Math.random() * 16).toString(16)
   }
-  return result
+  return result.toUpperCase()
 }
