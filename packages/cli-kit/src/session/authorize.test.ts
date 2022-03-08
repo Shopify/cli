851cd8aacca978697b57d9b8b1ff7d5fe14dfc2c
Isaac Roldán <isaac.roldan@shopify.com>
2022-03-08 11:06:53 +0100

    Add missing mock to test

--- packages/cli-kit/src/session/authorize.test.ts
+++ packages/cli-kit/src/session/authorize.test.ts
@@ -6,11 +6,13 @@ import {identity} from '../environment/fqdn'
 
 import {authorize, MismatchStateError} from './authorize'
 import {listenRedirect} from './redirect-listener'
+import {clientId} from './identity'
 
 vi.mock('../system')
 vi.mock('./redirect-listener')
 vi.mock('../string')
 vi.mock('../environment/fqdn')
+vi.mock('./identity')
 
 const port = 3456
 const host = '127.0.0.1'
@@ -26,13 +28,14 @@ describe('authorize', () => {
     vi.mocked(generateRandomChallengePair).mockReturnValue(challenge)
     vi.mocked(listenRedirect).mockResolvedValue({code: 'code', state: 'state'})
     vi.mocked(identity).mockResolvedValue('fqdn.com')
+    vi.mocked(clientId).mockResolvedValue('clientId')
 
     // When
     const got = await authorize(['scope1', 'scope2'], 'state')
 
     // Then
     const url =
-      'http://fqdn.com/oauth/authorize?client_id=fbdb2649-e327-4907-8f67-908d24cfd7e3&scope=scope1+scope2&redirect_uri=http%3A%2F%2F127.0.0.1%3A3456&state=state&response_type=code&code_challenge_method=S256&code_challenge=challenge'
+      'http://fqdn.com/oauth/authorize?client_id=clientId&scope=scope1+scope2&redirect_uri=http%3A%2F%2F127.0.0.1%3A3456&state=state&response_type=code&code_challenge_method=S256&code_challenge=challenge'
 
     expect(open).toHaveBeenCalledWith(url)
     expect(listenRedirect).toHaveBeenCalledWith(host, port)
