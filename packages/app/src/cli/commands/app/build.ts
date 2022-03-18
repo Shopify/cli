5115d31886a50ec1588e5183f8f06634d22fbc61
Isaac Roldán <isaac.roldan@shopify.com>
2022-03-18 12:04:44 +0100

    Remove consolelog

--- packages/app/src/cli/commands/app/build.ts
+++ packages/app/src/cli/commands/app/build.ts
@@ -1,12 +1,13 @@
 import {Command} from '@oclif/core'
 import {session} from '@shopify/cli-kit'
+import {success} from '@shopify/cli-kit/src/output'
 
 export default class Build extends Command {
   static description = 'Build a block or an app'
 
   async run(): Promise<void> {
     const token = await session.ensureAuthenticatedPartners()
-    console.log('TOKEN:', token)
+    success(`TOKEN: ${token}`)
     // await session.ensureAuthenticated({})
   }
 }
