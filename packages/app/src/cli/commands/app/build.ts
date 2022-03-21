c48e180376abc9272f0e8f8131619c7f3c195707
Isaac Roldán <isaac.roldan@shopify.com>
2022-03-21 17:07:02 +0100

    restore build

--- packages/app/src/cli/commands/app/build.ts
+++ packages/app/src/cli/commands/app/build.ts
@@ -1,11 +1,13 @@
 import {Command} from '@oclif/core'
-import {session, ui} from '@shopify/cli-kit'
-import {message} from '@shopify/cli-kit/src/output'
+import {session} from '@shopify/cli-kit'
+import {success} from '@shopify/cli-kit/src/output'
 
 export default class Build extends Command {
   static description = 'Build a block or an app'
 
   async run(): Promise<void> {
-    await session.ensureAuthenticated({})
+    const token = await session.ensureAuthenticated({})
+    success(`TOKEN: ${token}`)
+    // await session.ensureAuthenticated({})
   }
 }
