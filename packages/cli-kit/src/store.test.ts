8fa9b235cf9e0ebbf59412020b3cb84e6934333b
Isaac Roldán <isaac.roldan@shopify.com>
2022-03-30 16:02:52 +0200

    Add tests for store.ts

---
+++ packages/cli-kit/src/store.test.ts
@@ -0,0 +1,56 @@
+import {describe, expect, it} from 'vitest'
+import {cliKit, getAppInfo, setAppInfo} from '$store'
+
+const APP1 = {appId: 'app1', storeFqdn: 'store1', orgId: 'org1'}
+const APP2 = {appId: 'app2', storeFqdn: 'store2', orgId: 'org2'}
+const APP1Updated = {appId: 'app1', storeFqdn: 'store1-updated', orgId: 'org1-updated'}
+
+describe('getAppInfo', () => {
+  it('returns cached info if exists', () => {
+    // Given
+    cliKit.set('appInfo', [APP1, APP2])
+
+    // When
+    const got = getAppInfo('app1')
+
+    // Then
+    expect(got).toEqual(APP1)
+  })
+
+  it('returns undefined if it does not exists', () => {
+    // Given
+    cliKit.set('appInfo', [APP1, APP2])
+
+    // When
+    const got = getAppInfo('app3')
+
+    // Then
+    expect(got).toEqual(undefined)
+  })
+})
+
+describe('setAppInfo', () => {
+  it('updates cached info if exists', () => {
+    // Given
+    cliKit.set('appInfo', [APP1, APP2])
+
+    // When
+    setAppInfo('app1', {storeFqdn: 'store1-updated', orgId: 'org1-updated'})
+    const got = cliKit.get('appInfo')
+
+    // Then
+    expect(got).toEqual([APP1Updated, APP2])
+  })
+
+  it('creates new info if it does not exists', () => {
+    // Given
+    cliKit.set('appInfo', [APP1])
+
+    // When
+    setAppInfo('app2', {storeFqdn: APP2.storeFqdn, orgId: APP2.orgId})
+    const got = cliKit.get('appInfo')
+
+    // Then
+    expect(got).toEqual([APP1, APP2])
+  })
+})
