aa02856bca1f7a6baeaa1bed3a7a92cdc1d4266c
Pedro Piñera Buendía <pedro@craftweg.com>
2022-04-01 17:22:34 +0200

    Glue everything

--- packages/app/src/cli/services/dev.ts
+++ packages/app/src/cli/services/dev.ts
@@ -1,28 +1,29 @@
 import {ensureDevEnvironment} from './dev/environment'
 import {updateURLs} from './dev/update-urls'
 import {createTunnel} from './dev/tunnel'
-import {App} from '../models/app/app'
-import {output} from '@shopify/cli-kit'
+import {App, Home} from '../models/app/app'
+import {output, system} from '@shopify/cli-kit'
 
 interface DevOptions {
   appInfo: App
 }
 
-async function dev({app}: DevOptions) {
-  await ensureDevEnvironment(app)
+async function dev({appInfo}: DevOptions) {
+  const {app, store} = await ensureDevEnvironment(appInfo)
   const url = await createTunnel()
-  if (!app.configuration.id) return
-  await updateURLs(app.configuration.id, url)
-  output.success(`Your app is available at: ${url}/auth?shop=development-lifecycle-store.myshopify.com`)
+  await updateURLs(app.id, url)
+  output.success(`Your app is available at: ${url}/auth?${store.shopDomain}`)
+  devHome(appInfo.home)
+}
 
-  const {org, app, store} = await ensureDevEnvironment(appInfo)
-  output.info(`Running dev with ${app.title} on ${store.shopName}`)
-//   log('Connecting to the platform...')
-  await new Promise((resolve, reject) => setInterval(resolve, 1 * 1000))
-//   log(
-//     `Your app is available at: https://54b7-2003-fb-ef0b-39ff-990c-d5ba-10e2-ff79.ngrok.io/auth?shop=development-lifecycle-store.myshopify.com`,
-//   )
-  await new Promise((resolve, reject) => setInterval(resolve, 20 * 1000))
+async function devHome(home: Home) {
+  const script = home.configuration.commands.dev
+  if (!script) {
+    return
+  }
+
+  const [cmd, ...args] = script.split(' ')
+  await system.exec(cmd, args, {cwd: home.directory})
 }
 
 export default dev
