54065193e39274dc8a479037a16fbd45ddd1f2d7
Isaac Roldán <isaac.roldan@shopify.com>
2022-04-07 15:54:43 +0200

    Add some comments

--- packages/cli-kit/src/ruby.ts
+++ packages/cli-kit/src/ruby.ts
@@ -4,11 +4,12 @@ import {concurrent} from '$output'
 
 export async function exec(args: string[], token: string) {
   await validateRubyEnv()
-  await concurrent(0, 'ruby', async (stdout) => {
+  // PENDING: Call the ruby CLI once integrated
+  await concurrent(0, 'themes', async (stdout) => {
     try {
       await system.exec('ruby', args, {stdout, env: {...process.env, SHOPIFY_ADMIN_TOKEN: token}})
     } catch (error: any) {
-      throw new Fatal(`Ruby command "${error.command}" failed\n${error.stderr}`)
+      throw new Fatal(`Command "${error.command}" failed\n${error.stderr}`)
     }
   })
 }
@@ -17,6 +18,9 @@ async function validateRubyEnv() {
   try {
     await system.exec('ruby', ['-v'])
   } catch (error) {
-    throw new Fatal('Ruby environment not found')
+    throw new Fatal(
+      'Ruby environment not found',
+      'Make sure you have ruby installed on your system: https://www.ruby-lang.org/en/documentation/installation/',
+    )
   }
 }
