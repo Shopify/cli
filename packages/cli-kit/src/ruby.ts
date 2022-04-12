4c84b1bb9be2a6a2dd211c876f89756259711617
Isaac Roldán <isaac.roldan@shopify.com>
2022-04-12 12:46:11 +0200

    execute ruby cli command

--- packages/cli-kit/src/ruby.ts
+++ packages/cli-kit/src/ruby.ts
@@ -1,26 +1,56 @@
 import * as system from './system'
 import {Fatal} from './error'
-import {concurrent} from './output'
+import {spawn} from 'child_process'
 
 export async function exec(args: string[], token: string) {
   await validateRubyEnv()
+  await installCLIIfNeeded()
   // PENDING: Call the ruby CLI once integrated
-  await concurrent(0, 'themes', async (stdout) => {
-    try {
-      await system.exec('ruby', args, {stdout, env: {...process.env, SHOPIFY_ADMIN_TOKEN: token}})
-    } catch (error: any) {
-      throw new Fatal(`Command "${error.command}" failed\n${error.stderr}`)
-    }
+  // await concurrent(0, 'themes', async (stdout) => {
+  //   try {
+  const allArgs = ['exec', 'shopify'].concat(args)
+  // await system.exec('bundle', allArgs, {stdout, env: {...process.env, SHOPIFY_ADMIN_TOKEN: token}})
+  const op = spawn('bundle', allArgs, {
+    stdio: 'inherit',
+    shell: true,
   })
+  // op.on('message', (message, sendHandle) => {
+  //   console.log('message', message, sendHandle)
+  // })
+  // op.stdout.on('data', (data) => {
+  //   console.log(`stdout: ${data}`)
+  //   // if (data && typeof data.toString === 'function') {
+  //   //   // result = data.toString()
+  //   // }
+  // })
+
+  // op.on('close', (code, ...args) => {
+  //   console.log(`child process exited with code ${code}`, args)
+  // })
+  // } catch (error: any) {
+  //   throw new Fatal(`Command "${error.command}" failed\n${error.stderr}`)
+  // }
 }
 
-async function validateRubyEnv() {
+export async function validateRubyEnv() {
   try {
     await system.exec('ruby', ['-v'])
-  } catch (error) {
+  } catch {
     throw new Fatal(
       'Ruby environment not found',
       'Make sure you have ruby installed on your system: https://www.ruby-lang.org/en/documentation/installation/',
     )
   }
+
+  try {
+    await system.exec('bundler', ['-v'])
+  } catch {
+    throw new Fatal('Bundler not found', 'Make sure you have Bundler installed on your system: https://bundler.io/')
+  }
+}
+
+async function installCLIIfNeeded() {
+  const version = '0.12'
+  await system.exec('bundle', ['config', 'set', '--local', 'path', `~/.shopify-cli/${version}`])
+  await system.exec('bundle', ['install'])
 }
--- packages/theme/src/cli/commands/theme/pull.ts
+++ packages/theme/src/cli/commands/theme/pull.ts
@@ -1,7 +1,11 @@
 import {Command} from '@oclif/core'
+import {ruby, session} from '@shopify/cli-kit'
 
 export default class Pull extends Command {
   static description = 'Download your remote theme files locally.'
 
-  async run(): Promise<void> {}
+  async run(): Promise<void> {
+    const adminSession = await session.ensureAuthenticatedAdmin('isaacroldan')
+    await ruby.exec(['theme', 'pull'], adminSession.token)
+  }
 }
