2fed5aa5503e6f7e8a1416b17e411198dc22703f
Isaac Roldán <isaac.roldan@shopify.com>
2022-04-12 16:39:25 +0200

    Remove unnecessary changes

--- packages/theme/src/cli/commands/theme/pull.ts
+++ packages/theme/src/cli/commands/theme/pull.ts
@@ -1,11 +1,7 @@
 import {Command} from '@oclif/core'
-import {ruby, session} from '@shopify/cli-kit'
 
 export default class Pull extends Command {
   static description = 'Download your remote theme files locally.'
 
-  async run(): Promise<void> {
-    const adminSession = await session.ensureAuthenticatedAdmin('isaacroldan')
-    await ruby.exec(['theme', 'pull'], adminSession.token)
-  }
+  async run(): Promise<void> {}
 }
