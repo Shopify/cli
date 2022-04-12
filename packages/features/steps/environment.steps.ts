14af5d5eebcbd7a7c4f5bc7a48353fb3df193472
Pedro Piñera Buendía <pedro@craftweg.com>
2022-04-12 12:09:45 +0200

    Fully sandbox acceptance tests

--- packages/features/steps/environment.steps.ts
+++ packages/features/steps/environment.steps.ts
@@ -17,6 +17,18 @@ if (process.env.DEBUG === '1') {
 
 Given('I have a working directory', async function () {
   this.temporaryDirectory = tempy.directory()
+  const dataHomeDirectory = path.join(this.temporaryDirectory, 'XDG_DATA_HOME')
+  const configHomeDirectory = path.join(this.temporaryDirectory, 'XDG_CONFIG_HOME')
+  const stateHomeDirectory = path.join(this.temporaryDirectory, 'XDG_STATE_HOME')
+  const cacheHomeDirectory = path.join(this.temporaryDirectory, 'XDG_CACHE_HOME')
+
+  this.temporaryEnv = {
+    XDG_DATA_HOME: dataHomeDirectory,
+    XDG_CONFIG_HOME: configHomeDirectory,
+    XDG_STATE_HOME: stateHomeDirectory,
+    XDG_CACHE_HOME: cacheHomeDirectory,
+  }
+
   // When we run the acceptance tests in CI, the .npmrc is scoped to the project.
   // Because of that, the projects that we create from acceptance tests fail to
   // install dependencies because the package manager doesn't know which package registry
