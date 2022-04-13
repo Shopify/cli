810a0cf59dd844b49b8ab7b344293b11aaa46bbb
Isaac Roldán <isaac.roldan@shopify.com>
2022-04-13 17:45:41 +0200

    Add store flag to theme pull

--- packages/theme/src/cli/commands/theme/pull.ts
+++ packages/theme/src/cli/commands/theme/pull.ts
@@ -1,10 +1,24 @@
-import {Command} from '@oclif/core'
-import {ruby} from '@shopify/cli-kit'
+import {Command, Flags} from '@oclif/core'
+import {ruby, session, string} from '@shopify/cli-kit'
 
 export default class Pull extends Command {
   static description = 'Download your remote theme files locally.'
 
+  static flags = {
+    store: Flags.string({
+      hidden: false,
+      char: 's',
+      description: 'Store',
+      env: 'SHOPIFY_FLAG_STORE',
+      parse: (input, _) => Promise.resolve(string.normalizeStoreName(input)),
+    }),
+  }
+
   async run(): Promise<void> {
+    const {args, flags} = await this.parse(Pull)
+    const store = flags.store || 'mystore'
+    const adminSession = await session.ensureAuthenticatedAdmin(store)
+    console.log(adminSession)
     await ruby.exec(['theme', 'pull'], '')
   }
 }
