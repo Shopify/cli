f1f670a0fb0e386241d83c9b29e7e2bb8933b038
Pedro Piñera Buendía <pedro@craftweg.com>
2022-04-19 13:01:54 +0200

    Fix the selection of colors in the concurrent function

--- packages/cli-kit/src/output.ts
+++ packages/cli-kit/src/output.ts
@@ -265,7 +265,8 @@ export async function concurrent(
   const colors = [token.yellow, token.cyan, token.magenta, token.green]
 
   function linePrefix() {
-    const color = colors[colors.length % (index + 1)]
+    const colorIndex = index < colors.length ? index : index % colors.length
+    const color = colors[colorIndex]
     const linePrefix = color(`[${prefix}]: `)
     return linePrefix
   }
