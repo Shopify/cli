a4894a06835fcccb5883e1b8b4eefb4ad43abbd3
Isaac Roldán <isaac.roldan@shopify.com>
2022-04-21 14:25:52 +0200

    Add Init command

--- packages/theme/src/cli/commands/theme/delete.ts
+++ packages/theme/src/cli/commands/theme/delete.ts
@@ -9,26 +9,22 @@ export default class Delete extends Command {
 
   static flags = {
     development: Flags.boolean({
-      hidden: false,
       char: 'd',
       description: 'Delete your development theme.',
-      env: 'SHOPIFY_CLI_THEME_DELETE_DEVELOPMENT',
+      env: 'SHOPIFY_FLAG_THEME_DELETE_DEVELOPMENT',
     }),
     // eslint-disable-next-line @typescript-eslint/naming-convention
     'show-all': Flags.boolean({
-      hidden: false,
       char: 'a',
       description: 'Include others development themes in theme list.',
-      env: 'SHOPIFY_CLI_THEME_DELETE_SHOW_ALL',
+      env: 'SHOPIFY_FLAG_THEME_DELETE_SHOW_ALL',
     }),
     force: Flags.boolean({
-      hidden: false,
       char: 'f',
       description: 'Skip confirmation.',
-      env: 'SHOPIFY_CLI_THEME_DELETE_FORCE',
+      env: 'SHOPIFY_FLAG_THEME_DELETE_FORCE',
     }),
     store: Flags.string({
-      hidden: false,
       char: 's',
       description: 'Store URL',
       env: 'SHOPIFY_FLAG_STORE',
--- packages/theme/src/cli/commands/theme/init.ts
+++ packages/theme/src/cli/commands/theme/init.ts
@@ -1,10 +1,21 @@
-import {Command} from '@oclif/core'
+import {Command, Flags} from '@oclif/core'
 import {ruby} from '@shopify/cli-kit'
 
 export default class Init extends Command {
   static description = 'Create a new theme'
 
+  static flags = {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    'clone-url': Flags.string({
+      char: 'u',
+      description:
+        "The Git URL to clone from. Defaults to Shopify's example theme, Dawn: https://github.com/Shopify/dawn.git",
+      env: 'SHOPIFY_FLAG_CLONE_URL',
+    }),
+  }
+
   async run(): Promise<void> {
-    await ruby.exec(['theme', 'init'], '')
+    const command = ['theme', 'init'].concat(this.argv)
+    await ruby.exec(command, '')
   }
 }
