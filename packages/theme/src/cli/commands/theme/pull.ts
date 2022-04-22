66ab67bff311c17e698cd7a712a72cd8b1e6a2be
Isaac Roldán <isaac.roldan@shopify.com>
2022-04-22 11:41:11 +0200

    Add pull command

--- packages/theme/src/cli/commands/theme/pull.ts
+++ packages/theme/src/cli/commands/theme/pull.ts
@@ -1,7 +1,79 @@
-import {Command} from '@oclif/core'
+import {Command, Flags} from '@oclif/core'
+import {ruby, session, string} from '@shopify/cli-kit'
+import {getThemeStore} from '$cli/utilities/theme-store'
 
 export default class Pull extends Command {
   static description = 'Download your remote theme files locally.'
 
-  async run(): Promise<void> {}
+  static flags = {
+    theme: Flags.string({
+      description: 'Theme ID or name of the remote theme.',
+      env: 'SHOPIFY_FLAG_THEME_ID',
+    }),
+    development: Flags.boolean({
+      char: 'd',
+      description: 'Pull theme files from your remote development theme.',
+      env: 'SHOPIFY_FLAG_THEME_DEVELOPMENT',
+    }),
+    live: Flags.boolean({
+      char: 'l',
+      description: 'Pull theme files from your remote live theme.',
+      env: 'SHOPIFY_FLAG_THEME_LIVE',
+    }),
+    nodelete: Flags.boolean({
+      char: 'n',
+      description: 'Runs the pull command without deleting local files.',
+      env: 'SHOPIFY_FLAG_THEME_NODELETE',
+    }),
+    only: Flags.boolean({
+      char: 'o',
+      description: 'Download only the specified files (Multiple flags allowed).',
+      env: 'SHOPIFY_FLAG_THEME_ONLY',
+    }),
+    ignore: Flags.boolean({
+      char: 'n',
+      description: 'Skip downloading the specified files (Multiple flags allowed).',
+      env: 'SHOPIFY_FLAG_THEME_IGNORE',
+    }),
+    path: Flags.string({
+      description: 'The path to your theme',
+      default: '.',
+      env: 'SHOPIFY_FLAG_PATH',
+    }),
+    store: Flags.string({
+      char: 's',
+      description: 'Store URL',
+      env: 'SHOPIFY_FLAG_STORE',
+      parse: (input, _) => Promise.resolve(string.normalizeStoreName(input)),
+    }),
+  }
+
+  async run(): Promise<void> {
+    const {flags} = await this.parse(Pull)
+
+    const command = ['theme', 'pull', flags.path]
+    if (flags.theme) {
+      command.push('-t')
+      command.push(flags.theme)
+    }
+    if (flags.development) {
+      command.push('-d')
+    }
+    if (flags.live) {
+      command.push('-l')
+    }
+    if (flags.nodelete) {
+      command.push('-n')
+    }
+    if (flags.only) {
+      command.push('-o')
+    }
+    if (flags.ignore) {
+      command.push('-n')
+    }
+    console.log(command)
+    const store = getThemeStore(flags)
+    const adminSession = await session.ensureAuthenticatedAdmin(store)
+    await ruby.exec(command, adminSession)
+  }
 }
