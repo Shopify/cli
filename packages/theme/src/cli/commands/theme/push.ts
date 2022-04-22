b21c4a05220ed6bf293bcc7bba2330455e11d520
Isaac Roldán <isaac.roldan@shopify.com>
2022-04-22 11:06:45 +0200

    Revert not needed changes

--- packages/theme/src/cli/commands/theme/push.ts
+++ packages/theme/src/cli/commands/theme/push.ts
@@ -1,11 +1,8 @@
 import {Command} from '@oclif/core'
-import {ruby} from '@shopify/cli-kit'
 
 export default class Push extends Command {
   static description =
     'Uploads your local theme files to the connected store, overwriting the remote version if specified.'
 
-  async run(): Promise<void> {
-    await ruby.exec(['theme', 'push'], '')
-  }
+  async run(): Promise<void> {}
 }
