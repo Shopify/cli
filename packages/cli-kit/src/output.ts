983b575b7b5badec6e8aa6595feb46005d1e2b4c
Ariel Caplan <ariel.caplan@shopify.com>
2022-05-06 14:40:23 -0400

    Keep ERROR on the same line as the log line
    The prior output was like
    
    ERROR
    some_process | Something went wrong!
    ERROR
    some_process | The error we found was:
    ERROR
    some_process |   "No whosiwhatsit found for the humperdinck", app.ts:73
    
    and now it will be:
    
    some_process | ERROR Something went wrong!
    some_process | ERROR The error we found was:
    some_process | ERROR   "No whosiwhatsit found for the humperdinck", app.ts:73
    
    Much better, right?

--- packages/cli-kit/src/output.ts
+++ packages/cli-kit/src/output.ts
@@ -319,8 +319,7 @@ export async function concurrent(processes: OutputProcess[]) {
           write(chunk, _encoding, next) {
             const lines = stripAnsiEraseCursorEscapeCharacters(chunk.toString('ascii')).split(/\n/)
             for (const line of lines) {
-              consoleLog('ERROR')
-              message(content`${linePrefix(process.prefix, index)}${line}`, 'error')
+              message(content`${linePrefix(process.prefix, index)}${colors.bold('ERROR')} ${line}`, 'error')
             }
             next()
           },
