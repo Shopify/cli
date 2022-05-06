d92fe9858d05e479ab94aafa8a167034b60c5fd5
Isaac Roldán <isaac.roldan@shopify.com>
2022-05-06 17:14:06 +0200

    Add some initial request to deploy functions

---
+++ packages/cli-kit/src/api/graphql/functions/function_service_proxy.ts
@@ -0,0 +1,7 @@
+import {gql} from 'graphql-request'
+
+export const ScriptServiceProxyQuery = gql`
+  query ProxyRequest($api_key: String, $query: String!, $variables: String) {
+    scriptServiceProxy(apiKey: $api_key, query: $query, variables: $variables)
+  }
+`
