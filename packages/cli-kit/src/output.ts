9d0f7f064376c0f63b8f4d425b8b529cd8529bd7
Ariel Caplan <ariel.caplan@shopify.com>
2022-05-11 12:19:25 -0400

    Bold the separator line

--- packages/cli-kit/src/output.ts
+++ packages/cli-kit/src/output.ts
@@ -300,7 +300,7 @@ export async function concurrent(processes: OutputProcess[]) {
   function linePrefix(prefix: string, index: number) {
     const colorIndex = index < concurrentColors.length ? index : index % concurrentColors.length
     const color = concurrentColors[colorIndex]
-    return color(`${prefix}${' '.repeat(prefixColumnSize - prefix.length)} | `)
+    return color(`${prefix}${' '.repeat(prefixColumnSize - prefix.length)} ${colors.bold('|')} `)
   }
 
   try {
