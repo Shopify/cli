57f9db1bc5eadebee4c12d8354c990675ca30df5
Pedro Piñera Buendía <pedro@craftweg.com>
2022-05-13 18:33:53 -0400

    Add tests

--- packages/app/src/cli/services/dependencies.test.ts
+++ packages/app/src/cli/services/dependencies.test.ts
@@ -1,7 +1,7 @@
 import {installAppNPMDependencies} from './dependencies'
 import {App} from '../models/app/app'
 import {describe, expect, test, vi} from 'vitest'
-import {dependency} from '@shopify/cli-kit'
+import {dependency, ui} from '@shopify/cli-kit'
 
 vi.mock('@shopify/cli-kit')
 
@@ -23,12 +23,22 @@ describe('installAppNPMDependencies', () => {
       webs: [],
       configurationPath: '/tmp/project/shopify.app.toml',
     }
+    const listRun = vi.fn().mockResolvedValue(undefined)
+    const list: any = {run: listRun}
+    vi.mocked(ui.Listr).mockReturnValue(list)
     vi.mocked(dependency)
 
     // When
     await installAppNPMDependencies(app)
 
     // Then
+    expect(vi.mocked(ui.Listr).mock.calls.length).toEqual(1)
+    const tasks = vi.mocked(ui.Listr).mock.calls[0][0] as any
+    expect(tasks.length).toEqual(1)
+    const task = tasks[0]
+    expect(task.title).not.toBe('')
+    await task.task(undefined, {title: vi.fn()})
+    expect(listRun).toHaveBeenCalled()
     expect(dependency.installNPMDependenciesRecursively).toHaveBeenCalledWith({
       dependencyManager: 'yarn',
       directory: '/tmp/project',
--- packages/app/src/cli/services/dependencies.ts
+++ packages/app/src/cli/services/dependencies.ts
@@ -1,5 +1,5 @@
 import {App} from '../models/app/app'
-import {dependency} from '@shopify/cli-kit'
+import {dependency, ui} from '@shopify/cli-kit'
 
 /**
  * Given an app, it installs its NPM dependencies by traversing
@@ -8,8 +8,17 @@ import {dependency} from '@shopify/cli-kit'
  * @param app {App} App whose dependencies will be installed.
  */
 export async function installAppNPMDependencies(app: App) {
-  await dependency.installNPMDependenciesRecursively({
-    dependencyManager: app.dependencyManager,
-    directory: app.directory,
-  })
+  const list = new ui.Listr([
+    {
+      title: 'Installing any necessary dependencies',
+      task: async (_, task) => {
+        await dependency.installNPMDependenciesRecursively({
+          dependencyManager: app.dependencyManager,
+          directory: app.directory,
+        })
+        task.title = 'Template downloaded'
+      },
+    },
+  ])
+  await list.run()
 }
