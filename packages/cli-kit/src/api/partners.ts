611aaa0db47a6353f27975d1f2060b358b4f1dc7
Pedro Piñera Buendía <pedro@craftweg.com>
2022-05-16 18:45:35 +0200

    Add additional verbose logs

--- packages/cli-kit/src/api/partners.ts
+++ packages/cli-kit/src/api/partners.ts
@@ -1,10 +1,18 @@
 import {buildHeaders} from './common'
 import {partners as partnersFqdn} from '../environment/fqdn'
+import {debug} from '../output'
 import {request as graphqlRequest} from 'graphql-request'
 
 export async function request<T>(query: any, token: string, variables?: any): Promise<T> {
   const fqdn = await partnersFqdn()
   const url = `https://${fqdn}/api/cli/graphql`
   const headers = await buildHeaders(token)
+  debug(`
+  Sending Partners GraphQL request:
+  ${query}
+
+  With variables:
+  ${variables}
+  `)
   return graphqlRequest<T>(url, query, variables, headers)
 }
