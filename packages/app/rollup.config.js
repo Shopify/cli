6ac88f1dada08214fbf7b9dbba1efa274943b784
Pedro Piñera Buendía <pedro@craftweg.com>
2022-05-16 09:36:06 +0200

    Remove the runtime modules

--- packages/app/rollup.config.js
+++ packages/app/rollup.config.js
@@ -16,11 +16,6 @@ const featureCommands = fg.sync([
   `!${path.join(__dirname, `/src/cli/commands/**/*.test.ts`)}`,
 ])
 
-const runtimeModules = fg.sync([
-  path.join(__dirname, `/src/runtime/**/*.ts`),
-  `!${path.join(__dirname, `/src/runtime/**/*.test.ts`)}`,
-])
-
 const configuration = () => [
   // CLI
   {
@@ -34,26 +29,6 @@ const configuration = () => [
           if (chunkInfo.facadeModuleId.includes('src/cli/commands')) {
             // Preserves the commands/... path
             return `commands/${chunkInfo.facadeModuleId.split('src/cli/commands').pop().replace('ts', 'js')}`
-          } else {
-            return '[name].js'
-          }
-        },
-      },
-    ],
-    plugins: plugins(__dirname),
-    external: appExternal,
-  },
-  {
-    input: [...runtimeModules],
-    output: [
-      {
-        dir: distDir(__dirname),
-        format: 'esm',
-        sourcemap: true,
-        entryFileNames: (chunkInfo) => {
-          if (chunkInfo.facadeModuleId.includes('src/runtime')) {
-            // Preserves the commands/... path
-            return `runtime/${chunkInfo.facadeModuleId.split('src/runtime').pop().replace('ts', 'js')}`
           } else {
             return '[name].js'
           }
