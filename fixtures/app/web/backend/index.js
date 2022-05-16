55b6ad7d46692963fb4cca7366f1d1c38f520da9
Pedro Piñera Buendía <pedro@craftweg.com>
2022-05-16 13:00:11 +0200

    Set the HOST_SCHEME when initializing the Shopify.Context

--- fixtures/app/web/backend/index.js
+++ fixtures/app/web/backend/index.js
@@ -22,6 +22,7 @@ Shopify.Context.initialize({
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
   SCOPES: process.env.SCOPES.split(","),
   HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
+  HOST_SCHEME: process.env.HOST.split("://")[0],
   API_VERSION: ApiVersion.April22,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
