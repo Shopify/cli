dd0b4da54c860ca077c26dee186501fc1da5fabc
Isaac Roldán <isaac.roldan@shopify.com>
2022-05-18 13:55:25 +0200

    Create extension in API

---
+++ packages/cli-kit/src/api/graphql/extension_create.ts
@@ -0,0 +1,57 @@
+import {gql} from 'graphql-request'
+
+export const ExtensionCreateQuery = gql`
+  mutation ExtensionCreate($apiKey: String!, $type: ExtensionType!, $title: String!, $config: JSON!) {
+    extensionCreate(input: {apiKey: $apiKey, type: $type, title: $title, config: $config}) {
+      extensionRegistration {
+        id
+        uuid
+        type
+        title
+        draftVersion {
+          registrationId
+          lastUserInteractionAt
+          validationErrors {
+            field
+            message
+          }
+        }
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`
+
+export interface ExtensionCreateVariables {
+  apiKey: string
+  type: string
+  title: string
+  config: any
+  context?: string | null
+}
+
+export interface ExtensionCreateSchema {
+  extensionCreate: {
+    extensionRegistration: {
+      id: string
+      uuid: string
+      type: string
+      title: string
+      draftVersion: {
+        registrationId: string
+        lastUserInteractionAt: string
+        validationErrors: {
+          field: string[]
+          message: string
+        }[]
+      }
+    }
+    userErrors: {
+      field: string[]
+      message: string
+    }[]
+  }
+}
