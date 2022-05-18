cbc58f15dd099acf3b4b2035d5f77094175f246d
Pedro Piñera Buendía <pedro@craftweg.com>
2022-05-18 13:09:36 +0200

    Add completion message

--- packages/app/src/cli/services/scaffold/extension.ts
+++ packages/app/src/cli/services/scaffold/extension.ts
@@ -72,6 +72,7 @@ async function uiExtensionInit({name, extensionType, app}: ExtensionInitOptions)
             },
           }),
         })
+        task.title = 'Dependencies installed'
       },
     },
     {
