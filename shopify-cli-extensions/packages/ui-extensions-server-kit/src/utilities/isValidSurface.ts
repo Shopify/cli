acb0bdfdd4df9fd7321ccf4eb4c04f3b90dc3108
Trish Ta <trish.ta@shopify.com>
2022-05-20 14:07:23 -0400

    Add valid surfaces as a constant array

--- shopify-cli-extensions/packages/ui-extensions-server-kit/src/utilities/isValidSurface.ts
+++ shopify-cli-extensions/packages/ui-extensions-server-kit/src/utilities/isValidSurface.ts
@@ -1,5 +1,7 @@
 import type {Surface} from '../ExtensionServerClient';
 
+const AVAILABLE_SURFACES = ['admin', 'checkout', 'post-checkout'];
+
 export function isValidSurface(surface: any): surface is Surface {
-  return (surface && surface === 'admin') || surface === 'checkout' || surface === 'post-purchase';
+  return surface && AVAILABLE_SURFACES.includes(surface);
 }
