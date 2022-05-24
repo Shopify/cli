a6c24d7435d9005285177c37f70ded28f03716c9
Isaac Roldán <isaac.roldan@shopify.com>
2022-05-24 14:20:19 +0200

    Fix imports

--- packages/app/src/cli/commands/app/scaffold/extension.ts
+++ packages/app/src/cli/commands/app/scaffold/extension.ts
@@ -1,10 +1,10 @@
 import {appFlags} from '../../../flags'
 import {output, path, cli, error} from '@shopify/cli-kit'
 import {Command, Flags} from '@oclif/core'
-import {extensions, limitedExtensions} from 'cli/constants'
-import scaffoldExtensionPrompt from 'cli/prompts/scaffold/extension'
-import {load as loadApp, App} from 'cli/models/app/app'
-import scaffoldExtensionService from 'cli/services/scaffold/extension'
+import {extensions, limitedExtensions} from '../../../constants'
+import scaffoldExtensionPrompt from '../../../prompts/scaffold/extension'
+import {load as loadApp, App} from '../../../models/app/app'
+import scaffoldExtensionService from '../../../services/scaffold/extension'
 
 export default class AppScaffoldExtension extends Command {
   static description = 'Scaffold an Extension'
