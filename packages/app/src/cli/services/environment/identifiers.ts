f74f152f41ce7988c9d7131ce3b1939ef7a4e22a
Pedro Piñera Buendía <pedro@craftweg.com>
2022-05-25 16:25:20 +0200

    wip

--- packages/app/src/cli/services/environment/identifiers.ts
+++ packages/app/src/cli/services/environment/identifiers.ts
@@ -1,5 +1,8 @@
 import {App, Extension, Identifiers} from '../../models/app/app'
 import {fetchAppExtensionRegistrations} from '../dev/fetch'
+import {dotEnvFileNames} from '../../constants'
+import {error} from '@shopify/cli-kit'
+import {Abort} from '@shopify/cli-kit/src/error'
 
 export interface EnsureDeploymentIdsPresenceOptions {
   app: App
@@ -15,6 +18,7 @@ export async function ensureDeploymentIdsPresence(options: EnsureDeploymentIdsPr
 
   const localId = (extension: Extension) => (envIdentifiers.extensions ?? {})[extension.localIdentifier]
   const hasLocalId = (extension: Extension) => localId(extension) !== undefined
+  const allExtensionsHaveLocalId = localExtensions.every(hasLocalId)
   const existsRemotely = (extension: Extension) =>
     remoteSpecifications.app.extensionRegistrations.find((registration) => registration.uuid === localId(extension))
   const needsCreation = (extension: Extension) => {
@@ -26,25 +30,43 @@ export async function ensureDeploymentIdsPresence(options: EnsureDeploymentIdsPr
   const remoteSpecificationsContainAllLocalIds = Object.values(envIdentifiers.extensions ?? {}).every((extensionId) => {
     return remoteSpecifications.app.extensionRegistrations.find(({uuid}) => uuid === extensionId) !== undefined
   })
+  const localExtensionsWithLocalIdButNotRemote = localExtensions.filter(
+    (extension) => hasLocalId(extension) && !existsRemotely(extension),
+  )
 
-  const extensionsToCreate: Extension[] = localExtensions.filter(needsCreation)
+  let extensionsToCreate: Extension[]
+  let error: error.Abort | undefined
 
   // Case 1: All local extensions have identifiers
-  if (localExtensionsWithEnvIdentifiers.length === localExtensions.length) {
+  if (allExtensionsHaveLocalId) {
     // Case 1.1: All local ids exist in the remote specifications
     if (remoteSpecificationsContainAllLocalIds) {
       return {
         app: options.appId,
         extensions: envIdentifiers.extensions ?? {},
       }
-      // Case 2.2: Some extensions need to be created
+      // Case 2.2: There's a mismatch we can't handle (ERROR)
+    } else {
+      const extensionNames = localExtensionsWithLocalIdButNotRemote
+        .map((extension) => extension.localIdentifier)
+        .join(', ')
+      const extensionEnvVariables = localExtensionsWithLocalIdButNotRemote
+        .map((extension) => extension.idEnvironmentVariableName)
+        .join(', ')
+      error = new Abort(
+        `The extensions ${extensionNames} don't belong to the partners' app with API key ${options.appId}`,
+        `Deploy to the right app, create a new app, or delete the keys ${extensionEnvVariables} from the project's ${dotEnvFileNames.production} file.`,
+      )
     }
-    // Case 2.2: Some extensions need to be created
+  } else {
   }
 
-  const extensionsIdentifiers: Identifiers = {...envIdentifiers}
+  if (error) {
+    throw error
+  }
+
   return {
     app: options.appId,
-    extensions: extensionsIdentifiers,
+    extensions: {},
   }
 }
