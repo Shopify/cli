796863fd5f7b6fafcd6370500151a7030b378bea
Pedro Piñera Buendía <pedro@craftweg.com>
2022-05-25 15:11:43 +0200

    Add utility functions to implement control flows for the various use cases

--- packages/app/src/cli/services/environment/identifiers.ts
+++ packages/app/src/cli/services/environment/identifiers.ts
@@ -12,26 +12,39 @@ export async function ensureDeploymentIdsPresence(options: EnsureDeploymentIdsPr
   const remoteSpecifications = await fetchAppExtensionRegistrations({token: options.token, apiKey: options.appId})
   const localExtensions: Extension[] = [...options.app.extensions.ui, ...options.app.extensions.function]
   const envIdentifiers = options.envIdentifiers
-  const localExtensionsWithEnvIdentifiers = localExtensions.filter(
-    (extension) => (envIdentifiers.extensions ?? {})[extension.localIdentifier] !== undefined,
-  )
-  const localExtensionsWithoutEnvIdentifiers = localExtensions.filter(
-    (extension) => (envIdentifiers.extensions ?? {})[extension.localIdentifier] === undefined,
-  )
-  const remoteSpecificationsContainAllLocalIds = Object.values(envIdentifiers.extensions ?? {}).every((extensionId) => {
-    return remoteSpecifications.app.extensionRegistrations.includes(({uuid}) => uuid === extensionId)
-  })
 
-  // Case: All local extensions have identifiers
-  if (localExtensionsWithEnvIdentifiers.length === localExtensions.length) {
-    return {
-      app: options.appId,
-      extensions: envIdentifiers.extensions ?? {},
-    }
+  const localId = (extension: Extension) => (envIdentifiers.extensions ?? {})[extension.localIdentifier]
+  const hasLocalId = (extension: Extension) => localId(extension) !== undefined
+  const existsRemotely = (extension: Extension) =>
+    remoteSpecifications.app.extensionRegistrations.find((registration) => registration.uuid === localId(extension))
+  const needsCreation = (extension: Extension) => {
+    return !hasLocalId(extension) || !existsRemotely(extension)
   }
 
+  const localExtensionsWithEnvIdentifiers = localExtensions.filter(hasLocalId)
+  const localExtensionsWithoutEnvIdentifiers = localExtensions.filter((ext) => !hasLocalId(ext))
+  const remoteSpecificationsContainAllLocalIds = Object.values(envIdentifiers.extensions ?? {}).every((extensionId) => {
+    return remoteSpecifications.app.extensionRegistrations.find(({uuid}) => uuid === extensionId) !== undefined
+  })
+
+  const extensionsToCreate: Extension[] = localExtensions.filter(needsCreation)
+
+  // Case 1: All local extensions have identifiers
+  if (localExtensionsWithEnvIdentifiers.length === localExtensions.length) {
+    // Case 1.1: All local ids exist in the remote specifications
+    if (remoteSpecificationsContainAllLocalIds) {
+      return {
+        app: options.appId,
+        extensions: envIdentifiers.extensions ?? {},
+      }
+      // Case 2.2: Some extensions need to be created
+    }
+    // Case 2.2: Some extensions need to be created
+  }
+
+  const extensionsIdentifiers: Identifiers = {...envIdentifiers}
   return {
     app: options.appId,
-    extensions: {},
+    extensions: extensionsIdentifiers,
   }
 }
