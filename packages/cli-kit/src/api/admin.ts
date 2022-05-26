4bcc20ecd6538473bd7a5bd1eb73e812c79dc593
Isaac Roldán <isaac.roldan@shopify.com>
2022-05-26 11:13:43 +0200

    Fetch product variant for checkout ui extension

--- packages/cli-kit/src/api/admin.ts
+++ packages/cli-kit/src/api/admin.ts
@@ -1,7 +1,21 @@
 import {buildHeaders} from './common'
 import {AdminSession} from '../session'
 import {debug} from '../output'
+import {Bug} from '../error'
 import {request as graphqlRequest, gql, RequestDocument, Variables} from 'graphql-request'
+
+const UnauthorizedAccessError = () => {
+  return new Bug(
+    `You can't use Shopify CLI with development stores if you only have Partner staff member access.
+If you want to use Shopify CLI to work on a development store, then you should be the store owner or create a staff account on the store`,
+    `If you're the store owner, then you need to log in to the store directly using the store URL at least once (for example, using %s.myshopify.com/admin) before you log in using Shopify CLI.
+Logging in to the Shopify admin directly connects the development store with your Shopify login.`,
+  )
+}
+
+const UnknownError = () => {
+  return new Bug(`Unknown error connecting to your store`)
+}
 
 export async function request<T>(query: RequestDocument, session: AdminSession, variables?: Variables): Promise<T> {
   debug(`
@@ -28,7 +42,10 @@ ${query}
   `)
   const data = await graphqlRequest<{
     publicApiVersions: {handle: string; supported: boolean}[]
-  }>(url, query, {}, headers)
+  }>(url, query, {}, headers).catch((err) => {
+    throw err.response.status === 403 ? UnauthorizedAccessError() : UnknownError()
+  })
+
   return data.publicApiVersions
     .filter((item) => item.supported)
     .map((item) => item.handle)
