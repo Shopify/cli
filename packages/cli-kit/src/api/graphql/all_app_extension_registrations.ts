3a2bb2d29f4cba293f4d1a994ef16e4218d99735
Isaac Roldán <isaac.roldan@shopify.com>
2022-05-27 14:43:12 +0200

    Add registration graphql queries

---
+++ packages/cli-kit/src/api/graphql/all_app_extension_registrations.ts
@@ -0,0 +1,29 @@
+import {gql} from 'graphql-request'
+
+export const AllAppExtensionRegistrationsQuery = gql`
+  query allAppExtensionRegistrations($apiKey: String!) {
+    app(apiKey: $apiKey) {
+      extensionRegistrations {
+        id
+        uuid
+        title
+        type
+      }
+    }
+  }
+`
+
+export interface AllAppExtensionRegistrationsQueryVariables {
+  apiKey: string
+}
+
+export interface AllAppExtensionRegistrationsQuerySchema {
+  app: {
+    extensionRegistrations: {
+      id: string
+      uuid: string
+      title: string
+      type: string
+    }[]
+  }
+}
