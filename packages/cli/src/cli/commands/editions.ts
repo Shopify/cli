db309f0e53cc058b21791f81a5632eb476435f28
Surma <surma@shopify.com>
2022-05-30 15:02:30 +0100

    Add editions sub commands

--- packages/cli/src/cli/commands/editions.ts
+++ packages/cli/src/cli/commands/editions.ts
@@ -1,7 +1,27 @@
 import {Command} from '@oclif/core'
-import {output} from '@shopify/cli-kit'
-import {Message, TokenizedString} from '@shopify/cli-kit/src/output'
+import {output, ui} from '@shopify/cli-kit'
 
+enum Choice {
+  B2B,
+  BFS,
+  Hydrogen,
+  Functions,
+}
+
+const subcommands: Record<Choice, () => void | Promise<void>> = {
+  [Choice.B2B]: () => {
+    output.info('Tell the user more about B2B')
+  },
+  [Choice.BFS]: () => {
+    output.info('Tell the user more about BFS')
+  },
+  [Choice.Hydrogen]: () => {
+    output.info('Tell the user more about Hydrogen')
+  },
+  [Choice.Functions]: () => {
+    output.info('Tell the user more about Functions')
+  },
+}
 export default class Editions extends Command {
   static description = 'Shopify editions'
 
@@ -30,8 +50,38 @@ export default class Editions extends Command {
 
         Welcome to Dev Mode for the Summer 2022 Edition
         https://editions.shopify.dev ???? FIXME
+
+
       `),
     )
+
+    const answer = await ui.prompt([
+      {
+        name: 'editionschoice',
+        type: 'select',
+        message: 'What are you looking to solve?',
+        choices: [
+          {
+            value: Choice[Choice.Functions],
+            name: 'Create customizations for checkout & across Shopify',
+          },
+          {
+            value: Choice[Choice.Hydrogen],
+            name: 'Build hyper fast storefronts for headless clients',
+          },
+          {
+            value: Choice[Choice.BFS],
+            name: 'Easy templates to get started & get featured in Admin and App Store',
+          },
+          {
+            value: Choice[Choice.B2B],
+            name: 'Build for B2B merchants',
+          },
+        ],
+      },
+    ])
+
+    await subcommands[Choice[answer.editionschoice]]?.()
   }
 }
 
