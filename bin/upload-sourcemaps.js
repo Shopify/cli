45eec47c4ed974f76ea34abff8889628dd8071ed
Ariel Caplan <ariel.caplan@shopify.com>
2022-05-31 20:17:12 +0300

    Upload sourcemaps to bugsnag

--- bin/upload-sourcemaps.js
+++ bin/upload-sourcemaps.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { node } from "@bugsnag/source-maps";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const appVersion = JSON.parse(fs.readFileSync(`${__dirname}/../packages/cli/package.json`)).version;
+
+node.uploadMultiple({
+  apiKey: '9e1e6889176fd0c795d5c659225e0fae',
+  appVersion,
+  overwrite: true,
+  projectRoot: `${__dirname}/..`,
+  directory: '.'
+});
