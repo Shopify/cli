bb07611df43adb6157c73efaf9663525228034be
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2022-06-01 09:32:24 +0200

    Fix copy

--- packages/create-app/src/services/init.ts
+++ packages/create-app/src/services/init.ts
@@ -118,7 +118,7 @@ async function init(options: InitOptions) {
   })
 
   output.info(output.content`
-  ${hyphenizedName} is ready for you to build! Remember to cd into ${outputDirectory}.
+  ${hyphenizedName} is ready for you to build! Remember to cd ${outputDirectory}.
   To preview your project, run ${output.token.command(dependencyManager, 'dev')}
   To add extensions, run ${output.token.command(dependencyManager, 'scaffold extension')}
   For more details on all that you can build, see the docs: ${output.token.link(
