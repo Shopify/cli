4a578a4e59ba27d749e2c5a1ceb61d139277d676
Nausher Rao <nausher.rao@shopify.com>
2022-06-06 15:04:00 -0400

    Add file to contain HTML in

---
+++ packages/cli-kit/src/html/postAuthScreens.js
@@ -0,0 +1,9 @@
+export const EmptyUrlError = '<h1>We received the authentication redirect but the URL is empty.</h1>'
+
+export const MissingCodeError =
+  "<h1>The authentication can't continue because the redirect doesn't include the code.</h1>"
+
+export const MissingStateError =
+  "<h1>The authentication can't continue because the redirect doesn't include the state.</h1>"
+
+export const success = "<h1>You're logged in on the Shopify CLI in your terminal</h1>"
