c110fc5959620488dc8792990a02498e12586e3d
Ariel Caplan <ariel.caplan@shopify.com>
2022-06-06 13:56:45 +0300

    Minimum requirements for getting stack trace

--- bin/update-bugsnag.js
+++ bin/update-bugsnag.js
@@ -9,15 +9,15 @@ import reportBuild from 'bugsnag-build-reporter';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const appVersion = JSON.parse(fs.readFileSync(`${__dirname}/../packages/cli/package.json`)).version;
-const apiKey = '9e1e6889176fd0c795d5c659225e0fae'
+const apiKey = '9e1e6889176fd0c795d5c659225e0fae';
 
 node.uploadMultiple({
   apiKey,
   appVersion,
   overwrite: true,
-  projectRoot: `${__dirname}/..`,
+  projectRoot: `${__dirname}/../packages`,
   directory: '.'
 });
 reportBuild({apiKey, appVersion}, {})
   .then(() => console.log('Build reported!'))
-  .catch(err => console.log('Failed to report build!', err.messsage))
+  .catch(err => console.log('Failed to report build!', err.messsage));
