2b37f5da132efc7f39a49e7d91a533a7b5b2bc5c
Ariel Caplan <ariel.caplan@shopify.com>
2022-06-06 09:53:52 +0300

    Report builds to bugsnag

--- bin/upload-sourcemaps.js
+++ bin/upload-sourcemaps.js
@@ -4,15 +4,20 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { node } from "@bugsnag/source-maps";
+import reportBuild from 'bugsnag-build-reporter';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const appVersion = JSON.parse(fs.readFileSync(`${__dirname}/../packages/cli/package.json`)).version;
+const apiKey = '9e1e6889176fd0c795d5c659225e0fae'
 
 node.uploadMultiple({
-  apiKey: '9e1e6889176fd0c795d5c659225e0fae',
+  apiKey,
   appVersion,
   overwrite: true,
   projectRoot: `${__dirname}/..`,
   directory: '.'
 });
+reportBuild({apiKey, appVersion}, {})
+  .then(() => console.log('Build reported!'))
+  .catch(err => console.log('Failed to report build!', err.messsage))
