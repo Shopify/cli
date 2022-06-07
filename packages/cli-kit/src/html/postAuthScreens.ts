994b63c6434a8e7ff4d2bb5fb09ae70f37e1c919
Nausher Rao <nausher.rao@shopify.com>
2022-06-07 13:42:28 -0400

    Update HTML for screens and add stylsheet

--- packages/cli-kit/src/html/postAuthScreens.ts
+++ packages/cli-kit/src/html/postAuthScreens.ts
@@ -1,16 +1,24 @@
+/* eslint-disable no-restricted-imports */
 import fs from 'fs'
+import path from 'path'
+
+const DIRECTORY = path.join(path.resolve(), 'packages/cli-kit/src/html/')
 
 export const getEmptyUrlHTML = (): Buffer => {
-  return fs.readFileSync('./empty-url.html')
+  return fs.readFileSync(path.join(DIRECTORY, 'empty-url.html'))
 }
 export const getMissingCodeHTML = (): Buffer => {
-  return fs.readFileSync('./missing-code.html')
+  return fs.readFileSync(path.join(DIRECTORY, 'missing-code.html'))
 }
 
 export const getMissingStateHTML = (): Buffer => {
-  return fs.readFileSync('./missing-state.html')
+  return fs.readFileSync(path.join(DIRECTORY, 'missing-state.html'))
 }
 
 export const getSuccessHTML = (): Buffer => {
-  return fs.readFileSync('./success.html')
+  return fs.readFileSync(path.join(DIRECTORY, 'success.html'))
+}
+
+export const getStylesheet = (): Buffer => {
+  return fs.readFileSync(path.join(DIRECTORY, 'style.css'))
 }
