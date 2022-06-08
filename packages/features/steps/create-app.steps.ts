58093ebb1d2ea44846481de6f0e63b61657679dd
Isaac Roldán <isaac.roldan@shopify.com>
2022-06-08 10:54:43 +0200

    fix some feature tests

--- packages/features/steps/create-app.steps.ts
+++ packages/features/steps/create-app.steps.ts
@@ -39,7 +39,7 @@ Then(
   },
 )
 
-Then(/I can build the app/, {}, async function () {
+Then(/I can build the app/, {timeout: 2 * 60 * 1000}, async function () {
   await exec(executables.cli, ['app', 'build', '--path', this.appDirectory], {
     env: {...process.env, ...this.temporaryEnv},
   })
