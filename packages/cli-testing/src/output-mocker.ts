283005a3904e0c349eb7a8f63424b363c260b1c3
Pedro Piñera Buendía <pedro@craftweg.com>
2022-06-09 16:13:51 +0200

    Fix tests

--- packages/cli-testing/src/output-mocker.ts
+++ packages/cli-testing/src/output-mocker.ts
@@ -2,16 +2,6 @@ import {output} from '@shopify/cli-kit'
 import {vi} from 'vitest'
 import stripAnsi from 'strip-ansi'
 
-interface OutputMock {
-  output: () => string
-  info: () => string
-  debug: () => string
-  success: () => string
-  completed: () => string
-  warn: () => string
-  clear: () => void
-}
-
 export function mockAndCapture() {
   const collectedOutput: string[] = []
   const collectedInfo: string[] = []
@@ -21,24 +11,24 @@ export function mockAndCapture() {
   const collectedWarn: string[] = []
 
   const infoSpy = vi.spyOn(output, 'info').mockImplementation((content) => {
-    collectedOutput.push(stripAnsi(output.stringifyMessage(content)))
-    collectedInfo.push(stripAnsi(output.stringifyMessage(content)))
+    collectedOutput.push(stripAnsi(output.stringifyMessage(content) ?? ''))
+    collectedInfo.push(stripAnsi(output.stringifyMessage(content) ?? ''))
   })
   const debugSpy = vi.spyOn(output, 'debug').mockImplementation((content) => {
-    collectedOutput.push(stripAnsi(output.stringifyMessage(content)))
-    collectedDebug.push(stripAnsi(output.stringifyMessage(content)))
+    collectedOutput.push(stripAnsi(output.stringifyMessage(content) ?? ''))
+    collectedDebug.push(stripAnsi(output.stringifyMessage(content) ?? ''))
   })
   const successSpy = vi.spyOn(output, 'success').mockImplementation((content) => {
-    collectedOutput.push(stripAnsi(output.stringifyMessage(content)))
-    collectedSuccess.push(stripAnsi(output.stringifyMessage(content)))
+    collectedOutput.push(stripAnsi(output.stringifyMessage(content) ?? ''))
+    collectedSuccess.push(stripAnsi(output.stringifyMessage(content) ?? ''))
   })
   const completedSpy = vi.spyOn(output, 'completed').mockImplementation((content) => {
-    collectedOutput.push(stripAnsi(output.stringifyMessage(content)))
-    collectedCompleted.push(stripAnsi(output.stringifyMessage(content)))
+    collectedOutput.push(stripAnsi(output.stringifyMessage(content) ?? ''))
+    collectedCompleted.push(stripAnsi(output.stringifyMessage(content) ?? ''))
   })
   const outputSpy = vi.spyOn(output, 'warn').mockImplementation((content) => {
-    collectedOutput.push(stripAnsi(output.stringifyMessage(content)))
-    collectedWarn.push(stripAnsi(output.stringifyMessage(content)))
+    collectedOutput.push(stripAnsi(output.stringifyMessage(content) ?? ''))
+    collectedWarn.push(stripAnsi(output.stringifyMessage(content) ?? ''))
   })
   return {
     output: () => collectedOutput.join('\n'),
