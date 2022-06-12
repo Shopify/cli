bfbbfdac7d69958a87797efb443e987bf3556eba
Ariel Caplan <ariel.caplan@shopify.com>
2022-06-12 21:53:50 +0300

    Allow setting a path for upgrades

--- packages/cli-main/src/cli/commands/upgrade.ts
+++ packages/cli-main/src/cli/commands/upgrade.ts
@@ -1,4 +1,4 @@
-import {Command} from '@oclif/core'
+import {Command, Flags} from '@oclif/core'
 import {dependency, error, file, output, path} from '@shopify/cli-kit'
 
 type JSONDepsOpts = [dependency.DependencyType, {[key: string]: string}]
@@ -6,9 +6,34 @@ type JSONDepsOpts = [dependency.DependencyType, {[key: string]: string}]
 export default class Upgrade extends Command {
   static description = 'Upgrade the Shopify CLI'
 
+  static flags = {
+    path: Flags.string({
+      hidden: false,
+      description: 'The path to your project directory.',
+      parse: (input, _) => Promise.resolve(path.resolve(input)),
+      env: 'SHOPIFY_FLAG_PATH',
+    }),
+  }
+
   async run(): Promise<void> {
+    const {flags} = await this.parse(Upgrade)
+    const directory = flags.path ? path.resolve(flags.path) : process.cwd()
+
+    const projectDir = await this.getProjectDir(directory)
+    if (!projectDir) {
+      throw new error.Abort(
+        output.content`Couldn't find the configuration file for ${output.token.path(
+          directory,
+        )}, are you in a Shopify project directory?`,
+      )
+    }
+    const packageJson = JSON.parse(await file.read(path.join(projectDir, 'package.json')))
+    const packageJsonDependencies: {[key: string]: string} = packageJson.dependencies || {}
+    const packageJsonDevDependencies: {[key: string]: string} = packageJson.devDependencies || {}
+
     const cliDependency = '@shopify/cli'
-    const currentVersion = this.config.version
+    let currentVersion: string = {...packageJsonDependencies, ...packageJsonDevDependencies}[cliDependency]
+    if (currentVersion.slice(0, 1).match(/[\^~]/)) currentVersion = this.config.version
     const newestVersion = await dependency.checkForNewVersion(cliDependency, currentVersion)
 
     if (!newestVersion) {
@@ -24,25 +49,6 @@ export default class Upgrade extends Command {
       )}...`,
     )
 
-    const projectDir = await path.findUp(async (dir: string) => {
-      console.log(dir)
-      const configFilesExist = await Promise.all(
-        ['shopify.app.toml', 'hydrogen.config.js', 'hydrogen.config.ts'].map(async (configFile) => {
-          return file.exists(path.join(dir, configFile))
-        }),
-      )
-      if (configFilesExist.some((bool) => bool)) return dir
-    })
-    if (!projectDir) {
-      throw new error.Abort(
-        output.content`Couldn't find the configuration file for ${output.token.path(
-          process.cwd(),
-        )}, are you in a Shopify project directory?`,
-      )
-    }
-    const packageJson = JSON.parse(await file.read(path.join(projectDir, 'package.json')))
-    const packageJsonDependencies: {[key: string]: string} = packageJson.dependencies || {}
-    const packageJsonDevDependencies: {[key: string]: string} = packageJson.devDependencies || {}
     await Promise.all(
       (
         [
@@ -63,6 +69,23 @@ export default class Upgrade extends Command {
           directory: projectDir,
         })
       }),
+    )
+  }
+
+  async getProjectDir(directory: string) {
+    return path.findUp(
+      async (dir: string) => {
+        const configFilesExist = await Promise.all(
+          ['shopify.app.toml', 'hydrogen.config.js', 'hydrogen.config.ts'].map(async (configFile) => {
+            return file.exists(path.join(dir, configFile))
+          }),
+        )
+        if (configFilesExist.some((bool) => bool)) return dir
+      },
+      {
+        cwd: directory,
+        type: 'directory',
+      },
     )
   }
 }
