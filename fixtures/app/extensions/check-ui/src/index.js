8a454d772c30d4cf8c3c37041a70e22132a116f3
Isaac Roldán <isaac.roldan@shopify.com>
2022-06-13 10:59:54 +0200

    Remove extension

--- fixtures/app/extensions/check-ui/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {useExtensionApi, render, Text} from '@shopify/checkout-ui-extensions-react';
-
-render('Checkout::Dynamic::Render', () => <App />);
-
-function App() {
-  const {extensionPoint, i18n} = useExtensionApi();
-  return <Text>{i18n.translate('welcome', {extensionPoint})}</Text>;
-}
