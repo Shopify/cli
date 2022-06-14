b8db97891d4a494edd9050120be434fa8f7c084b
Ariel Caplan <ariel.caplan@shopify.com>
2022-06-14 23:40:09 +0300

    Validate theme files' extensions before upload

--- packages/app/src/cli/services/deploy/themeExtensionConfig.ts
+++ packages/app/src/cli/services/deploy/themeExtensionConfig.ts
@@ -1,23 +1,49 @@
 import {ThemeExtension} from '../../models/app/app'
-import {error, file, path} from '@shopify/cli-kit'
+import {error, file, output, path} from '@shopify/cli-kit'
 
-interface ThemeExtensionConfig {
+export interface ThemeExtensionConfig {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   theme_extension: {
     files: {[key: string]: string}
   }
 }
 
+interface FilenameValidation {
+  validator: RegExp
+  failureMessage: (filename: string) => string
+}
+
 const kilobytes = 1024
 const megabytes = kilobytes * 1024
 
-const SUPPORTED_BUCKETS = ['assets', 'blocks', 'snippets', 'locales']
 const BUNDLE_SIZE_LIMIT_MB = 10
 const BUNDLE_SIZE_LIMIT = BUNDLE_SIZE_LIMIT_MB * megabytes
 const LIQUID_SIZE_LIMIT_KB = 100
 const LIQUID_SIZE_LIMIT = LIQUID_SIZE_LIMIT_KB * kilobytes
+
 const SUPPORTED_ASSET_EXTS = ['.jpg', '.js', '.css', '.png', '.svg']
 const SUPPORTED_LOCALE_EXTS = ['.json']
+const SUPPORTED_EXTS: {[dirname: string]: FilenameValidation} = {
+  assets: {
+    validator: new RegExp(`${SUPPORTED_ASSET_EXTS.join('|')}$`),
+    failureMessage: (filename: string) =>
+      `Only these filetypes are supported in assets: ${SUPPORTED_ASSET_EXTS.join(', ')}`,
+  },
+  blocks: {
+    validator: /.liquid$/,
+    failureMessage: (filename: string) => `Only .liquid files are allowed in blocks.`,
+  },
+  locales: {
+    validator: new RegExp(`${SUPPORTED_LOCALE_EXTS.join('|')}$`),
+    failureMessage: (filename: string) =>
+      `Only these filetypes are supported in locales: ${SUPPORTED_LOCALE_EXTS.join(', ')}`,
+  },
+  snippets: {
+    validator: /.liquid$/,
+    failureMessage: (filename: string) => `Only .liquid files are allowed in snippets.`,
+  },
+}
+const SUPPORTED_BUCKETS = Object.keys(SUPPORTED_EXTS)
 
 export async function themeExtensionConfig(themeExtension: ThemeExtension): Promise<ThemeExtensionConfig> {
   const files: {[key: string]: string} = {}
@@ -27,7 +53,8 @@ export async function themeExtensionConfig(themeExtension: ThemeExtension): Prom
   await Promise.all(
     themeFiles.map(async (filepath) => {
       const relativePath = path.relative(themeExtension.directory, filepath)
-      const dirname = path.dirname(filepath)
+      const dirname = path.dirname(relativePath)
+      validateFile(relativePath, dirname)
       const filesize = await file.size(filepath)
       extensionBytes.push(filesize)
       if (['blocks', 'snippets'].includes(dirname)) liquidBytes.push(filesize)
@@ -54,6 +81,22 @@ export async function themeExtensionConfig(themeExtension: ThemeExtension): Prom
   }
   // eslint-disable-next-line @typescript-eslint/naming-convention
   return {theme_extension: {files}}
+}
+
+function validateFile(filepath: string, dirname: string): void {
+  if (!SUPPORTED_BUCKETS.includes(dirname)) {
+    throw new error.Abort(
+      output.content`Your theme app extension includes files in an unsupported directory, ${output.token.path(
+        dirname,
+      )}`,
+      `Make sure all theme app extension files are in the supported directories: ${SUPPORTED_BUCKETS.join(', ')}`,
+    )
+  }
+  const filenameValidation = SUPPORTED_EXTS[dirname]
+  if (!filepath.match(filenameValidation.validator)) {
+    throw new error.Abort(`Invalid filename in your theme app extension: ${filepath}
+${filenameValidation.failureMessage(filepath)}`)
+  }
 }
 
 function arraySum(array: number[]): number {
