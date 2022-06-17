bd6ea75fd695009c548b66b05063d3c5e83e7451
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2022-06-17 14:27:44 +0200

    Call cli-kit function for dev instead of manual hook

--- packages/cli-main/src/hooks/monorail.ts
+++
@@ -1,9 +0,0 @@
-import {monorail} from '@shopify/cli-kit'
-
-// This hook is called manually from the code. More info: https://oclif.io/docs/hooks#custom-events
-export const hook = async (options: {id: string}) => {
-  const command = options.id.split(' ').pop() || ''
-  const commandIndex = process.argv.indexOf(command)
-  const args = process.argv.slice(commandIndex + 1)
-  await monorail.reportEvent(options.id, args)
-}
