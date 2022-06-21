0fe1a1659c4b3b6d9d87ede16257be075b3e8757
Antoine Grant <antoine.grant@shopify.com>
2022-06-21 12:22:35 -0700

    Update web-pixels template, add @shopify/web-pixels-extension dependency

--- shopify-cli-extensions/create/templates/projects/web_pixel_extension/package.json.tpl
+++ shopify-cli-extensions/create/templates/projects/web_pixel_extension/package.json.tpl
@@ -2,7 +2,8 @@
   "name": "{{.Type}}",
   "license": "MIT",
   "devDependencies": {
-    "@shopify/shopify-cli-extensions": "latest"
+    "@shopify/shopify-cli-extensions": "latest",
+    "@shopify/web-pixels-extension": "latest"
   },
   "scripts": {
     "build": "shopify-cli-extensions build",
--- shopify-cli-extensions/create/templates/shared/web_pixel_extension/index.js
+++ shopify-cli-extensions/create/templates/shared/web_pixel_extension/index.js
@@ -1,12 +1,10 @@
-shopify.extend(
-  "WebPixel::Render",
-  (api) => {
-    const { analytics, context, configuration } = api;
+import {register} from "@shopify/web-pixels-extension";
+
+register(({ configuration, analytics, browser }) => {
     // Bootstrap and insert pixel script tag here
 
     // Sample subscribe to page view
     analytics.subscribe('page_viewed', (event) => {
-     
+      console.log('Page viewed', event);
     });
-  }
-);
+});
