c0953d1f36b40bfc5d1093b5ddd7bd569ca0c88f
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2022-06-22 15:08:26 +0200

    Catch secure-store errors

--- packages/cli-kit/src/secure-store.ts
+++ packages/cli-kit/src/secure-store.ts
@@ -8,9 +8,19 @@ import {content as outputContent, debug} from './output'
  */
 export async function fetch(identifier: string): Promise<string | null> {
   debug(outputContent`Reading ${identifier} from the secure store...`)
-  const keytar = await import('keytar')
-  const content = await keytar.getPassword(constants.keychain.service, identifier)
-  return content
+  try {
+    const keytar = await import('keytar')
+    const content = await keytar.getPassword(constants.keychain.service, identifier)
+    return content
+    // eslint-disable-next-line no-catch-all/no-catch-all
+  } catch (error) {
+    let message = 'Unable to read from the secure store'
+    if (error instanceof Error) {
+      message = message.concat(`: ${error.message}`)
+    }
+    debug(message)
+    return null
+  }
 }
 
 /**
@@ -21,8 +31,17 @@ export async function fetch(identifier: string): Promise<string | null> {
  */
 export async function store(identifier: string, content: string): Promise<void> {
   debug(outputContent`Updating ${identifier} in the secure store with new content...`)
-  const keytar = await import('keytar')
-  await keytar.default.setPassword(constants.keychain.service, identifier, content)
+  try {
+    const keytar = await import('keytar')
+    await keytar.default.setPassword(constants.keychain.service, identifier, content)
+    // eslint-disable-next-line no-catch-all/no-catch-all
+  } catch (error) {
+    let message = 'Unable to update the secure store'
+    if (error instanceof Error) {
+      message = message.concat(`: ${error.message}`)
+    }
+    debug(message)
+  }
 }
 
 /**
@@ -32,7 +51,17 @@ export async function store(identifier: string, content: string): Promise<void> 
  */
 export async function remove(identifier: string): Promise<boolean> {
   debug(outputContent`Removing ${identifier} from the secure store...`)
-  const keytar = await import('keytar')
-  const result = await keytar.default.deletePassword(constants.keychain.service, identifier)
-  return result
+  try {
+    const keytar = await import('keytar')
+    const result = await keytar.default.deletePassword(constants.keychain.service, identifier)
+    return result
+    // eslint-disable-next-line no-catch-all/no-catch-all
+  } catch (error) {
+    let message = 'Unable to remove from the secure store'
+    if (error instanceof Error) {
+      message = message.concat(`: ${error.message}`)
+    }
+    debug(message)
+    return false
+  }
 }
