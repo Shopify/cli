332a7cf44370522a0d89205cc6c7e75c95f7b7e4
Ariel Caplan <ariel.caplan@shopify.com>
2022-06-23 17:35:55 +0300

    Workaround for CLI not accepting shebangs

--- packages/features/steps/hydrogen.steps.ts
+++ packages/features/steps/hydrogen.steps.ts
@@ -3,5 +3,5 @@ import {exec} from '../lib/system'
 import {Then} from '@cucumber/cucumber'
 
 Then(/I see Hydrogen's help menu/, async function () {
-  await exec(executables.cli, ['hydrogen', '--help'], {env: {...process.env, ...this.temporaryEnv}})
+  await exec('node', [executables.cli, 'hydrogen', '--help'], {env: {...process.env, ...this.temporaryEnv}})
 })
