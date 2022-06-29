b456a66d56732bc49f4507023c693f58c0031add
Isaac Roldán <isaac.roldan@shopify.com>
2022-06-29 19:27:12 +0200

    Add comment

--- packages/cli-kit/src/colors.ts
+++ packages/cli-kit/src/colors.ts
@@ -1,4 +1,8 @@
 import {createRequire} from 'module'
 
+/**
+ * ansi-colors is a commonjs dependency that can be imported as a module.
+ * This file is a wrapper to require and export ansi-colors.
+ */
 const require = createRequire(import.meta.url)
 export const colors = require('ansi-colors')
