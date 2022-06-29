adc4f07a263e81aba3f6b8bc9088ac76c5e78144
Isaac Roldán <isaac.roldan@shopify.com>
2022-06-29 19:54:53 +0200

    Add references

--- packages/theme/src/cli/utilities/theme-store.ts
+++ packages/theme/src/cli/utilities/theme-store.ts
@@ -1,10 +1,10 @@
 import {store as conf, error} from '@shopify/cli-kit'
 
 export function getTheme(flags: {store: string | undefined}): string {
-  const store = flags.store || conf.getTheme()
+  const store = flags.store || conf.cliKitStore().getTheme()
   if (!store) {
     throw new error.Abort('A store is required', 'Specify the store using --store={your_store}')
   }
-  conf.setTheme(store)
+  conf.cliKitStore().setTheme(store)
   return store
 }
