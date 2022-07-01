194a9bf71ad0dab5d2bfbf28e96f2894466b1c35
Pedro Piñera Buendía <pedro@craftweg.com>
2022-07-01 17:54:47 +0200

    Extract the logic to run the CLI

--- packages/cli-main/src/index.ts
+++ packages/cli-main/src/index.ts
@@ -1,59 +1,10 @@
-// CLI
-import {run, settings, flush} from '@oclif/core'
-import Bugsnag from '@bugsnag/js'
-import {error as kitError, environment, output, store, constants, analytics} from '@shopify/cli-kit'
+import {runCLI} from '@shopify/cli-kit/node/cli'
 
-async function runCLI() {
-  await store.initializeCliKitStore()
-  output.initiateLogging({filename: 'shopify.cli.log'})
-  if (environment.local.isDebug()) {
-    settings.debug = true
-  } else {
-    Bugsnag.start({
-      apiKey: '9e1e6889176fd0c795d5c659225e0fae',
-      logger: null,
-      appVersion: await constants.versions.cliKit(),
-      autoTrackSessions: false,
-    })
-  }
-
-  run(undefined, import.meta.url)
-    .then(flush)
-    .catch((error: Error): Promise<void | Error> => {
-      if (error instanceof kitError.AbortSilent) {
-        process.exit(1)
-      }
-      const kitMapper = kitError.mapper
-      const kitHandle = kitError.handler
-      // eslint-disable-next-line promise/no-nesting
-      return kitMapper(error)
-        .then(reportError)
-        .then((error: Error) => {
-          return kitHandle(error)
-        })
-        .then(() => {
-          process.exit(1)
-        })
-    })
+async function runShopifyCLI() {
+  await runCLI({
+    moduleURL: import.meta.url,
+    logFilename: 'shopify.cli.log',
+  })
 }
 
-const reportError = async (errorToReport: Error): Promise<Error> => {
-  await analytics.reportEvent({errorMessage: errorToReport.message})
-
-  if (!settings.debug && kitError.shouldReport(errorToReport)) {
-    let mappedError: Error
-    // eslint-disable-next-line no-prototype-builtins
-    if (Object.prototype.isPrototypeOf(errorToReport)) {
-      const mappedError = Object.assign(Object.create(errorToReport), {})
-      if (mappedError.stack) mappedError.stack = mappedError.stack.replace(new RegExp('file:///', 'g'), '/')
-    } else {
-      mappedError = errorToReport
-    }
-    await new Promise((resolve, reject) => {
-      Bugsnag.notify(mappedError, undefined, resolve)
-    })
-  }
-  return Promise.resolve(errorToReport)
-}
-
-export default runCLI
+export default runShopifyCLI
--- packages/create-app/src/index.ts
+++ packages/create-app/src/index.ts
@@ -1,33 +1,10 @@
-import {run, flush, settings} from '@oclif/core'
-import {error as kitError, environment, output} from '@shopify/cli-kit'
+import {runCreateCLI} from '@shopify/cli-kit/node/cli'
 
-function runCreateApp() {
-  output.initiateLogging({filename: 'shopify.create-app.log'})
-  const initIndex = process.argv.findIndex((arg) => arg.includes('init'))
-  if (initIndex === -1) {
-    const initIndex = process.argv.findIndex((arg) => arg.match(/bin(\/|\\)(create-app|dev|run)/)) + 1
-    process.argv.splice(initIndex, 0, 'init')
-  }
-
-  if (environment.local.isDebug()) {
-    settings.debug = true
-  }
-
-  // Start the CLI
-  run(undefined, import.meta.url)
-    .then(flush)
-    .catch((error: Error): Promise<void | Error> => {
-      const kitMapper = kitError.mapper
-      const kitHandle = kitError.handler
-      // eslint-disable-next-line promise/no-nesting
-      return kitMapper(error)
-        .then((error: Error) => {
-          return kitHandle(error)
-        })
-        .then(() => {
-          process.exit(1)
-        })
-    })
+async function runCreateAppCLI() {
+  await runCreateCLI('app', {
+    moduleURL: import.meta.url,
+    logFilename: 'shopify.create-app.log',
+  })
 }
 
-export default runCreateApp
+export default runCreateAppCLI
