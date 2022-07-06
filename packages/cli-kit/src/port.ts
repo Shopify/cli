740f73acc33ebecbd7a6f9e8980a1deb5ed62a8b
Alvaro Gutierrez <alvaro.gutierrezblanco@shopify.com>
2022-07-06 08:31:20 +0200

    #109 Retrying implementation for obtaining a random port

--- packages/cli-kit/src/port.ts
+++ packages/cli-kit/src/port.ts
@@ -1,4 +1,6 @@
 import {debug, content, token} from './output.js'
+import {Abort} from './error.js'
+import {sleep} from './system.js'
 import * as port from 'get-port-please'
 
 /**
@@ -7,7 +9,24 @@ import * as port from 'get-port-please'
  */
 export async function getRandomPort(): Promise<number> {
   debug(content`Getting a random port...`)
-  const randomPort = await port.getRandomPort()
-  debug(content`Random port obtained: ${token.raw(`${randomPort}`)}`)
-  return randomPort
+  const maxTries = 5
+  const waitTimeInSeconds = 1
+  let retryCount = 1
+  while (true) {
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      const randomPort = await port.getRandomPort()
+      debug(content`Random port obtained: ${token.raw(`${randomPort}`)}`)
+      return randomPort
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      if (retryCount++ < maxTries) {
+        debug(content`Unknown problem getting a random port: ${error.message}`)
+        // eslint-disable-next-line no-await-in-loop
+        await sleep(waitTimeInSeconds)
+      } else {
+        throw new Abort(error.message)
+      }
+    }
+  }
 }
