8535776f3f58141bea1a5b131b86dbe11148f1bf
Geon George <geongeorge98@gmail.com>
2022-07-06 14:23:45 +0530

    Removes binary value for encoding

--- packages/app/src/cli/services/dev/output.ts
+++ packages/app/src/cli/services/dev/output.ts
@@ -66,7 +66,7 @@ function outputThemeExtensionsMessage(extensions: ThemeExtension[]) {
 
 function buildAppURL(storeFqdn: string, publicURL: string) {
   const hostUrl = `${storeFqdn}/admin`
-  const hostParam = Buffer.from(hostUrl, 'binary').toString('base64').replace(/[=]/g, '')
+  const hostParam = Buffer.from(hostUrl).toString('base64').replace(/[=]/g, '')
   return `${publicURL}?shop=${storeFqdn}&host=${hostParam}`
 }
 
