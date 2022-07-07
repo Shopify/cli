91357c6cbba2b8f6aeb3d049740f787fcc0ea743
Pedro Piñera Buendía <pedro@craftweg.com>
2022-07-07 15:56:29 +0200

    Remove unnecessary log

--- bin/update-bugsnag.js
+++ bin/update-bugsnag.js
@@ -11,8 +11,6 @@ const __dirname = path.dirname(__filename);
 const appVersion = JSON.parse(fs.readFileSync(`${__dirname}/../packages/cli-main/package.json`)).version;
 const apiKey = '9e1e6889176fd0c795d5c659225e0fae';
 
-console.log("uploading")
-
 (async () => {
   try {
     await node.uploadMultiple({
