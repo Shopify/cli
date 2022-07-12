e75dcb84d7e5eb13ab5a63dd275bab2900934d27
Bala Clark <bala.clark@shopify.com>
2022-07-12 13:37:55 +0000

    use the useTranslate hook in the react scaffold
    Closes: https://github.com/Shopify/shopify-cli-extensions/issues/362

--- shopify-cli-extensions/create/templates/shared/checkout_ui_extension/react.js
+++ shopify-cli-extensions/create/templates/shared/checkout_ui_extension/react.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import {useExtensionApi, render, Banner} from '@shopify/checkout-ui-extensions-react';
+import {
+  useExtensionApi,
+  render,
+  Banner,
+  useTranslate,
+} from '@shopify/checkout-ui-extensions-react';
 
 render('Checkout::Dynamic::Render', () => <App />);
 
 function App() {
-  const {extensionPoint, i18n} = useExtensionApi();
-  return <Banner>{i18n.translate('welcome', {extensionPoint})}</Banner>;
+  const {extensionPoint} = useExtensionApi();
+  const translate = useTranslate();
+  return <Banner>{translate('welcome', {extensionPoint})}</Banner>;
 }
