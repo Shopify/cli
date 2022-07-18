089b28fdce21a248f5446c0ecd4bce91fe6704ba
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2022-07-18 11:25:21 +0200

    Do not finish hydrogen dev command until the server closes

--- packages/cli-hydrogen/src/cli/services/dev.ts
+++ packages/cli-hydrogen/src/cli/services/dev.ts
@@ -1,4 +1,4 @@
-import {createServer} from 'vite'
+import {createServer, ViteDevServer} from 'vite'
 import {error as kitError} from '@shopify/cli-kit'
 
 interface DevOptions {
@@ -20,12 +20,21 @@ async function dev({directory, force, host}: DevOptions) {
     await server.listen()
     server.printUrls()
     server.config.logger.info('')
+    await closeEvent(server)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     const abortError = new kitError.Abort(error.message)
     abortError.stack = error.stack
     throw abortError
   }
+}
+
+function closeEvent(server: ViteDevServer): Promise<void> {
+  return new Promise((resolve) => {
+    server.ws.on('close', () => {
+      return resolve()
+    })
+  })
 }
 
 export default dev
