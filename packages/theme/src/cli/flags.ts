e37ed918f372df36f117366fd05a8e5000fc520e
Ariel Caplan <ariel.caplan@shopify.com>
2022-07-21 11:36:53 +0300

    Add --path option to specify theme init directory

---
+++ packages/theme/src/cli/flags.ts
@@ -0,0 +1,15 @@
+import {Flags} from '@oclif/core'
+import {path} from '@shopify/cli-kit'
+
+/**
+ * An object that contains the flags that
+ * are shared across all the app commands.
+ */
+export const themeFlags = {
+  path: Flags.string({
+    hidden: false,
+    description: 'The path to your theme directory.',
+    parse: (input, _) => Promise.resolve(path.resolve(input)),
+    env: 'SHOPIFY_FLAG_PATH',
+  }),
+}
