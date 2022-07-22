343f246e0a5be13f6addaa48a7268febedaec7be
Shaun Stanworth <shaun.stanworth@shopify.com>
2022-07-22 15:08:33 +0100

    Linting correction

--- packages/cli-kit/src/node/base-command.ts
+++ packages/cli-kit/src/node/base-command.ts
@@ -4,14 +4,14 @@ import {Command} from '@oclif/core'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default abstract class extends Command {
+  async catch(error: Error & {exitCode?: number | undefined}) {
+    errorHandler(error)
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   protected async init(): Promise<any> {
     // This function runs just prior to `run`
     registerCleanBugsnagErrorsFromWithinPlugins(this.config.plugins)
     return super.init()
-  }
-
-  async catch(error: Error & {exitCode?: number | undefined}) {
-    errorHandler(error)
   }
 }
