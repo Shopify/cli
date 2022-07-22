d20897aac5c5356c9010b1b47fd4628b1341e2da
Shaun Stanworth <shaun.stanworth@shopify.com>
2022-07-22 14:39:05 +0100

    Clean error traces from OCLIF plugins

--- packages/cli-kit/src/node/base-command.ts
+++ packages/cli-kit/src/node/base-command.ts
@@ -1,8 +1,16 @@
 import {errorHandler} from './error-handler.js'
+import {registerCleanBugsnagErrorsFromWithinPlugins} from '../bugsnag.js'
 import {Command} from '@oclif/core'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default abstract class extends Command {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  protected async init(): Promise<any> {
+    // This function runs just prior to `run`
+    registerCleanBugsnagErrorsFromWithinPlugins(this.config.plugins)
+    return super.init()
+  }
+
   async catch(error: Error & {exitCode?: number | undefined}) {
     errorHandler(error)
   }
