53b1b8592920ba3f2dd665426193b558ba945496
Ariel Caplan <ariel.caplan@shopify.com>
2022-07-27 14:59:08 +0300

    Flag corrections for theme dev

--- packages/theme/src/cli/commands/theme/dev.ts
+++ packages/theme/src/cli/commands/theme/dev.ts
@@ -1,15 +1,17 @@
+import {themeFlags} from '../../flags.js'
 import {getTheme} from '../../utilities/theme-store.js'
+import ThemeCommand from '../theme-command.js'
 import {Flags} from '@oclif/core'
-import {cli, path, session, string} from '@shopify/cli-kit'
+import {cli, session, string} from '@shopify/cli-kit'
 import {execCLI2} from '@shopify/cli-kit/node/ruby'
-import Command from '@shopify/cli-kit/node/base-command'
 
-export default class Dev extends Command {
+export default class Dev extends ThemeCommand {
   static description =
     'Uploads the current theme as a development theme to the connected store, then prints theme editor and preview URLs to your terminal. While running, changes will push to the store in real time.'
 
   static flags = {
     ...cli.globalFlags,
+    ...themeFlags,
     host: Flags.string({
       description: 'Set which network interface the web server listens on. The default value is 127.0.0.1.',
       env: 'SHOPIFY_FLAG_HOST',
@@ -23,12 +25,6 @@ export default class Dev extends Command {
       default: 'hot-reload',
       options: ['hot-reload', 'full-page', 'off'],
       env: 'SHOPIFY_FLAG_LIVE_RELOAD',
-    }),
-    path: Flags.string({
-      description: 'The path to your theme',
-      default: '.',
-      env: 'SHOPIFY_FLAG_PATH',
-      parse: (input, _) => Promise.resolve(path.resolve(input)),
     }),
     poll: Flags.boolean({
       description: 'Force polling to detect file changes.',
@@ -54,28 +50,10 @@ export default class Dev extends Command {
   async run(): Promise<void> {
     const {flags, args} = await this.parse(Dev)
 
+    const flagsToPass = this.passThroughFlags(flags, {exclude: ['path', 'store', 'verbose']})
+    const command = ['theme', 'serve', flags.path, ...flagsToPass]
+
     const store = getTheme(flags)
-
-    const command = ['theme', 'serve', flags.path]
-
-    if (flags.poll) {
-      command.push('--poll')
-    }
-    if (flags['theme-editor-sync']) {
-      command.push('--theme-editor-sync')
-    }
-    if (flags.port) {
-      command.push('--port')
-      command.push(flags.port)
-    }
-    if (flags.host) {
-      command.push('--host')
-      command.push(flags.host)
-    }
-    if (flags['live-reload']) {
-      command.push('--live-reload')
-      command.push(flags['live-reload'])
-    }
 
     const adminSession = await session.ensureAuthenticatedAdmin(store)
     await execCLI2(command, {adminSession})
