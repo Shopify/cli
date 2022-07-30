668e3a6c17f90388cba15291a09918869b242e2f
Shaun Stanworth <shaun.stanworth@shopify.com>
2022-07-30 12:42:16 +0100

    Standalone module for Monorail

---
+++ packages/cli-kit/src/monorail.test.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import * as http from './http.js'
+import {publishEvent} from './monorail.js'
+import {mockAndCaptureOutput} from './testing/output.js'
+import {afterEach, beforeEach, describe, expect, it, vi} from 'vitest'
+
+describe('monorail', () => {
+  const currentDate = new Date(Date.UTC(2022, 1, 1, 10, 0, 0))
+  const expectedURL = 'https://monorail-edge.shopifysvc.com/v1/produce'
+  const expectedHeaders = {
+    'Content-Type': 'application/json; charset=utf-8',
+    'X-Monorail-Edge-Event-Created-At-Ms': '1643709600000',
+    'X-Monorail-Edge-Event-Sent-At-Ms': '1643709600000',
+  }
+
+  beforeEach(() => {
+    vi.setSystemTime(currentDate)
+    vi.mock('./http.js')
+    vi.mocked(http.fetch).mockResolvedValue({status: 200} as any)
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('shows an error if the Monorail request fails', async () => {
+    vi.mocked(http.fetch).mockResolvedValueOnce({status: 500, statusText: 'Monorail is down'} as any)
+    const outputMock = mockAndCaptureOutput()
+    const res = await publishEvent('fake_schema/0.0', {foo: 'bar'})
+    expect(res.type).toEqual('error')
+    expect(outputMock.debug()).toMatch('Failed to report usage analytics: Monorail is down')
+  })
+
+  it('builds a request', async () => {
+    const res = await publishEvent('fake_schema/0.0', {foo: 'bar'})
+    expect(res.type).toEqual('ok')
+    expect(http.fetch).toHaveBeenCalledWith(expectedURL, {
+      method: 'POST',
+      body: JSON.stringify({schema_id: 'fake_schema/0.0', payload: {foo: 'bar'}}),
+      headers: expectedHeaders,
+    })
+  })
+})
---
+++ packages/cli-kit/src/monorail.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import {fetch} from './http.js'
+import {debug, content, token} from './output.js'
+import {JsonMap} from './json.js'
+
+const url = 'https://monorail-edge.shopifysvc.com/v1/produce'
+
+type Optional<T> = T | null
+
+export interface Schemas {
+  'app_cli3_command/1.0': {
+    partner_id?: Optional<number>
+    command: string
+    args: string
+    project_type?: Optional<string>
+    time_start: number
+    time_end: number
+    total_time: number
+    success: boolean
+    error_message?: Optional<string>
+    api_key?: Optional<string>
+    cli_version: string
+    uname: string
+    metadata?: Optional<string>
+    ruby_version: string
+    node_version: string
+    is_employee: boolean
+  }
+  [schemaId: string]: JsonMap
+}
+
+type MonorailResult = {type: 'ok'} | {type: 'error'; message: string}
+
+export async function publishEvent<TSchemaId extends keyof Schemas, TPayload extends Schemas[TSchemaId]>(
+  schemaId: TSchemaId,
+  payload: TPayload,
+): Promise<MonorailResult> {
+  try {
+    const currentTime = new Date().getTime()
+    const body = JSON.stringify({schema_id: schemaId, payload})
+    const headers = buildHeaders(currentTime)
+
+    const response = await fetch(url, {method: 'POST', body, headers})
+
+    if (response.status === 200) {
+      debug(content`Analytics event sent: ${token.json(payload)}`)
+      return {type: 'ok'}
+    } else {
+      debug(`Failed to report usage analytics: ${response.statusText}`)
+      return {type: 'error', message: response.statusText}
+    }
+    // eslint-disable-next-line no-catch-all/no-catch-all
+  } catch (error) {
+    let message = 'Failed to report usage analytics'
+    if (error instanceof Error) {
+      message = message.concat(`: ${error.message}`)
+    }
+    debug(message)
+    return {type: 'error', message}
+  }
+}
+
+const buildHeaders = (currentTime: number) => {
+  return {
+    'Content-Type': 'application/json; charset=utf-8',
+    'X-Monorail-Edge-Event-Created-At-Ms': currentTime.toString(),
+    'X-Monorail-Edge-Event-Sent-At-Ms': currentTime.toString(),
+  }
+}
