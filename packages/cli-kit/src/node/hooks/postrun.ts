de3d950b697aaf0a1ece8dc9864ec074589a0152
Shaun Stanworth <shaun.stanworth@shopify.com>
2022-07-30 17:08:15 +0100

    Correction to merge

--- packages/cli-kit/src/node/hooks/postrun.ts
+++ packages/cli-kit/src/node/hooks/postrun.ts
@@ -3,8 +3,8 @@ import {debug} from '../../output.js'
 import {Hook} from '@oclif/core'
 
 // This hook is called after each successful command run. More info: https://oclif.io/docs/hooks
-export const hookPost: Hook.Postrun = async ({config}) => {
+export const hookPost: Hook.Postrun = async ({config, Command}) => {
   await reportEvent({config})
-  const command = options.Command?.id?.replace(/:/g, ' ')
+  const command = Command?.id?.replace(/:/g, ' ')
   debug(`Completed command ${command}`)
 }
