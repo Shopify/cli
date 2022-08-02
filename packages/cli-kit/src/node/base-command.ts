63d6fb4ecf935dbd82e385a2f343bb8875da7a9d
Ariel Caplan <ariel.caplan@shopify.com>
2022-08-02 13:10:41 +0300

    Use local and global preset files

--- packages/cli-kit/src/node/base-command.ts
+++ packages/cli-kit/src/node/base-command.ts
@@ -1,8 +1,8 @@
 import {errorHandler, registerCleanBugsnagErrorsFromWithinPlugins} from './error-handler.js'
-import {findUp} from '../path.js'
-import {read as fileRead} from '../file.js'
+import {findUp, join as pathJoin} from '../path.js'
+import {exists as fileExists, read as fileRead} from '../file.js'
 import {decode as decodeTOML} from '../toml.js'
-import {isDebug} from '../environment/local.js'
+import {homeDirectory, isDebug} from '../environment/local.js'
 import {Command, Interfaces} from '@oclif/core'
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -40,17 +40,27 @@ export default abstract class extends Command {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 async function presets(flags: {[name: string]: any}): Promise<{[name: string]: any}> {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let retval: {[name: string]: any} = {}
-  if (!flags.preset) return retval
-  const presetsFile = await findUp('shopify.presets.toml', {
+  if (!flags.preset) return {}
+  const globalPresetsFile = pathJoin(homeDirectory(), 'shopify.presets.toml')
+  const localPresetsFile = await findUp('shopify.presets.toml', {
     type: 'file',
     cwd: flags.path ?? '.',
   })
-  if (presetsFile) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const presetSettings: {[name: string]: any} = decodeTOML(await fileRead(presetsFile))
-    if (typeof presetSettings[flags.preset] === 'object') retval = {retval, ...presetSettings[flags.preset]}
+  return {
+    ...(await presetsFromFile(globalPresetsFile, flags.preset)),
+    ...(await presetsFromFile(localPresetsFile, flags.preset)),
   }
-  return retval
+}
+
+async function presetsFromFile(
+  filepath: string | undefined,
+  presetName: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): Promise<{[name: string]: any}> {
+  if (filepath && (await fileExists(filepath))) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const presetSettings: {[name: string]: any} = decodeTOML(await fileRead(filepath))
+    if (typeof presetSettings[presetName] === 'object') return presetSettings[presetName]
+  }
+  return {}
 }
