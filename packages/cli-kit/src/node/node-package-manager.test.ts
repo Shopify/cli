9e27c5a3ab77c1fd4a9f6975ae5d3f47e35d47f4
Shaun Stanworth <shaun.stanworth@shopify.com>
2022-08-05 10:09:27 +0100

    Node package test was wrong

--- packages/cli-kit/src/node/node-package-manager.test.ts
+++ packages/cli-kit/src/node/node-package-manager.test.ts
@@ -45,12 +45,24 @@ describe('packageManagerUsedForCreating', () => {
     expect(got).toBe('pnpm')
   })
 
+  it('returns npm if the npm_config_user_agent variable contains npm', () => {
+    // Given
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const env = {npm_config_user_agent: 'npm'}
+
+    // When
+    const got = packageManagerUsedForCreating(env)
+
+    // Then
+    expect(got).toBe('npm')
+  })
+
   it('returns npm when the package manager cannot be detected', () => {
     // When
     const got = packageManagerUsedForCreating({})
 
     // Then
-    expect(got).toBe('npm')
+    expect(got).toBe('unknown')
   })
 })
 
