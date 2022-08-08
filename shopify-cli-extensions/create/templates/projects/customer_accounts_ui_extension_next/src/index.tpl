Shopify/shopify-cli-extensions

Add customer accounts extensions

create/templates/projects/customer_accounts_ui_extension_next/src/index.tpl
@@ -1,0 +1,5 @@
+{{- if .Development.UsesReact -}}
+{{ template "shared/customer_account_ui_extension/react.js" . }}
+{{- else -}}
+{{ template "shared/customer_account_ui_extension/javascript.js" . }}
+{{- end -}}

Shopify/cli

shopify-cli-extensions/create/templates/projects/customer_accounts_ui_extension_next/src/index.tpl
@@ -1,0 +1,5 @@
+{{- if .Development.UsesReact -}}
+{{ template "shared/customer_accounts_ui_extension/react.js" . }}
+{{- else -}}
+{{ template "shared/customer_accounts_ui_extension/javascript.js" . }}
+{{- end -}}