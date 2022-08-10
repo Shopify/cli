cb11282e1e5d4d2a8668d244dd28236477c4984a
Alvaro Gutierrez <alvaro.gutierrezblanco@shopify.com>
2022-08-10 17:07:43 +0200

    Fixed lint problems

--- packages/ui-extensions-test-utils/src/withProviders.ts
+++ packages/ui-extensions-test-utils/src/withProviders.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
 
 export type ProviderComponent<TProps = any> = React.ComponentType<React.PropsWithChildren<TProps>>
