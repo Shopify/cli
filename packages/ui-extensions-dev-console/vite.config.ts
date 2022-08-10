b7cc00393e085df58e976dfcdc4100fb0fb6446d
Alvaro Gutierrez <alvaro.gutierrezblanco@shopify.com>
2022-08-10 12:54:48 +0200

    Adjust integration go binary and dev console

--- packages/ui-extensions-dev-console/vite.config.ts
+++ packages/ui-extensions-dev-console/vite.config.ts
@@ -1,23 +1,19 @@
-import path from 'path';
-
-import {defineConfig} from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import {defineConfig} from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
-    outDir: '../../api/dev-console',
+    outDir: '../ui-extensions-go-cli/api/dev-console',
     assetsDir: 'extensions/dev-console/assets',
   },
   resolve: {
     alias: {
       // vite.config.ts, tsconfig.json, and jest.config.ts all need to define their own aliases
       '@': path.resolve(__dirname, './src'),
-      '@shopify/ui-extensions-server-kit': path.resolve(
-        __dirname,
-        '../ui-extensions-server-kit/src',
-      ),
+      '@shopify/ui-extensions-server-kit': path.resolve(__dirname, '../ui-extensions-server-kit/src'),
     },
   },
-});
+})
