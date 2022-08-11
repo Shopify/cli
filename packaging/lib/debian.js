326524c8e0ca272bacc836b793003a714fab3ac7
Ariel Caplan <ariel.caplan@shopify.com>
2022-08-11 16:35:23 +0300

    Specify debian package version

--- packaging/lib/debian.js
+++ packaging/lib/debian.js
@@ -1,6 +1,6 @@
 import {path, system} from '@shopify/cli-kit'
 
-export async function packageDebian(packagingDir) {
+export async function packageDebian(packagingDir, cliVersion) {
   console.log(await system.captureOutput('pwd', [], {cwd: path.join(packagingDir, 'debian')}))
-  await system.exec('dpkg-deb', ['-b', 'shopify-cli'], {cwd: path.join(packagingDir, 'debian')})
+  await system.exec('dpkg-deb', ['-b', 'shopify-cli', `shopify-cli-${cliVersion}.deb`], {cwd: path.join(packagingDir, 'debian')})
 }
