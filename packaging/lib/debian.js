f6f3dd54be92a2359c54bbd315408be628c28db3
Ariel Caplan <ariel.caplan@shopify.com>
2022-08-11 16:37:08 +0300

    Linting

--- packaging/lib/debian.js
+++ packaging/lib/debian.js
@@ -1,6 +1,7 @@
 import {path, system} from '@shopify/cli-kit'
 
 export async function packageDebian(packagingDir, cliVersion) {
-  console.log(await system.captureOutput('pwd', [], {cwd: path.join(packagingDir, 'debian')}))
-  await system.exec('dpkg-deb', ['-b', 'shopify-cli', `shopify-cli-${cliVersion}.deb`], {cwd: path.join(packagingDir, 'debian')})
+  await system.exec('dpkg-deb', ['-b', 'shopify-cli', `shopify-cli-${cliVersion}.deb`], {
+    cwd: path.join(packagingDir, 'debian'),
+  })
 }
