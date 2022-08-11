69aeddc8ded4da460925944b2897325bb4427120
Ariel Caplan <ariel.caplan@shopify.com>
2022-08-11 15:00:33 +0300

    Extract homebrew-specific logic to its own file

--- bin/package.js
+++ bin/package.js
@@ -1,53 +1,19 @@
 #!/usr/bin/env node
 
-import {constants, file, http, output, path, system, template} from '@shopify/cli-kit';
-import {createHash} from 'node:crypto';
+import {constants, output, path, template} from '@shopify/cli-kit';
+import {homebrewVariables, copyHomebrew} from '../packaging/lib/homebrew.js';
 
 output.initiateLogging();
 
 const cliVersion = await constants.versions.cliKit();
-
-async function tarballForPackage(pkg) {
-  const response = await http.fetch(`https://registry.npmjs.com/${pkg}`);
-  return (await response.json()).versions[cliVersion].dist.tarball;
-}
-
-async function sha256ForTarball(url) {
-  const hash = createHash('sha256').setEncoding('hex');
-  const response = await http.fetch(url);
-  const stream = response.body.pipe(hash);
-  await new Promise(resolve => stream.on("finish", resolve));
-  return hash.read();
-}
-
-async function tarballAndShaForPackage(pkg) {
-  const tarball = await tarballForPackage(pkg);
-  const sha = await sha256ForTarball(tarball);
-  return [tarball, sha];
-}
-
-const [
-  [cliTarball, cliSha],
-  [themeTarball, themeSha]
-] = await Promise.all([
-  tarballAndShaForPackage('@shopify/cli'),
-  tarballAndShaForPackage('@shopify/theme')
-]);
-
 const packagingDir = path.join(path.dirname(import.meta.url), '../packaging').replace(/^file:/, '')
 
 await template.recursiveDirectoryCopy(
   path.join(packagingDir, 'src'),
   path.join(packagingDir, 'dist'),
   {
-    cliTarball,
-    cliSha,
-    themeTarball,
-    themeSha,
+    ...(await homebrewVariables(cliVersion)),
   },
 );
 
-const homebrewFormulaDest = path.normalize(await system.captureOutput('/opt/dev/bin/dev', ['project-path', 'homebrew-shopify']))
-(await path.glob(path.join(packagingDir, 'dist/homebrew/*'))).forEach(async (sourceFile) => {
-  await file.copy(sourceFile, path.join(homebrewFormulaDest, path.basename(sourceFile)));
-})
+await copyHomebrew(packagingDir)
---
+++ packaging/lib/homebrew.js
@@ -0,0 +1,41 @@
+import {file, http, path, system} from '@shopify/cli-kit'
+import {createHash} from 'node:crypto'
+
+async function tarballForPackage(pkg, cliVersion) {
+  const response = await http.fetch(`https://registry.npmjs.com/${pkg}`)
+  return (await response.json()).versions[cliVersion].dist.tarball
+}
+
+async function sha256ForTarball(url) {
+  const hash = createHash('sha256').setEncoding('hex')
+  const response = await http.fetch(url)
+  const stream = response.body.pipe(hash)
+  await new Promise((resolve) => stream.on('finish', resolve))
+  return hash.read()
+}
+
+async function tarballAndShaForPackage(pkg, cliVersion) {
+  const tarball = await tarballForPackage(pkg, cliVersion)
+  const sha = await sha256ForTarball(tarball)
+  return [tarball, sha]
+}
+
+export async function homebrewVariables(cliVersion) {
+  const [[cliTarball, cliSha], [themeTarball, themeSha]] = await Promise.all([
+    tarballAndShaForPackage('@shopify/cli', cliVersion),
+    tarballAndShaForPackage('@shopify/theme', cliVersion),
+  ])
+  return {cliTarball, cliSha, themeTarball, themeSha}
+}
+
+export async function copyHomebrew(packagingDir) {
+  const homebrewFormulaDest = path.normalize(
+    await system.captureOutput('/opt/dev/bin/dev', ['project-path', 'homebrew-shopify']),
+  )
+  const homebrewFormulas = await path.glob(path.join(packagingDir, 'dist/homebrew/*'))
+  await Promise.all(
+    homebrewFormulas.map(async (sourceFile) => {
+      await file.copy(sourceFile, path.join(homebrewFormulaDest, path.basename(sourceFile)))
+    }),
+  )
+}
