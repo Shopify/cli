07398580734e92ca36ec4bfc6051cfa4a544c2ab
Vincent Lynch <vincent.lynch@shopify.com>
2022-08-15 14:35:16 -0400

    Making sure all commands are being runned inside proper directory

--- packages/cli-kit/src/git.ts
+++ packages/cli-kit/src/git.ts
@@ -1,3 +1,4 @@
+import {path} from '.'
 import {Abort} from './error.js'
 import {hasGit, isTerminalInteractive} from './environment/local.js'
 import {content, token, debug} from './output.js'
@@ -14,6 +15,27 @@ export const GitNotPresentError = () => {
 
 export const OutsideGitDirectoryError = () => {
   return new Abort('Must be inside a Git directory to continue')
+}
+
+export const MalformedRemoteUrlError = () => {
+  return new Abort(
+    'Git remote origin URL is malformed.',
+    content`Run ${token.genericShellCommand('git remote -v')} to validate URL.`,
+  )
+}
+
+export const NoCommitError = () => {
+  return new Abort(
+    'Must have at least one commit to run command',
+    content`Run ${token.genericShellCommand("git commit -m 'Initial commit'")} to create your first commit.`,
+  )
+}
+
+export const DetachedHeadError = () => {
+  return new Abort(
+    "Git HEAD can't be detached to run command",
+    content`Run ${token.genericShellCommand('git checkout [branchName]')} to reattach HEAD`,
+  )
 }
 
 export async function initializeRepository(directory: string) {
@@ -64,6 +86,43 @@ export async function downloadRepository({
   }
 }
 
+export async function getRemoteRepository() {
+  const remoteUrl = await git().getConfig('remote.origin.url', 'local')
+
+  if (remoteUrl.value) {
+    try {
+      const urlObj = new URL(remoteUrl.value)
+      const parsedPath = path.parse(urlObj.pathname)
+      const repository = `${parsedPath.dir}/${parsedPath.name}`
+      return repository.charAt(0) === '/' ? repository.substring(1) : repository
+    } catch {
+      throw MalformedRemoteUrlError()
+    }
+  }
+}
+
+export async function getLatestCommit() {
+  try {
+    const logs = await git().log({
+      maxCount: 1,
+    })
+    if (!logs.latest) throw NoCommitError()
+    return logs.latest
+  } catch {
+    throw NoCommitError()
+  }
+}
+
+export async function getHeadSymbolicRef() {
+  try {
+    const ref = await git().raw('symbolic-ref', '-q', 'HEAD')
+    if (!ref) throw DetachedHeadError()
+    return ref.trim()
+  } catch {
+    throw DetachedHeadError()
+  }
+}
+
 /**
  * If "git" is not present in the environment it throws
  * an abort error.
@@ -78,7 +137,10 @@ export async function ensurePresentOrAbort() {
  * If command run from outside a .git directory tree
  * it throws an abort error.
  */
-export async function ensureInsideGitDirectory() {
+export async function ensureInsideGitDirectory(workingDirectory?: string) {
+  const cwd = process.cwd()
+  if (workingDirectory && cwd !== workingDirectory) process.chdir(workingDirectory)
+
   if (!(await git().checkIsRepo())) {
     throw OutsideGitDirectoryError()
   }
