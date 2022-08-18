a74d8cbf697b264d512ce052977843c3da0d565a
Pedro Piñera Buendía <pedro@craftweg.com>
2022-08-18 14:46:05 +0200

    Break the dependency with @shopify/cli-kit

--- bin/package.js
+++ bin/package.js
@@ -1,19 +1,105 @@
 #!/usr/bin/env node
 
-import {constants, output, path, template} from '@shopify/cli-kit';
-import {homebrewVariables, copyHomebrew} from '../packaging/lib/homebrew.js';
+// ES Modules
+import {findUp} from "find-up"
+import {createHash} from 'node:crypto'
+import {createRequire} from 'module'
+import {fileURLToPath} from "node:url"
+import path from "pathe"
+import fetch from 'node-fetch'
+import glob from 'fast-glob';
+import {Liquid} from 'liquidjs'
 
-output.initiateLogging();
+// CJS Modules
+const require = createRequire(import.meta.url)
+const {readFile, mkdir, lstat, copy, outputFile, pathExists, rm} = require('fs-extra')
+const {program} = require('commander')
+const colors = require('ansi-colors')
 
-const cliVersion = await constants.versions.cliKit();
-const packagingDir = path.join(path.dirname(import.meta.url), '../packaging').replace(/^file:/, '')
+const packagingDirectory = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "../packaging")
+const defaultOutputDirectory = path.join(packagingDirectory, "dist")
 
-await template.recursiveDirectoryCopy(
-  path.join(packagingDir, 'src'),
-  path.join(packagingDir, 'dist'),
-  {
-    ...(await homebrewVariables(cliVersion)),
-  },
-);
+program
+  .description('Packages the CLI for distribution through package managers')
+  .requiredOption('-o, --output <string>', 'the directory where the distribution artifacts will be exported into', defaultOutputDirectory)
+  .action(async (options) => {
+    // Constants
+    const version = await versionToRelease()
+    console.log(`${colors.green(colors.bold("Version to package:"))} ${version}`)
+    const outputDirectory = path.resolve(options.output)
+    console.log(`${colors.green(colors.bold("Output directory:"))} ${outputDirectory}`)
+    const homebrewVariables = await getHomebrewVariables(version)
+    console.log(`We'll populate the Homebrew formula with the following content:`)
+    console.log(homebrewVariables)
 
-await copyHomebrew(packagingDir)
+    await recursiveDirectoryCopy(path.join(packagingDirectory), outputDirectory, homebrewVariables)
+  })
+program.parse()
+
+async function versionToRelease() {
+  const cliKitPackageJsonPath = await findUp("packages/cli-kit/package.json", {type: "file"})
+  return JSON.parse(await readFile(cliKitPackageJsonPath)).version
+}
+
+async function getHomebrewVariables(cliVersion) {
+  const [[cliTarball, cliSha], [themeTarball, themeSha]] = await Promise.all([
+    getTarballAndShaForPackage('@shopify/cli', cliVersion),
+    getTarballAndShaForPackage('@shopify/theme', cliVersion),
+  ])
+  return {cliTarball, cliSha, themeTarball, themeSha}
+}
+
+async function getTarballAndShaForPackage(pkg, cliVersion) {
+  const tarball = await getTarballForPackage(pkg, cliVersion)
+  const sha = await getSha256ForTarball(tarball)
+  return [tarball, sha]
+}
+
+async function getTarballForPackage(pkg, cliVersion) {
+  const response = await fetch(`https://registry.npmjs.com/${pkg}`)
+  return (await response.json()).versions[cliVersion].dist.tarball
+}
+
+async function getSha256ForTarball(url) {
+  const hash = createHash('sha256').setEncoding('hex')
+  const response = await fetch(url)
+  const stream = response.body.pipe(hash)
+  await new Promise((resolve) => stream.on('finish', resolve))
+  return hash.read()
+}
+
+async function recursiveDirectoryCopy(from, to, data) {
+  const engine = new Liquid()
+  const templateFiles = await glob(path.join(from, '**/*'), {dot: true})
+
+  if (await pathExists(to)) {
+    await rm(to, {recursive: true})
+  }
+  await mkdir(to)
+
+  return;
+  const sortedTemplateFiles = templateFiles
+    .map((path) => path.split('/'))
+    .sort((lhs, rhs) => (lhs.length < rhs.length ? 1 : -1))
+    .map((components) => components.join('/'))
+  await Promise.all(
+    sortedTemplateFiles.map(async (templateItemPath) => {
+      const outputPath = await engine.render(engine.parse(path.join(to, path.relative(from, templateItemPath))), data)
+      const isDirectory = (await lstat(templateItemPath)).isDirectory()
+      if (isDirectory) {
+        if (!await pathExists(outputPath)) {
+          await mkdir(outputPath)
+        }
+      } else if (templateItemPath.endsWith('.liquid')) {
+        await mkdir(path.dirname(outputPath))
+        const content = await readFile(templateItemPath)
+        const contentOutput = await engine.render(engine.parse(content), data)
+        const outputPathWithoutLiquid = outputPath.replace('.liquid', '')
+        await copy(templateItemPath, outputPathWithoutLiquid)
+        await outputFile(outputPathWithoutLiquid, contentOutput)
+      } else {
+        await copy(templateItemPath, outputPath)
+      }
+    }),
+  )
+}
