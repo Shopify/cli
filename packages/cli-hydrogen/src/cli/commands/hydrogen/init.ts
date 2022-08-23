0eca1c1cb387dce134dd10de3d35c9b08a823b29
Nausher Rao <nausher.rao@shopify.com>
2022-08-23 16:23:49 -0400

    Add char for CLI flags

--- packages/cli-hydrogen/src/cli/commands/hydrogen/init.ts
+++ packages/cli-hydrogen/src/cli/commands/hydrogen/init.ts
@@ -13,16 +13,19 @@ export default class Build extends Command {
       description: 'The Shopify storefront domain.',
       env: 'SHOPIFY_FLAG_DOMAIN',
       default: 'hydrogen-preview.myshopify.com',
+      char: 'd',
     }),
     token: Flags.string({
       description: 'The Shopify storefront token.',
       env: 'SHOPIFY_FLAG_TOKEN',
       default: '3b580e70970c4528da70c98e097c2fa0',
+      char: 't',
     }),
     apiVersion: Flags.string({
       description: 'The Shopify storefront API version.',
       env: 'SHOPIFY_FLAG_API_VERSION',
       default: '2022-07',
+      char: 'a',
     }),
   }
 
