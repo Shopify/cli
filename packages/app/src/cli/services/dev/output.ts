eb9547917cbd986ddf38857f8a5efccf42e6601f
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2022-08-25 14:03:56 +0200

    Fix app URL output

--- packages/app/src/cli/services/dev/output.ts
+++ packages/app/src/cli/services/dev/output.ts
@@ -34,9 +34,7 @@ export function outputUpdatedURLFirstTime(url: string, dashboardURL: string) {
 export function outputAppURL(storeFqdn: string, url: string) {
   const heading = output.token.heading('Shareable app URL')
   const appURL = buildAppURL(storeFqdn, url)
-  const formattedAppURL = output.token.link(appURL, appURL)
-  const notes = '' // one note per line, start notes with \n
-  output.info(output.content`\n\n${heading}\n\n  ${formattedAppURL}\n${notes}`)
+  output.info(output.content`\n\n${heading}\n\n  ${appURL}\n`)
 }
 
 export function outputExtensionsMessages(app: AppInterface, storeFqdn: string, url: string) {
