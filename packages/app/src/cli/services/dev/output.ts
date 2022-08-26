a55258fe4468b5a3a6353ee3247043620dcd4e5e
Alvaro Gutierrez <alvaro.gutierrezblanco@shopify.com>
2022-08-26 08:48:54 +0200

    Fixed lint problem

--- packages/app/src/cli/services/dev/output.ts
+++ packages/app/src/cli/services/dev/output.ts
@@ -35,7 +35,8 @@ export function outputAppURL(storeFqdn: string, url: string) {
   const heading = output.token.heading('Shareable app URL')
   const appURL = buildAppURL(storeFqdn, url)
   const formattedAppURL = output.token.link(appURL, appURL)
-  const notes = '' // one note per line, start notes with \n
+  // one note per line, start notes with \n
+  const notes = ''
   output.info(output.content`\n\n${heading}\n\n  ${formattedAppURL}\n${notes}`)
 }
 
