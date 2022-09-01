3a270efdf94226218c9ac7119b67505852d64cbd
Matteo Depalo <matteo.depalo@shopify.com>
2022-09-01 16:56:05 +0100

    Fix app test data

--- packages/app/src/cli/models/app/app.test-data.ts
+++ packages/app/src/cli/models/app/app.test-data.ts
@@ -14,6 +14,7 @@ export function testApp(app: Partial<AppInterface> = {}): AppInterface {
     app.extensions?.ui ?? [],
     app.extensions?.theme ?? [],
     app.extensions?.function ?? [],
+    app.usesWorkspaces ?? false,
     app.dotenv,
     app.errors,
   )
