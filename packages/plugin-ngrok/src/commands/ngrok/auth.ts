ab7ba94ee5d1ed6b757f97c1c63e115f7405c92b
Isaac Roldán <isaac.roldan@shopify.com>
2022-09-01 17:45:02 +0200

    Add ngrok plugin code to CLI repo

---
+++ packages/plugin-ngrok/src/commands/ngrok/auth.ts
@@ -0,0 +1,16 @@
+import {output} from '@shopify/cli-kit'
+import {Command} from '@oclif/core'
+import {authenticate} from '../../tunnel.js'
+
+export default class NgrokAuth extends Command {
+  static description =
+    'Saves a token to authenticate against ngrok. Visit https://dashboard.ngrok.com/signup to create an account.'
+
+  static args = [{name: 'token'}]
+
+  async run() {
+    const {args} = await this.parse(NgrokAuth)
+    await authenticate(args.token)
+    output.success('Auth token saved')
+  }
+}
