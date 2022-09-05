0bcb070278a569db759978a66e450e1682630ef9
Alvaro Gutierrez <alvaro.gutierrezblanco@shopify.com>
2022-09-05 13:30:09 +0200

    Removed unused import

--- packages/app/src/cli/services/environment/identifiers.ts
+++ packages/app/src/cli/services/environment/identifiers.ts
@@ -7,7 +7,6 @@ import {fetchAppExtensionRegistrations} from '../dev/fetch.js'
 import {createExtension} from '../dev/create-extension.js'
 import {error, output, session, ui} from '@shopify/cli-kit'
 import {PackageManager} from '@shopify/cli-kit/node/node-package-manager'
-import {} from '@shopify/cli-kit/common/result'
 
 const DeployError = (appName: string, packageManager: PackageManager) => {
   return new error.Abort(
