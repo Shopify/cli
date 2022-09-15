fede332e092715be34529b145fcc10b3de127fc6
Guilherme Carreiro <karreiro@gmail.com>
2022-09-15 10:43:30 +0200

    Set `RubyCLIVersion` to `2.25.0`

--- packages/cli-kit/src/node/ruby.ts
+++ packages/cli-kit/src/node/ruby.ts
@@ -9,7 +9,7 @@ import {AdminSession} from '../session.js'
 import {content, token} from '../output.js'
 import {Writable} from 'node:stream'
 
-const RubyCLIVersion = '2.23.0'
+const RubyCLIVersion = '2.25.0'
 const ThemeCheckVersion = '1.10.3'
 const MinBundlerVersion = '2.3.8'
 const MinRubyVersion = '2.7.5'
