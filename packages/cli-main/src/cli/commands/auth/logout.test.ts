d39995d9a0ccbdfb6395d755bab597dbc94320ce
Ju Liu <ju.liu@shopify.com>
2022-09-16 11:58:46 +0100

    Add simple test for auth logout

---
+++ packages/cli-main/src/cli/commands/auth/logout.test.ts
@@ -0,0 +1,36 @@
+import Logout from './logout'
+import {beforeEach, describe, expect, it, vi} from 'vitest'
+import {session, store} from '@shopify/cli-kit'
+
+beforeEach(() => {
+  vi.mock('@shopify/cli-kit', async () => {
+    const module: any = await vi.importActual('@shopify/cli-kit')
+    return {
+      ...module,
+      session: {
+        ...module.session,
+        logout: vi.fn(),
+      },
+      store: {
+        ...module.store,
+        removeSession: vi.fn(),
+        clearAllAppInfo: vi.fn(),
+      },
+    }
+  })
+})
+
+describe('logs out', () => {
+  it('removes session', async () => {
+    await Logout.run()
+
+    expect(session.logout).toHaveBeenCalledOnce()
+    expect(store.removeSession).toHaveBeenCalledOnce()
+  })
+
+  it('removes all app information', async () => {
+    await Logout.run()
+
+    expect(store.clearAllAppInfo).toHaveBeenCalledOnce()
+  })
+})
