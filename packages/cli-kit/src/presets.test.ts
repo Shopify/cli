4d07b1d304ba20c4d17198b29718026456600e0a
Ariel Caplan <ariel.caplan@shopify.com>
2022-09-28 16:50:03 +0300

    Use given/when/then for clarity and add missing test

--- packages/cli-kit/src/presets.test.ts
+++ packages/cli-kit/src/presets.test.ts
@@ -4,49 +4,83 @@ import {join as pathJoin} from './path.js'
 import {encode as tomlEncode} from './toml.js'
 import {describe, expect, test} from 'vitest'
 
+const preset1 = {
+  flag1: 'value',
+  flag2: true,
+  flag3: 0,
+  flag4: ['hello', 'world'],
+}
+const preset2 = {
+  flag1: 'value2',
+  flag2: false,
+  flag3: 1,
+  flag4: ['goodbye', 'world'],
+}
+
 describe('loading presets', async () => {
-  test('returns empty array when no presets file exists', async () => {
+  test('returns an empty object when no presets file exists', async () => {
     await inTemporaryDirectory(async (tmpDir) => {
-      expect(await presets.loadPresetsFromDirectory(tmpDir)).toEqual({})
+      // When
+      const loaded = await presets.loadPresetsFromDirectory(tmpDir)
+
+      // Then
+      expect(loaded).toEqual({})
+    })
+  })
+
+  test('returns an empty object when an empty presets file exists', async () => {
+    await inTemporaryDirectory(async (tmpDir) => {
+      // Given
+      await fileWrite(pathJoin(tmpDir, 'shopify.presets.toml'), '# no content')
+
+      // When
+      const loaded = await presets.loadPresetsFromDirectory(tmpDir)
+
+      // Then
+      expect(loaded).toEqual({})
     })
   })
 
   test('returns available presets when they exist', async () => {
     await inTemporaryDirectory(async (tmpDir) => {
-      const preset1 = {
-        flag1: 'value',
-        flag2: true,
-        flag3: 0,
-        flag4: ['hello', 'world'],
-      }
-      const preset2 = {
-        flag1: 'value2',
-        flag2: false,
-        flag3: 1,
-        flag4: ['goodbye', 'world'],
-      }
-      await fileWrite(`${tmpDir}/shopify.presets.toml`, tomlEncode({preset1, preset2}))
-      expect(await presets.loadPresetsFromDirectory(tmpDir)).toEqual({preset1, preset2})
+      // Given
+      await fileWrite(pathJoin(tmpDir, 'shopify.presets.toml'), tomlEncode({preset1, preset2}))
+
+      // When
+      const loaded = await presets.loadPresetsFromDirectory(tmpDir)
+
+      // Then
+      expect(loaded).toEqual({preset1, preset2})
     })
   })
 
   test('does not search upwards for presets when no file exists and searching up is disabled', async () => {
     await inTemporaryDirectory(async (tmpDir) => {
-      const preset1 = {flag1: 'value'}
+      // Given
       await fileWrite(`${tmpDir}/shopify.presets.toml`, tomlEncode({preset1}))
       const subdir = pathJoin(tmpDir, 'subdir')
       await mkdir(subdir)
-      expect(await presets.loadPresetsFromDirectory(subdir)).toEqual({})
+
+      // When
+      const loaded = await presets.loadPresetsFromDirectory(subdir)
+
+      // Then
+      expect(loaded).toEqual({})
     })
   })
 
   test('searches upwards for presets when no file exists and searching up is enabled', async () => {
     await inTemporaryDirectory(async (tmpDir) => {
-      const preset1 = {flag1: 'value'}
+      // Given
       await fileWrite(`${tmpDir}/shopify.presets.toml`, tomlEncode({preset1}))
       const subdir = pathJoin(tmpDir, 'subdir')
       await mkdir(subdir)
-      expect(await presets.loadPresetsFromDirectory(subdir, {findUp: true})).toEqual({preset1})
+
+      // When
+      const loaded = await presets.loadPresetsFromDirectory(subdir, {findUp: true})
+
+      // Then
+      expect(loaded).toEqual({preset1})
     })
   })
 })
