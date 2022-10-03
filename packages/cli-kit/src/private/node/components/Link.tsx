30cc1fcda18c2c0c21fa46115f2997028f4fdec5
Matteo Depalo <matteo.depalo@shopify.com>
2022-10-03 11:40:10 +0100

    Add documentation for kitchen-sink command

--- packages/cli-kit/src/private/node/components/Link.tsx
+++ packages/cli-kit/src/private/node/components/Link.tsx
@@ -6,7 +6,13 @@ interface Props {
   url: string
 }
 
-const Link: React.FC<Props> = ({url}) => {
+/**
+ * `Link` displays a clickable link when supported by the terminal.
+ *
+ * @param {React.PropsWithChildren<Props>} props
+ * @returns {JSX.Element}
+ */
+const Link: React.FC<Props> = ({url}: React.PropsWithChildren<Props>): JSX.Element => {
   return (
     <Transform transform={(children) => terminalLink(children, url, {fallback: false})}>
       <Text>{url}</Text>
--- packages/cli-kit/src/private/node/components/List.tsx
+++ packages/cli-kit/src/private/node/components/List.tsx
@@ -4,18 +4,25 @@ import React from 'react'
 interface Props {
   title: string
   items: string[]
+  ordered: boolean
 }
 
 const DOT = '•'
 
-const List: React.FC<Props> = ({title, items}) => {
+/**
+ * `List` displays an unordered or ordered list with text aligned with the bullet point
+ * and wrapped to the container width.
+ * @param {React.PropsWithChildren<Props>} props
+ * @returns {JSX.Element}
+ */
+const List: React.FC<Props> = ({title, items, ordered = false}: React.PropsWithChildren<Props>): JSX.Element => {
   return (
     <Box flexDirection="column">
       <Text dimColor>{title}</Text>
       {items.map((item, index) => (
         <Box key={index}>
           <Box>
-            <Text dimColor>{`  ${DOT}`}</Text>
+            <Text dimColor>{`  ${ordered ? `${index}.` : DOT}`}</Text>
           </Box>
 
           <Box flexGrow={1} marginLeft={1}>
