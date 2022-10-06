aa3c14b873b32a45f93e88be69c4148d7fe519d4
Matteo Depalo <matteo.depalo@shopify.com>
2022-10-06 14:25:27 +0100

    Add TestStream

--- packages/cli-kit/src/private/node/ui.tsx
+++ packages/cli-kit/src/private/node/ui.tsx
@@ -4,16 +4,54 @@ import {ReactElement} from 'react'
 import {render as inkRender} from 'ink'
 import {EventEmitter} from 'events'
 
+export class TestStream extends EventEmitter {
+  columns: number
+  logLevel: LogLevel
+
+  constructor(options: {columns: number; logLevel: LogLevel}) {
+    super()
+    this.columns = options.columns
+    this.logLevel = options.logLevel
+  }
+
+  write = (frame: string) => {
+    collectLog(this.logLevel, frame)
+  }
+}
+
+export function renderOnce(element: JSX.Element, logLevel: LogLevel = 'info', logger: Logger = consoleLog) {
+  const {output, unmount} = renderString(element)
+
+  if (output) {
+    if (isUnitTest()) collectLog(logLevel, output)
+    outputWhereAppropriate(logLevel, logger, output)
+  }
+
+  unmount()
+}
+
+export function render(element: JSX.Element) {
+  const stdout = isUnitTest() ? new TestStream({columns: 80, logLevel: 'info'}) : process.stdout
+  const stderr = isUnitTest() ? new TestStream({columns: 80, logLevel: 'error'}) : process.stderr
+
+  inkRender(element, {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    stdout: stdout as any,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    stderr: stderr as any,
+  })
+}
+
 interface Instance {
   output: string | undefined
   unmount: () => void
   cleanup: () => void
-  stdout: OutputStream
-  stderr: OutputStream
+  stdout: RenderStringStream
+  stderr: RenderStringStream
   frames: string[]
 }
 
-export class OutputStream extends EventEmitter {
+export class RenderStringStream extends EventEmitter {
   columns: number
   readonly frames: string[] = []
   private _lastFrame?: string
@@ -33,24 +71,9 @@ export class OutputStream extends EventEmitter {
   }
 }
 
-export function renderOnce(element: JSX.Element, logLevel: LogLevel = 'info', logger: Logger = consoleLog) {
-  const {output, unmount} = renderString(element)
-
-  if (output) {
-    if (isUnitTest()) collectLog(logLevel, output)
-    outputWhereAppropriate(logLevel, logger, output)
-  }
-
-  unmount()
-}
-
-export function render(element: JSX.Element) {
-  inkRender(element)
-}
-
 export const renderString = (element: ReactElement): Instance => {
-  const stdout = new OutputStream({columns: process.stdout.columns})
-  const stderr = new OutputStream({columns: process.stderr.columns})
+  const stdout = new RenderStringStream({columns: process.stdout.columns})
+  const stderr = new RenderStringStream({columns: process.stderr.columns})
 
   const instance = inkRender(element, {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
