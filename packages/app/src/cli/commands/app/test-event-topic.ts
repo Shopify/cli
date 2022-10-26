c0f6c5639998a2eb5152a6aedbe8cdac933a8fb0
Paco Castro <paco.castro@shopify.com>
2022-10-26 18:30:25 +0200

    test-event-topic command for app topic

---
+++ packages/app/src/cli/commands/app/test-event-topic.ts
@@ -0,0 +1,114 @@
+import {
+  requestSample,
+  sendLocal,
+  collectCliOptions,
+  DELIVERY_METHOD,
+  TestWebhookFlags,
+} from '../../services/test-event-topic.js'
+import {Command, Flags} from '@oclif/core'
+import {output} from '@shopify/cli-kit'
+
+export default class TopicTesting extends Command {
+  static description = 'Trigger sample event topic payload to be sent to a designated address.'
+
+  static flags = {
+    help: Flags.help({
+      required: false,
+      hidden: false,
+      char: 'h',
+      env: 'SHOPIFY_FLAG_HELP',
+      description: `CLI test-event-topic help:
+        The command will prompt for any values not passed as command-line arguments.
+        For security reasons Shared Secret is not allowed via flags.
+        SHOPIFY_FLAG_SHARED_SECRET env variable can be used to avoid interactive prompt.`,
+    }),
+    topic: Flags.string({
+      required: false,
+      hidden: false,
+      char: 't',
+      env: 'SHOPIFY_FLAG_TOPIC',
+      description: 'Requested event topic.',
+    }),
+    'api-version': Flags.string({
+      required: false,
+      hidden: false,
+      char: 'v',
+      env: 'SHOPIFY_FLAG_API_VERSION',
+      description: 'Event tpic API Version.',
+    }),
+    'delivery-method': Flags.string({
+      required: false,
+      hidden: false,
+      char: 'm',
+      options: [DELIVERY_METHOD.CONSOLE, DELIVERY_METHOD.HTTP, DELIVERY_METHOD.PUBSUB, DELIVERY_METHOD.EVENTBRIDGE],
+      env: 'SHOPIFY_FLAG_DELIVERY_METHOD',
+      description: `Method chosen to deliver the topic payload.`,
+    }),
+    address: Flags.string({
+      required: false,
+      hidden: false,
+      char: 'a',
+      env: 'SHOPIFY_FLAG_ADDRESS',
+      description: `Destination url (only for ${DELIVERY_METHOD.HTTP}, ${DELIVERY_METHOD.PUBSUB} or ${DELIVERY_METHOD.EVENTBRIDGE} delivery methods).`,
+    }),
+    port: Flags.string({
+      hidden: false,
+      env: 'SHOPIFY_FLAG_PORT',
+      description: `Destination port (only for ${DELIVERY_METHOD.HTTP} delivery method when address is localhost).`,
+    }),
+    'url-path': Flags.string({
+      hidden: false,
+      env: 'SHOPIFY_FLAG_URL_PATH',
+      description: `Endpoint path (only for ${DELIVERY_METHOD.HTTP} delivery method when address is localhost).`,
+    }),
+  }
+
+  public async run() {
+    const {flags} = await this.parse(TopicTesting)
+
+    const usedFlags: TestWebhookFlags = {
+      topic: flags.topic,
+      apiVersion: flags['api-version'],
+      deliveryMethod: flags['delivery-method'],
+      address: flags.address,
+      port: flags.port,
+      urlPath: flags['url-path'],
+    }
+
+    const options = await collectCliOptions(usedFlags)
+
+    const sample = await requestSample(
+      options.topic,
+      options.apiVersion,
+      options.deliveryMethod,
+      options.address,
+      options.sharedSecret,
+    )
+
+    if (!sample.success) {
+      await output.consoleError(JSON.stringify(sample.userErrors))
+      return
+    }
+
+    if (options.deliveryMethod === DELIVERY_METHOD.CONSOLE) {
+      output.info(sample.samplePayload)
+      return
+    }
+
+    if (options.deliveryMethod === DELIVERY_METHOD.LOCALHOST) {
+      const result = await sendLocal(options.address, sample.samplePayload, sample.headers)
+
+      if (result) {
+        output.success('Localhost delivery sucessful')
+        return
+      }
+
+      await output.consoleError('Localhost delivery failed')
+      return
+    }
+
+    if (sample.samplePayload === JSON.stringify({})) {
+      output.success('Webhook will be delivered shortly')
+    }
+  }
+}
---
+++ packages/app/src/cli/services/test-event-topic.ts
@@ -0,0 +1,159 @@
+import {
+  topicPrompt,
+  addressPrompt,
+  apiVersionPrompt,
+  localPortPrompt,
+  localUrlPathPrompt,
+  sharedSecretPrompt,
+  deliveryMethodPrompt,
+} from '../prompts/test-event-topic.js'
+import {api, http, session} from '@shopify/cli-kit'
+
+export interface SamplePayloadSchema {
+  sendSampleWebhook: {
+    samplePayload: string
+    headers: string
+    success: boolean
+    userErrors: UserErrors[]
+  }
+}
+
+export interface UserErrors {
+  message: string
+  fields: string[]
+}
+
+export const DELIVERY_METHOD = {
+  CONSOLE: 'console',
+  LOCALHOST: 'localhost',
+  HTTP: 'http',
+  PUBSUB: 'google-pub-sub',
+  EVENTBRIDGE: 'event-bridge',
+}
+
+const sendSampleWebhookMutation = `
+  mutation samplePayload($topic: String!, $api_version: String!, $address: String!, $delivery_method: String!, $shared_secret: String!) {
+    sendSampleWebhook(input: {topic: $topic, apiVersion: $api_version, address: $address, deliveryMethod: $delivery_method, sharedSecret: $shared_secret}) {
+        samplePayload
+        success
+        headers
+        userErrors {
+          message
+        }
+    }
+  }
+`
+
+export async function requestSample(
+  topic: string,
+  apiVersion: string,
+  deliveryMethod: string,
+  address: string,
+  sharedSecret: string,
+) {
+  const token = await session.ensureAuthenticatedPartners()
+
+  const variables = {
+    topic,
+    api_version: apiVersion,
+    address,
+    delivery_method: deliveryMethod,
+    shared_secret: sharedSecret,
+  }
+
+  const {sendSampleWebhook: result}: SamplePayloadSchema = await api.partners.request(
+    sendSampleWebhookMutation,
+    token,
+    variables,
+  )
+
+  return result
+}
+
+export async function sendLocal(address: string, body: string, headers: string) {
+  const options = {
+    method: 'POST',
+    body,
+    headers: {
+      'Content-Type': 'application/json',
+      ...JSON.parse(headers),
+    },
+  }
+  const response = await http.fetch(address, options)
+  return response.status === 200
+}
+
+export interface TestWebhookOptions {
+  topic: string
+  apiVersion: string
+  deliveryMethod: string
+  sharedSecret: string
+  localhostPort: string
+  localhostUrlPath: string
+  address: string
+}
+
+export interface TestWebhookFlags {
+  topic?: string
+  apiVersion?: string
+  deliveryMethod?: string
+  port?: string
+  address?: string
+  urlPath?: string
+}
+
+export async function collectCliOptions(flags: TestWebhookFlags): Promise<TestWebhookOptions> {
+  const options: TestWebhookOptions = {
+    topic: await collectParam(flags.topic as string, topicPrompt),
+    apiVersion: await collectParam(flags.apiVersion as string, apiVersionPrompt),
+    sharedSecret: await collectParam(process.env.SHOPIFY_FLAG_SHARED_SECRET as string, sharedSecretPrompt),
+    deliveryMethod: await collectParam(flags.deliveryMethod as string, deliveryMethodPrompt),
+    address: '',
+    localhostPort: '',
+    localhostUrlPath: '',
+  }
+
+  if (options.deliveryMethod === DELIVERY_METHOD.CONSOLE) {
+    return options
+  }
+
+  if (options.deliveryMethod === DELIVERY_METHOD.HTTP) {
+    options.address = await collectParam(flags.address as string, addressPrompt, 'http://localhost')
+
+    const url = new URL(options.address)
+    if (isLocal(url)) {
+      options.deliveryMethod = DELIVERY_METHOD.LOCALHOST
+      options.localhostPort = await collectParam(flagOverAddressField(flags.port as string, url.port), localPortPrompt)
+      url.port = options.localhostPort
+
+      const urlPath = url.pathname === '/' ? '' : url.pathname
+      options.localhostUrlPath = await collectParam(
+        flagOverAddressField(flags.urlPath as string, urlPath),
+        localUrlPathPrompt,
+      )
+      url.pathname = options.localhostUrlPath
+
+      options.address = url.toString()
+    }
+  } else {
+    options.address = await collectParam(flags.address as string, addressPrompt)
+  }
+
+  return options
+}
+
+async function collectParam(
+  value: string,
+  prompt: (defaultValue?: string) => Promise<string>,
+  defaultValue?: string,
+): Promise<string> {
+  return value ? value : prompt(defaultValue)
+}
+
+function flagOverAddressField(flag: string, addressField: string): string {
+  return flag ? flag : addressField
+}
+
+function isLocal(url: URL): boolean {
+  return url.hostname === 'localhost'
+}
