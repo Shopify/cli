a607a47fab43bd7a33f044d801a44f59d477d49d
Richard Powell <richard.powell@shopify.com>
2022-10-28 11:54:56 -0400

    Remove yaml utils from cli-kit
    The yaml utils were only in use becaue of the go CLI. Since we removed all the code to call to the GO CLI the yaml utils are no longer needed.

--- packages/cli-kit/src/yaml.ts
+++
@@ -1,9 +0,0 @@
-import * as yaml from 'js-yaml'
-
-export function decode(input: string): unknown {
-  return yaml.load(input)
-}
-
-export function encode(content: unknown): string {
-  return yaml.dump(content)
-}
