9fa98edef90df495b3ce84c47f8cc2a9e0d6f29c
Pedro Piñera <pedro@craftweg.com>
2022-11-04 13:51:52 +0100

    Integrate with Netlify

--- fixtures/integration-app/remix.config.js
+++ fixtures/integration-app/remix.config.js
@@ -2,4 +2,6 @@
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
   appDirectory: 'home',
+  serverBuildTarget: 'netlify-edge',
+  server: process.env.NETLIFY || process.env.NETLIFY_LOCAL ? './server.js' : undefined,
 }
