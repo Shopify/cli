4a24890a2ac8a37890810d81ea4f76232ce7b18a
Alvaro Gutierrez <alvaro.gutierrezblanco@shopify.com>
2022-11-07 11:54:13 +0100

    Added array merge strategy parameter to deep merge using the union one as default

--- packages/cli-kit/src/public/common/object.ts
+++ packages/cli-kit/src/public/common/object.ts
@@ -1,14 +1,20 @@
 import deepMerge from 'deepmerge'
 
 /**
- * Deep merges the two objects and returns a new object with the merge result. Union strategy used for arrays so no
- * duplicates are generated.
+ * Deep merges the two objects and returns a new object with the merge result.
+ *
  * @param lhs - One of the objects to be merged.
  * @param rhs - Another object to be merged.
+ * @param arrayMergeStrategy - Strategy used to merge the array typed fields. Union strategy is used by default to avoid
+ * duplicated elements.
  * @returns A Javascrip tobject with th emerged objects.
  */
-export function deepMergeObjects<T1, T2>(lhs: Partial<T1>, rhs: Partial<T2>): T1 & T2 {
-  return deepMerge(lhs, rhs, {arrayMerge: unionArrayStrategy})
+export function deepMergeObjects<T1, T2>(
+  lhs: Partial<T1>,
+  rhs: Partial<T2>,
+  arrayMergeStrategy: (destinationArray: unknown[], sourceArray: unknown[]) => unknown[] = unionArrayStrategy,
+): T1 & T2 {
+  return deepMerge(lhs, rhs, {arrayMerge: arrayMergeStrategy})
 }
 
 function unionArrayStrategy(destinationArray: unknown[], sourceArray: unknown[]): unknown[] {
