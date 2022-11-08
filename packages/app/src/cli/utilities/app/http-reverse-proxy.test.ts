a7660add4aa20f7f4e4feb28c5bc5c4481583788
Pedro Piñera <pedro@craftweg.com>
2022-11-08 18:19:00 +0100

    Rename port to tcp and not export it through the cli-kit's index.ts

--- packages/app/src/cli/utilities/app/http-reverse-proxy.test.ts
+++ packages/app/src/cli/utilities/app/http-reverse-proxy.test.ts
@@ -1,12 +1,13 @@
 import {runConcurrentHTTPProcessesAndPathForwardTraffic} from './http-reverse-proxy.js'
 import httpProxy from 'http-proxy'
 import {beforeAll, describe, expect, test, vi} from 'vitest'
-import {port} from '@shopify/cli-kit'
+import {getAvailableTCPPort} from '@shopify/cli-kit/node/tcp'
 import {renderConcurrent} from '@shopify/cli-kit/node/ui'
 
 beforeAll(() => {
   vi.mock('@shopify/cli-kit')
   vi.mock('@shopify/cli-kit/node/ui')
+  vi.mock('@shopify/cli-kit/node/tcp')
   vi.mock('http-proxy', () => {
     return {
       default: {
@@ -31,8 +32,8 @@ describe('runConcurrentHTTPProcessesAndPathForwardTraffic', () => {
   test('proxies to all the targets using the HTTP Proxy', async () => {
     // Given
     const server: any = {register: vi.fn(), listen: vi.fn(), close: vi.fn()}
-    vi.mocked(port.getRandomPort).mockResolvedValueOnce(3001)
-    vi.mocked(port.getRandomPort).mockResolvedValueOnce(3002)
+    vi.mocked(getAvailableTCPPort).mockResolvedValueOnce(3001)
+    vi.mocked(getAvailableTCPPort).mockResolvedValueOnce(3002)
 
     // When
     const got = await runConcurrentHTTPProcessesAndPathForwardTraffic(
@@ -65,7 +66,7 @@ describe('runConcurrentHTTPProcessesAndPathForwardTraffic', () => {
   test('uses a random port when no port is passed', async () => {
     // Given
     const server: any = {register: vi.fn(), listen: vi.fn(), close: vi.fn()}
-    vi.mocked(port.getRandomPort).mockResolvedValueOnce(4000)
+    vi.mocked(getAvailableTCPPort).mockResolvedValueOnce(4000)
 
     // When
     const got = await runConcurrentHTTPProcessesAndPathForwardTraffic(undefined, [], [])
