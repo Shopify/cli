258730adb29ec046576c4b16192cde0957454cbf
Isaac Roldán <isaac.roldan@shopify.com>
2022-11-10 16:38:44 +0100

    Fix lint

--- packages/app/src/cli/utilities/extensions/locales-configuration.ts
+++ packages/app/src/cli/utilities/extensions/locales-configuration.ts
@@ -4,20 +4,6 @@ import fs from 'fs'
 const L10N_FILE_SIZE_LIMIT = 16 * 1024
 const L10N_BUNDLE_SIZE_LIMIT = 256 * 1024
 
-const BigBundleError = () => {
-  return new error.Abort(
-    `Error loading ${'CHECKOUT_UI_EXTENSION_KEY'}`,
-    `Total size of all locale files must be less than ${L10N_BUNDLE_SIZE_LIMIT}`,
-  )
-}
-
-const BigFileError = (filename: string) => {
-  return new error.Abort(
-    `Error loading ${'CHECKOUT_UI_EXTENSION_KEY'}`,
-    `Locale file ${filename} size must be less than ${L10N_FILE_SIZE_LIMIT}`,
-  )
-}
-
 export async function loadLocalesConfig(extensionPath: string, extensionIdentifier: string) {
   const localesPaths = await path.glob(path.join(extensionPath, 'locales/*.json'))
   if (localesPaths.length === 0) return {}
@@ -38,14 +24,22 @@ export async function loadLocalesConfig(extensionPath: string, extensionIdentifi
       `There must be one (and only one) locale identified as the default locale: e.g. "en.default.json"`,
     )
 
-  if (totalBundleSize > L10N_BUNDLE_SIZE_LIMIT) throw BigBundleError()
+  if (totalBundleSize > L10N_BUNDLE_SIZE_LIMIT)
+    throw new error.Abort(
+      `Error loading ${extensionIdentifier}`,
+      `Total size of all locale files must be less than ${L10N_BUNDLE_SIZE_LIMIT}`,
+    )
 
   // Locale validations
   for (const locale of localesPaths) {
     const size = fs.statSync(locale).size
-    if (size > L10N_FILE_SIZE_LIMIT) throw BigFileError(locale)
+    if (size > L10N_FILE_SIZE_LIMIT)
+      throw new error.Abort(
+        `Error loading ${extensionIdentifier}`,
+        `Locale file ${locale} size must be less than ${L10N_FILE_SIZE_LIMIT}`,
+      )
     if (size === 0)
-      throw new error.Abort(`Error loading ${'CHECKOUT_UI_EXTENSION_KEY'}`, `Locale file ${locale} can't be empty`)
+      throw new error.Abort(`Error loading ${extensionIdentifier}`, `Locale file ${locale} can't be empty`)
   }
 
   return {
