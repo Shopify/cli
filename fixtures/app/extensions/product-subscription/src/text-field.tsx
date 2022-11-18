5ae96b509b553ff6d18b136c0c486f8e848a35ea
Trish Ta <trish.ta@shopify.com>
2022-11-17 20:00:18 -0500

    Use onInput to persist changes in real time

--- fixtures/app/extensions/product-subscription/src/text-field.tsx
+++ fixtures/app/extensions/product-subscription/src/text-field.tsx
@@ -1,78 +1,52 @@
-import React, {useState, useMemo, useCallback, useEffect} from 'react';
-import {BlockStack, TextField, Text, useExtensionApi} from '@shopify/admin-ui-extensions-react';
-import {
-  makeStatefulSubscribable,
-  RemoteSubscribable,
-} from '@remote-ui/async-subscription';
+import React, {useState, useMemo, useCallback, useEffect} from 'react'
+import {BlockStack, TextField, Text, useExtensionApi} from '@shopify/admin-ui-extensions-react'
+import {makeStatefulSubscribable, RemoteSubscribable} from '@remote-ui/async-subscription'
 
 export function TextFieldExample() {
-  const {extensionState: subscribaleExtensionState, applyStateUpdates} = useExtensionApi();
-  const extensionState = useMemo(() => makeStatefulSubscribable<RemoteSubscribable<any>>(subscribaleExtensionState), []);
+  const {subscribableState, applyStateUpdates} = useExtensionApi()
+  const extensionState = useMemo(() => makeStatefulSubscribable<RemoteSubscribable<any>>(subscribableState), [])
 
-  const [firstName, setFirstName] = useState(extensionState.current.firstName || '');
-  const clearFirstName = useCallback(() => setFirstName(''), []);
-  const [lastName, setLastName] = useState('');
-  const clearLastName = useCallback(() => setLastName(''), []);
-  const [search, setSearch] = useState('');
-  const [review, setReview] = useState('');
-  const [numberValue, setNumberValue] = useState('0');
+  const initialValue = extensionState.current
+  const [firstName, setFirstName] = useState(initialValue.firstName || '')
+  const clearFirstName = useCallback(() => applyStateUpdates({firstName: ''}), [])
+  const [lastName, setLastName] = useState(initialValue.lastName || '')
+  const clearLastName = useCallback(() => applyStateUpdates({firstName: ''}), [])
 
   useEffect(() => {
     return extensionState.subscribe(() => {
-    console.log('updated', extensionState.current);
-    if(extensionState.current.hasOwnProperty('firstName')) {
-      setFirstName(extensionState.current.firstName)
-    }
-  });
-  }, [setFirstName, extensionState.subscribe]);
+      const updatedState = extensionState.current;
+
+      console.log('updated: ', updatedState);
+
+      setFirstName(updatedState.firstName || '');
+      setLastName(updatedState.lastName || '');
+    })
+  }, [setFirstName, setLastName, extensionState.subscribe]);
 
   return (
-      <BlockStack>
-        <TextField
-          label="First name"
-          placeholder="Type your first name (onChange)"
-          value={firstName}
-          onChange={(value) => {
-            setFirstName(value);
-            applyStateUpdates({firstName: value});
-          }}
-          clearButton
-          onClearButtonPress={clearFirstName}
-        />
-        {firstName && <Text>First name: {firstName}</Text>}
-        <TextField
-          label="Last name"
-          placeholder="Type your last name (onInput)"
-          value={lastName}
-          onInput={setLastName}
-          clearButton
-          onClearButtonPress={clearLastName}
-        />
-        {lastName && <Text>Last name: {lastName}</Text>}
-        <TextField
-          label="Reviews"
-          type="search"
-          placeholder="Search for reviews"
-          value={search}
-          onChange={setSearch}
-        />
-        <TextField
-          label="Reply"
-          placeholder="Add a reply to this review..."
-          multiline
-          value={review}
-          onChange={setReview}
-        />
-        <TextField label="Number" type="number" value={numberValue} onChange={setNumberValue} />
-        <TextField label="Error" value="Inline error" error="This field is invalid" />
-        <TextField value="42" type="number" suffix="%" label="A real percent" />
-        <TextField value="stuff around" prefix="Only the best" label="Cool things" />
-        <TextField
-          type="search"
-          value="cool tech"
-          prefix="I'm interested in"
-          label="Search prefix"
-        />
-      </BlockStack>
-  );
+    <BlockStack>
+      <TextField
+        label="First name"
+        placeholder="Type your first name (onChange)"
+        value={firstName}
+        onInput={(value) => {
+          applyStateUpdates({firstName: value})
+        }}
+        clearButton
+        onClearButtonPress={clearFirstName}
+      />
+      {firstName && <Text>First name: {firstName}</Text>}
+      <TextField
+        label="Last name"
+        placeholder="Type your last name (onInput)"
+        value={lastName}
+        onInput={(value) => {
+          applyStateUpdates({lastName: value})
+        }}
+        clearButton
+        onClearButtonPress={clearLastName}
+      />
+      {lastName && <Text>Last name: {lastName}</Text>}
+    </BlockStack>
+  )
 }
