a35b76ac9135dd8880fb9465cf6d75a826aef40c
Isaac Roldán <isaac.roldan@shopify.com>
2022-11-23 11:42:31 +0100

    Add temporary workaround for some extensions

--- packages/app/src/cli/utilities/extensions/fetch-extension-specifications.ts
+++ packages/app/src/cli/utilities/extensions/fetch-extension-specifications.ts
@@ -10,9 +10,15 @@ export async function fetchExtensionSpecifications(token: string, apiKey: string
   const result: api.graphql.ExtensionSpecificationsQuerySchema = await api.partners.request(query, token, {
     api_key: apiKey,
   })
-  const extensionSpecifications = result.extensionSpecifications.filter(
-    (specification) => specification.options.managementExperience === 'cli',
-  )
+  const extensionSpecifications = result.extensionSpecifications
+    .filter((specification) => specification.options.managementExperience === 'cli')
+    .map((spec) => {
+      // WORKAROUND: The identifiers in the API are different for these extensions to the ones the CLI
+      // has been using so far. This is a workaround to keep the CLI working until the API is updated.
+      if (spec.identifier === 'theme_app_extension') spec.identifier = 'theme'
+      if (spec.identifier === 'subscription_management') spec.identifier = 'product_subscription'
+      return spec
+    })
 
   return extensionSpecifications
 }
