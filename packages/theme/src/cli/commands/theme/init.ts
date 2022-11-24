bebc01a667606b0ed5172b9c6fb5d9e68283cf0b
Shaun Stanworth <shaun.stanworth@shopify.com>
2022-11-24 17:17:34 +0000

    Provide a default name for theme init prompt

--- packages/theme/src/cli/commands/theme/init.ts
+++ packages/theme/src/cli/commands/theme/init.ts
@@ -3,6 +3,7 @@ import ThemeCommand from '../../utilities/theme-command.js'
 import {Flags} from '@oclif/core'
 import {cli, path, ui} from '@shopify/cli-kit'
 import {execCLI2} from '@shopify/cli-kit/node/ruby'
+import {generateRandomNameForSubdirectory} from '@shopify/cli-kit/node/fs'
 
 export default class Init extends ThemeCommand {
   static description = 'Clones a Git repository to use as a starting point for building a new theme.'
@@ -29,15 +30,22 @@ export default class Init extends ThemeCommand {
   async run(): Promise<void> {
     const {args, flags} = await this.parse(Init)
     const directory = flags.path ? path.resolve(flags.path) : process.cwd()
-    const name = args.name || (await this.promptName())
+    const name = args.name || (await this.promptName(directory))
     const command = ['theme', 'init', name]
     await execCLI2(command, {
       directory,
     })
   }
 
-  async promptName() {
-    const question: ui.Question = {type: 'input', name: 'name', message: 'Name of the new theme'}
+  async promptName(directory: string) {
+    const defaultName = await generateRandomNameForSubdirectory({suffix: 'theme', directory, family: 'creative'})
+
+    const question: ui.Question = {
+      type: 'input',
+      name: 'name',
+      message: 'Name of the new theme',
+      default: defaultName,
+    }
     const {name} = await ui.prompt([question])
     return name
   }
