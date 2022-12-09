bc79f6b3fa2855bcb78a59cff1d90609cc17293c
Igor Kozlov <igor.kozlov@shopify.com>
2022-12-09 12:39:27 -0500

    Update Checkout UI Extensions

--- packages/app/src/cli/models/extensions/ui-specifications/checkout_ui_extension.ts
+++ packages/app/src/cli/models/extensions/ui-specifications/checkout_ui_extension.ts
@@ -3,7 +3,7 @@ import {BaseUIExtensionSchema} from '../schemas.js'
 import {loadLocalesConfig} from '../../../utilities/extensions/locales-configuration.js'
 import {schema} from '@shopify/cli-kit'
 
-const dependency = {name: '@shopify/checkout-ui-extensions-react', version: '^0.20.0'}
+const dependency = {name: '@shopify/checkout-ui-extensions-react', version: '^0.21.2'}
 
 const CheckoutSchema = BaseUIExtensionSchema.extend({
   extensionPoints: schema.define.array(schema.define.string()).optional(),
