5e0c3c295a2f583dedbd83279d1fda66c9f57f83
Alvaro Gutierrez <alvaro.gutierrezblanco@shopify.com>
2022-12-09 18:05:52 +0100

    Fix flaky test in sessions

--- packages/cli-kit/src/environment/service.test.ts
+++ packages/cli-kit/src/environment/service.test.ts
@@ -1,6 +1,12 @@
 import {isSpinEnvironment, serviceEnvironment} from './service.js'
 import {Environment} from '../network/service.js'
-import {expect, it, describe} from 'vitest'
+import {expect, it, describe, afterEach} from 'vitest'
+
+const OLD_ENV = {...process.env}
+
+afterEach(() => {
+  process.env = {...OLD_ENV}
+})
 
 describe('serviceEnvironment', () => {
   it('returns local when the environment variable points to the local environment', () => {
