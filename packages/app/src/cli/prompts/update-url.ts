a83a01a15cbb46b13d6f905432d499fa740eb4ff
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2022-12-21 13:28:23 +0100

    URL prompts

---
+++ packages/app/src/cli/prompts/update-url.ts
@@ -0,0 +1,34 @@
+import {validURL} from '../services/dev/urls.js'
+import {ui} from '@shopify/cli-kit'
+
+export async function appUrlPrompt(defaultValue: string): Promise<string> {
+  const input = await ui.prompt([
+    {
+      type: 'input',
+      name: 'url',
+      message: 'App URL',
+      default: defaultValue,
+      validate: (value: string) => {
+        if (validURL(value)) return true
+        return 'Invalid URL'
+      },
+    },
+  ])
+  return input.url
+}
+
+export async function allowedRedirectionURLsPrompt(defaultValue: string): Promise<string[]> {
+  const input = await ui.prompt([
+    {
+      type: 'input',
+      name: 'urls',
+      message: 'Allowed redirection URLs (comma separated)',
+      default: defaultValue,
+      validate: (value: string) => {
+        if (value.split(',').every((url) => validURL(url))) return true
+        return 'Invalid URLs'
+      },
+    },
+  ])
+  return input.urls.split(',')
+}
