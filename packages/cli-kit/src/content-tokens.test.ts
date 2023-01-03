bb610e62d719196842d8183c370fe44517075c61
Isaac Roldán <isaac.roldan@shopify.com>
2023-01-03 16:08:40 +0100

    Add test for LinkContentToken

---
+++ packages/cli-kit/src/content-tokens.test.ts
@@ -0,0 +1,12 @@
+import {describe, expect, test} from 'vitest'
+import {LinkContentToken} from './content-tokens.js'
+
+describe('LinkContentToken', () => {
+  test('the link includes spaces between the URL and the parenthesis for command/control click to work', () => {
+    // When
+    const got = new LinkContentToken('Shopify Web', 'https://shopify.com')
+
+    // Then
+    expect(got.output()).toEqual('\u001b[32mShopify Web\u001b[39m ( https://shopify.com )')
+  })
+})
