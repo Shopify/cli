c16e9a2cf41ecd92846fd0ae817164e986288af5
Richard Powell <richard.powell@shopify.com>
2023-01-04 09:22:20 -0500

    Add an Icon component
    This is taken from Polaris. See: https://github.com/Shopify/polaris/tree/main/polaris-react/src/components/Icon  .  The implementation is simplified because our needs are a little simpler

---
+++ packages/ui-extensions-dev-console/src/components/Icon/Icon.tsx
@@ -0,0 +1,22 @@
+import styles from './Icon.module.scss'
+import {VisuallyHidden} from '../VisuallyHidden/VisuallyHidden.js'
+import React from 'react'
+
+export interface IconProps {
+  /** The SVG contents to display in the icon (icons should fit in a 20 × 20 pixel viewBox) */
+  source: React.FunctionComponent<React.SVGProps<SVGSVGElement>> | string
+
+  /** Descriptive text to be read to screenreaders */
+  accessibilityLabel: string
+}
+
+export function Icon({source, accessibilityLabel}: IconProps) {
+  const SourceComponent = source
+
+  return (
+    <span className={styles.Icon}>
+      <VisuallyHidden>{accessibilityLabel}</VisuallyHidden>
+      <SourceComponent className={styles.Svg} focusable="false" aria-hidden="true" />
+    </span>
+  )
+}
