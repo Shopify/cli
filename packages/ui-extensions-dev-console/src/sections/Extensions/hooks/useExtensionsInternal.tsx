3bfd1d335e84f5cb4c253b4204857c451f1629d9
Richard Powell <richard.powell@shopify.com>
2023-01-04 17:10:32 -0500

    Added a new PreviewLinks component which we will render inside the ExtensionRow
    Also update the hook to allow us to navigate to any arbitary URL, rather than just an extension

--- packages/ui-extensions-dev-console/src/sections/Extensions/hooks/useExtensionsInternal.tsx
+++ packages/ui-extensions-dev-console/src/sections/Extensions/hooks/useExtensionsInternal.tsx
@@ -28,7 +28,6 @@ export function useExtensionsInternal() {
     // dispatch events
     focus: (extension: ExtensionPayload) => extensionServer.client.emit('focus', [{uuid: extension.uuid}]),
     unfocus: () => extensionServer.client.emit('unfocus'),
-    navigate: (extension: ExtensionPayload) =>
-      extensionServer.client.emit('navigate', {url: extension.development.resource.url}),
+    navigate: (url: string) => extensionServer.client.emit('navigate', url),
   }
 }
