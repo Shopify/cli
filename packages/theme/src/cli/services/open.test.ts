e1cf2d3dbfd27a063d9f1b493e22e7a1b6685397
Guilherme Carreiro <karreiro@gmail.com>
2023-01-05 12:52:01 +0100

    Adopt CLI UI kit on `shopify theme open` command

---
+++ packages/theme/src/cli/services/open.test.ts
@@ -0,0 +1,84 @@
+import {open} from './open.js'
+import {findOrSelectTheme} from '../utilities/theme-selector.js'
+import {Theme} from '../models/theme.js'
+import {test, describe, expect, vi} from 'vitest'
+import {system} from '@shopify/cli-kit'
+import {renderInfo} from '@shopify/cli-kit/node/ui'
+
+vi.mock('@shopify/cli-kit')
+vi.mock('@shopify/cli-kit/node/ui')
+vi.mock('../utilities/theme-selector.js')
+
+const session = {
+  token: 'token',
+  storeFqdn: 'my-shop.myshopify.com',
+}
+
+const theme = {
+  id: 1,
+  name: 'my theme',
+} as Theme
+
+const options = {
+  development: false,
+  live: false,
+  editor: false,
+  theme: '1',
+}
+
+describe('open', () => {
+  test('opens the preview URL', async () => {
+    // Given
+    vi.mocked(findOrSelectTheme).mockResolvedValue(theme)
+
+    // When
+    await open(session, options)
+
+    // Then
+    expect(system.open).toBeCalledWith('https://my-shop.myshopify.com?preview_theme_id=1')
+  })
+
+  test('opens the editor URL with `editor` flag', async () => {
+    // Given
+    vi.mocked(findOrSelectTheme).mockResolvedValue(theme)
+
+    // When
+    await open(session, {...options, editor: true})
+
+    // Then
+    expect(system.open).toBeCalledWith('https://my-shop.myshopify.com/admin/themes/1/editor')
+  })
+
+  test('renders the theme links', async () => {
+    // Given
+    vi.mocked(findOrSelectTheme).mockResolvedValue(theme)
+
+    // When
+    await open(session, options)
+
+    // Then
+    expect(renderInfo).toBeCalledWith({
+      headline: {
+        userInput: 'my theme',
+      },
+      body: {
+        list: {
+          items: [
+            {
+              link: {
+                label: 'Preview your theme',
+                url: 'https://my-shop.myshopify.com?preview_theme_id=1',
+              },
+            },
+            {
+              link: {
+                label: 'Customize your theme at the theme editor',
+                url: 'https://my-shop.myshopify.com/admin/themes/1/editor',
+              },
+            },
+          ],
+        },
+      },
+    })
+  })
+})
---
+++ packages/theme/src/cli/services/open.ts
@@ -0,0 +1,39 @@
+import {findOrSelectTheme} from '../utilities/theme-selector.js'
+import {themeEditorUrl, themePreviewUrl} from '../utilities/theme-urls.js'
+import {session, system} from '@shopify/cli-kit'
+import {renderInfo} from '@shopify/cli-kit/node/ui'
+
+type AdminSession = session.AdminSession
+
+export async function open(
+  adminSession: AdminSession,
+  options: {development: boolean; live: boolean; editor: boolean; theme: string | undefined},
+) {
+  const theme = await findOrSelectTheme(adminSession, {
+    header: 'Select a theme to open',
+    filter: options,
+  })
+
+  const previewUrl = themePreviewUrl(theme, adminSession)
+  const editorUrl = themeEditorUrl(theme, adminSession)
+
+  renderInfo({
+    headline: {
+      userInput: theme.name,
+    },
+    body: {
+      list: {
+        items: [
+          {link: {label: 'Preview your theme', url: previewUrl}},
+          {link: {label: 'Customize your theme at the theme editor', url: editorUrl}},
+        ],
+      },
+    },
+  })
+
+  if (options.editor) {
+    await system.open(editorUrl)
+  } else {
+    await system.open(previewUrl)
+  }
+}
