0e4002414bd883446ec5552a46754949f1f756bb
Isaac Roldán <isaac.roldan@shopify.com>
2023-01-09 16:35:32 +0100

    add docs

--- packages/cli-kit/src/public/node/toml.ts
+++ packages/cli-kit/src/public/node/toml.ts
@@ -1,9 +1,15 @@
 import * as toml from '@iarna/toml'
 
+/**
+ * Given a TOML string, it returns a JSON object.
+ */
 export function decodeToml(input: string): object {
   return toml.parse(input)
 }
 
+/**
+ * Given a JSON object, it returns a TOML string.
+ */
 export function encodeToml(content: toml.JsonMap): string {
   return toml.stringify(content)
 }
