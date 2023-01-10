6ff6ca3496a0c379181d20d9022c39853c87cb72
Isaac Roldán <isaac.roldan@shopify.com>
2023-01-10 19:26:43 +0100

    revert wrong change

--- packages/app/src/cli/models/extensions/function-specifications/delivery_customization.ts
+++ packages/app/src/cli/models/extensions/function-specifications/delivery_customization.ts
@@ -4,7 +4,6 @@ const spec = createFunctionSpecification({
   identifier: 'delivery_customization',
   externalIdentifier: 'delivery_customization',
   externalName: 'Function - Delivery customization',
-  supportedFlavors: [{name: 'Javascript', value: 'vanilla-js'}],
   gated: false,
   templatePath: (lang) => `checkout/${lang}/delivery-customization/default`,
 })
