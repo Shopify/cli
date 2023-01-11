0c68ab5c125a4f4f46bd0bebf3e4ecc42544a478
Richard Powell <richard.powell@shopify.com>
2023-01-10 19:28:03 -0500

    Fix some small issues with our Buttons, and use the new classNames utility

--- packages/ui-extensions-dev-console/src/components/IconButton/IconButton.tsx
+++ packages/ui-extensions-dev-console/src/components/IconButton/IconButton.tsx
@@ -1,16 +1,15 @@
 import * as styles from './IconButton.module.scss'
 import React from 'react'
 import {Icon, IconProps} from '@/components/Icon'
+import {classNames} from '@/utilities/css'
 
 export interface ButtonProps extends React.HTMLProps<HTMLButtonElement>, IconProps {
   type: 'button' | 'submit' | 'reset' | undefined
 }
 
 export function IconButton({className, selected, source, accessibilityLabel, ...props}: ButtonProps) {
-  const classNames = `${className ? `${className} ` : ''}${styles.IconButton}`
-
   return (
-    <button {...props} className={classNames} aria-pressed={selected}>
+    <button {...props} className={classNames(className, styles.IconButton)} aria-pressed={selected}>
       <span className={styles.Icon}>
         <Icon source={source} accessibilityLabel={accessibilityLabel} />
       </span>
