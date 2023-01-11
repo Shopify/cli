c8bafceb4a80cb6706ea70c3e00f48a1e4e1e233
Pedro Piñera <pedro@craftweg.com>
2023-01-11 15:57:18 +0100

    Fix benchmarking

--- workspace/src/os-metadata.js
+++
@@ -1,9 +0,0 @@
-import {totalmem, cpus, arch} from 'node:os'
-
-export function osMetadata() {
-  const metadata = {}
-  metadata.memory = totalmem()
-  metadata.arch = arch()
-  metadata.cpus = cpus()
-  return metadata
-}
