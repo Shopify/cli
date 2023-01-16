6cbb675a3e57d65195e42d93d119d949af74e9d9
Trish Ta <trish.ta@shopify.com>
2023-01-16 12:38:56 -0500

    Rename resourceURLToString to assetToString for better accuracy

---
+++ packages/ui-extensions-server-kit/src/utilities/assetToString.test.ts
@@ -0,0 +1,16 @@
+import {assetToString} from './assetToString'
+import type {Asset} from '../types'
+
+describe('assetToString tests', () => {
+  test('creates a URL string from an asset', () => {
+    const asset: Asset = {
+      name: 'main',
+      url: 'http://localhost:8000/extensions/00000000/assets/main.js',
+      lastUpdated: 1637004124,
+    }
+
+    const url = assetToString(asset)
+
+    expect(url).toBe(`${asset.url}?lastUpdated=${asset.lastUpdated}`)
+  })
+})
