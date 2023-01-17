c633502e0c12fcceb27e9c47ed4dc6b7ed329246
Guilherme Carreiro <karreiro@gmail.com>
2023-01-17 09:51:12 +0100

    Update theme utilities   - Introduce theme UI components   - Introduce `findThemes` function

--- packages/theme/src/cli/utilities/theme-selector.ts
+++ packages/theme/src/cli/utilities/theme-selector.ts
@@ -9,21 +9,21 @@ import {AdminSession} from '@shopify/cli-kit/node/session.js'
  * @param session - Current Admin session
  * @param options - Options to select a theme:
  *  - header: the header presented when users select a theme
- *  - filter: the filter {@link FilterProps} applied in the list
+ *  - filter: the filter ({@link FilterProps}) applied in the list
  *            of themes in the store
  *
  * @returns the selected {@link Theme}
  */
-export async function findOrSelectTheme(session: AdminSession, opts: {header: string; filter: FilterProps}) {
+export async function findOrSelectTheme(session: AdminSession, options: {header: string; filter: FilterProps}) {
   const themes = await fetchStoreThemes(session)
-  const filter = new Filter(opts.filter)
+  const filter = new Filter(options.filter)
   const store = session.storeFqdn
 
   if (filter.any()) {
     return filterThemes(store, themes, filter)[0]!
   }
 
-  const message = opts.header
+  const message = options.header
   const choices = themes.map((theme) => {
     return {
       value: theme,
@@ -35,4 +35,25 @@ export async function findOrSelectTheme(session: AdminSession, opts: {header: st
     message,
     choices,
   })
+}
+
+/**
+ * Finds themes in the store.
+ *
+ * @param session - Current Admin session
+ * @param filterProps - The filter ({@link FilterProps}) applied in the list
+ *                      of themes in the store
+ *
+ * @returns a collection of {@link Theme}
+ */
+export async function findThemes(session: AdminSession, filterProps: FilterProps) {
+  const themes = await fetchStoreThemes(session)
+  const filter = new Filter(filterProps)
+  const store = session.storeFqdn
+
+  if (filter.any()) {
+    return filterThemes(store, themes, filter)
+  }
+
+  return []
 }
