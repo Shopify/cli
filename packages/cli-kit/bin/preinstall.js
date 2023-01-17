f03948d565fd043a6ad0c379d0ec2863c0805763
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2023-01-17 12:50:04 +0100

    Update the cli-kit version as a prerelease step instead of preinstall

--- packages/cli-kit/bin/preinstall.js
+++
@@ -1,5 +0,0 @@
-import {readFileSync, writeFile, writeFileSync} from 'fs';
-
-const version = JSON.parse(readFileSync('package.json')).version
-const content = `export const CLI_KIT_VERSION = '${version}'\n`
-writeFileSync('src/public/common/version.ts', content)
