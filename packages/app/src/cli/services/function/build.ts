bb3c1d7600edd68ab7c02d5407d214d5d8dba962
Ju Liu <ju.liu@shopify.com>
2023-01-18 17:03:32 +0000

    Play a bit with esbuild

--- packages/app/src/cli/services/function/build.ts
+++ packages/app/src/cli/services/function/build.ts
@@ -1,6 +1,7 @@
 import {FunctionExtension} from '../../models/app/extensions.js'
 import {exec} from '@shopify/cli-kit/node/system'
-// import {build as esBuild} from 'esbuild'
+import {path} from '@shopify/cli-kit'
+import {build as esBuild} from 'esbuild'
 
 export async function buildFunction(fun: FunctionExtension) {
   await buildGraphqlTypes(fun.directory)
@@ -11,13 +12,12 @@ export async function buildFunction(fun: FunctionExtension) {
 export async function buildGraphqlTypes(directory: string) {
   return exec('npm', ['exec', '--', 'graphql-code-generator', '-c', '.graphqlrc'], {
     cwd: directory,
-    stdout: process.stdout,
     stderr: process.stderr,
   })
 }
 
 export async function bundleExtension(fun: FunctionExtension) {
-  // const esbuildOptions = getESBuildOptions()
+  // const esbuildOptions = getESBuildOptions(fun.directory)
   // return esBuild(esbuildOptions)
   return exec('npm', ['exec', '--', 'vite', 'build'], {
     cwd: fun.directory,
@@ -26,21 +26,18 @@ export async function bundleExtension(fun: FunctionExtension) {
   })
 }
 
-// function getESBuildOptions(): Parameters<typeof esBuild>[0] {
-//   const esbuildOptions: Parameters<typeof esBuild>[0] = {
-//     outfile: 'dist',
-//     bundle: true,
-//     loader: {
-//       '.esnext': 'ts',
-//       '.js': 'jsx',
-//     },
-//     legalComments: 'none',
-//     minify: true,
-//     target: 'es6',
-//     resolveExtensions: ['.tsx', '.ts', '.js', '.json', '.esnext', '.mjs', '.ejs'],
-//   }
-//   return esbuildOptions
-// }
+function getESBuildOptions(directory: string): Parameters<typeof esBuild>[0] {
+  const esbuildOptions: Parameters<typeof esBuild>[0] = {
+    outfile: path.join(directory, 'dist/function.js'),
+    entryPoints: [path.join(directory, 'src/index.js')],
+    logLevel: 'info',
+    bundle: true,
+    legalComments: 'none',
+    target: 'es2022',
+    format: 'esm',
+  }
+  return esbuildOptions
+}
 
 export async function runJavy(fun: FunctionExtension) {
   return exec('npm', ['exec', '--', 'javy', '-o', fun.buildWasmPath(), 'dist/function.js'], {
