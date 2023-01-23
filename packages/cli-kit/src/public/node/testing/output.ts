f889da23e4f9aefe6257b4c0600c0e01d4fe558d
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2023-01-23 01:02:45 +0100

    Fix type-check

--- packages/cli-kit/src/public/node/testing/output.ts
+++ packages/cli-kit/src/public/node/testing/output.ts
@@ -1,10 +1,21 @@
 import * as output from '../../../output.js'
 
+interface OutputMock {
+  output: () => string
+  info: () => string
+  debug: () => string
+  success: () => string
+  completed: () => string
+  warn: () => string
+  error: () => string
+  clear: () => void
+}
+
 /**
  * Returns a set of functions to get the outputs ocurred during a test run.
  *
  * @returns An mock object with all the output functions.
- */ export function mockAndCaptureOutput(): object {
+ */ export function mockAndCaptureOutput(): OutputMock {
   return {
     output: () => (output.collectedLogs.output ?? []).join('\n'),
     info: () => (output.collectedLogs.info ?? []).join('\n'),
