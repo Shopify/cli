289c0b74c9b0ab76e7f4cf71c7cee05baa37fb1d
Moritz Löwenstein <moritz.loewenstein@gmx.de>
2023-01-23 21:18:05 +0100

    add trivial base64URLEncode test

--- packages/cli-kit/src/public/node/crypto.test.ts
+++ packages/cli-kit/src/public/node/crypto.test.ts
@@ -1,4 +1,4 @@
-import {hashString} from './crypto.js'
+import {hashString, base64URLEncode} from './crypto.js'
 import {describe, expect, it} from 'vitest'
 
 describe('hashString', () => {
@@ -7,5 +7,12 @@ describe('hashString', () => {
     const hash2 = hashString('hello')
     expect(hash1).toEqual(hash2)
     expect(hash1).toMatch(/[a-f0-9]{40}/)
+  })
+})
+
+describe('base64URLEncode', () => {
+  it('encodes a string to its base64 url encoded representation', () => {
+    const encoded = base64URLEncode(Buffer.from('hello+/='))
+    expect(encoded).toEqual("aGVsbG8rLz0")
   })
 })
