984220e71834fc97ee93ce44dd9328092f15a0e9
jfisher727 <jason.fisher@shopify.com>
2023-01-30 14:07:52 -0500

    Adjusted parameter based on feedback

--- packages/cli-kit/src/private/node/constants.ts
+++ packages/cli-kit/src/private/node/constants.ts
@@ -23,7 +23,7 @@ export const environmentVariables = {
   themeToken: 'SHOPIFY_CLI_THEME_TOKEN',
   unitTest: 'SHOPIFY_UNIT_TEST',
   verbose: 'SHOPIFY_FLAG_VERBOSE',
-  noThemeBundling: 'SHOPIFY_NO_THEME_BUNDLING',
+  noThemeBundling: 'SHOPIFY_CLI_NO_THEME_BUNDLING',
   // Variables to detect if the CLI is running in a cloud environment
   codespaceName: 'CODESPACE_NAME',
   codespaces: 'CODESPACES',
