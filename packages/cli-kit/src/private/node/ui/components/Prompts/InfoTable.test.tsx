8ab3601668fffa66a42a8de5d8afaae6a3363ca1
Matteo Depalo <matteo.depalo@shopify.com>
2023-02-01 11:07:02 +0900

    Update infoTable tests

--- packages/cli-kit/src/private/node/ui/components/Prompts/InfoTable.test.tsx
+++ packages/cli-kit/src/private/node/ui/components/Prompts/InfoTable.test.tsx
@@ -6,13 +6,35 @@ import {render} from 'ink-testing-library'
 
 describe('InfoTable', async () => {
   test('renders a horizontal table with bullet points', async () => {
-    const {lastFrame} = render(<InfoTable table={{'header 1': ['some', 'items'], 'header 2': ['one item']}} />)
+    const {lastFrame} = render(
+      <InfoTable
+        table={{
+          'header 1': ['some', 'items'],
+          'header 2': [['one item', {link: {label: 'Shopify', url: 'https://shopify.com'}}]],
+        }}
+      />,
+    )
 
     expect(unstyled(lastFrame()!)).toMatchInlineSnapshot(`
       "Header 1:  • some
                  • items
 
-      Header 2:  • one item"
+      Header 2:  • one item Shopify ( https://shopify.com )"
+    `)
+  })
+
+  test('supports an empty header value', async () => {
+    const {lastFrame} = render(
+      <InfoTable
+        table={{
+          '': ['some', 'items'],
+        }}
+      />,
+    )
+
+    expect(unstyled(lastFrame()!)).toMatchInlineSnapshot(`
+      "• some
+      • items"
     `)
   })
 })
