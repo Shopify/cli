df924ef7aca0b30ebac3831ed4cdb9be7c081d1c
Kris Tremblay <kris.tremblay@shopify.com>
2023-02-05 16:09:15 -0600

    refactor tooltip and popover, add bound chks

--- packages/ui-extensions-dev-console/src/components/Tooltip/Tooltip.tsx
+++ packages/ui-extensions-dev-console/src/components/Tooltip/Tooltip.tsx
@@ -1,35 +1,20 @@
 import styles from './Tooltip.module.css'
 import {TooltipPopover} from './TooltipPopover'
-import React, {useRef, useReducer} from 'react'
-import type {TooltipProps, TooltipState, TooltipAction} from './types'
+import React, {useRef, useState} from 'react'
 import {classNames} from '@/utilities/css'
 
-function tooltipReducer(state: TooltipState, action: TooltipAction) {
-  switch (action.type) {
-    case 'show':
-      return {...state, isVisible: true}
-    case 'hide':
-      return {...state, isVisible: false}
-    case 'position':
-      return {...state, position: action.payload}
-    default:
-      throw new Error()
-  }
+export interface TooltipProps {
+  children: JSX.Element | string
+  text: string
 }
 
 export function Tooltip({children, text}: TooltipProps) {
   const ref = useRef<HTMLDivElement | null>(null)
 
-  const [state, dispatch] = useReducer(tooltipReducer, {
-    isVisible: false,
-    position: {
-      x: 0,
-      y: 0,
-    },
-  })
+  const [isVisible, setIsVisible] = useState(false)
 
   const handleShow = () => {
-    if (!ref.current || state.isVisible) return
+    if (!ref.current || isVisible) return
 
     // This is currently my best solution for avoid the tooltip-wrapped element
     // from needing two tab presses to get past.  I'm not sure if this is the best
@@ -38,18 +23,7 @@ export function Tooltip({children, text}: TooltipProps) {
       ;(ref.current.firstChild as HTMLElement).tabIndex = -1
     }
 
-    const {x, y, height} = ref.current.getBoundingClientRect()
-    const {scrollY, scrollX} = window
-
-    dispatch({
-      type: 'position',
-      payload: {
-        x: x + scrollX,
-        y: y + height + 10 + scrollY,
-      },
-    })
-
-    dispatch({type: 'show'})
+    setIsVisible(true)
   }
 
   // This is part of the double-tab solution menmtioned above. Since we will
@@ -68,14 +42,14 @@ export function Tooltip({children, text}: TooltipProps) {
       className={classNames(styles.Tooltip, typeof children === 'string' && styles.TooltipUnderline)}
       onMouseEnter={handleShow}
       onFocus={handleShow}
-      onBlur={() => dispatch({type: 'hide'})}
-      onMouseLeave={() => dispatch({type: 'hide'})}
+      onBlur={() => setIsVisible(false)}
+      onMouseLeave={() => setIsVisible(false)}
       onKeyUp={handleEnter}
       ref={ref}
       tabIndex={0}
     >
       {children}
-      {state.isVisible && <TooltipPopover position={state.position} text={text} />}
+      {isVisible && <TooltipPopover text={text} targetRef={ref} />}
     </div>
   )
 }
--- packages/ui-extensions-dev-console/src/components/Tooltip/TooltipPopover.tsx
+++ packages/ui-extensions-dev-console/src/components/Tooltip/TooltipPopover.tsx
@@ -1,14 +1,51 @@
 import styles from './TooltipPopover.module.css'
-import React, {useRef, useLayoutEffect} from 'react'
-import type {TooltipPopoverProps} from './types'
+import React, {useLayoutEffect, useRef} from 'react'
 
-export function TooltipPopover({position, text}: TooltipPopoverProps) {
+export interface Position {
+  x: number
+  y: number
+}
+
+export interface TooltipPopoverProps {
+  text: string
+  targetRef: React.RefObject<HTMLElement>
+}
+
+const TOOLTIP_VERTICAL_OFFSET = 10
+
+export function TooltipPopover({targetRef, text}: TooltipPopoverProps) {
   const ref = useRef<HTMLDivElement | null>(null)
 
+  const getPositionRelativeToRef = (): Position => {
+    const {x, y, height} = targetRef.current!.getBoundingClientRect()
+    const {scrollY, scrollX} = window
+
+    return {
+      x: x + scrollX,
+      y: y + height + TOOLTIP_VERTICAL_OFFSET + scrollY,
+    }
+  }
+
   useLayoutEffect(() => {
+    if (!ref.current || !targetRef) return
+
+    // check if ref is still inside the viewport
+    let {x, y} = getPositionRelativeToRef()
+    const {innerWidth, innerHeight} = window
+    const {height, width} = ref.current.getBoundingClientRect()
+
+    if (x + width > innerWidth) {
+      x -= width - targetRef.current!.getBoundingClientRect().width
+    }
+
+    if (y + height > innerHeight) {
+      y -= height + targetRef.current!.getBoundingClientRect().height + 2 * TOOLTIP_VERTICAL_OFFSET
+    }
+    // end check
+
     if (ref.current) {
-      ref.current.style.top = `${position.y}px`
-      ref.current.style.left = `${position.x}px`
+      ref.current.style.top = `${y}px`
+      ref.current.style.left = `${x}px`
     }
   }, [])
 
