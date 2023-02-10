1433088872ce32267632013f86eef918420bc0c5
Kris Tremblay <kris.tremblay@shopify.com>
2023-02-10 13:43:45 -0600

    fix sizing and alignment of tooltip

--- packages/ui-extensions-dev-console/src/components/Tooltip/TooltipPopover.tsx
+++ packages/ui-extensions-dev-console/src/components/Tooltip/TooltipPopover.tsx
@@ -1,6 +1,5 @@
 import styles from './TooltipPopover.module.css'
 import React, {useLayoutEffect, useRef} from 'react'
-import {classNames} from '@/utilities/css'
 
 export interface Position {
   x: number
@@ -12,7 +11,6 @@ export interface TooltipPopoverProps {
   targetRef: React.RefObject<HTMLElement>
 }
 
-const TOOLTIP_MAX_CHAR_WIDTH = 30
 const TOOLTIP_VERTICAL_OFFSET = 10
 
 export function TooltipPopover({targetRef, text}: TooltipPopoverProps) {
@@ -52,11 +50,7 @@ export function TooltipPopover({targetRef, text}: TooltipPopoverProps) {
   }, [])
 
   return (
-    <div
-      className={classNames(styles.Popover, text.length > TOOLTIP_MAX_CHAR_WIDTH && styles.LongText)}
-      role="tooltip"
-      ref={ref}
-    >
+    <div className={styles.Popover} role="tooltip" ref={ref}>
       {text}
     </div>
   )
