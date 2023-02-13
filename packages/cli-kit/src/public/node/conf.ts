09781aedb6e98aa96d754fc0ce3148546dd68c4c
Isaac <isaac.roldan@gmail.com>
2023-02-13 14:22:23 +0100

    Wrap conf into our own class to avoid re-exporting 3p dependencies

--- packages/cli-kit/src/public/node/conf.ts
+++ packages/cli-kit/src/public/node/conf.ts
@@ -1,3 +1,25 @@
-import Conf, {Schema} from 'conf'
+import Config from 'conf'
 
-export {Conf, Schema}
+export class Conf<T extends {[key: string]: unknown}> {
+  private readonly config: Config<T>
+
+  constructor(options: {projectName: string}) {
+    this.config = new Config<T>(options)
+  }
+
+  get<TKey extends keyof T>(key: TKey): T[TKey] | undefined {
+    return this.config.get(key)
+  }
+
+  set<TKey extends keyof T>(key: TKey, value?: T[TKey]): void {
+    this.config.set(key, value)
+  }
+
+  delete<TKey extends keyof T>(key: TKey): void {
+    this.config.delete(key)
+  }
+
+  clear(): void {
+    this.config.clear()
+  }
+}
