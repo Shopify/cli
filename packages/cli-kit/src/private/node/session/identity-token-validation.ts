c98298eb38bc1e0846f2bf2cd7d6ecd99bb3949b
Ariel Caplan <ariel.caplan@shopify.com>
2023-02-13 21:49:55 +0200

    Refactor with Result types

--- packages/cli-kit/src/private/node/session/identity-token-validation.ts
+++ packages/cli-kit/src/private/node/session/identity-token-validation.ts
@@ -2,13 +2,8 @@ import {identityFqdn} from '../../../public/node/context/fqdn.js'
 import {outputDebug} from '../../../public/node/output.js'
 import {shopifyFetch} from '../../../public/node/http.js'
 import {cacheFetch} from '../conf-store.js'
-
-class NotFoundError extends Error {
-  constructor(message: string) {
-    super(message)
-    this.name = 'NotFoundError'
-  }
-}
+import {err, ok, Result} from '../../../public/node/result.js'
+import {AbortError} from '../../../public/node/error.js'
 
 export async function validateIdentityToken(token: string): Promise<boolean> {
   try {
@@ -26,16 +21,16 @@ export async function validateIdentityToken(token: string): Promise<boolean> {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const json: any = await response.json()
         outputDebug(`The identity token is valid: ${json.valid}`)
-        return json.valid
+        return ok(json.valid)
       } else if (response.status === 404) {
-        throw new NotFoundError('Identity Introspection endpoint not found')
+        return err(new AbortError(`The introspection endpoint returned a 404: ${introspectionURL}`))
       } else {
         const text = await response.text()
         outputDebug(`The Introspection request failed with:
  - status: ${response.status}
  - www-authenticate header: ${JSON.stringify(response.headers.get('www-authenticate'))}
  - body: ${JSON.stringify(text)}`)
-        return false
+        return ok(false)
       }
     })
     // eslint-disable-next-line no-catch-all/no-catch-all
@@ -45,19 +40,18 @@ export async function validateIdentityToken(token: string): Promise<boolean> {
   }
 }
 
-async function withIntrospectionURL<T>(fn: (introspectionUrl: string) => Promise<T>): Promise<T> {
+async function withIntrospectionURL<T>(fn: (introspectionUrl: string) => Promise<Result<T, AbortError>>): Promise<T> {
   const week = 7 * 24 * 60 * 60 * 1000
   const introspectionURL = await cacheFetch('identity-introspection-url', getIntrospectionURL, week)
-  try {
-    return fn(introspectionURL)
-  } catch (error) {
-    if (error instanceof NotFoundError) {
-      // If the introspection URL is invalid, clear the cache and try again
-      await cacheFetch('identity-introspection-url', getIntrospectionURL, 0)
-      return fn(introspectionURL)
-    } else {
-      throw error
-    }
+  let result: Result<T, AbortError> = await fn(introspectionURL)
+  if (result.isErr()) {
+    await cacheFetch('identity-introspection-url', getIntrospectionURL, 0)
+    result = await fn(introspectionURL)
+  }
+  if (result.isErr()) {
+    throw result.error
+  } else {
+    return result.value
   }
 }
 
