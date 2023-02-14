ebd8df8afac7b9e252cb313005d712ac94a5362d
Isaac <isaac.roldan@gmail.com>
2023-02-14 19:42:04 +0100

    Add remove re-exports from semver

--- packages/cli-kit/src/public/node/semver.ts
+++ packages/cli-kit/src/public/node/semver.ts
@@ -3,5 +3,28 @@ import {createRequire} from 'module'
 const require = createRequire(import.meta.url)
 const {coerce, SemVer} = require('semver')
 
-export {SemVer as Version}
-export {coerce as coerceSemverVersion}
+// export {SemVer as Version}
+// export {coerce as coerceSemverVersion}
+
+/**
+ * Coerces a version string to a valid semver version.
+ *
+ * @param version - The version to coerce.
+ * @returns The coerced version.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function coerceSemverVersion(version: string): any {
+  return coerce(version)
+}
+
+/**
+ * Creates a new SemVer instance.
+ *
+ * @param version - The version to create.
+ * @returns The SemVer instance.
+ */
+export class Version extends SemVer {
+  constructor(version: string) {
+    super(coerceSemverVersion(version))
+  }
+}
