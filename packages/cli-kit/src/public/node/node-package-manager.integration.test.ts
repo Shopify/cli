1d7d2711268bf1b49d85c459fbbec5abf89ebd73
Ju Liu <ju.liu@shopify.com>
2023-02-25 22:52:40 +0000

    Add retry to NPM install test

--- packages/cli-kit/src/public/node/node-package-manager.integration.test.ts
+++ packages/cli-kit/src/public/node/node-package-manager.integration.test.ts
@@ -4,27 +4,31 @@ import {joinPath, dirname} from './path.js'
 import {describe, expect, test} from 'vitest'
 
 describe('installNPMDependenciesRecursively', () => {
-  test('runs install in all the directories containing a package.json', async () => {
-    await inTemporaryDirectory(async (tmpDir) => {
-      // Given
-      const rootPackage = joinPath(tmpDir, 'package.json')
-      const webPackage = joinPath(tmpDir, 'web/package.json')
-      const backendPackage = joinPath(tmpDir, 'web/backend/package.json')
+  test(
+    'runs install in all the directories containing a package.json',
+    async () => {
+      await inTemporaryDirectory(async (tmpDir) => {
+        // Given
+        const rootPackage = joinPath(tmpDir, 'package.json')
+        const webPackage = joinPath(tmpDir, 'web/package.json')
+        const backendPackage = joinPath(tmpDir, 'web/backend/package.json')
 
-      await mkdir(dirname(webPackage))
-      await mkdir(dirname(backendPackage))
+        await mkdir(dirname(webPackage))
+        await mkdir(dirname(backendPackage))
 
-      await writeFile(rootPackage, JSON.stringify({}))
-      await writeFile(webPackage, JSON.stringify({}))
-      await writeFile(backendPackage, JSON.stringify({}))
+        await writeFile(rootPackage, JSON.stringify({}))
+        await writeFile(webPackage, JSON.stringify({}))
+        await writeFile(backendPackage, JSON.stringify({}))
 
-      // When
-      await expect(
-        installNPMDependenciesRecursively({
-          directory: tmpDir,
-          packageManager: 'pnpm',
-        }),
-      ).resolves.toBe(undefined)
-    })
-  })
+        // When
+        await expect(
+          installNPMDependenciesRecursively({
+            directory: tmpDir,
+            packageManager: 'pnpm',
+          }),
+        ).resolves.toBe(undefined)
+      })
+    },
+    {retry: 3},
+  )
 })
