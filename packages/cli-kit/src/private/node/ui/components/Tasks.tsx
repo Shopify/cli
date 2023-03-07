ea294e70b4dab3e874e21e50ff1d8cf7e0bccbde
Matteo Depalo <matteo.depalo@shopify.com>
2023-03-07 09:53:36 +0900

    Put back isRawModeSupported

--- packages/cli-kit/src/private/node/ui/components/Tasks.tsx
+++ packages/cli-kit/src/private/node/ui/components/Tasks.tsx
@@ -92,7 +92,7 @@ function Tasks<TContext>({tasks, silent = isUnitTest()}: React.PropsWithChildren
         return null
       }
     },
-    {isActive: false},
+    {isActive: isRawModeSupported},
   )
 
   if (silent) {
