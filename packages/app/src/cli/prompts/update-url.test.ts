8cdcec7c0a4e2202ed117fb0b9f0802e955ebc29
Isaac <isaac.roldan@shopify.com>
2023-03-16 17:48:09 +0100

    More update-url tests

--- packages/app/src/cli/prompts/update-url.test.ts
+++ packages/app/src/cli/prompts/update-url.test.ts
@@ -1,4 +1,4 @@
-import {allowedRedirectionURLsPrompt, appUrlPrompt} from './update-url.js'
+import {allowedRedirectionURLsPrompt, appProxyPathPrompt, appProxyUrlPrompt, appUrlPrompt} from './update-url.js'
 import {describe, it, expect, vi} from 'vitest'
 import {renderTextPrompt} from '@shopify/cli-kit/node/ui'
 
@@ -35,6 +35,42 @@ describe('allowedRedirectionURLsPrompt', () => {
     expect(renderTextPrompt).toHaveBeenCalledWith({
       message: 'Allowed redirection URLs (comma separated)',
       defaultValue: 'https://example.com/callback',
+      validate: expect.any(Function),
+    })
+  })
+})
+
+describe('appProxyUrlPrompt', () => {
+  it('asks the user to write a URL and returns it', async () => {
+    // Given
+    vi.mocked(renderTextPrompt).mockResolvedValue('https://my-app.example.com')
+
+    // When
+    const got = await appProxyUrlPrompt('https://example.com')
+
+    // Then
+    expect(got).toEqual('https://my-app.example.com')
+    expect(renderTextPrompt).toHaveBeenCalledWith({
+      message: 'App Proxy URL',
+      defaultValue: 'https://example.com',
+      validate: expect.any(Function),
+    })
+  })
+})
+
+describe('appProxyPathPrompt', () => {
+  it('asks the user to write a path and returns it', async () => {
+    // Given
+    vi.mocked(renderTextPrompt).mockResolvedValue('new-path')
+
+    // When
+    const got = await appProxyPathPrompt('path')
+
+    // Then
+    expect(got).toEqual('new-path')
+    expect(renderTextPrompt).toHaveBeenCalledWith({
+      message: 'App Proxy Path',
+      defaultValue: 'path',
       validate: expect.any(Function),
     })
   })
