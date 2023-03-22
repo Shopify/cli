a08b2d26360bf00221b1aacd966c694124355d2a
Ariel Caplan <ariel.caplan@shopify.com>
2023-03-22 15:04:56 +0200

    Exclude system files in theme extensions
    List drawn from https://github.com/Shopify/cli/blob/6735253e6f6055b7f79093e409574dcda28f378a/packages/cli-kit/assets/cli-ruby/lib/shopify_cli/theme/ignore_filter.rb#L12-L26

--- packages/app/src/cli/services/deploy/theme-extension-config.ts
+++ packages/app/src/cli/services/deploy/theme-extension-config.ts
@@ -2,6 +2,22 @@ import {ThemeExtension} from '../../models/app/extensions.js'
 import {readFile, glob} from '@shopify/cli-kit/node/fs'
 import {joinPath, relativePath, dirname} from '@shopify/cli-kit/node/path'
 
+const ignoredFilePatterns = [
+  '.git',
+  '.hg',
+  '.bzr',
+  '.svn',
+  '_darcs',
+  'CVS',
+  '.sublime-(project|workspace)',
+  '.DS_Store',
+  '.sass-cache',
+  'Thumbs.db',
+  'desktop.ini',
+  'config.yml',
+  'node_modules',
+]
+
 export interface ThemeExtensionConfig {
   theme_extension: {
     files: {[key: string]: string}
@@ -10,7 +26,7 @@ export interface ThemeExtensionConfig {
 
 export async function themeExtensionConfig(themeExtension: ThemeExtension): Promise<ThemeExtensionConfig> {
   const files: {[key: string]: string} = {}
-  const themeFiles = await glob(joinPath(themeExtension.directory, '*/*'))
+  const themeFiles = await glob(joinPath(themeExtension.directory, '*/*'), {ignore: ignoredFilePatterns.map((pattern) => joinPath(themeExtension.directory, '*', pattern))})
   await Promise.all(
     themeFiles.map(async (filepath) => {
       const relativePathName = relativePath(themeExtension.directory, filepath)
