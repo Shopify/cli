ba01cc485cb8f92eb0b45920b5a01bbf693e9f1a
Ju Liu <ju.liu@shopify.com>
2023-03-22 15:28:53 +0000

    Remove heroku helpers

--- packages/cli-kit/assets/cli-ruby/test/test_helpers.rb
+++ packages/cli-kit/assets/cli-ruby/test/test_helpers.rb
@@ -8,7 +8,6 @@ module TestHelpers
   autoload :FakeFS, "test_helpers/fake_fs"
   autoload :FakeProject, "test_helpers/fake_project"
   autoload :FakeUI, "test_helpers/fake_ui"
-  autoload :Heroku, "test_helpers/heroku"
   autoload :Partners, "test_helpers/partners"
   autoload :Project, "test_helpers/project"
   autoload :Schema, "test_helpers/schema"
