b2476023840d194fc255a7d4afd69d77059c93cd
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2023-03-29 14:49:37 +0200

    Ignore cached introspection URL when empty

--- packages/cli-kit/src/private/node/conf-store.test.ts
+++ packages/cli-kit/src/private/node/conf-store.test.ts
@@ -53,7 +53,6 @@ describe('removeSession', () => {
 
 describe('cacheRetrieveOrRepopulate', () => {
   // flaky test
-  // eslint-disable-next-line vitest/no-disabled-tests
   test.skip('returns the cached contents when they exist', async () => {
     await inTemporaryDirectory(async (cwd) => {
       // Given
@@ -80,13 +79,15 @@ describe('cacheRetrieveOrRepopulate', () => {
       // Given
       const config = new LocalStorage<ConfSchema>({cwd})
 
-      // Then
+      // When
       const got = await cacheRetrieveOrRepopulate(
         'identity-introspection-url-IDENTITYURL',
         async () => 'URL1',
         1000,
         config,
       )
+
+      // Then
       expect(got).toEqual('URL1')
     })
   })
@@ -95,19 +96,42 @@ describe('cacheRetrieveOrRepopulate', () => {
     await inTemporaryDirectory(async (cwd) => {
       // Given
       const config = new LocalStorage<ConfSchema>({cwd})
+      const cacheValue = {
+        'identity-introspection-url-IDENTITYURL': {value: 'URL1', timestamp: Date.now() - 3600 * 1000},
+      }
+      config.set('cache', cacheValue)
 
       // When
-      // populate the cache
-      await cacheRetrieveOrRepopulate('identity-introspection-url-IDENTITYURL', async () => 'URL1', 1000, config)
-
-      // Then
       const got = await cacheRetrieveOrRepopulate(
         'identity-introspection-url-IDENTITYURL',
         async () => 'URL2',
         0,
         config,
       )
+
+      // Then
       // Fetches a new value because the old one is outdated per the current request
+      expect(got).toEqual('URL2')
+    })
+  })
+
+  test('re-derives the cached contents when the cache is invalid', async () => {
+    await inTemporaryDirectory(async (cwd) => {
+      // Given
+      const config = new LocalStorage<any>({cwd})
+      const cacheValue = {'identity-introspection-url-IDENTITYURL': {value: undefined, timestamp: Date.now()}}
+      config.set('cache', cacheValue)
+
+      // When
+      const got = await cacheRetrieveOrRepopulate(
+        'identity-introspection-url-IDENTITYURL',
+        async () => 'URL2',
+        3600 * 1000,
+        config,
+      )
+
+      // Then
+      // Fetches a new value because the old one is wrong
       expect(got).toEqual('URL2')
     })
   })
