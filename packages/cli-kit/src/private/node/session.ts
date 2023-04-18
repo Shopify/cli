509dcc2383af1acd0e8e18d726d45d80ac398142
Alvaro Gutierrez <alvaro.gutierrezblanco@shopify.com>
2023-04-18 13:28:08 +0200

    Set indentity token expiry time to 30 days

--- packages/cli-kit/src/private/node/session.ts
+++ packages/cli-kit/src/private/node/session.ts
@@ -357,7 +357,7 @@ function buildIdentityTokenFromEnv(
 ) {
   return {
     ...identityTokenInformation,
-    expiresAt: new Date(Date.now() + 15865002 * 1000),
+    expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     scopes,
   }
 }
