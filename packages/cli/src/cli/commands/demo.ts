64356d24736756da2cfe21e3e2703f6a7958175a
Ariel Caplan <ariel.caplan@shopify.com>
2023-04-23 23:01:22 +0300

    Remove irrelevant comment

--- packages/cli/src/cli/commands/demo.ts
+++ packages/cli/src/cli/commands/demo.ts
@@ -4,11 +4,6 @@ import Command from '@shopify/cli-kit/node/base-command'
 import {readFile} from '@shopify/cli-kit/node/fs'
 import {resolvePath, cwd} from '@shopify/cli-kit/node/path'
 
-/**
- * This command is used to output all the UI components of the CLI.
- * It's useful to test how they behave under different terminal sizes
- * and to help update the documentation when they change.
- */
 export default class Demo extends Command {
   static description = 'Demo a command design defined in a file'
   static hidden = true
