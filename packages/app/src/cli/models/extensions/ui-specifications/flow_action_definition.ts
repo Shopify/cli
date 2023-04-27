6106ed325e42fd41886f8ff89fcfc3ed2880f9ed
Maxime Leduc <maxime.leduc@shopify.com>
2023-04-27 20:15:41 +0000

    cleanup

--- packages/app/src/cli/models/extensions/ui-specifications/flow_action_definition.ts
+++ packages/app/src/cli/models/extensions/ui-specifications/flow_action_definition.ts
@@ -13,12 +13,10 @@ const FlowExtensionSchema = BaseUIExtensionSchema.extend({
 })
 
 /**
- * Extension specification with all properties and methods needed to load a theme extension.
+ * Extension specification with all properties and methods needed to load a UI extension.
  */
 const themeSpecification = createUIExtensionSpecification({
   identifier: 'flow_action_definition',
-  // these two fields are going to be overridden by the extension specification API response,
-  // but we need them to have a default value for tests
   schema: FlowExtensionSchema,
   supportedFlavors: [],
   singleEntryPath: false,
--- packages/app/templates/ui-extensions/projects/flow_action_definition/index.js
+++ packages/app/templates/ui-extensions/projects/flow_action_definition/index.js
@@ -1,1 +1,1 @@
-// placeholder for now - figure out if we can get rid of this for the build to pass.
+// placeholder
