e66451b4a95c978a91db0c349f10a2f21a124d86
Isaac <isaac.roldan@shopify.com>
2023-04-27 19:56:54 +0200

    fix lint issue

--- packages/plugin-cloudflare/src/stop.ts
+++ packages/plugin-cloudflare/src/stop.ts
@@ -1,5 +1,5 @@
 import {TUNNEL_PROVIDER} from './provider.js'
-import {stopTunnel} from '@shopify/cli-kit/node/plugins/tunnel'
 import {stopCloudflareProcess} from './tunnel.js'
+import {stopTunnel} from '@shopify/cli-kit/node/plugins/tunnel'
 
 export default stopTunnel({provider: TUNNEL_PROVIDER, action: stopCloudflareProcess})
