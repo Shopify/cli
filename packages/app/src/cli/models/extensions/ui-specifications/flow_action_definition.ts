b4ec3159789c5bfb69eb1fde7168cc7058470dd4
Maxime Leduc <maxime.leduc@shopify.com>
2023-05-10 15:06:05 +0000

    generated full flow action

--- packages/app/src/cli/models/extensions/ui-specifications/flow_action_definition.ts
+++ packages/app/src/cli/models/extensions/ui-specifications/flow_action_definition.ts
@@ -3,31 +3,49 @@ import {BaseUIExtensionSchema} from '../schemas.js'
 
 import {zod} from '@shopify/cli-kit/node/schema'
 
+// TODO: we need to figure out if we can do inter field validation (e.g. CCP requires Preview and validation URLs)
 const FlowExtensionSchema = BaseUIExtensionSchema.extend({
   name: zod.string(),
   type: zod.literal('flow_action_definition'),
   title: zod.string(),
   description: zod.string(),
   url: zod.string(),
-  fields: zod.array(zod.object({id: zod.string()})),
+  fields: zod.array(
+    zod.object({
+      id: zod.string(),
+      name: zod.string(),
+      label: zod.string(),
+      description: zod.string().optional(),
+      required: zod.boolean(),
+      uiType: zod.string(),
+    }),
+  ),
+  validationUrl: zod.string().optional(),
+  customConfigurationPageUrl: zod.string().optional(),
+  customConfigurationPagePreviewUrl: zod.string().optional(),
 })
 
 /**
  * Extension specification with all properties and methods needed to load a UI extension.
  */
-const themeSpecification = createUIExtensionSpecification({
+const flowActionDefinitionSpecification = createUIExtensionSpecification({
   identifier: 'flow_action_definition',
   schema: FlowExtensionSchema,
   supportedFlavors: [],
   singleEntryPath: false,
   deployConfig: async (config, _) => {
+    // TODO: we'll need to figure out a solution for how we manage generating UUIDs for fields
+    // we can either ask the partner to provide them, or we can generate them for them but we'll need to update the config file
     return {
       title: config.title,
       description: config.description,
       url: config.url,
       fields: config.fields,
+      validation_url: config.validationUrl,
+      custom_configuration_page_url: config.customConfigurationPageUrl,
+      custom_configuration_page_preview_url: config.customConfigurationPagePreviewUrl,
     }
   },
 })
 
-export default themeSpecification
+export default flowActionDefinitionSpecification
