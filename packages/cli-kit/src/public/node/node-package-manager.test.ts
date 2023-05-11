7fe64f38a8b9508d82fe1bd5bef3989727b6dffe
Gonzalo Riestra <gonzaloriestra@gmail.com>
2023-05-11 17:13:22 +0200

    Revert "Install pos_ui_extension node dependencies as exact versions (main)"

--- packages/cli-kit/src/public/node/node-package-manager.test.ts
+++ packages/cli-kit/src/public/node/node-package-manager.test.ts
@@ -380,30 +380,6 @@ describe('addNPMDependenciesIfNeeded', () => {
     })
   })
 
-  test("runs the right command when it's npm and exact versions", async () => {
-    await inTemporaryDirectory(async (tmpDir) => {
-      // Given
-      const packageJsonPath = joinPath(tmpDir, 'package.json')
-      const packageJson = {
-        dependencies: {existing: '1.2.3'},
-      }
-      await writeFile(packageJsonPath, JSON.stringify(packageJson))
-
-      // When
-      await addNPMDependenciesIfNeeded([{name: 'new', version: 'version'}], {
-        type: 'prod',
-        packageManager: 'npm',
-        directory: tmpDir,
-        exact: true,
-      })
-
-      // Then
-      expect(mockedExec).toHaveBeenCalledWith('npm', ['install', 'new@version', '--save-prod', '--save-exact'], {
-        cwd: tmpDir,
-      })
-    })
-  })
-
   test("runs the right command when it's yarn and dev dependencies", async () => {
     await inTemporaryDirectory(async (tmpDir) => {
       // Given
@@ -473,30 +449,6 @@ describe('addNPMDependenciesIfNeeded', () => {
     })
   })
 
-  test("runs the right command when it's yarn and exact versions", async () => {
-    await inTemporaryDirectory(async (tmpDir) => {
-      // Given
-      const packageJsonPath = joinPath(tmpDir, 'package.json')
-      const packageJson = {
-        dependencies: {existing: '1.2.3'},
-      }
-      await writeFile(packageJsonPath, JSON.stringify(packageJson))
-
-      // When
-      await addNPMDependenciesIfNeeded([{name: 'new', version: 'version'}], {
-        type: 'prod',
-        packageManager: 'yarn',
-        directory: tmpDir,
-        exact: true,
-      })
-
-      // Then
-      expect(mockedExec).toHaveBeenCalledWith('yarn', ['add', 'new@version', '--prod', '--exact'], {
-        cwd: tmpDir,
-      })
-    })
-  })
-
   test("runs the right command when it's pnpm and dev dependencies", async () => {
     await inTemporaryDirectory(async (tmpDir) => {
       // Given
@@ -561,30 +513,6 @@ describe('addNPMDependenciesIfNeeded', () => {
 
       // Then
       expect(mockedExec).toHaveBeenCalledWith('pnpm', ['add', 'new@version', '--save-peer'], {
-        cwd: tmpDir,
-      })
-    })
-  })
-
-  test("runs the right command when it's pnpm and exact versions", async () => {
-    await inTemporaryDirectory(async (tmpDir) => {
-      // Given
-      const packageJsonPath = joinPath(tmpDir, 'package.json')
-      const packageJson = {
-        dependencies: {existing: '1.2.3'},
-      }
-      await writeFile(packageJsonPath, JSON.stringify(packageJson))
-
-      // When
-      await addNPMDependenciesIfNeeded([{name: 'new', version: 'version'}], {
-        type: 'prod',
-        packageManager: 'pnpm',
-        directory: tmpDir,
-        exact: true,
-      })
-
-      // Then
-      expect(mockedExec).toHaveBeenCalledWith('pnpm', ['add', 'new@version', '--save-prod', '--save-exact'], {
         cwd: tmpDir,
       })
     })
--- packages/cli-kit/src/public/node/node-package-manager.ts
+++ packages/cli-kit/src/public/node/node-package-manager.ts
@@ -327,9 +327,6 @@ interface AddNPMDependenciesIfNeededOptions {
 
   /** Abort signal to stop the process */
   signal?: AbortSignal
-
-  /** Whether or not to install exact versions of the dependencies */
-  exact?: boolean
 }
 
 /**
@@ -408,20 +405,14 @@ export async function addNPMDependencies(
       // makes the task easier and npm can then proceed.
       for (const dep of dependenciesWithVersion) {
         // eslint-disable-next-line no-await-in-loop
-        await installDependencies(options, argumentsToAddDependenciesWithNPM([dep], options.type, options.exact))
+        await installDependencies(options, argumentsToAddDependenciesWithNPM([dep], options.type))
       }
       break
     case 'yarn':
-      await installDependencies(
-        options,
-        argumentsToAddDependenciesWithYarn(dependenciesWithVersion, options.type, options.exact),
-      )
+      await installDependencies(options, argumentsToAddDependenciesWithYarn(dependenciesWithVersion, options.type))
       break
     case 'pnpm':
-      await installDependencies(
-        options,
-        argumentsToAddDependenciesWithPNPM(dependenciesWithVersion, options.type, options.exact),
-      )
+      await installDependencies(options, argumentsToAddDependenciesWithPNPM(dependenciesWithVersion, options.type))
       break
   }
 }
@@ -453,7 +444,7 @@ export async function addNPMDependenciesWithoutVersionIfNeeded(
  * @param type - The dependency type.
  * @returns An array with the arguments.
  */
-function argumentsToAddDependenciesWithNPM(dependencies: string[], type: DependencyType, exact = false): string[] {
+function argumentsToAddDependenciesWithNPM(dependencies: string[], type: DependencyType): string[] {
   let command = ['install']
   command = command.concat(dependencies)
   switch (type) {
@@ -467,7 +458,6 @@ function argumentsToAddDependenciesWithNPM(dependencies: string[], type: Depende
       command.push('--save-prod')
       break
   }
-  if (exact) command.push('--save-exact')
   return command
 }
 
@@ -477,7 +467,7 @@ function argumentsToAddDependenciesWithNPM(dependencies: string[], type: Depende
  * @param type - The dependency type.
  * @returns An array with the arguments.
  */
-function argumentsToAddDependenciesWithYarn(dependencies: string[], type: DependencyType, exact = false): string[] {
+function argumentsToAddDependenciesWithYarn(dependencies: string[], type: DependencyType): string[] {
   let command = ['add']
   command = command.concat(dependencies)
   switch (type) {
@@ -491,7 +481,6 @@ function argumentsToAddDependenciesWithYarn(dependencies: string[], type: Depend
       command.push('--prod')
       break
   }
-  if (exact) command.push('--exact')
   return command
 }
 
@@ -501,7 +490,7 @@ function argumentsToAddDependenciesWithYarn(dependencies: string[], type: Depend
  * @param type - The dependency type.
  * @returns An array with the arguments.
  */
-function argumentsToAddDependenciesWithPNPM(dependencies: string[], type: DependencyType, exact = false): string[] {
+function argumentsToAddDependenciesWithPNPM(dependencies: string[], type: DependencyType): string[] {
   let command = ['add']
   command = command.concat(dependencies)
   switch (type) {
@@ -515,7 +504,6 @@ function argumentsToAddDependenciesWithPNPM(dependencies: string[], type: Depend
       command.push('--save-prod')
       break
   }
-  if (exact) command.push('--save-exact')
   return command
 }
 
