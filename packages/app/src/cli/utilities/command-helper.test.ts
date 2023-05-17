29c8b9f62fc5957c81f04fb0bc526b5dd446d42f
Shaun Stanworth <shaun.stanworth@shopify.com>
2023-05-17 14:12:41 +0100

    Add command factory function

---
+++ packages/app/src/cli/utilities/command-helper.test.ts
@@ -0,0 +1,48 @@
+import {askFor, command} from './command-helpers.js'
+import {test, expect, describe, vi, beforeEach} from 'vitest'
+import {Flags} from '@oclif/core'
+import * as loader from '../models/app/loader.js'
+
+vi.mock('../models/app/loader.js')
+
+describe('command factory', () => {
+  beforeEach(() => {})
+  test('does the thing', async () => {
+    vi.mocked(loader.load).mockReturnValue({app: true} as any)
+
+    // Given
+    const serviceFn = vi.fn()
+    const CommandClass = command(
+      'test',
+      {
+        notProvidedSoUsesPrompt: Flags.string(),
+        withDefault: Flags.string({default: 'has default'}),
+        mandatory: Flags.string({required: true}),
+        providedAsFlag: Flags.string(),
+      },
+      askFor([
+        {key: 'providedViaPrompt', ask: () => Promise.resolve(123)},
+        {key: 'notProvidedSoUsesPrompt', ask: () => Promise.resolve('from prompt')},
+        {key: 'providedAsFlag', ask: () => Promise.resolve("shouldn't happen")},
+      ] as const),
+      serviceFn,
+    )
+
+    // When
+    const commandInstance = new CommandClass(['--mandatory', 'provided', '--providedAsFlag', 'from flag'], {} as any)
+    await commandInstance.run()
+
+    // Expect
+    expect(serviceFn).toBeCalledWith(
+      {app: true},
+      {
+        providedViaPrompt: 123,
+        notProvidedSoUsesPrompt: 'from prompt',
+        path: expect.any(String),
+        withDefault: 'has default',
+        mandatory: 'provided',
+        providedAsFlag: 'from flag',
+      },
+    )
+  })
+})
---
+++ packages/app/src/cli/utilities/command-helpers.ts
@@ -0,0 +1,110 @@
+import {appFlags} from '../flags.js'
+import type {AppInterface} from '../models/app/app.js'
+import {load as loadApp} from '../models/app/loader.js'
+import Command from './app-command.js'
+import type {Config} from '@oclif/core'
+import {resolvePath} from '@shopify/cli-kit/node/path'
+import {globalFlags} from '@shopify/cli-kit/node/cli'
+import type {FlagOutput, ParserOutput, FlagInput, ArgOutput} from '@oclif/core/lib/interfaces/parser.js'
+
+interface BuiltInFlags {
+  preset?: string
+  verbose: boolean
+  path: string
+  environment?: string
+}
+
+/**
+ * Given something like `[{key: 'foo', ask: () => Promise.resolve('abc')}, {key: 'bar', ask: () => Promise.resolve(1)}]`, produces `{foo: string, bar: number}`
+ */
+type PromptListAsObject<T extends ReadonlyArray<{key: string; ask: () => Promise<unknown>}>> = {
+  [K in T[number] as K['key']]: Awaited<ReturnType<K['ask']>>
+}
+
+/**
+ * A function that converts post-parsing flags from one structure to another
+ *
+ * @typeParam TIn - Shape of input flags, post-parsing
+ * @typeParam TOut - Shape of output flags
+ */
+type ParsedFlagProcessor<TIn extends FlagOutput, TOut extends FlagOutput> = (
+  flags: ParserOutput<TIn, ArgOutput>['flags'],
+) => Promise<ParserOutput<TOut, ArgOutput>['flags']>
+
+/**
+ * A flag processor that extends the input flags, based on a list of prompt functions
+ */
+type PromptBasedFlagProcessor<
+  TIn extends FlagOutput,
+  TPromptList extends ReadonlyArray<{key: string; ask: () => Promise<unknown>}>,
+> = ParsedFlagProcessor<TIn, PromptListAsObject<TPromptList> & TIn>
+
+/**
+ * Takes a collection of parsed flags, applies the prompts for values not given via a flag, and returns the combined data set
+ *
+ * @param options List of prompts to use for anything not provided via a flag. It's best to give this with `as const`
+ * @returns Parsed flag outputs, with prompts applied if necessary
+ */
+export function askFor<
+  TIn extends FlagOutput,
+  TPromptList extends ReadonlyArray<{key: string; ask: () => Promise<unknown>}>,
+  TExtension = PromptListAsObject<TPromptList>,
+>(options: TPromptList): PromptBasedFlagProcessor<TIn, TPromptList> {
+  return async (flags) => {
+    const answers = {} as TExtension
+    for (const {key, ask} of options) {
+      if (flags[key] === undefined || flags[key] === null) {
+        // eslint-disable-next-line no-await-in-loop
+        const answer = (await ask()) as TExtension[keyof TExtension]
+        answers[key as keyof TExtension] = answer
+      }
+    }
+    const res: ParserOutput<TIn & TExtension, ArgOutput>['flags'] = {
+      ...flags,
+      ...answers,
+    }
+    return res
+  }
+}
+
+type ConcreteCommand = new (argv: string[], config: Config) => Command
+
+/**
+ * Creates a command class, using our preferred patterns
+ *
+ * @param description Description for the command
+ * @param flags Command flags
+ * @param applyPrompts Function that takes the flags, post-parsing, and applies extra values, usually via prompts
+ * @param runService Service function that does the work of the command
+ * @returns Whatever the service function returns
+ */
+export function command<TFlags extends FlagOutput, TServiceOptions extends TFlags = TFlags>(
+  description: string,
+  flags: FlagInput<TFlags>,
+  applyPrompts: ParsedFlagProcessor<TFlags & BuiltInFlags, TServiceOptions & BuiltInFlags>,
+  runService: (app: AppInterface, options: TServiceOptions & BuiltInFlags) => Promise<unknown>,
+): ConcreteCommand {
+  const builtInFlagInput: FlagInput<BuiltInFlags> = {
+    ...globalFlags,
+    ...appFlags,
+  }
+  class CustomCommand extends Command {
+    static description = description
+    static flags = {
+      ...builtInFlagInput,
+      ...flags,
+    } as FlagInput<TFlags & BuiltInFlags>
+
+    public async run(): Promise<unknown> {
+      const {flags} = await this.parse<TFlags & BuiltInFlags, FlagOutput, ArgOutput>(CustomCommand)
+      const directory = flags.path ? resolvePath(flags.path) : process.cwd()
+
+      const app: AppInterface = await loadApp({directory, specifications: []})
+
+      const finalOptions = await applyPrompts(flags)
+
+      return await runService(app, finalOptions)
+    }
+  }
+  return CustomCommand
+}
