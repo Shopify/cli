8ddb198a72f0122d334b086cbcf53f568627e3a1
Isaac Roldán <isaac.roldan@shopify.com>
2023-05-18 12:07:13 +0200

    Fix flatten

--- packages/app/src/cli/private/plugins/extension.ts
+++ packages/app/src/cli/private/plugins/extension.ts
@@ -3,9 +3,10 @@ import {HookReturnPerExtensionPlugin} from '../../public/plugins/extension.js'
 import {Config} from '@oclif/core'
 import {getArrayRejectingUndefined} from '@shopify/cli-kit/common/array'
 import {fanoutHooks} from '@shopify/cli-kit/node/plugins'
+import flatten from 'lodash-es/flatten.js'
 
 export async function loadUIExtensionSpecificiationsFromPlugins(config: Config): Promise<ExtensionSpecification[]> {
   const hooks = await fanoutHooks<HookReturnPerExtensionPlugin, 'extension_specs'>(config, 'extension_specs', {})
-  const specs = getArrayRejectingUndefined(Object.values(hooks)).flat()
+  const specs = flatten(getArrayRejectingUndefined(Object.values(hooks)))
   return specs
 }
