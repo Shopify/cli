386d825ff586e1ffe558caf3a7809103313379c9
Isaac Roldán <isaac.roldan@shopify.com>
2023-05-23 12:27:44 +0200

    Use inherit when running javy

--- packages/app/src/cli/services/function/build.ts
+++ packages/app/src/cli/services/function/build.ts
@@ -109,8 +109,8 @@ function getESBuildOptions(directory: string, entryPoint: string, userFunction: 
 export async function runJavy(fun: FunctionExtension, options: JSFunctionBuildOptions) {
   return exec('npm', ['exec', '--', 'javy', 'compile', '-d', '-o', fun.buildWasmPath, 'dist/function.js'], {
     cwd: fun.directory,
-    stdout: options.stdout,
-    stderr: options.stderr,
+    stdout: 'inherit',
+    stderr: 'inherit',
     signal: options.signal,
   })
 }
