ef2ab878fee3af031bb82462b728aee0d1efd1fa
Madhav Makkena <madhav.makkena@shopify.com>
2023-05-30 13:31:05 -0400

    updated app generate schema to output to either to schema.graphql or stdout based on --stdout flag

--- packages/app/src/cli/commands/app/function/schema.ts
+++ packages/app/src/cli/commands/app/function/schema.ts
@@ -4,6 +4,8 @@ import {Flags} from '@oclif/core'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
 import Command from '@shopify/cli-kit/node/base-command'
 import {outputInfo} from '@shopify/cli-kit/node/output'
+import {writeFile} from '@shopify/cli-kit/node/fs'
+import {joinPath} from '@shopify/cli-kit/node/path'
 
 export default class FetchSchema extends Command {
   static description = 'Fetch the latest GraphQL schema for a Function.'
@@ -17,12 +19,23 @@ export default class FetchSchema extends Command {
       required: false,
       env: 'SHOPIFY_FLAG_APP_API_KEY',
     }),
+    stdout: Flags.boolean({
+      description: 'Output the schema to stdout instead of writing to a file.',
+      required: false,
+      default: false,
+      env: 'SHOPIFY_FLAG_STDOUT',
+    }),
   }
 
   public async run(): Promise<void> {
     const {flags, args} = await this.parse(FetchSchema)
     await inFunctionContext(this.config, flags.path, async (app, ourFunction) => {
-      outputInfo(await generateSchemaService({app, extension: ourFunction, apiKey: flags['api-key']}))
+      const outputSchema = await generateSchemaService({app, extension: ourFunction, apiKey: flags['api-key']})
+      if (flags.stdout) {
+        outputInfo(outputSchema)
+      } else {
+        await writeFile(joinPath(flags.path, 'schema.graphql'), outputSchema)
+      }
     })
   }
 }
