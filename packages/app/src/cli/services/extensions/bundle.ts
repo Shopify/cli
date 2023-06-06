8fbecf024756c7226420bfdca2c281492c2e47e9
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-06-06 11:23:58 -0700

    Add unit tests

--- packages/app/src/cli/services/extensions/bundle.ts
+++ packages/app/src/cli/services/extensions/bundle.ts
@@ -80,7 +80,14 @@ export async function bundleThemeExtension(
 
   await Promise.all(
     files.map(function (filepath) {
-      if (!(filepath.includes('.gitkeep') || filepath.includes('.toml') || filepath.includes('.DS_Store') || filepath.includes('.shopifyignore'))) {
+      if (
+        !(
+          filepath.includes('.gitkeep') ||
+          filepath.includes('.toml') ||
+          filepath.includes('.DS_Store') ||
+          filepath.includes('.shopifyignore')
+        )
+      ) {
         const relativePathName = relativePath(extension.directory, filepath)
         const outputFile = joinPath(extension.outputPath, relativePathName)
         return copyFile(filepath, outputFile)
