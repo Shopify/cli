d6fa7a69e6cd2136f8ff7e260d5da2ffe2f7ffb3
Ryan Bahan <ryan.bahan@shopify.com>
2023-06-12 15:19:37 -0600

    scaffold service

--- packages/app/src/cli/commands/app/config/push.ts
+++ packages/app/src/cli/commands/app/config/push.ts
@@ -1,6 +1,8 @@
 import {appFlags} from '../../../flags.js'
 import Command from '../../../utilities/app-command.js'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
+import {pushConfig} from '../../../services/app/config/push.js'
+import {Flags} from '@oclif/core'
 
 export default class ConfigPush extends Command {
   static hidden = true
@@ -10,9 +12,16 @@ export default class ConfigPush extends Command {
   static flags = {
     ...globalFlags,
     ...appFlags,
+    'api-key': Flags.string({
+        hidden: false,
+        description: 'The API key of your app.',
+        env: 'SHOPIFY_FLAG_APP_API_KEY',
+      }),
   }
 
   public async run(): Promise<void> {
     const {flags} = await this.parse(ConfigPush)
+
+    await pushConfig({apiKey: flags['api-key']})
   }
 }
---
+++ packages/app/src/cli/services/app/config/push.ts
@@ -0,0 +1,33 @@
+import {gql} from 'graphql-request'
+import {ensureAuthenticatedPartners} from '@shopify/cli-kit/node/session'
+import {selectApp} from '../select-app.js'
+import {partnersRequest} from '@shopify/cli-kit/node/api/partners'
+import {AbortError} from '@shopify/cli-kit/node/error'
+
+export const AppUpdateQuery = gql`
+  mutation appUpdate($apiKey: String!) {
+    appUpdate(input: {apiKey: $apiKey}) {
+      userErrors {
+        message
+        field
+      }
+    }
+  }
+`
+
+export async function pushConfig(options: any) {
+  const token = await ensureAuthenticatedPartners()
+  const apiKey = options.apiKey || (await selectApp()).apiKey
+  const query = AppUpdateQuery
+
+  const variables = {apiKey}
+
+  const result: any = await partnersRequest(query, token, variables)
+
+  console.log({result})
+
+  if (result.appUpdate.userErrors.length > 0) {
+    const errors = result.appUpdate.userErrors.map((error: any) => error.message).join(', ')
+    throw new AbortError(errors)
+  }
+}
