b3e4143441b72857060491096f726b181e2ca974
Isaac Roldán <isaac.roldan@shopify.com>
2023-06-29 10:25:50 +0200

    Move files to flow folder

--- packages/app/src/cli/services/flow/fetch.ts
+++ packages/app/src/cli/services/flow/fetch.ts
@@ -4,9 +4,9 @@ export async function getActiveDashboardExtensions({token, apiKey}: {token: stri
   const initialRemoteExtensions = await fetchAppExtensionRegistrations({token, apiKey})
   const {dashboardManagedExtensionRegistrations} = initialRemoteExtensions.app
   return dashboardManagedExtensionRegistrations.filter((ext) => {
-    const isFLow = ext.type === 'flow_action_definition' || ext.type === 'flow_trigger_definition'
+    const isFlow = ext.type === 'flow_action_definition' || ext.type === 'flow_trigger_definition'
     const hasActiveVersion = ext.activeVersion && ext.activeVersion.config
     const hasDraftVersion = ext.draftVersion && ext.draftVersion.config
-    return isFLow && (hasActiveVersion || hasDraftVersion)
+    return isFlow && (hasActiveVersion || hasDraftVersion)
   })
 }
