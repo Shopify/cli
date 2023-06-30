4b052c4917620fc7e39604b80f69ca1a30763084
Mike Ragalie <mike.ragalie@shopify.com>
2023-06-30 15:58:46 -0400

    Add none template

--- packages/create-app/src/prompts/init.test.ts
+++ packages/create-app/src/prompts/init.test.ts
@@ -1,6 +1,6 @@
 import init from './init.js'
 import {describe, expect, vi, test} from 'vitest'
-import {renderTextPrompt} from '@shopify/cli-kit/node/ui'
+import {renderSelectPrompt, renderTextPrompt} from '@shopify/cli-kit/node/ui'
 
 vi.mock('@shopify/cli-kit/node/ui')
 
@@ -38,5 +38,33 @@ describe('init', () => {
     // Then
     expect(renderTextPrompt).not.toHaveBeenCalled()
     expect(got).toEqual({...options, ...answers, templateType: 'custom'})
+  })
+
+  test('it renders the label for the template options', async () => {
+    const answers = {
+      name: 'app',
+      template: 'https://github.com/Shopify/shopify-app-template-none',
+    }
+    const options = {directory: '/'}
+
+    // Given
+    vi.mocked(renderTextPrompt).mockResolvedValueOnce(answers.name)
+    vi.mocked(renderSelectPrompt).mockResolvedValueOnce('none')
+
+    // When
+    const got = await init(options)
+
+    // Then
+    expect(renderSelectPrompt).toHaveBeenCalledWith({
+      choices: [
+        {label: 'node', value: 'node'},
+        {label: 'php', value: 'php'},
+        {label: 'ruby', value: 'ruby'},
+        {label: 'none (build an app with extensions only)', value: 'none'},
+      ],
+      message: 'Which template would you like to use?',
+      defaultValue: 'node',
+    })
+    expect(got).toEqual({...options, ...answers, templateType: 'none'})
   })
 })
