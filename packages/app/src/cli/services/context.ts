6c9ae21ab979fb1997d21a6fa2b4b9d198617bb6
Ryan DJ Lee <ryan.d.lee@shopify.com>
2023-07-11 12:38:05 -0400

    Set scopes when creating new launchable apps

--- packages/app/src/cli/services/context.ts
+++ packages/app/src/cli/services/context.ts
@@ -462,7 +462,8 @@ export async function fetchOrCreateOrganizationApp(app: AppInterface, token: str
   const orgId = await selectOrg(token)
   const {organization, apps} = await fetchOrgsAppsAndStores(orgId, token)
   const isLaunchable = appIsLaunchable(app)
-  const partnersApp = await selectOrCreateApp(app.name, apps, organization, token, isLaunchable)
+  const scopes = app.configuration.scopes
+  const partnersApp = await selectOrCreateApp(app.name, apps, organization, token, isLaunchable, scopes)
   return partnersApp
 }
 
