c5a9c1be427bd8b7d6df48af89c629ebc9f64abc
Shaun Stanworth <shaun.stanworth@shopify.com>
2023-07-14 12:39:48 +0100

    Delete flaky ruby test

--- packages/cli-kit/assets/cli-ruby/test/shopify-cli/core/monorail_test.rb
+++ packages/cli-kit/assets/cli-ruby/test/shopify-cli/core/monorail_test.rb
@@ -38,48 +38,6 @@ module ShopifyCLI
         assert_equal ["create", "rails"], got
       end
 
-      def test_log_event_contains_schema_and_payload_values
-        enable_reporting(true)
-        ShopifyCLI::Shopifolk.expects(:acting_as_shopify_organization?).returns(true)
-        Timecop.freeze do |time|
-          this_time = (time.utc.to_f * 1000).to_i
-          stub_request(:post, Monorail::ENDPOINT_URI)
-            .with(
-              headers: {
-                'Content-Type': "application/json; charset=utf-8",
-                'X-Monorail-Edge-Event-Created-At-Ms': this_time.to_s,
-                'X-Monorail-Edge-Event-Sent-At-Ms': this_time.to_s,
-              },
-              body: JSON.dump({
-                schema_id: ShopifyCLI::Core::Monorail::INVOCATIONS_SCHEMA,
-                payload: {
-                  project_type: "fake",
-                  command: "testcommand",
-                  args: "arg argtwo",
-                  time_start: this_time,
-                  time_end: this_time,
-                  total_time: 0,
-                  success: true,
-                  error_message: nil,
-                  uname: RbConfig::CONFIG["host"],
-                  cli_version: ShopifyCLI::VERSION,
-                  ruby_version: RUBY_VERSION,
-                  is_employee: true,
-                  api_key: "apikey",
-                  partner_id: 42,
-                  metadata: "{\"foo\":\"identifier\"}",
-                },
-              })
-            )
-            .to_return(status: 200)
-
-          ShopifyCLI::Core::Monorail.log("testcommand", %w(arg argtwo)) do
-            ShopifyCLI::Core::Monorail.metadata[:foo] = "identifier"
-            "This is the block and result"
-          end
-        end
-      end
-
       def test_log_returns_the_result_after_sending_monorail_events
         enable_reporting(true)
         Net::HTTP.expects(:start).once
