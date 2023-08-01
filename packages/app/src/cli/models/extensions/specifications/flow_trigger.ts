0cfe76ad4a4a69782ee6519c8613e377a14049e9
Elias Hawa <elias.hawa@shopify.com>
2023-08-01 19:56:43 +0000

    Get schema patch data for triggers

--- packages/app/src/cli/models/extensions/specifications/flow_trigger.ts
+++ packages/app/src/cli/models/extensions/specifications/flow_trigger.ts
@@ -2,6 +2,8 @@ import {BaseSchemaWithHandle, FieldSchema} from '../schemas.js'
 import {createExtensionSpecification} from '../specification.js'
 import {validateFieldShape} from '../../../services/flow/validation.js'
 import {serializeFields} from '../../../services/flow/serialize-fields.js'
+import {joinPath} from '@shopify/cli-kit/node/path'
+import {glob, readFile} from '@shopify/cli-kit/node/fs'
 import {zod} from '@shopify/cli-kit/node/schema'
 
 const FlowTriggerSettingsSchema = FieldSchema.extend({
@@ -15,6 +17,7 @@ const FlowTriggerSettingsSchema = FieldSchema.extend({
 
 export const FlowTriggerExtensionSchema = BaseSchemaWithHandle.extend({
   type: zod.literal('flow_trigger'),
+  schema: zod.string().optional(),
   settings: zod
     .object({
       fields: zod.array(FlowTriggerSettingsSchema).optional(),
@@ -29,6 +32,25 @@ export const FlowTriggerExtensionSchema = BaseSchemaWithHandle.extend({
 })
 
 /**
+ * Loads the schema from the partner defined file.
+ */
+const loadSchemaPatchFromPath = async (extensionPath: string, patchPath: string | undefined) => {
+  if (!patchPath) {
+    return ''
+  }
+
+  const path = await glob(joinPath(extensionPath, patchPath))
+
+  if (path.length > 1) {
+    throw new Error('Multiple files found for schema patch path')
+  } else if (path.length === 0) {
+    throw new Error('No file found for schema patch path')
+  }
+
+  return readFile(path[0] as string)
+}
+
+/**
  * Extension specification with all properties and methods needed to load a Flow Trigger.
  */
 const flowTriggerSpecification = createExtensionSpecification({
@@ -39,12 +61,13 @@ const flowTriggerSpecification = createExtensionSpecification({
   // https://github.com/Shopify/cli/blob/73ac91c0f40be0a57d1b18cb34254b12d3a071af/packages/app/src/cli/services/deploy.ts#L107
   // Should be removed after unified deployment is 100% rolled out
   appModuleFeatures: (_) => ['bundling'],
-  deployConfig: async (config) => {
+  deployConfig: async (config, extensionPath) => {
     return {
       title: config.name,
       handle: config.handle,
       description: config.description,
       fields: serializeFields('flow_trigger', config.settings?.fields),
+      schema_patch: await loadSchemaPatchFromPath(extensionPath, config.schema),
     }
   },
 })
