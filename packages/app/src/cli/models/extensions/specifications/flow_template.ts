a3039128c786b8c0f98a553e73ef2c938a16341e
Tomas Baena <tomas.baena@shopify.com>
2023-08-02 20:38:00 +0000

    Update schema and spec to support multiple templates per ext in the future

--- packages/app/src/cli/models/extensions/specifications/flow_template.ts
+++ packages/app/src/cli/models/extensions/specifications/flow_template.ts
@@ -9,12 +9,22 @@ import fs from 'fs'
 
 const FlowTemplateExtensionSchema = BaseSchemaWithHandle.extend({
   type: zod.literal('flow_template'),
-  title: zod.string(),
-  description: zod.string(),
-  categories: zod.array(zod.string()),
-  require_app: zod.boolean(),
-  visible: zod.boolean(),
-  enabled: zod.boolean(),
+  name: zod.string(),
+  templates: zod
+    .array(
+      zod.object({
+        key: zod.string(),
+        name: zod.string(),
+        description: zod.string(),
+        categories: zod.array(zod.string()),
+        require_app: zod.boolean(),
+        listed: zod.boolean(),
+        enabled: zod.boolean(),
+      }),
+    )
+    // technically we'll be able to support more than one template per extension but restricting to one for now
+    .min(1)
+    .max(1),
 })
 
 const spec = createExtensionSpecification({
@@ -24,36 +34,31 @@ const spec = createExtensionSpecification({
   appModuleFeatures: (_) => ['bundling'],
   deployConfig: async (config, extensionPath) => {
     return {
-      title: config.title,
-      handle: config.handle,
-      description: config.description,
-      uuid: config.handle,
-      categories: config.categories,
-      require_app: config.require_app,
-      visible: config.visible,
-      enabled: config.enabled,
-      localization: await loadLocalesConfig(extensionPath, 'flow_template'),
-      definition: await loadWorkflow(extensionPath, config.handle),
+      templates: await Promise.all(
+        config.templates.map(async (template) => {
+          return {
+            key: template.key,
+            name: template.name,
+            description: template.description,
+            categories: template.categories,
+            require_app: template.require_app,
+            listed: template.listed,
+            enabled: template.enabled,
+            definition: await loadWorkflow(extensionPath, template.key),
+            localization: await loadLocalesConfig(joinPath(extensionPath, template.key), template.key),
+          }
+        }),
+      ),
     }
   },
 })
 
-async function loadWorkflow(path: string, handle: string) {
-  const flowFilePaths = await glob(joinPath(path, '/template.flow'))
+async function loadWorkflow(path: string, key: string) {
+  const flowFilePaths = await glob(joinPath(path, key, `${key}.flow`))
   const flowFilePath = flowFilePaths[0]
-
   if (!flowFilePath) {
-    throw new AbortError(
-      `Missing .flow file in ${path} `,
-      'Make sure you have built and exported a flow file from a Shop.',
-    )
-  } else if (flowFilePaths.length > 1) {
-    throw new AbortError(
-      `More than one .flow file found in ${path} `,
-      'Make sure you have only one .flow file in your extension folder.',
-    )
+    throw new AbortError(`Missing ${key}.flow file in ${joinPath(path, key)}`)
   }
-
   return fs.readFileSync(flowFilePath, 'base64')
 }
 
