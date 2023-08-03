465369a1bb30a38b70845fbef5ad7a20367a814e
Isaac Roldán <isaac.roldan@shopify.com>
2023-08-03 12:38:21 +0200

    Add tests

--- packages/cli/src/cli/services/upgrade.ts
+++ packages/cli/src/cli/services/upgrade.ts
@@ -79,7 +79,7 @@ async function upgradeLocalShopify(projectDir: string, currentVersion: string): 
 
   await installJsonDependencies('prod', packageJsonDependencies, projectDir)
   await installJsonDependencies('dev', packageJsonDevDependencies, projectDir)
-  return newestCLIVersion
+  return newestCLIVersion ?? newestAppVersion
 }
 
 async function upgradeGlobalShopify(
