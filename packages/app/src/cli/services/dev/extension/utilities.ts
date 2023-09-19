507b7d2226e1ebffe93eb0d7eae933a97289505c
JR <jr.rafols@shopify.com>
2023-09-19 09:43:41 -0600

    Refactor

--- packages/app/src/cli/services/dev/extension/utilities.ts
+++ packages/app/src/cli/services/dev/extension/utilities.ts
@@ -2,6 +2,13 @@ import {ExtensionInstance} from '../../../models/extensions/extension-instance.j
 
 import {fetchProductVariant} from '../../../utilities/extensions/fetch-product-variant.js'
 
+const CUSTOMER_ACCOUNT_CHECKOUT_RENDERED_TARGETS = new Set([
+  'customer-account.order-status.cart-line-item.render-after',
+  'customer-account.order-status.cart-line-list.render-after',
+  'customer-account.order-status.customer-information.render-after',
+  'customer-account.order-status.block.render',
+])
+
 /**
  * To prepare UI Extensions targeting Checkout for dev'ing we need to retrieve a valid product variant ID
  * @param extensions - The UI Extensions to dev
@@ -40,7 +47,7 @@ export function getExtensionPointTargetSurface(extensionPointTarget: string) {
     // Covers Customer Accounts UI extensions (future)
     case 'customeraccount':
     case 'customer-account': {
-      if (isCheckoutRenderedCustomerAccountTarget(extensionPointTarget)) {
+      if (CUSTOMER_ACCOUNT_CHECKOUT_RENDERED_TARGETS.has(extensionPointTarget)) {
         return 'checkout'
       }
 
@@ -56,15 +63,4 @@ export function getExtensionPointTargetSurface(extensionPointTarget: string) {
       // Covers Admin UI extensions
       return domain
   }
-}
-
-function isCheckoutRenderedCustomerAccountTarget(extensionPointTarget: string) {
-  const customerAccountCheckoutRenderedTargets = new Set([
-    'customer-account.order-status.cart-line-item.render-after',
-    'customer-account.order-status.cart-line-list.render-after',
-    'customer-account.order-status.customer-information.render-after',
-    'customer-account.order-status.block.render',
-  ])
-
-  return customerAccountCheckoutRenderedTargets.has(extensionPointTarget)
 }
