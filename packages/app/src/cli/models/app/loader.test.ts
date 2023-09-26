49d5f25a8117f63115663ab6aa48e9e7caaa8bba
Bill Fienberg <bill.fienberg@shopify.com>
2023-09-26 13:32:02 -0500

    chore: add missing word to test description

--- packages/app/src/cli/models/app/loader.test.ts
+++ packages/app/src/cli/models/app/loader.test.ts
@@ -1683,7 +1683,7 @@ automatically_update_urls_on_dev = true
     }
   })
 
-  test('does not the app with an app access module when the app configuration does not include app access', async () => {
+  test('does not load the app with an app access module when the app configuration does not include app access', async () => {
     // Given
     await writeConfig(linkedAppConfiguration)
 
