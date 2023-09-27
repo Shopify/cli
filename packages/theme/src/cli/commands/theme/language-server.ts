4cc298aa884cb2a7eb3b947aa533c1597e80b1e1
Albert Chu <albert.chu@shopify.com>
2023-09-27 11:53:46 -0600

    Wireup shopify theme language-server

--- packages/theme/src/cli/commands/theme/language-server.ts
+++ packages/theme/src/cli/commands/theme/language-server.ts
@@ -2,6 +2,7 @@ import ThemeCommand from '../../utilities/theme-command.js'
 import {themeDevPreviewFlag} from '../../flags.js'
 import {execCLI2} from '@shopify/cli-kit/node/ruby'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
+import {startServer} from '@shopify/theme-language-server-node'
 
 export default class LanguageServer extends ThemeCommand {
   static description = 'Start a Language Server Protocol server.'
@@ -12,6 +13,13 @@ export default class LanguageServer extends ThemeCommand {
   }
 
   async run(): Promise<void> {
+    const {flags} = await this.parse(LanguageServer)
+
+    if (flags['dev-preview']) {
+      startServer()
+      return
+    }
+
     await execCLI2(['theme', 'language-server'])
   }
 }
