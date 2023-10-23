412b8ca74226ce7771ded7ad76f77be934e6e586
Guilherme Carreiro <karreiro@gmail.com>
2023-10-23 10:24:26 +0200

    Clarify `SHOPIFY_CLI_BUNDLED_THEME_CLI` environment variable should not be used, yet resolve its compatibility issue

---
+++ .changeset/sour-toys-scream.md
@@ -0,0 +1,5 @@
+---
+'@shopify/theme': patch
+---
+
+Clarify `SHOPIFY_CLI_BUNDLED_THEME_CLI` environment variable should not be used, yet resolve its compatibility issue
--- packages/theme/src/cli/commands/theme/pull.test.ts
+++ packages/theme/src/cli/commands/theme/pull.test.ts
@@ -6,11 +6,15 @@ import {Config} from '@oclif/core'
 import {execCLI2} from '@shopify/cli-kit/node/ruby'
 import {ensureAuthenticatedThemes} from '@shopify/cli-kit/node/session'
 import {Theme} from '@shopify/cli-kit/node/themes/models/theme'
+import {useEmbeddedThemeCLI} from '@shopify/cli-kit/node/context/local'
+import {renderWarning} from '@shopify/cli-kit/node/ui'
 
 vi.mock('../../utilities/development-theme-manager.js')
 vi.mock('../../utilities/theme-store.js')
 vi.mock('@shopify/cli-kit/node/ruby')
 vi.mock('@shopify/cli-kit/node/session')
+vi.mock('@shopify/cli-kit/node/context/local')
+vi.mock('@shopify/cli-kit/node/ui')
 
 describe('Pull', () => {
   describe('run', () => {
@@ -18,6 +22,7 @@ describe('Pull', () => {
     const path = '/my-theme'
 
     async function run(argv: string[], theme?: Theme) {
+      vi.mocked(renderWarning).mockReturnValue('shhh!')
       vi.mocked(ensureThemeStore).mockReturnValue('example.myshopify.com')
       vi.mocked(ensureAuthenticatedThemes).mockResolvedValue(adminSession)
       if (theme) {
@@ -39,6 +44,8 @@ describe('Pull', () => {
     }
 
     test('should pass development theme from local storage to CLI 2', async () => {
+      vi.mocked(useEmbeddedThemeCLI).mockReturnValue(true)
+
       const theme = new Theme(1, 'Theme', 'development')
       await run([], theme)
 
@@ -48,6 +55,8 @@ describe('Pull', () => {
     })
 
     test('should pass theme and development theme from local storage to CLI 2', async () => {
+      vi.mocked(useEmbeddedThemeCLI).mockReturnValue(true)
+
       const themeId = 2
       const theme = new Theme(3, 'Theme', 'development')
       await run([`--theme=${themeId}`], theme)
@@ -55,7 +64,18 @@ describe('Pull', () => {
       expectCLI2ToHaveBeenCalledWith(`theme pull ${path} --theme ${themeId} --development-theme-id ${theme.id}`)
     })
 
+    test("should not pass development theme to CLI 2 when user isn't using the embedded CLI", async () => {
+      vi.mocked(useEmbeddedThemeCLI).mockReturnValue(false)
+
+      const themeId = 2
+      const theme = new Theme(3, 'Theme', 'development')
+      await run([`--theme=${themeId}`], theme)
+
+      expectCLI2ToHaveBeenCalledWith(`theme pull ${path} --theme ${themeId}`)
+    })
+
     test('should not pass development theme to CLI 2 if local storage is empty', async () => {
+      vi.mocked(useEmbeddedThemeCLI).mockReturnValue(true)
       await run([])
 
       expect(DevelopmentThemeManager.prototype.find).not.toHaveBeenCalled()
@@ -64,6 +84,7 @@ describe('Pull', () => {
     })
 
     test('should pass theme and development theme to CLI 2', async () => {
+      vi.mocked(useEmbeddedThemeCLI).mockReturnValue(true)
       const theme = new Theme(4, 'Theme', 'development')
       await run(['--development'], theme)
 
