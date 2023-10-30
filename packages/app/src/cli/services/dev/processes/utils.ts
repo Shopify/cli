ba5e3c4982843ec0fdebb6d5a04badb45f4f9b9e
Ryan Bahan <ryan.bahan@shopify.com>
2023-10-30 10:21:21 -0600

    properly type object flattener

--- packages/app/src/cli/services/dev/processes/utils.ts
+++ packages/app/src/cli/services/dev/processes/utils.ts
@@ -6,3 +6,30 @@ export function frontAndBackendConfig(webs: Web[]) {
   const backendConfig = webs.find((web) => isWebType(web, WebType.Backend))
   return {frontendConfig, backendConfig}
 }
+
+interface Obj {
+  [key: string]: {[key: string]: string} | string | undefined
+}
+
+export function flattenObject(obj: Obj) {
+  const result: Obj = {}
+
+  function recurse(current: Obj, path = '') {
+    for (const key in current) {
+      if (key) {
+        const value = current[key]
+        const newPath = path ? `${path}.${key}` : key
+
+        if (value && typeof value === 'object') {
+          recurse(value, newPath)
+        } else {
+          result[newPath] = value
+        }
+      }
+    }
+  }
+
+  recurse(obj)
+
+  return result
+}
