5c7e285037a582c8dbcdd21d69e448f33b875c38
Henry Zhou <henry.zhou@shopify.com>
2023-11-09 03:35:30 +0000

    WMP x CMP prototype

--- packages/app/src/cli/models/extensions/specifications/web_pixel_extension.ts
+++ packages/app/src/cli/models/extensions/specifications/web_pixel_extension.ts
@@ -10,10 +10,24 @@ const BUNDLE_SIZE_LIMIT = BUNDLE_SIZE_LIMIT_KB * kilobytes
 
 const dependency = '@shopify/web-pixels-extension'
 
+enum DataSaleConfig {
+  SellsData = 'sells_data',
+  DoNotSellData = 'do_not_sell_data',
+  SupportsLduDowngrade = 'supports_ldu_downgrade',
+}
+
 const WebPixelSchema = BaseSchema.extend({
   runtime_context: zod.string(),
   version: zod.string().optional(),
   configuration: zod.any(),
+  privacy_settings: zod
+    .object({
+      analytics: zod.boolean().optional(),
+      preferences: zod.boolean().optional(),
+      marketing: zod.boolean().optional(),
+      sale_of_data: zod.string().optional(),
+    })
+    .optional(),
   settings: zod.any(),
 })
 
@@ -26,6 +40,7 @@ const spec = createExtensionSpecification({
   deployConfig: async (config, _) => {
     return {
       runtime_context: config.runtime_context,
+      privacy_settings: config.privacy_settings,
       runtime_configuration_definition: config.settings,
     }
   },
@@ -44,6 +59,19 @@ const spec = createExtensionSpecification({
       throw new AbortError(
         `The property configuration is deprecated and no longer supported.`,
         `It has been replaced by settings.`,
+      )
+    }
+    const saleOfDataSelection = extension.configuration.privacy_settings?.sale_of_data
+    if (
+      !(
+        saleOfDataSelection === DataSaleConfig.SellsData ||
+        saleOfDataSelection === DataSaleConfig.DoNotSellData ||
+        saleOfDataSelection === DataSaleConfig.SupportsLduDowngrade
+      )
+    ) {
+      throw new AbortError(
+        `Invalid sale of data privacy configuration selected.`,
+        `Value must be one of: ${DataSaleConfig.SellsData}, ${DataSaleConfig.DoNotSellData}, ${DataSaleConfig.SupportsLduDowngrade}`,
       )
     }
     return Promise.resolve()
