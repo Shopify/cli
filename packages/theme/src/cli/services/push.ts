df05568ca745ac7a45a490fc38f21501203784fa
James Meng <james.meng@shopify.com>
2024-01-17 11:07:58 -0800

    Initialize Push Service

---
+++ packages/theme/src/cli/services/push.ts
@@ -0,0 +1,6 @@
+import {AdminSession} from '@shopify/cli-kit/node/session'
+import {Theme} from '@shopify/cli-kit/node/themes/types'
+
+interface PushOptions {}
+
+export async function push(theme: Theme, session: AdminSession, options: PushOptions) {}
