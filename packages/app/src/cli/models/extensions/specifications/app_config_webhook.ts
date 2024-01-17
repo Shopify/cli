4af6465d56ab013e9c31e5829eee466e49012261
Rebecca Friedman <rebecca.friedman@shopify.com>
2024-01-17 18:34:13 +0000

    Update the validation for CLI URIs

--- packages/app/src/cli/models/extensions/specifications/app_config_webhook.ts
+++ packages/app/src/cli/models/extensions/specifications/app_config_webhook.ts
@@ -1,5 +1,5 @@
 import {transformToWebhookConfig, transformWebhookConfig} from './transform/app_config_webhook.js'
-import {UriValidation, ensureHttpsOnlyUrl, removeTrailingSlash} from './validation/common.js'
+import {UriValidation, removeTrailingSlash} from './validation/common.js'
 import {webhookValidator} from './validation/app_config_webhook.js'
 import {CustomTransformationConfig, createConfigExtensionSpecification} from '../specification.js'
 import {zod} from '@shopify/cli-kit/node/schema'
@@ -17,9 +17,9 @@ const WebhooksSchema = zod.object({
   api_version: zod.string(),
   privacy_compliance: zod
     .object({
-      customer_deletion_url: ensureHttpsOnlyUrl.optional(),
-      customer_data_request_url: ensureHttpsOnlyUrl.optional(),
-      shop_deletion_url: ensureHttpsOnlyUrl.optional(),
+      customer_deletion_url: UriValidation.optional(),
+      customer_data_request_url: UriValidation.optional(),
+      shop_deletion_url: UriValidation.optional(),
     })
     .optional(),
   subscriptions: zod.array(WebhookSubscriptionSchema).optional(),
