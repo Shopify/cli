302bd59c43aa773d3ddbe7862bb213f6493f8d8a
Alvaro Gutierrez <alvaro.gutierrezblanco@shopify.com>
2024-01-17 18:29:11 +0100

    Rename branding app_handle to handle

--- packages/app/src/cli/models/extensions/specifications/app_config_branding.ts
+++ packages/app/src/cli/models/extensions/specifications/app_config_branding.ts
@@ -1,20 +1,26 @@
-import {createConfigExtensionSpecification} from '../specification.js'
+import {TransformationConfig, createConfigExtensionSpecification} from '../specification.js'
 import {zod} from '@shopify/cli-kit/node/schema'
 
 const BrandingSchema = zod.object({
   name: zod.string().max(30),
-  app_handle: zod
+  handle: zod
     .string()
     .max(256)
     .refine((value) => value && /^\w*(?!-)[a-z0-9-]+(?<!-)$/.test(value))
     .optional(),
 })
 
+const BrandingTransformConfig: TransformationConfig = {
+  name: 'name',
+  app_handle: 'handle',
+}
+
 export const BrandingSpecIdentifier = 'branding'
 
 const spec = createConfigExtensionSpecification({
   identifier: BrandingSpecIdentifier,
   schema: BrandingSchema,
+  transformConfig: BrandingTransformConfig,
 })
 
 export default spec
