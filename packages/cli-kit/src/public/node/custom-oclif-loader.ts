fb5355ab2d5199dcdc296db46b7423115bd0afb4
Isaac Roldán <isaac.roldan@shopify.com>
2024-01-22 15:47:50 +0100

    Custom priority loader

--- packages/cli-kit/src/public/node/custom-oclif-loader.ts
+++ packages/cli-kit/src/public/node/custom-oclif-loader.ts
@@ -1,13 +1,27 @@
 import {joinPath, cwd} from './path.js'
 import {fileExistsSync} from './fs.js'
-import {Config} from '@oclif/core'
+import {Command, Config} from '@oclif/core'
 import {minimatch} from 'minimatch'
 import glob from 'fast-glob'
 import yargs from 'yargs'
+import {Options} from '@oclif/core/lib/interfaces/plugin.js'
 import fs from 'fs'
 import util from 'util'
 
 export class ShopifyConfig extends Config {
+  constructor(options: Options) {
+    super(options)
+    // eslint-disable-next-line dot-notation
+    this['determinePriority'] = (commands: Command.Loadable[]) => {
+      const isHydrogenCommand = commands.some((command) => command.id.startsWith('hydrogen:'))
+      if (!isHydrogenCommand) return this.privateDeterminePriority(commands)
+
+      const hydrogenPlugin = commands.find((command) => command.pluginAlias === '@shopify/cli-hydrogen')
+      const bestChoice = hydrogenPlugin ?? this.privateDeterminePriority(commands)
+      return bestChoice
+    }
+  }
+
   async loadCorePlugins(): Promise<void> {
     // Find potential plugins based on sniffing the `--path` argument
     const matches: [string, string[]][] = []
@@ -28,6 +42,47 @@ export class ShopifyConfig extends Config {
       // eslint-disable-next-line no-await-in-loop
       await this.loadPlugins(root, 'custom', names)
     }
+  }
+
+  privateDeterminePriority(commands: Command.Loadable[]): Command.Loadable | undefined {
+    const oclifPlugins = this.pjson.oclif?.plugins ?? []
+    const commandPlugins = commands.sort((aCommand, bCommand) => {
+      // eslint-disable-next-line no-restricted-syntax
+      const pluginAliasA = aCommand.pluginAlias ?? 'A-Cannot-Find-This'
+      // eslint-disable-next-line no-restricted-syntax
+      const pluginAliasB = bCommand.pluginAlias ?? 'B-Cannot-Find-This'
+      const aIndex = oclifPlugins.indexOf(pluginAliasA)
+      const bIndex = oclifPlugins.indexOf(pluginAliasB)
+      // When both plugin types are 'core' plugins sort based on index
+      if (aCommand.pluginType === 'core' && bCommand.pluginType === 'core') {
+        // If b appears first in the pjson.plugins sort it first
+        return aIndex - bIndex
+      }
+
+      // if b is a core plugin and a is not sort b first
+      if (bCommand.pluginType === 'core' && aCommand.pluginType !== 'core') {
+        return 1
+      }
+
+      // if a is a core plugin and b is not sort a first
+      if (aCommand.pluginType === 'core' && bCommand.pluginType !== 'core') {
+        return -1
+      }
+
+      // if a is a jit plugin and b is not sort b first
+      if (aCommand.pluginType === 'jit' && bCommand.pluginType !== 'jit') {
+        return 1
+      }
+
+      // if b is a jit plugin and a is not sort a first
+      if (bCommand.pluginType === 'jit' && aCommand.pluginType !== 'jit') {
+        return -1
+      }
+
+      // neither plugin is core, so do not change the order
+      return 0
+    })
+    return commandPlugins[0]
   }
 }
 
