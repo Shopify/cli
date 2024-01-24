bb587ce4b0b450a821f3ece2f3cb5fcb44aaac7e
Isaac Roldán <isaac.roldan@shopify.com>
2024-01-24 18:15:27 +0100

    Create bundle script

---
+++ bin/bundle-app.js
@@ -0,0 +1,20 @@
+import {build as esBuild} from 'esbuild'
+import {commonjs} from '@hyrious/esbuild-plugin-commonjs'
+import commonjsPlugin from '@chialab/esbuild-plugin-commonjs';
+
+
+await esBuild({
+  bundle: true,
+  entryPoints: ['./packages/app/src/**/*.ts'],
+  outdir: './packages/app/bundled',
+  platform: 'node',
+  format: 'esm',
+  inject: ['./bin/cjs-shims.js'],
+  external: ['react-devtools-core', 'yoga-wasm-web', 'stacktracey'],
+  loader: {'.node': 'copy'},
+  splitting: true,
+  plugins: [
+    // commonjsPlugin(),
+  ],
+})
+
