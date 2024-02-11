352ddac2ff33709cf9da3b428da21479177fa6b2
John Duff <duff.john@gmail.com>
2024-02-11 08:29:46 -0500

    Pass input through liquid renderer before parsing as TOML

--- packages/cli-kit/src/public/node/toml.test.ts
+++ packages/cli-kit/src/public/node/toml.test.ts
@@ -27,4 +27,57 @@ describe('decodeToml', () => {
     const result = decodeToml(input)
     expect(result).toStrictEqual({access: {admin: {direct_api_mode: 'online'}}})
   })
+
+  test('evaluates liquid if given in the input', () => {
+    const input = `
+    [webhooks]
+    api_version = "{{ 2024 | minus: 1 }}-07"
+    `
+    const result = decodeToml(input)
+    expect(result).toStrictEqual({webhooks: {api_version: '2023-07'}})
+  })
+
+  test('evaluates environment variables from the process in liquid if given in the input', () => {
+    vi.stubEnv('YEAR', 2024)
+
+    const input = `
+    [webhooks]
+    api_version = "{{ env.YEAR | minus: 1 }}-07"
+    `
+    const result = decodeToml(input)
+    expect(result).toStrictEqual({webhooks: {api_version: '2023-07'}})
+    vi.unstubAllEnvs()
+  })
+
+  test('evaluates environment variables from .env in liquid if given in the input', () => {
+    await inTemporaryDirectory(async (tmpDir) => {
+      const dotEnvPath = joinPath(tmpDir, '.env')
+      await writeFile(dotEnvPath, 'YEAR=2024')
+
+      const input = `
+      [webhooks]
+      api_version = "{{ env.YEAR | minus: 1 }}-07"
+      `
+      const result = decodeToml(input)
+      expect(result).toStrictEqual({webhooks: {api_version: '2023-07'}})
+    })
+  })
+
+  test('evaluates environment variables from .env over process.env in liquid if given in the input', () => {
+    await inTemporaryDirectory(async (tmpDir) => {
+      vi.stubEnv('YEAR', 2025)
+
+      const dotEnvPath = joinPath(tmpDir, '.env')
+      await writeFile(dotEnvPath, 'YEAR=2024')
+
+      const input = `
+      [webhooks]
+      api_version = "{{ env.YEAR | minus: 1 }}-07"
+      `
+      const result = decodeToml(input)
+      expect(result).toStrictEqual({webhooks: {api_version: '2023-07'}})
+
+      vi.unstubAllEnvs()
+    })
+  })
 })
