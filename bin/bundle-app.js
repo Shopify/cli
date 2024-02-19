fb675958cba3c398d82578981e88032ee3c9d2cf
Isaac Roldán <isaac.roldan@shopify.com>
2024-02-19 17:42:27 +0100

    update bundle script

--- bin/bundle-app.js
+++ bin/bundle-app.js
@@ -3,30 +3,14 @@ import {build as esBuild} from 'esbuild'
 // import commonjsPlugin from '@chialab/esbuild-plugin-commonjs';
 
 
-// await esBuild({
-//   bundle: true,
-//   entryPoints: ['./packages/app/src/**/*.ts'],
-//   outdir: './packages/app/bundled',
-//   platform: 'node',
-//   format: 'esm',
-//   inject: ['./bin/cjs-shims.js'],
-//   external: ['react-devtools-core', 'yoga-wasm-web', 'git-diff'],
-
-//   loader: {'.node': 'copy'},
-//   splitting: true,
-//   plugins: [
-//     // commonjsPlugin(),
-//   ],
-// })
-
 await esBuild({
   bundle: true,
-  entryPoints: ['./packages/theme/src/**/*.ts'],
-  outdir: './packages/theme/bundled',
+  entryPoints: ['./packages/app/src/**/*.ts'],
+  outdir: './packages/app/dist/cli',
   platform: 'node',
   format: 'esm',
   inject: ['./bin/cjs-shims.js'],
-  external: ['react-devtools-core', 'yoga-wasm-web'],
+  external: ['react-devtools-core', 'yoga-wasm-web', 'git-diff', '@shopify/cli-kit'],
 
   loader: {'.node': 'copy'},
   splitting: true,
@@ -34,4 +18,20 @@ await esBuild({
     // commonjsPlugin(),
   ],
 })
+
+// await esBuild({
+//   bundle: true,
+//   entryPoints: ['./packages/theme/src/**/*.ts'],
+//   outdir: './packages/theme/bundled',
+//   platform: 'node',
+//   format: 'esm',
+//   inject: ['./bin/cjs-shims.js'],
+//   external: ['react-devtools-core', 'yoga-wasm-web'],
+
+//   loader: {'.node': 'copy'},
+//   splitting: true,
+//   plugins: [
+//     // commonjsPlugin(),
+//   ],
+// })
 
