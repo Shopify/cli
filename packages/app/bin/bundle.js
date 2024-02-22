5da1afcc22e42aed7d607d46bf33d3d65da33ae0
Isaac Roldán <isaac.roldan@shopify.com>
2024-02-22 16:25:41 +0100

    Reexport ink and react from cli-kit

--- packages/app/bin/bundle.js
+++ packages/app/bin/bundle.js
@@ -3,9 +3,6 @@ import cleanBundledDependencies from '../../../bin/clean-bundled-dependencies.js
 
 const external =[
   '@shopify/cli-kit',
-  'react-devtools-core', // react-devtools-core can't be bundled (part of ink)
-  'yoga-wasm-web',  // yoga-wasm-web can't be bundled (part of ink)
-  'react', // Excluded because we can't have two reacts (app and cli-kit  )
   '@shopify/plugin-cloudflare', // Plugins need to be external so that they can be loaded dynamically
   'esbuild', // esbuild can't be bundled
   'javy-cli', // This needs to be external so that we can invoke it with `npm exec -- javy`
---
+++ packages/cli-kit/src/public/node/react.ts
@@ -0,0 +1,2 @@
+export {FunctionComponent, useEffect, useMemo, useRef, useState, default as React} from 'react'
+export {renderToStaticMarkup} from 'react-dom/server'
