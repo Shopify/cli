e2713922bdc51eefbae1fbf447fa488b8ac48b03
Isaac Roldán <isaac.roldan@shopify.com>
2024-02-23 17:45:56 +0100

    Add stacktracey

--- packages/cli/bin/bundle.js
+++ packages/cli/bin/bundle.js
@@ -4,7 +4,6 @@ import cleanBundledDependencies from '../../../bin/clean-bundled-dependencies.js
 const external = [
   'react-devtools-core',  // react-devtools-core can't be bundled (part of ink)
   'yoga-wasm-web', // yoga-wasm-web can't be bundled (part of ink)
-  'esbuild', // esbuild can't be bundled
   'stacktracey',
   '@shopify/app', // Plugins need to be external so that they can be loaded dynamically
   '@shopify/theme', // Plugins need to be external so that they can be loaded dynamically
