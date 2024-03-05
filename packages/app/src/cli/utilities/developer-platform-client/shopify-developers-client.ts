1eae8e8988059f45e792f449c240a94d93c7165c
Ariel Caplan <ariel.caplan@shopify.com>
2024-03-05 16:23:57 +0200

    Add orgScopedShopifyDevelopersRequest to cli-kit and use it

--- packages/app/src/cli/utilities/developer-platform-client/shopify-developers-client.ts
+++ packages/app/src/cli/utilities/developer-platform-client/shopify-developers-client.ts
@@ -26,10 +26,8 @@ import {FunctionUploadUrlGenerateResponse} from '@shopify/cli-kit/node/api/partn
 import {randomUUID} from '@shopify/cli-kit/node/crypto'
 import {isUnitTest} from '@shopify/cli-kit/node/context/local'
 import {AbortError, BugError} from '@shopify/cli-kit/node/error'
-import {graphqlRequest, GraphQLVariables, GraphQLResponse} from '@shopify/cli-kit/node/api/graphql'
-import {partnersFqdn} from '@shopify/cli-kit/node/context/fqdn'
+import {orgScopedShopifyDevelopersRequest} from '@shopify/cli-kit/node/api/shopify-developers'
 import {getArrayRejectingUndefined} from '@shopify/cli-kit/common/array'
-import Bottleneck from 'bottleneck'
 
 const ORG1 = {
   id: '1',
@@ -115,7 +113,7 @@ export class ShopifyDevelopersClient implements DeveloperPlatformClient {
     // // This should be the actual query, but it's not working at the moment...
     // const query = SpecificationsQuery
     // const variables: SpecificationsQueryVariables = {appId}
-    // const result = await developerPlatformRequest<SpecificationsQuerySchema>(ORG1.id, query, await this.token(), variables)
+    // const result = await orgScopedShopifyDevelopersRequest<SpecificationsQuerySchema>(ORG1.id, query, await this.token(), variables)
     // console.log(JSON.stringify(result, null, 2))
     // return result.specifications.map((spec): ExtensionSpecification => ({
       // externalName: spec.name,
@@ -141,7 +139,7 @@ export class ShopifyDevelopersClient implements DeveloperPlatformClient {
     const variables = createAppVars(name, options?.isLaunchable, options?.scopesArray)
 
     const mutation = CreateAppMutation
-    const result = await developerPlatformRequest<CreateAppMutationSchema>(org.id, mutation, await this.token(), variables)
+    const result = await orgScopedShopifyDevelopersRequest<CreateAppMutationSchema>(org.id, mutation, await this.token(), variables)
     if (result.appCreate.userErrors?.length > 0) {
       const errors = result.appCreate.userErrors.map((error) => error.message).join(', ')
       throw new AbortError(errors)
@@ -164,7 +162,7 @@ export class ShopifyDevelopersClient implements DeveloperPlatformClient {
   async activeAppVersion(appId: string, orgId: string): Promise<ActiveAppVersionQuerySchema> {
     const query = ActiveAppReleaseQuery
     const variables: ActiveAppReleaseQueryVariables = {appId}
-    const result = await developerPlatformRequest<ActiveAppReleaseQuerySchema>(orgId, query, await this.token(), variables)
+    const result = await orgScopedShopifyDevelopersRequest<ActiveAppReleaseQuerySchema>(orgId, query, await this.token(), variables)
     return {
       app: {
         activeAppVersion: {
@@ -203,67 +201,6 @@ export class ShopifyDevelopersClient implements DeveloperPlatformClient {
   async deploy(_input: AppDeployVariables): Promise<AppDeploySchema> {
     throw new BugError('Not implemented: deploy')
   }
-}
-
-// API Rate limiter for partners API (Limit is 10 requests per second)
-// Jobs are launched every 150ms to add an extra 50ms margin per request.
-// Only 10 requests can be executed concurrently.
-const limiter = new Bottleneck({
-  minTime: 150,
-  maxConcurrent: 10,
-})
-
-/**
- * Executes a GraphQL query against the Partners API.
- *
- * @param query - GraphQL query to execute.
- * @param token - Partners token.
- * @param variables - GraphQL variables to pass to the query.
- * @returns The response of the query of generic type <T>.
- */
-async function developerPlatformRequest<T>(orgId: string, query: string, token: string, variables?: GraphQLVariables): Promise<T> {
-  const api = 'Shopify Developers'
-  const fqdn = (await partnersFqdn()).replace(/^partners/, 'app.shopify')
-  const url = `https://${fqdn}/organization/${orgId}/graphql`
-  const result = limiter.schedule<T>(() =>
-    graphqlRequest({
-      query,
-      api,
-      url,
-      token,
-      variables,
-      responseOptions: {onResponse: handleDeprecations},
-    }),
-  )
-
-  return result
-}
-
-interface Deprecation {
-  supportedUntilDate?: string
-}
-
-interface WithDeprecations {
-  deprecations: Deprecation[]
-}
-
-/**
- * Sets the next deprecation date from [GraphQL response extensions](https://www.apollographql.com/docs/resources/graphql-glossary/#extensions)
- * if `response.extensions.deprecations` objects contain a `supportedUntilDate` (ISO 8601-formatted string).
- *
- * @param response - The response of the query.
- */
-export function handleDeprecations<T>(response: GraphQLResponse<T>): void {
-  if (!response.extensions) return
-
-  const deprecationDates: Date[] = []
-  for (const deprecation of (response.extensions as WithDeprecations).deprecations) {
-    if (deprecation.supportedUntilDate) {
-      deprecationDates.push(new Date(deprecation.supportedUntilDate))
-    }
-  }
-
-  // setNextDeprecationDate(deprecationDates)
 }
 
 interface AppModule {
