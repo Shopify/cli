40c399aabe31e52f9c11de58c06c5f2af053ab6a
Isaac Roldán <isaac.roldan@shopify.com>
2024-03-05 18:07:23 +0100

    Add tests to install cloudflare file

--- packages/plugin-cloudflare/src/install-cloudflared.ts
+++ packages/plugin-cloudflare/src/install-cloudflared.ts
@@ -1,13 +1,19 @@
 // Install script for cloudflared, derived from https://github.com/JacobLinCool/node-cloudflared
 import {basename, dirname, joinPath} from '@shopify/cli-kit/node/path'
 import {outputDebug} from '@shopify/cli-kit/node/output'
+import {
+  chmod,
+  fileExistsSync,
+  mkdirSync,
+  renameFile,
+  unlinkFileSync,
+  createFileWriteStream,
+} from '@shopify/cli-kit/node/fs'
 import {fileURLToPath} from 'url'
 import util from 'util'
 import {pipeline} from 'stream'
 // eslint-disable-next-line no-restricted-imports
 import {execSync, execFileSync} from 'child_process'
-import {createHash} from 'node:crypto'
-import {chmodSync, existsSync, mkdirSync, renameSync, unlinkSync, createWriteStream, readFileSync} from 'fs'
 
 const EXPECTED_CLOUDFLARE_VERSION = '2024.2.1'
 const CLOUDFLARE_REPO = `https://github.com/cloudflare/cloudflared/releases/download/${EXPECTED_CLOUDFLARE_VERSION}/`
@@ -30,41 +36,52 @@ const WINDOWS_URL: {[key: string]: string} = {
   arm64: 'cloudflared-windows-amd64.exe',
 }
 
-const URL: {[key: string]: string | undefined} = {
-  linux: LINUX_URL[process.arch],
-  darwin: MACOS_URL[process.arch],
-  win32: WINDOWS_URL[process.arch],
+const URL: {[key: string]: {[key: string]: string}} = {
+  linux: LINUX_URL,
+  darwin: MACOS_URL,
+  win32: WINDOWS_URL,
+}
+
+function getURL(platform = process.platform, arch = process.arch) {
+  const keys = Object.keys(URL)
+  if (!keys.includes(platform)) throw new Error(`Unsupported system platform: ${platform}`)
+
+  const fileName = URL[platform]![arch]
+  if (fileName === undefined) {
+    throw new Error(`Unsupported system platform: ${platform} or arch: ${arch}`)
+  }
+  return CLOUDFLARE_REPO + fileName
 }
 
 /**
  * Get the path where the binary should be installed.
  * If the environment variable SHOPIFY_CLI_CLOUDFLARED_PATH is set, use that.
  */
-function getBinPathTarget() {
-  if (process.env.SHOPIFY_CLI_CLOUDFLARED_PATH) {
-    return process.env.SHOPIFY_CLI_CLOUDFLARED_PATH
+function getBinPathTarget(env = process.env, platform = process.platform) {
+  if (env.SHOPIFY_CLI_CLOUDFLARED_PATH) {
+    return env.SHOPIFY_CLI_CLOUDFLARED_PATH
   }
   return joinPath(
     dirname(fileURLToPath(import.meta.url)),
     '..',
     'bin',
-    process.platform === 'win32' ? 'cloudflared.exe' : 'cloudflared',
+    platform === 'win32' ? 'cloudflared.exe' : 'cloudflared',
   )
 }
 
-export default async function install() {
+export default async function install(env = process.env, platform = process.platform, arch = process.arch) {
   // Don't install cloudflare if the SHOPIFY_CLI_IGNORE_CLOUDFLARED environment variable is set
-  if (process.env.SHOPIFY_CLI_IGNORE_CLOUDFLARED) return
+  if (env.SHOPIFY_CLI_IGNORE_CLOUDFLARED) return
 
-  const fileName = URL[process.platform]
+  const fileName = URL[platform]
   if (fileName === undefined) {
-    throw new Error(`Unsupported system platform: ${process.platform} or arch: ${process.arch}`)
+    throw new Error(`Unsupported system platform: ${platform} or arch: ${arch}`)
   }
 
-  const fileUrlPath = CLOUDFLARE_REPO + fileName
-  const binTarget = getBinPathTarget()
+  const fileUrlPath = getURL(platform, arch)
+  const binTarget = getBinPathTarget(env, platform)
 
-  if (existsSync(binTarget)) {
+  if (fileExistsSync(binTarget)) {
     // --version returns an string like "cloudflared version 2023.3.1 (built 2023-03-13-1444 UTC)"
     try {
       const versionArray = execFileSync(binTarget, ['--version'], {encoding: 'utf8'}).split(' ')
@@ -80,14 +97,14 @@ export default async function install() {
     }
   }
 
-  if (process.platform === 'linux') {
+  if (platform === 'linux') {
     await installLinux(fileUrlPath, binTarget)
-  } else if (process.platform === 'darwin') {
+  } else if (platform === 'darwin') {
     await installMacos(fileUrlPath, binTarget)
-  } else if (process.platform === 'win32') {
+  } else if (platform === 'win32') {
     await installWindows(fileUrlPath, binTarget)
   } else {
-    throw new Error(`Unsupported platform: ${process.platform}`)
+    throw new Error(`Unsupported platform: ${platform}`)
   }
 }
 
@@ -107,7 +124,7 @@ function versionIsGreaterThan(versionA: string, versionB: string) {
 
 async function installLinux(file: string, binTarget: string) {
   await downloadFile(file, binTarget)
-  chmodSync(binTarget, '755')
+  await chmod(binTarget, '755')
 }
 
 async function installWindows(file: string, binTarget: string) {
@@ -118,24 +135,19 @@ async function installMacos(file: string, binTarget: string) {
   await downloadFile(file, `${binTarget}.tgz`)
   const filename = basename(`${binTarget}.tgz`)
   execSync(`tar -xzf ${filename}`, {cwd: dirname(binTarget)})
-  unlinkSync(`${binTarget}.tgz`)
-  renameSync(`${dirname(binTarget)}/cloudflared`, binTarget)
+  unlinkFileSync(`${binTarget}.tgz`)
+  await renameFile(`${dirname(binTarget)}/cloudflared`, binTarget)
 }
 
 async function downloadFile(url: string, to: string) {
-  if (!existsSync(dirname(to))) {
+  if (!fileExistsSync(dirname(to))) {
     mkdirSync(dirname(to))
   }
   const streamPipeline = util.promisify(pipeline)
   const response = await fetch(url, {redirect: 'follow'})
   if (!response.ok || !response.body)
     throw new Error(`Couldn't download file ${url} (${response.status} ${response.statusText})`)
-  const fileObject = createWriteStream(to)
+  const fileObject = createFileWriteStream(to)
   await streamPipeline(response.body, fileObject)
   return to
-}
-
-function sha256(filePath: string) {
-  const fileBuffer = readFileSync(filePath)
-  return createHash('sha256').update(fileBuffer).digest('hex')
 }
