1079357c8e6854a7875c13b3e59e93475f3badf9
Isaac Roldán <isaac.roldan@shopify.com>
2024-03-07 17:25:26 +0100

    Fix import of plugin-plugins

--- packages/cli/src/cli/commands/plugins/index.ts
+++ packages/cli/src/cli/commands/plugins/index.ts
@@ -1,10 +1,10 @@
-import PluginsIndex from '@oclif/plugin-plugins/lib/commands/plugins/index.js'
+import {commands} from '@oclif/plugin-plugins'
 import Command from '@shopify/cli-kit/node/base-command'
 
 export default class Index extends Command {
   static hidden = true
 
   async run(): Promise<void> {
-    await PluginsIndex.default.run(this.argv)
+    await commands.plugins.run(this.argv)
   }
 }
