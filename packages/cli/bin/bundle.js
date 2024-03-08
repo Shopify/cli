d07d1865df4e6eaaa05d8d6d7155bfcc6e3c378a
Isaac Roldán <isaac.roldan@shopify.com>
2024-03-08 14:30:53 +0100

    Only bundle cli

--- packages/cli/bin/bundle.js
+++ packages/cli/bin/bundle.js
@@ -5,7 +5,6 @@ import requireResolvePlugin from '@chialab/esbuild-plugin-require-resolve';
 const external = [
   'react-devtools-core',  // react-devtools-core can't be bundled (part of ink)
   'yoga-wasm-web', // yoga-wasm-web can't be bundled (part of ink)
-  'stacktracey',
   'esbuild',
   '@luckycatfactory/esbuild-graphql-loader',
   'vscode-json-languageservice'
@@ -22,8 +21,34 @@ await esBuild({
   loader: {'.node': 'copy'},
   splitting: true,
   plugins: [
-    requireResolvePlugin(),
+    requireResolvePlugin(), // To allow using require.resolve in esbuild
+    myPlugin()
   ],
 })
 
 await cleanBundledDependencies(external)
+
+
+
+function myPlugin ({greeting = "world"} = {}) {
+  return {
+      name: "MyPlugin",
+      setup(build) {
+        build.onResolve({ filter: /stacktracey\.js$/ }, (args) => {
+          return {
+              path: args.path,
+              namespace: 'vanilla'
+          }
+        })
+
+        build.onLoad({ filter: /.*/, namespace: "vanilla" }, async (args) => {
+          const contents = await readFile(args.path, 'utf8')
+          return {
+              // a simple substitution just to prove that we've
+              // loaded the file and are transforming it
+              contents: contents.replace('nodeRequire(', 'module.require(')
+          }
+        })
+      }
+  }
+}
