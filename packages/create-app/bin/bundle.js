298b51c5f9ce49510fbe72aebf73655f0359f82e
Isaac Roldán <isaac.roldan@shopify.com>
2024-03-18 15:54:17 +0100

    Add bundling to create-app

---
+++ packages/create-app/bin/bundle.js
@@ -0,0 +1,48 @@
+import {build as esBuild} from 'esbuild'
+import cleanBundledDependencies from '../../../bin/clean-bundled-dependencies.js'
+import requireResolvePlugin from '@chialab/esbuild-plugin-require-resolve';
+import { readFile } from 'fs/promises';
+
+const external = [
+  'react-devtools-core',  // react-devtools-core is a dev dependency, no need to bundle it.
+  'yoga-wasm-web', // yoga-wasm-web can't be bundled because it's a wasm file (maybe fixable via plugin?)
+  'esbuild', // esbuild can't be bundled per design
+]
+
+/**
+ * Custom plugin to solve some issues with specific dependencies.
+ */
+function ShopifyESBuildPlugin ({greeting = "world"} = {}) {
+  return {
+      name: "ShopifyESBuildPlugin",
+      setup(build) {
+
+        // Stacktracey has a custom require implementation that doesn't work with esbuild
+        build.onLoad({ filter: /.*stacktracey\.js/ }, async (args) => {
+          const contents = await readFile(args.path, 'utf8')
+          return { contents: contents.replaceAll('nodeRequire (', 'module.require(') }
+        })
+      }
+  }
+}
+
+await esBuild({
+  bundle: true,
+  entryPoints: ['./src/**/*.ts'],
+  outdir: './dist',
+  platform: 'node',
+  format: 'esm',
+  inject: ['../../bin/cjs-shims.js'],
+  external,
+  loader: {'.node': 'copy'},
+  splitting: true,
+  plugins: [
+    ShopifyESBuildPlugin(),
+    requireResolvePlugin() // To allow using require.resolve in esbuild
+  ],
+})
+
+await cleanBundledDependencies(external)
+
+
+
