c44f551e136d63ac60e54b95e1d14c3d54ebfe0d
Yigit Ozkavci <yigit.ozkavc@shopify.com>
2024-03-26 10:29:19 +0000

    Fix the test after slugifying the title

--- packages/app/src/cli/services/payments/extension-to-toml.test.ts
+++ packages/app/src/cli/services/payments/extension-to-toml.test.ts
@@ -8,9 +8,8 @@ describe('extension-to-toml', () => {
     const extension1: ExtensionRegistration = {
       id: '30366498817',
       uuid: '626ab61a-e494-4e16-b511-e8721ec011a4',
-      title: 'BogusPay',
+      title: 'Bogus Pay',
       type: 'payments_app',
-      handle: 'bogus-pay',
       draftVersion: {
         config:
           '{"start_payment_session_url":"https://bogus-app/payment-sessions/start","start_refund_session_url":"https://bogus-app/payment-sessions/refund","start_capture_session_url":"https://bogus-app/payment-sessions/capture","start_void_session_url":"https://bogus-app/payment-sessions/void","confirmation_callback_url":null,"supported_payment_methods":["visa","master","american_express","discover","diners_club","jcb"],"supported_countries":["GG","AF","AZ","BH"],"test_mode_available":true,"merchant_label":"Offsite Payments App Extension","default_buyer_label":null,"buyer_label_to_locale":null,"supports_3ds":true,"supports_oversell_protection":false,"api_version":"2023-10","supports_installments":true,"supports_deferred_payments":true,"multiple_capture":false}',
@@ -24,7 +23,7 @@ describe('extension-to-toml', () => {
     expect(got).toEqual(`api_version = "2023-10"
 
 [[extensions]]
-name = "BogusPay"
+name = "Bogus Pay"
 type = "payments_extension"
 handle = "bogus-pay"
 
