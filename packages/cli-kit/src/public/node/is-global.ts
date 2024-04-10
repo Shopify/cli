5f02c6121aca31b96f391825a1568194b1b5c4df
Isaac Roldán <isaac.roldan@shopify.com>
2024-04-10 15:40:56 +0200

    Add is-global with global utilities

--- packages/cli-kit/src/public/node/is-global.ts
+++ packages/cli-kit/src/public/node/is-global.ts
@@ -1,3 +1,8 @@
+import {PackageManager} from './node-package-manager.js'
+import {outputInfo} from './output.js'
+import {captureOutput, exec} from './system.js'
+import {renderSelectPrompt} from './ui.js'
+
 /**
  * Returns true if the current process is running in a global context.
  *
@@ -8,4 +13,71 @@ export function currentProcessIsGlobal(env = process.env): boolean {
   // npm, yarn, pnpm and bun define this if run locally.
   // If undefined, we can assume it's global (But there is no foolproof way to know)
   return env.npm_config_user_agent === undefined
+}
+
+/**
+ * Returns true if the global CLI is installed.
+ *
+ * @returns `true` if the global CLI is installed.
+ */
+export async function isGlobalCLIInstalled(): Promise<boolean> {
+  try {
+    await captureOutput('shopify', ['--help'])
+    return true
+    // eslint-disable-next-line no-catch-all/no-catch-all
+  } catch {
+    return false
+  }
+}
+
+/**
+ * Installs the global Shopify CLI, using the provided package manager.
+ *
+ * @param packageManager - The package manager to use.
+ */
+async function installGlobalShopifyCLI(packageManager: PackageManager): Promise<void> {
+  const args =
+    packageManager === 'yarn' ? ['global', 'add', '@shopify/cli@latest'] : ['install', '-g', '@shopify/cli@latest']
+  outputInfo(`Running ${packageManager} ${args.join(' ')}...`)
+  await exec(packageManager, args, {stdio: 'inherit'})
+}
+
+/**
+ * Prompts the user to install the global CLI.
+ *
+ * @param packageManager - The package manager to use.
+ * @returns `true` if the user has installed the global CLI.
+ */
+export async function installGlobalCLIIfNeeded(packageManager: PackageManager): Promise<boolean> {
+  if (await isGlobalCLIInstalled()) {
+    return true
+  }
+  const globalResult = await renderSelectPrompt({
+    message: 'We recommend installing Shopify CLI globally in your system. Would you like to install it now?',
+    choices: [
+      {value: 'yes', label: 'Yes'},
+      {value: 'no', label: 'No, just for this project'},
+    ],
+  })
+
+  if (globalResult === 'yes') {
+    await installGlobalShopifyCLI(packageManager)
+  }
+  return globalResult === 'yes'
+}
+
+/**
+ * Infers the package manager used by the global CLI.
+ *
+ * @returns The package manager used by the global CLI.
+ */
+export function inferPackageManagerForGlobalCLI(argv = process.argv, env = process.env): PackageManager {
+  if (!currentProcessIsGlobal(env)) return 'unknown'
+
+  // argv[1] contains the path of the executed binary
+  const processArgv = argv[1] ?? ''
+  if (processArgv.includes('yarn')) return 'yarn'
+  if (processArgv.includes('pnpm')) return 'pnpm'
+  if (processArgv.includes('bun')) return 'bun'
+  return 'npm'
 }
