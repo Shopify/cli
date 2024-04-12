22ca14528809a19056e217844a49eb6ee5e311f1
Isaac Roldán <isaac.roldan@shopify.com>
2024-04-12 09:43:46 +0200

    add some comments

--- bin/bundling/clean-bundled-dependencies.js
+++ bin/bundling/clean-bundled-dependencies.js
@@ -1,5 +1,7 @@
 import { readFile, writeFile } from "fs/promises"
 
+// This function removes all dependencies from the package.json file that are not in the external array
+// This is useful when bundling with esbuild, we only leave the dependencies that are not bundled.
 export default async function cleanBundledDependencies(external) {
   const file = await readFile('./package.json')
   const json = JSON.parse(file.toString())
