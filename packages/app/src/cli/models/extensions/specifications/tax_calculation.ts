b127bf170b651a3467d12bd2943c99186d736a4a
Michael Chemani <michaelchemani@michaels-mbp.home>
2024-04-19 10:57:03 -0400

    Added is_marketplace, use_shopify_tax and marketplace_registrations to the tax_calculation extension

--- packages/app/src/cli/models/extensions/specifications/tax_calculation.ts
+++ packages/app/src/cli/models/extensions/specifications/tax_calculation.ts
@@ -6,6 +6,9 @@ const TaxCalculationsSchema = BaseSchema.extend({
   production_api_base_url: zod.string(),
   benchmark_api_base_url: zod.string().optional(),
   calculate_taxes_api_endpoint: zod.string(),
+  is_marketplace: zod.boolean().optional(),
+  use_shopify_tax: zod.boolean().optional(),
+  marketplace_registrations: zod.array(zod.string()).optional(),
   input: zod
     .object({
       metafield_identifiers: zod
@@ -30,6 +33,9 @@ const spec = createExtensionSpecification({
       metafields: config.metafields,
       api_version: config.api_version,
       metafield_identifiers: config.input?.metafield_identifiers,
+      is_marketplace: config.is_marketplace,
+      use_shopify_tax: config.use_shopify_tax,
+      marketplace_registrations: config.marketplace_registrations,
     }
   },
 })
