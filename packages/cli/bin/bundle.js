60ae7762a9446bc687f5016bd4c6dd4014966e62
Isaac Roldán <isaac.roldan@shopify.com>
2024-04-26 10:50:29 +0200

    clean bundled dependencies

--- packages/cli/bin/bundle.js
+++ packages/cli/bin/bundle.js
@@ -1,5 +1,6 @@
 /* eslint-disable @shopify/cli/specific-imports-in-bootstrap-code */
 /* eslint-disable import/no-extraneous-dependencies */
+import cleanBundledDependencies from '../../../bin/bundling/clean-bundled-dependencies.js'
 import ShopifyStacktraceyPlugin from '../../../bin/bundling/esbuild-plugin-stacktracey.js'
 import ShopifyVSCodePlugin from '../../../bin/bundling/esbuild-plugin-vscode.js'
 import GraphiQLImportsPlugin from '../../../bin/bundling/esbuild-plugin-graphiql-imports.js'
@@ -78,4 +79,4 @@ esBuild({
   ],
 })
 
-// cleanBundledDependencies(external)
+cleanBundledDependencies(external)
