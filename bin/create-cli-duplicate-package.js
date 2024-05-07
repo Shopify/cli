eeaec5df974ef4ac1fd9a132142c53e4a3eb3ab0
Isaac Roldán <isaac.roldan@shopify.com>
2024-05-07 17:38:12 +0200

    Update script

--- bin/create-cli-duplicate-package.js
+++ bin/create-cli-duplicate-package.js
@@ -2,8 +2,15 @@ import { copyFile, cp, readFile, writeFile } from "fs/promises"
 
 await cp('./packages/cli', './packages/cli-copy', { recursive: true })
 
-const file = await readFile('./packages/cli-copy/package.json')
-const json = JSON.parse(file.toString())
-json.name = "shopify"
-await writeFile('./packages/cli-copy/package.json', JSON.stringify(json, null, 2))
+const packageJsonFile = await readFile('./packages/cli-copy/package.json')
+const packageJsonJson = JSON.parse(packageJsonFile.toString())
+packageJsonJson.name = "shopify"
+
+const projectJsonFile = await readFile('./packages/cli-copy/project.json')
+const projectJsonJson = JSON.parse(projectJsonFile.toString())
+projectJsonJson.name = "shopify"
+projectJsonJson.sourceRoot = "packages/cli-copy/src"
+
+await writeFile('./packages/cli-copy/package.json', JSON.stringify(packageJsonJson, null, 2))
+await writeFile('./packages/cli-copy/project.json', JSON.stringify(projectJsonJson, null, 2))
 
