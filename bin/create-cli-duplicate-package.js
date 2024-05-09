1ce5e47f326e97ea14b2c0085aef337ccf86fbc5
Isaac Roldán <isaac.roldan@shopify.com>
2024-05-09 16:42:18 +0200

    Update project.json

--- bin/create-cli-duplicate-package.js
+++ bin/create-cli-duplicate-package.js
@@ -6,8 +6,10 @@ const packageJsonFile = await readFile('./packages/cli-copy/package.json')
 const packageJsonJson = JSON.parse(packageJsonFile.toString())
 packageJsonJson.name = "shopify"
 
-const projectJsonFile = await readFile('./packages/cli-copy/project.json')
-const projectJsonJson = JSON.parse(projectJsonFile.toString())
+let projectJsonFile = (await readFile('./packages/cli-copy/project.json')).toString()
+projectJsonFile = projectJsonFile.replace(/packages\/cli/g, "packages/cli-copy", { recursive: true })
+
+const projectJsonJson = JSON.parse(projectJsonFile)
 projectJsonJson.name = "shopify"
 projectJsonJson.sourceRoot = "packages/cli-copy/src"
 
