e8c1645ebef69a5ccf2ac6b0b7120b6912b461cd
Isaac Roldán <isaac.roldan@shopify.com>
2024-05-13 13:39:26 +0200

    Transform webhook schema to load subscriptions as independent objects

--- packages/app/src/cli/models/extensions/specifications/app_config_webhook_schemas/webhooks_schema.ts
+++ packages/app/src/cli/models/extensions/specifications/app_config_webhook_schemas/webhooks_schema.ts
@@ -4,17 +4,28 @@ import {UriValidation} from '../validation/common.js'
 import {SingleWebhookSubscriptionSchema} from '../app_config_webhook_subscription.js'
 import {zod} from '@shopify/cli-kit/node/schema'
 
-const WebhooksConfigSchema = zod.object({
-  api_version: zod.string({required_error: 'String is required'}),
-  privacy_compliance: zod
-    .object({
-      customer_deletion_url: UriValidation.optional(),
-      customer_data_request_url: UriValidation.optional(),
-      shop_deletion_url: UriValidation.optional(),
+const WebhooksConfigSchema = zod
+  .object({
+    api_version: zod.string({required_error: 'String is required'}),
+    privacy_compliance: zod
+      .object({
+        customer_deletion_url: UriValidation.optional(),
+        customer_data_request_url: UriValidation.optional(),
+        shop_deletion_url: UriValidation.optional(),
+      })
+      .optional(),
+    subscriptions: zod.array(WebhookSubscriptionSchema).optional(),
+  })
+  .transform((value) => {
+    // Transform subscriptions from condensed state to expanded state (only 1 topic per subscription)
+    const expandedSubscriptions = value.subscriptions?.flatMap((subscription) => {
+      const {topics = [], ...otherFields} = subscription
+      return topics.map((topic) => {
+        return {topics: [topic], ...otherFields}
+      })
     })
-    .optional(),
-  subscriptions: zod.array(WebhookSubscriptionSchema).optional(),
-})
+    return {...value, subscriptions: expandedSubscriptions}
+  })
 
 export type SingleWebhookSubscriptionType = zod.infer<typeof SingleWebhookSubscriptionSchema>
 
