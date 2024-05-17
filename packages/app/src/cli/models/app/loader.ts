538e9f5b6a423a807c04d64c4709493d162c105c
Shaun Stanworth <shaun.stanworth@shopify.com>
2024-05-17 10:49:12 +0100

    Polish: valid app configuration name functions

--- packages/app/src/cli/models/app/loader.ts
+++ packages/app/src/cli/models/app/loader.ts
@@ -1031,20 +1031,40 @@ async function logMetadataFromAppLoadingProcess(loadMetadata: ConfigurationLoadR
 
 const appConfigurationFileNameRegex = /^shopify\.app(\.[-\w]+)?\.toml$/
 const appConfigurationFileNameGlob = 'shopify.app*.toml'
+type AppConfigurationFileName = 'shopify.app.toml' | `shopify.app.${string}.toml`
 
-export function getAppConfigurationFileName(configName?: string) {
+/**
+ * Gets the name of the app configuration file (e.g. `shopify.app.production.toml`) based on a provided config name.
+ *
+ * @param configName - Optional config name to base the file name upon
+ * @returns Either the default app configuration file name (`shopify.app.toml`), the given config name (if it matched the valid format), or `shopify.app.<config name>.toml` if it was an arbitrary string
+ */
+export function getAppConfigurationFileName(configName?: string): AppConfigurationFileName {
   if (!configName) {
     return configurationFileNames.app
   }
 
-  if (appConfigurationFileNameRegex.test(configName)) {
+  if (isValidFormatAppConfigurationFileName(configName)) {
     return configName
   } else {
     return `shopify.app.${slugify(configName)}.toml`
   }
 }
 
+/**
+ * Given a path to an app configuration file, extract the shorthand section from the file name.
+ *
+ * This is undefined for `shopify.app.toml` files, or returns e.g. `production` for `shopify.app.production.toml`.
+ */
 export function getAppConfigurationShorthand(path: string) {
   const match = basename(path).match(appConfigurationFileNameRegex)
   return match?.[1]?.slice(1)
+}
+
+/** Checks if configName is a valid one (`shopify.app.toml`, or `shopify.app.<something>.toml`) */
+function isValidFormatAppConfigurationFileName(configName: string): configName is AppConfigurationFileName {
+  if (appConfigurationFileNameRegex.test(configName)) {
+    return true
+  }
+  return false
 }
