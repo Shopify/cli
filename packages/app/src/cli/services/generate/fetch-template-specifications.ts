fb159e85bd60c574f92c2c6311481f99473b5a9f
Ariel Caplan <ariel.caplan@shopify.com>
2024-06-05 10:51:21 +0300

    Remove local templates
    We have moved everything into Partners, so we don't need these anymore.

--- packages/app/src/cli/services/generate/fetch-template-specifications.ts
+++ packages/app/src/cli/services/generate/fetch-template-specifications.ts
@@ -1,7 +1,4 @@
 import {ExtensionTemplate} from '../../models/app/template.js'
-import themeExtension from '../../models/templates/theme-specifications/theme.js'
-import productSubscriptionUIExtension from '../../models/templates/ui-specifications/product_subscription.js'
-import webPixelUIExtension from '../../models/templates/ui-specifications/web_pixel_extension.js'
 import {DeveloperPlatformClient} from '../../utilities/developer-platform-client.js'
 
 export async function fetchExtensionTemplates(
@@ -10,17 +7,10 @@ export async function fetchExtensionTemplates(
   availableSpecifications: string[],
 ): Promise<ExtensionTemplate[]> {
   const remoteTemplates: ExtensionTemplate[] = await developerPlatformClient.templateSpecifications(apiKey)
-  const remoteIDs = remoteTemplates.map((template) => template.identifier)
   // Filter out local templates that are already available remotely to avoid duplicates
-  const lcoalTemplates = localExtensionTemplates().filter((template) => !remoteIDs.includes(template.identifier))
-  const allTemplates = remoteTemplates.concat(lcoalTemplates)
-  return allTemplates.filter(
+  return remoteTemplates.filter(
     (template) =>
       availableSpecifications.includes(template.identifier) ||
       availableSpecifications.includes(template.types[0]!.type),
   )
-}
-
-function localExtensionTemplates(): ExtensionTemplate[] {
-  return [themeExtension, productSubscriptionUIExtension, webPixelUIExtension]
 }
