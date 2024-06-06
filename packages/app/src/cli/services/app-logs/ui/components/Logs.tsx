1b76f25c094017d811a3f7f13f56458f1a98d0eb
Mehdi Salemi <mehdi.salemi@shopify.com>
2024-06-06 17:08:58 -0400

    saving progress - testing with ConcurrentOutput, time to create similar component for logs output

--- packages/app/src/cli/services/app-logs/ui/components/Logs.tsx
+++ packages/app/src/cli/services/app-logs/ui/components/Logs.tsx
@@ -4,6 +4,7 @@ import React, {useMemo, FunctionComponent} from 'react'
 import {OutputProcess} from '@shopify/cli-kit/node/output'
 import {AbortController, AbortSignal} from '@shopify/cli-kit/node/abort'
 import {Text} from '@shopify/cli-kit/node/ink'
+import {ConcurrentOutput} from '@shopify/cli-kit/node/ui/components'
 import {Writable} from 'stream'
 
 export interface LogsProps {
@@ -16,7 +17,18 @@ export interface LogsProps {
   }
 }
 
+// fix: we dont need to pass in process's here, there is only a single process
+// leaving for now to keep code consistent with dev for now
+// same for other spots
+const calculatePrefixColumnSize = (processes: OutputProcess[], extensions: ExtensionInstance[]) => {
+  return Math.max(
+    ...processes.map((process) => process.prefix.length),
+    ...extensions.map((extension) => extension.handle.length),
+  )
+}
+
 const Logs: FunctionComponent<LogsProps> = ({logsProcess, app, abortController}) => {
+  const prefixColumnSize = calculatePrefixColumnSize([logsProcess], app.extensions)
   const errorHandledProcesses = useMemo(() => {
     return [logsProcess].map((process) => {
       return {
@@ -36,6 +48,13 @@ const Logs: FunctionComponent<LogsProps> = ({logsProcess, app, abortController})
   return (
     <>
       <Text color="blueBright">{'Testing, hello from <Log />'}</Text>
+      {/* TESTING - Next steps, create a similar component to this, and use that for output */}
+      <ConcurrentOutput
+        processes={errorHandledProcesses}
+        prefixColumnSize={prefixColumnSize}
+        abortSignal={abortController.signal}
+        keepRunningAfterProcessesResolve={true}
+      />
     </>
   )
 }
