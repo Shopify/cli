b783baf52a568a3598446f0f6128ef1c0671b75e
SaifBenChahed <saif.benchahed@shopify.com>
2024-06-07 15:41:53 -0400

    network access logs

--- packages/app/src/cli/services/app-logs/poll-app-logs.ts
+++ packages/app/src/cli/services/app-logs/poll-app-logs.ts
@@ -65,24 +65,41 @@ export const pollAppLogs = async ({
   if (data.app_logs) {
     const {app_logs: appLogs} = data
 
-    const functionLogs = appLogs.filter((appLog) => appLog.event_type === 'function_run')
+    const functionLogs = appLogs.filter((appLog) => appLog.event_type === 'function_run' || appLog.event_type.startsWith("function_network_access"))
 
     for (const functionLog of functionLogs) {
       const payload = JSON.parse(functionLog.payload)
-      const fuel = (payload.fuel_consumed / ONE_MILLION).toFixed(4)
 
       // eslint-disable-next-line no-await-in-loop
       await useConcurrentOutputContext({outputPrefix: functionLog.source}, async () => {
-        if (functionLog.status === 'success') {
-          stdout.write(`Function executed successfully using ${fuel}M instructions.`)
-        } else if (functionLog.status === 'failure') {
-          stdout.write(`❌ Function failed to execute with error: ${payload.error_type}`)
-        }
-
-        // print the logs from the appLogs as well
-        const logs = JSON.parse(functionLog.payload).logs
-        if (logs.length > 0) {
-          stdout.write(logs)
+        if (functionLog.event_type === 'function_run') {
+          const fuel = (payload.fuel_consumed / ONE_MILLION).toFixed(4)
+          if (functionLog.status === 'success') {
+            stdout.write(`Function target (${payload.export}) executed successfully using ${fuel}M instructions.`)
+          } else if (functionLog.status === 'failure') {
+            stdout.write(`❌ Function target (${payload.export}) failed to execute with error: ${payload.error_type}`)
+          }
+          // print the logs from the appLogs as well
+          const logs = JSON.parse(functionLog.payload).logs
+          if (logs.length > 0) {
+            stdout.write(logs)
+          }
+        } else if (functionLog.event_type.startsWith("function_network_access")) {
+          if (functionLog.event_type === 'function_network_access.request_execution') {
+            if (functionLog.status === 'success') {
+              stdout.write(`Function network access executed successfully.\n`);
+            } else if (functionLog.status === 'failure') {
+              stdout.write(`❌ Function network access failed to execute with error: ${payload.error}\n`);
+            }
+          } else if (functionLog.event_type === 'function_network_access.cache_read'){
+            if (functionLog.status === 'success') {
+              stdout.write(`Function network access cache read (hit).\n`);
+            } else if (functionLog.status === 'failure') {
+              stdout.write(`Function network access cache read (miss).\n`);
+            }
+          } else if (functionLog.event_type === 'function_network_access.cache_write'){
+            stdout.write(`Function network access cache write.\n`);
+          }
         }
 
         await writeAppLogsToFile({
