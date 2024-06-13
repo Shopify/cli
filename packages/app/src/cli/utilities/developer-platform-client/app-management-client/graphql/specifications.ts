d9aeecfa2ccaa2d607638fb8dfd5e8e13c18fd71
Ariel Caplan <ariel.caplan@shopify.com>
2024-06-13 12:38:16 +0300

    Restore backend-based specifications

--- packages/app/src/cli/utilities/developer-platform-client/app-management-client/graphql/specifications.ts
+++ packages/app/src/cli/utilities/developer-platform-client/app-management-client/graphql/specifications.ts
@@ -1,29 +1,21 @@
 import {gql} from 'graphql-request'
 
 export const SpecificationsQuery = gql`
-  query fetchSpecifications($appId: ID!) {
-    specifications(appId: $appId) {
+  query fetchSpecifications {
+    specifications {
       name
       identifier
       externalIdentifier
-      experience
       features
-      appModuleLimit
     }
   }
 `
-
-export interface SpecificationsQueryVariables {
-  appId: string
-}
 
 interface RemoteSpecification {
   name: string
   identifier: string
   externalIdentifier: string
-  experience: 'EXTENSION' | 'CONFIGURATION' | 'DEPRECATED'
   features?: string[]
-  appModuleLimit: number
 }
 
 export interface SpecificationsQuerySchema {
