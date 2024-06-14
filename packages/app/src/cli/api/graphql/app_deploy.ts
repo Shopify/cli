f5a7c2e71e1de8aa6135de2a2ddc9826010ab7ce
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2024-06-14 19:43:39 +0200

    Fix the error title for invalid webhook subscriptions

--- packages/app/src/cli/api/graphql/app_deploy.ts
+++ packages/app/src/cli/api/graphql/app_deploy.ts
@@ -68,6 +68,7 @@ export interface AppDeployVariables {
 interface ErrorDetail {
   extension_id: number
   extension_title: string
+  specification_identifier: string
 }
 
 export interface AppDeploySchema {
