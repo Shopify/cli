b85dc548b7ce19a34529a9e5db0855298fdef08d
Kyra Stephen <kyra.stephen@shopify.com>
2024-06-17 19:25:58 +0000

    Add logic for deploying marketing activity extensions

--- packages/app/src/cli/models/extensions/load-specifications.ts
+++ packages/app/src/cli/models/extensions/load-specifications.ts
@@ -21,7 +21,7 @@ import paymentExtensionSpec from './specifications/payments_app_extension.js'
 import posUISpec from './specifications/pos_ui_extension.js'
 import productSubscriptionSpec from './specifications/product_subscription.js'
 import taxCalculationSpec from './specifications/tax_calculation.js'
-import marketingActivityExtensionSpec from './specifications/marketing_activity_extension.js'
+import marketingActivitySpec from './specifications/marketing_activity.js'
 import themeSpec from './specifications/theme.js'
 import uiExtensionSpec from './specifications/ui_extension.js'
 import webPixelSpec from './specifications/web_pixel_extension.js'
@@ -73,7 +73,7 @@ function loadSpecifications() {
     posUISpec,
     productSubscriptionSpec,
     taxCalculationSpec,
-    marketingActivityExtensionSpec,
+    marketingActivitySpec,
     themeSpec,
     uiExtensionSpec,
     webPixelSpec,
