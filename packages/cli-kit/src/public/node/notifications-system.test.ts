45f23aa1faf8f4367c586c7b43dbef4deb2cc523
Isaac Roldán <isaac.roldan@shopify.com>
2024-06-26 15:00:13 -0400

    add more tests

--- packages/cli-kit/src/public/node/notifications-system.test.ts
+++ packages/cli-kit/src/public/node/notifications-system.test.ts
@@ -64,6 +64,7 @@ const defaultInput = [
  * @param output - The expected filtered notifications
  */
 interface TestCase {
+  name: string
   input: Notification[]
   commandId: string
   version: string
@@ -73,16 +74,57 @@ interface TestCase {
 
 const testCases: TestCase[] = [
   {
+    name: 'all filters pass',
+    input: defaultInput,
+    commandId: 'app:dev',
+    version: '1.0.0',
+    date: '2000-02-01',
+    output: defaultInput,
+  },
+  {
+    name: 'Only for app:info command, excludes notifications for explicit commands',
     input: defaultInput,
     commandId: 'app:info',
-    version: '1.0',
-    date: '2024-01-01',
-    output: [betweenVersins1and2, betweenDatesIn2000],
+    version: '1.0.0',
+    date: '2000-02-01',
+    output: [betweenVersins1and2, betweenDatesIn2000, fromVersion1, upToVersion2, fromDateJan2000, upToDateDec2000],
+  },
+  {
+    name: 'Notifications for version 2.1.0',
+    input: defaultInput,
+    commandId: 'app:info',
+    version: '2.1.0',
+    date: '2000-02-01',
+    output: [betweenDatesIn2000, fromVersion1, fromDateJan2000, upToDateDec2000],
+  },
+  {
+    name: 'Notifications for year 9999',
+    input: defaultInput,
+    commandId: 'app:info',
+    version: '1.0.0',
+    date: '9999-02-01',
+    output: [betweenVersins1and2, fromVersion1, upToVersion2, fromDateJan2000],
+  },
+  {
+    name: 'Notifications for version 1.5, in year 1990',
+    input: defaultInput,
+    commandId: 'app:info',
+    version: '1.5.0',
+    date: '1990-02-01',
+    output: [betweenVersins1and2, fromVersion1, upToVersion2, upToDateDec2000],
+  },
+  {
+    name: 'Notifications for version 2.1, and year 2024 and dev command',
+    input: defaultInput,
+    commandId: 'app:dev',
+    version: '2.1.0',
+    date: '2024-02-01',
+    output: [fromVersion1, fromDateJan2000, onlyForDevCommand],
   },
 ]
 
 describe('notifications-system filter notifications', () => {
-  test.each(testCases)('Filters', ({input, commandId, version, date, output}) => {
+  test.each(testCases)('Filter for %name', ({input, commandId, version, date, output}) => {
     // When
     const result = filterNotifications(input, commandId, new Date(date), version)
 
