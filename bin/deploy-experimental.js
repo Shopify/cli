3bce055c1db038d83a547518ae456d897f516e5a
Isaac Roldán <isaac.roldan@shopify.com>
2024-06-28 10:51:18 -0400

    Add some cool logs

--- bin/deploy-experimental.js
+++ bin/deploy-experimental.js
@@ -2,20 +2,22 @@ import {execaSync} from 'execa'
 
 // get current branch
 const currentBranch = execaSync("git", ["branch","--show-current"]).stdout.trim()
-console.log("Preparing to deploy branch: ", currentBranch)
+console.log(`🏗️ Preparing to deploy "${currentBranch}" to experimental`)
 // validate a clean state
 
 // change to experimental
 execaSync("git", ["checkout", "experimental"])
 
 // reset experimental
-console.log("Updating experimental branch...")
+console.log("🔄 Updating experimental branch...")
 execaSync("git", ["reset", "--hard", currentBranch])
 
 // push force experimental
-console.log("Pushing experimental branch...")
+console.log("🙌 Pushing experimental branch...")
 execaSync("git", ["push", "origin", "experimental", "-f"])
 
+execaSync("git", ["checkout", currentBranch])
+
 // navigate to shipit
-console.log("Done! go to shipit and hit deploy! -> https://shipit.shopify.io/shopify/cli/experimental")
-open("https://shipit.shopify.io/shopify/cli/experimental")
+console.log("✅ Done! go to shipit and hit deploy! -> https://shipit.shopify.io/shopify/cli/experimental")
+execaSync("open", ["https://shipit.shopify.io/shopify/cli/experimental"])
