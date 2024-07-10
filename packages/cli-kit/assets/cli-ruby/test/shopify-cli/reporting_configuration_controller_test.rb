44d1194a03948b56a15b295833f5dfee7394866a
Isaac Roldán <isaac.roldan@shopify.com>
2024-07-10 10:57:12 +0200

    Remove tests

--- packages/cli-kit/assets/cli-ruby/test/shopify-cli/reporting_configuration_controller_test.rb
+++ packages/cli-kit/assets/cli-ruby/test/shopify-cli/reporting_configuration_controller_test.rb
@@ -20,42 +20,5 @@ module ShopifyCLI
       # When/Then
       ReportingConfigurationController.enable_reporting(true)
     end
-
-    def test_check_or_prompt_report_automatically_returns_false_when_the_environment_is_development
-      # Given
-      ShopifyCLI::Environment.expects(:development?).returns(true)
-
-      # When
-      got = ReportingConfigurationController.check_or_prompt_report_automatically(context: @context)
-
-      # Then
-      refute got
-    end
-
-    def test_check_or_prompt_report_automatically_returns_false_when_the_environment_is_test
-      # Given
-      ShopifyCLI::Environment.expects(:development?).returns(false)
-      ShopifyCLI::Environment.expects(:test?).returns(true)
-
-      # When
-      got = ReportingConfigurationController.check_or_prompt_report_automatically(context: @context)
-
-      # Then
-      refute got
-    end
-
-    def test_check_or_prompt_report_automatically_returns_false_when_the_environment_is_not_interactive
-      # Given
-      ShopifyCLI::Environment.expects(:test?).returns(false)
-      ShopifyCLI::Environment.expects(:development?).returns(false)
-      ShopifyCLI::Environment.expects(:interactive?).returns(false)
-
-      # When
-      got = ReportingConfigurationController.check_or_prompt_report_automatically(context: @context)
-
-      # Then
-      refute got
-    end
-
   end
 end
