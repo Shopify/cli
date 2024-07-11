07cd4a3e0f49a508484190f5cd124662e4845f62
James Meng <james.meng@shopify.com>
2024-07-11 13:12:01 -0700

    Validate storefront password + allow user to correct password if incorrect

--- packages/theme/src/cli/services/console.test.ts
+++ packages/theme/src/cli/services/console.test.ts
@@ -1,23 +1,62 @@
 import {ensureReplEnv} from './console.js'
-import {isStorefrontPasswordProtected} from '../utilities/theme-environment/storefront-session.js'
-import {promptPassword} from '../utilities/theme-environment/theme-password.js'
+import {
+  isStorefrontPasswordCorrect,
+  isStorefrontPasswordProtected,
+  promptPassword,
+} from '../utilities/theme-environment/storefront-session.js'
 import {describe, expect, test, vi} from 'vitest'
 
 vi.mock('../utilities/theme-environment/storefront-session.js')
-vi.mock('../utilities/theme-environment/theme-password.js')
 
 describe('ensureReplEnv', () => {
-  test('should prompt for password when password is not provided', async () => {
+  test('should prompt for password when storefront is password protected', async () => {
+    // Given
     vi.mocked(isStorefrontPasswordProtected).mockResolvedValue(true)
-    const store = 'test-store'
-    await ensureReplEnv(store)
+    vi.mocked(isStorefrontPasswordCorrect).mockResolvedValue(true)
+
+    // When
+    await ensureReplEnv('test-store')
+
+    // Then
     expect(promptPassword).toHaveBeenCalled()
   })
 
-  test('should skip prompt for password when password is not provided', async () => {
+  test('should skip prompt for password when storefront is not password protected', async () => {
+    // Given
+    vi.mocked(isStorefrontPasswordProtected).mockResolvedValue(false)
+    vi.mocked(isStorefrontPasswordCorrect).mockResolvedValue(true)
+
+    // When
+    await ensureReplEnv('test-store')
+
+    // Then
+    expect(promptPassword).not.toHaveBeenCalled()
+  })
+
+  test('should skip prompt for password when correct storefront password is provided', async () => {
+    // Given
     vi.mocked(isStorefrontPasswordProtected).mockResolvedValue(true)
-    const store = 'test-store'
-    await ensureReplEnv(store)
-    expect(promptPassword).toHaveBeenCalled()
+    vi.mocked(isStorefrontPasswordCorrect).mockResolvedValue(true)
+
+    // When
+    await ensureReplEnv('test-store', 'password')
+
+    // Then
+    expect(promptPassword).not.toHaveBeenCalled()
+  })
+
+  test('should prompt for correct password when incorrect password is provided', async () => {
+    // Given
+    vi.mocked(isStorefrontPasswordProtected).mockResolvedValue(true)
+    vi.mocked(isStorefrontPasswordCorrect)
+      .mockResolvedValueOnce(false)
+      .mockResolvedValueOnce(false)
+      .mockResolvedValue(true)
+
+    // When
+    await ensureReplEnv('test-store', 'asdf')
+
+    // Then
+    expect(promptPassword).toHaveBeenCalledTimes(2)
   })
 })
--- packages/theme/src/cli/services/console.ts
+++ packages/theme/src/cli/services/console.ts
@@ -1,11 +1,16 @@
-import {isStorefrontPasswordProtected} from '../utilities/theme-environment/storefront-session.js'
-import {promptPassword} from '../utilities/theme-environment/theme-password.js'
+import {
+  isStorefrontPasswordProtected,
+  promptPassword,
+  isStorefrontPasswordCorrect,
+} from '../utilities/theme-environment/storefront-session.js'
 import {AdminSession} from '@shopify/cli-kit/node/session'
 
 export async function ensureReplEnv(store: string, password?: string) {
   const themeId = await findOrCreateReplTheme()
 
-  const finalPassword = (await shouldPromptForPassword(password, store)) ? await promptPassword() : password
+  const finalPassword = (await isStorefrontPasswordProtected(store))
+    ? await promptValidPassword(password, store)
+    : password
 
   return {
     themeId,
@@ -13,16 +18,19 @@ export async function ensureReplEnv(store: string, password?: string) {
   }
 }
 
-async function findOrCreateReplTheme(): Promise<string> {
-  return ''
+async function promptValidPassword(password: string | undefined, store: string) {
+  let finalPassword = password || (await promptPassword('Enter your theme password: '))
+
+  // eslint-disable-next-line no-await-in-loop
+  while (!(await isStorefrontPasswordCorrect(finalPassword, store))) {
+    // eslint-disable-next-line no-await-in-loop
+    finalPassword = await promptPassword('Incorrect password provided. Please try again: ')
+  }
+  return finalPassword
 }
 
-async function shouldPromptForPassword(password: string | undefined, store: string) {
-  if (password) {
-    return false
-  } else {
-    return isStorefrontPasswordProtected(store)
-  }
+async function findOrCreateReplTheme(): Promise<string> {
+  return ''
 }
 
 export async function repl(
