067d2455d40f38f8babfe74d82cd75ae94b67a40
James Meng <james.meng@shopify.com>
2024-07-10 17:09:22 -0700

    Prompt user for password if storefront is password protected

--- packages/theme/src/cli/services/console.ts
+++ packages/theme/src/cli/services/console.ts
@@ -1,3 +1,28 @@
+import {isStorefrontPasswordProtected} from '../utilities/theme-environment/storefront-session.js'
+import {consoleLog} from '@shopify/cli-kit/node/output'
 import {AdminSession} from '@shopify/cli-kit/node/session'
+import {createInterface} from 'readline'
+
+export async function ensureReplEnv(store: string) {
+  const storefrontHasPassword = await isStorefrontPasswordProtected(store)
+  consoleLog(storefrontHasPassword.toString())
+  if (storefrontHasPassword) {
+    const password = await promptPassword()
+    consoleLog(password)
+  }
+}
 
 export async function repl(_adminSession: AdminSession, _storefrontToken: string, _password?: string) {}
+
+function promptPassword(): Promise<string> {
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  return new Promise((resolve) => {
+    readline.question('Enter your theme password: ', (password) => {
+      resolve(password)
+    })
+  })
+}
