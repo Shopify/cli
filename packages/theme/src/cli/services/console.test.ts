703a346bb3b2e6c619b9f4e59606ba72e51cdf5d
James Meng <james.meng@shopify.com>
2024-07-16 12:55:42 -0700

    refactor: Move password prompt logic to separate module

--- packages/theme/src/cli/services/console.test.ts
+++ packages/theme/src/cli/services/console.test.ts
@@ -3,15 +3,15 @@ import {
   isStorefrontPasswordCorrect,
   isStorefrontPasswordProtected,
 } from '../utilities/theme-environment/storefront-session.js'
+import {ensureValidPassword} from '../utilities/prompts.js'
 import {beforeEach, describe, expect, test, vi} from 'vitest'
-import {renderTextPrompt} from '@shopify/cli-kit/node/ui'
 
 vi.mock('../utilities/theme-environment/storefront-session.js')
-vi.mock('@shopify/cli-kit/node/ui')
+vi.mock('../utilities/prompts.js')
 
 describe('ensureReplEnv', () => {
   beforeEach(() => {
-    vi.mocked(renderTextPrompt).mockResolvedValue('testPassword')
+    vi.mocked(ensureValidPassword).mockResolvedValue('testPassword')
   })
 
   test('should prompt for password when storefront is password protected', async () => {
@@ -23,7 +23,7 @@ describe('ensureReplEnv', () => {
     const {storePassword} = await ensureReplEnv('test-store')
 
     // Then
-    expect(renderTextPrompt).toHaveBeenCalledWith({message: 'Enter your theme password', password: true})
+    expect(ensureValidPassword).toHaveBeenCalled()
     expect(storePassword).toBe('testPassword')
   })
 
@@ -33,36 +33,10 @@ describe('ensureReplEnv', () => {
     vi.mocked(isStorefrontPasswordCorrect).mockResolvedValue(true)
 
     // When
-    await ensureReplEnv('test-store')
+    const {storePassword} = await ensureReplEnv('test-store')
 
     // Then
-    expect(renderTextPrompt).not.toHaveBeenCalled()
-  })
-
-  test('should skip prompt for password when correct storefront password is provided', async () => {
-    // Given
-    vi.mocked(isStorefrontPasswordProtected).mockResolvedValue(true)
-    vi.mocked(isStorefrontPasswordCorrect).mockResolvedValue(true)
-
-    // When
-    await ensureReplEnv('test-store', 'password')
-
-    // Then
-    expect(renderTextPrompt).not.toHaveBeenCalled()
-  })
-
-  test('should prompt for correct password when incorrect password is provided', async () => {
-    // Given
-    vi.mocked(isStorefrontPasswordProtected).mockResolvedValue(true)
-    vi.mocked(isStorefrontPasswordCorrect)
-      .mockResolvedValueOnce(false)
-      .mockResolvedValueOnce(false)
-      .mockResolvedValue(true)
-
-    // When
-    await ensureReplEnv('test-store', 'asdf')
-
-    // Then
-    expect(renderTextPrompt).toHaveBeenCalledTimes(2)
+    expect(ensureValidPassword).not.toHaveBeenCalled()
+    expect(storePassword).toBeUndefined()
   })
 })
