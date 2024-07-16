07e774a44ed0bec3ff68272271bb81d73ac60086
James Meng <james.meng@shopify.com>
2024-07-16 12:24:54 -0700

    Use renderTextPrompt from UI-Kit rather than readline to prompt password

--- packages/theme/src/cli/services/console.test.ts
+++ packages/theme/src/cli/services/console.test.ts
@@ -2,23 +2,29 @@ import {ensureReplEnv} from './console.js'
 import {
   isStorefrontPasswordCorrect,
   isStorefrontPasswordProtected,
-  promptPassword,
 } from '../utilities/theme-environment/storefront-session.js'
-import {describe, expect, test, vi} from 'vitest'
+import {beforeEach, describe, expect, test, vi} from 'vitest'
+import {renderTextPrompt} from '@shopify/cli-kit/node/ui'
 
 vi.mock('../utilities/theme-environment/storefront-session.js')
+vi.mock('@shopify/cli-kit/node/ui')
 
 describe('ensureReplEnv', () => {
+  beforeEach(() => {
+    vi.mocked(renderTextPrompt).mockResolvedValue('testPassword')
+  })
+
   test('should prompt for password when storefront is password protected', async () => {
     // Given
     vi.mocked(isStorefrontPasswordProtected).mockResolvedValue(true)
     vi.mocked(isStorefrontPasswordCorrect).mockResolvedValue(true)
 
     // When
-    await ensureReplEnv('test-store')
+    const {storePassword} = await ensureReplEnv('test-store')
 
     // Then
-    expect(promptPassword).toHaveBeenCalled()
+    expect(renderTextPrompt).toHaveBeenCalledWith({message: 'Enter your theme password', password: true})
+    expect(storePassword).toBe('testPassword')
   })
 
   test('should skip prompt for password when storefront is not password protected', async () => {
@@ -30,7 +36,7 @@ describe('ensureReplEnv', () => {
     await ensureReplEnv('test-store')
 
     // Then
-    expect(promptPassword).not.toHaveBeenCalled()
+    expect(renderTextPrompt).not.toHaveBeenCalled()
   })
 
   test('should skip prompt for password when correct storefront password is provided', async () => {
@@ -42,7 +48,7 @@ describe('ensureReplEnv', () => {
     await ensureReplEnv('test-store', 'password')
 
     // Then
-    expect(promptPassword).not.toHaveBeenCalled()
+    expect(renderTextPrompt).not.toHaveBeenCalled()
   })
 
   test('should prompt for correct password when incorrect password is provided', async () => {
@@ -57,6 +63,6 @@ describe('ensureReplEnv', () => {
     await ensureReplEnv('test-store', 'asdf')
 
     // Then
-    expect(promptPassword).toHaveBeenCalledTimes(2)
+    expect(renderTextPrompt).toHaveBeenCalledTimes(2)
   })
 })
