a5a6028262a885d6dbc84453cb5285f524945f15
James Meng <james.meng@shopify.com>
2024-07-16 12:39:04 -0700

    Remove unnecessary store parameter from ensureReplEnv

--- packages/theme/src/cli/services/console.ts
+++ packages/theme/src/cli/services/console.ts
@@ -1,16 +1,16 @@
-import {ensureValidPassword} from '../utilities/prompts.js'
 import {isStorefrontPasswordProtected} from '../utilities/theme-environment/storefront-session.js'
 import {REPLThemeManager} from '../utilities/repl-theme-manager.js'
+import {ensureValidPassword} from '../utilities/prompts.js'
 import {DEVELOPMENT_THEME_ROLE} from '@shopify/cli-kit/node/themes/utils'
 import {CLI_KIT_VERSION} from '@shopify/cli-kit/common/version'
 import {AdminSession} from '@shopify/cli-kit/node/session'
 
-export async function ensureReplEnv(adminSession: AdminSession, store: string, storePasswordFlag?: string) {
+export async function ensureReplEnv(adminSession: AdminSession, storePasswordFlag?: string) {
   const themeId = await findOrCreateReplTheme(adminSession)
 
-  const storePassword = (await isStorefrontPasswordProtected(store))
-    ? await ensureValidPassword(storePasswordFlag, store)
-    : undefined
+  const storePassword = (await isStorefrontPasswordProtected(adminSession.storeFqdn))
+    ? await ensureValidPassword(storePasswordFlag, adminSession.storeFqdn)
+    : storePasswordFlag
 
   return {
     themeId,
