575534267b689bc29bd7797a8ff98ca5ef28df68
James Meng <james.meng@shopify.com>
2024-07-17 10:38:07 -0700

    Initiate REPL Loop

--- packages/theme/src/cli/services/console.ts
+++ packages/theme/src/cli/services/console.ts
@@ -1,7 +1,11 @@
 import {isStorefrontPasswordProtected} from '../utilities/theme-environment/storefront-session.js'
 import {REPLThemeManager} from '../utilities/repl-theme-manager.js'
 import {ensureValidPassword} from '../utilities/prompts.js'
+import {DevServerSession} from '../utilities/theme-environment/types.js'
+import {render} from '../utilities/theme-environment/storefront-renderer.js'
 import {AdminSession} from '@shopify/cli-kit/node/session'
+import {renderTextPrompt} from '@shopify/cli-kit/node/ui'
+import {consoleLog} from '@shopify/cli-kit/node/output'
 
 export async function ensureReplEnv(adminSession: AdminSession, storePasswordFlag?: string) {
   const themeId = await findOrCreateReplTheme(adminSession)
@@ -24,8 +28,48 @@ async function findOrCreateReplTheme(adminSession: AdminSession): Promise<string
 }
 
 export async function repl(
-  _adminSession: AdminSession,
-  _storefrontToken: string,
-  _themeId: string,
-  _password: string | undefined,
-) {}
+  adminSession: AdminSession,
+  storefrontToken: string,
+  themeId: string,
+  password: string | undefined,
+) {
+  const inputValue = await renderTextPrompt({message: 'Enter a value'})
+  const evaluatedValue = await evaluate(inputValue, adminSession, storefrontToken, themeId, password)
+  const regex = />([^<]+)</
+  const match = evaluatedValue.match(regex)
+
+  if (match && match[1]) {
+    consoleLog(match[1])
+  }
+  return repl(adminSession, storefrontToken, themeId, password)
+}
+
+export async function evaluate(
+  snippet: string,
+  adminSession: AdminSession,
+  storefrontToken: string,
+  themeId: string,
+  password: string | undefined,
+) {
+  const session: DevServerSession = {
+    ...adminSession,
+    storefrontToken,
+    storefrontPassword: password,
+    expiresAt: new Date(),
+  }
+
+  const response = await render(session, {
+    path: '/',
+    query: [],
+    themeId,
+    cookies: '',
+    sectionId: 'announcement-bar',
+
+    headers: {},
+    replaceTemplates: {
+      'sections/announcement-bar.liquid': `{{ ${snippet} }}`,
+    },
+  })
+
+  return response.text()
+}
