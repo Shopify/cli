75e4ebc1643f47c310f63c62adb332971feddff8
Fran Dios <fran.dios@shopify.com>
2024-08-06 18:52:37 +0200

    Render raw storefront

--- packages/theme/src/cli/utilities/theme-environment/theme-environment.ts
+++ packages/theme/src/cli/utilities/theme-environment/theme-environment.ts
@@ -1,5 +1,6 @@
 import {reconcileAndPollThemeEditorChanges} from './remote-theme-watcher.js'
 import {DevServerContext} from './types.js'
+import {render} from './storefront-renderer.js'
 import {uploadTheme} from '../theme-uploader.js'
 import {createApp, defineEventHandler} from 'h3'
 import {Theme} from '@shopify/cli-kit/node/themes/types'
@@ -28,12 +29,45 @@ async function ensureThemeEnvironmentSetup(theme: Theme, ctx: DevServerContext) 
   })
 }
 
-function startDevelopmentServer(_theme: Theme, ctx: DevServerContext) {
+function startDevelopmentServer(theme: Theme, ctx: DevServerContext) {
   const app = createApp()
 
   app.use(
-    defineEventHandler((_event) => {
-      return 'Hello, world!'
+    defineEventHandler(async (event) => {
+      const {req, res} = event
+      if (!req.url || req.method !== 'GET') {
+        // Mock the well-known route to avoid 404s
+        return req.url?.startsWith('/.well-known') ? '' : undefined
+      }
+
+      // eslint-disable-next-line no-console
+      console.log(`${req.method} ${req.url}`)
+
+      const reqForwardHeaders = Object.entries(req.headers).reduce((acc, [key, value]) => {
+        if (value && key !== 'host') acc[key] = Array.isArray(value) ? value.join(';') : value
+        return acc
+      }, {} as {[key: string]: string})
+
+      const response = await render(ctx.session, {
+        path: req.url,
+        query: [],
+        themeId: theme.id.toString(),
+        cookies: req.headers.cookie || '',
+        sectionId: '',
+        headers: reqForwardHeaders,
+        replaceTemplates: {},
+      })
+
+      res.statusCode = response.status
+      res.statusMessage = response.statusText
+      for (const [key, value] of Object.entries(response.headers.raw())) {
+        // headers.getSetCookie is not defined, use headers.raw
+        if (key !== 'content-encoding') {
+          res.setHeader(key, value)
+        }
+      }
+
+      return response.text()
     }),
   )
 
