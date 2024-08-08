0e259c5fd2a99d12a428413729941e83080623f3
Fran Dios <fran.dios@shopify.com>
2024-08-08 17:46:06 +0200

    Move assets logic

--- packages/theme/src/cli/utilities/theme-environment/assets.ts
+++ packages/theme/src/cli/utilities/theme-environment/assets.ts
@@ -1,7 +1,14 @@
 import {readFile} from '@shopify/cli-kit/node/fs'
 import {lookupMimeType} from '@shopify/cli-kit/node/mimes'
 import {H3Event, serveStatic} from 'h3'
+import {joinPath} from '@shopify/cli-kit/node/path'
 import {stat} from 'fs/promises'
+
+const ASSETS_RE = /^\/cdn\/shop\/t\/\d+\/assets\/(.+)$/
+
+export function isAssetsRequest(event: H3Event) {
+  return ASSETS_RE.test(event.path)
+}
 
 export function replaceLocalAssets(html: string) {
   return html.replace(
@@ -10,7 +17,12 @@ export function replaceLocalAssets(html: string) {
   )
 }
 
-export async function serveLocalAsset(event: H3Event, filepath: string) {
+export async function serveLocalAsset(event: H3Event, rootDir: string) {
+  const filename = event.path.match(ASSETS_RE)?.[1]
+  if (!filename) return
+
+  const filepath = joinPath(rootDir, 'assets', filename)
+
   return serveStatic(event, {
     getContents: () => readFile(filepath),
     getMeta: async () => {
