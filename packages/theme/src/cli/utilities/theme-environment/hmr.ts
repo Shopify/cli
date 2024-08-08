f4039e5d25219deb97bc40176dc095e00db4bf12
Fran Dios <fran.dios@shopify.com>
2024-08-08 20:22:15 +0200

    HMR

--- packages/theme/src/cli/utilities/theme-environment/hmr.ts
+++ packages/theme/src/cli/utilities/theme-environment/hmr.ts
@@ -1,30 +1,71 @@
+// eslint-disable-next-line spaced-comment, @typescript-eslint/triple-slash-reference
+/// <reference lib="dom" />
 import {render} from './storefront-renderer.js'
 import {DevServerContext} from './types.js'
+import {createEventStream, defineEventHandler} from 'h3'
 import {Theme} from '@shopify/cli-kit/node/themes/types'
+import {renderWarning} from '@shopify/cli-kit/node/ui'
 import EventEmitter from 'node:events'
 
-function createFastRefreshScript() {
-  function fastRefreshScript() {
-    console.log('fastRefreshScript')
-  }
-
-  return `<script>(${fastRefreshScript.toString()})()</script>`
-}
-
-export function injectFastRefreshScript(html: string) {
-  return html.replace(/<\/head>/, `${createFastRefreshScript()}</head>`)
-}
-
-const updatedTemplates = {} as {[key: string]: string}
 const eventEmitter = new EventEmitter()
+const updatedReplaceTemplates = {} as {[key: string]: string}
+
+type HmrEvent =
+  | {
+      type: 'section'
+      key: string
+      content: string
+    }
+  | {
+      type: 'other'
+      key: string
+    }
+
+function emitHmrEvent(event: HmrEvent) {
+  eventEmitter.emit('hmr', event)
+}
 
 export function getReplaceTemplates() {
-  return {...updatedTemplates}
+  return {...updatedReplaceTemplates}
 }
 
-export async function hmrSection(theme: Theme, ctx: DevServerContext, key: string) {
+export function setReplaceTemplate(key: string, content: string) {
+  updatedReplaceTemplates[key] = content
+}
+
+export function deleteReplaceTemplate(key: string) {
+  delete updatedReplaceTemplates[key]
+}
+
+export function getHmrHandler() {
+  return defineEventHandler((event) => {
+    if (event.path !== '/__hmr') return
+
+    const eventStream = createEventStream(event)
+
+    eventEmitter.on('hmr', (event: HmrEvent) => {
+      eventStream.push(JSON.stringify(event)).catch((error: Error) => {
+        renderWarning({headline: 'Failed to send HMR event.', body: error?.stack})
+      })
+    })
+
+    return eventStream.send()
+  })
+}
+
+export async function triggerHmr(theme: Theme, ctx: DevServerContext, key: string) {
   const sectionId = key.match(/^sections\/(.+)\.liquid$/)?.[1]
-  if (!sectionId) return
+
+  if (!sectionId) {
+    emitHmrEvent({type: 'other', key})
+    return
+  }
+
+  const sectionTemplate = updatedReplaceTemplates[key]
+  if (!sectionTemplate) {
+    renderWarning({headline: 'No template found for HMR event.', body: `Template ${key} not found.`})
+    return
+  }
 
   const response = await render(ctx.session, {
     path: '/',
@@ -33,14 +74,50 @@ export async function hmrSection(theme: Theme, ctx: DevServerContext, key: strin
     cookies: '',
     sectionId,
     headers: {},
-    replaceTemplates: {[key]: updatedTemplates[key]!},
+    replaceTemplates: {[key]: sectionTemplate},
   })
 
   const content = await response.text()
-  console.log('content:', content, response.status, response.statusText, {
-    key,
-    sectionId,
-  })
 
-  eventEmitter.emit('request', {key, content})
+  emitHmrEvent({type: 'section', key, content})
+}
+
+function injectFunction(fn: () => void) {
+  return `<script>(${fn.toString()})()</script>`
+}
+
+export function injectFastRefreshScript(html: string) {
+  // These function runs in the browser:
+
+  function fastRefreshScript() {
+    const evtSource = new EventSource('/__hmr', {withCredentials: true})
+
+    evtSource.onopen = () => {
+      // eslint-disable-next-line no-console
+      console.info('[HMR] Connected')
+    }
+
+    evtSource.onmessage = (event) => {
+      if (typeof event.data !== 'string') return
+
+      const data = JSON.parse(event.data) as HmrEvent
+      if (data.type === 'section') {
+        const id = data.content.match(/id="([^"]+)"/)?.[1]
+        if (id) {
+          const existingElement = document.getElementById(id)
+          if (existingElement) {
+            existingElement.outerHTML = data.content
+            // eslint-disable-next-line no-console
+            console.info('[HMR] Updated section:', data.key)
+          }
+        }
+      } else if (data.type === 'other') {
+        // eslint-disable-next-line no-console
+        console.info('[HMR] Full reload:', data.key)
+        window.location.reload()
+      }
+    }
+  }
+
+  return html.replace(/<\/head>/, `${injectFunction(fastRefreshScript)}</head>`)
 }
