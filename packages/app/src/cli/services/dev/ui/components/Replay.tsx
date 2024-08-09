16ed1b146c37d8a6a8428e0dc9a854645eeaedf1
Alex Bradley <alexandre.bradley@shopify.com>
2024-08-09 14:49:28 -0400

    instruction count and input handling

--- packages/app/src/cli/services/dev/ui/components/Replay.tsx
+++ packages/app/src/cli/services/dev/ui/components/Replay.tsx
@@ -11,6 +11,10 @@ import {FunctionConfigType} from '../../../../models/extensions/specifications/f
 import {AppInterface} from '../../../../models/app/app.js'
 import {Writable} from 'stream'
 import { prettyPrintJsonIfPossible } from '../../../app-logs/utils.js'
+import { handleCtrlC } from '@shopify/cli-kit/node/ui'
+import { useAbortSignal } from '@shopify/cli-kit/node/ui/hooks'
+import { isUnitTest } from '@shopify/cli-kit/node/context/local'
+import { treeKill } from '@shopify/cli-kit/node/tree-kill'
 
 export interface ReplayProps {
   selectedRun: FunctionRunData
@@ -24,6 +28,10 @@ interface FunctionRun {
   input: string
   output: string
   logs: string
+  name: string,
+  size: number,
+  memory_usage: number,
+  instructions: number,
 }
 
 interface SystemMessage {
@@ -34,16 +42,35 @@ interface SystemMessage {
 type ReplayLog = FunctionRun | SystemMessage
 
 const Replay: FunctionComponent<ReplayProps> = ({selectedRun, abortController, app, extension}) => {
-  const now = new Date()
-  const season = now.getMonth() > 3 ? 'Summer' : 'Winter'
-  const year = now.getFullYear()
+  const functionRunFromSelectedRun = {
+    type: 'functionRun',
+    input: selectedRun.payload.input,
+    output: selectedRun.payload.output,
+    logs: selectedRun.payload.logs,
+    name: selectedRun.source,
+    size: 0,
+    memory_usage: 0,
+    instructions: selectedRun.payload.fuelConsumed,
+  } as FunctionRun
 
   // const [functionRuns, setFunctionRuns] = useState<FunctionRun[]>([])
   // const [replayLogs, setReplayLogs] = useState<String[]>([])
   const [logs, setLogs] = useState<ReplayLog[]>([])
+  const [recentFunctionRuns, setRecentFunctionRuns] = useState<[FunctionRun, FunctionRun]>([functionRunFromSelectedRun, functionRunFromSelectedRun])
+
+  const [error, setError] = useState<string | undefined>(undefined)
 
   const {input, export: runExport} = selectedRun.payload
 
+
+  const {isRawModeSupported: canUseShortcuts} = useStdin()
+  const pollingInterval = useRef<NodeJS.Timeout>()
+
+  const delta = useMemo(() => {
+    return recentFunctionRuns[0].instructions - recentFunctionRuns[1].instructions
+  }, [recentFunctionRuns])
+
+  const [statusMessage, setStatusMessage] = useState("Default Status Message")
   useEffect(() => {
     const startWatchingFunction = async () => {
       const customStdout = new Writable({
@@ -64,7 +91,7 @@ const Replay: FunctionComponent<ReplayProps> = ({selectedRun, abortController, a
           // console.log("in onChange")
           // setLogs((logs) => [...logs, {type: 'systemMessage', message: 'Changes detected, rebuilding and rerunning'}])
           const functionRun = await runFunctionRunnerWithLogInput(extension, JSON.stringify(input), runExport)
-          // console.log("the functionRun in onChange")
+          // console.log("++++ the functionRun in onChange")
           // console.log(JSON.parse(functionRun.output).JsonOutput)
           // console.log("the function run to be added")
           // console.log(functionRun)
@@ -72,6 +99,17 @@ const Replay: FunctionComponent<ReplayProps> = ({selectedRun, abortController, a
           // functionRun.output = JSON.parse(functionRun.output).JsonOutput
           // console.log("all the functionRuns")
           // console.log(functionRuns)
+
+          setRecentFunctionRuns((recentFunctionRuns) => {
+            return [functionRun, recentFunctionRuns[0]]
+          })
+          // setPrevLatestFunctionRun(latestFunctionRun)
+          // setLatestFunctionRun(functionRun)
+          // console.log(">>>> new latest logs")
+          // console.log("prev")
+          // console.log(prevLatestFunctionRun)
+          // console.log("latest")
+          // console.log(latestFunctionRun)
           setLogs((logs) => [...logs, functionRun])
         },
         onReloadAndBuildError: async (error) => {
@@ -86,6 +124,45 @@ const Replay: FunctionComponent<ReplayProps> = ({selectedRun, abortController, a
 
     // TODO: return a way to clean up watcher
   }, [input, runExport, app, extension])
+
+
+  const {isAborted} = useAbortSignal(abortController.signal, async (err) => {
+    if (err) {
+      setStatusMessage('Shutting down replay watcher because of an error ...')
+    } else {
+      setStatusMessage('Shutting down replay watcher ...')
+      setTimeout(() => {
+        if (isUnitTest()) return
+        treeKill(process.pid, 'SIGINT', false, () => {
+          process.exit(0)
+        })
+      }, 2000)
+    }
+    clearInterval(pollingInterval.current)
+  })
+
+  useInput(
+    (input, key) => {
+      handleCtrlC(input, key, () => abortController.abort())
+
+      const onInput = async () => {
+        try {
+          setError('')
+
+          if (input === 'q') {
+            abortController.abort()
+          }
+          // eslint-disable-next-line no-catch-all/no-catch-all
+        } catch (_) {
+          setError('Failed to handle your input.')
+        }
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      onInput()
+    },
+    {isActive: Boolean(canUseShortcuts)},
+  )
 
   return (
     <>
@@ -111,24 +188,27 @@ const Replay: FunctionComponent<ReplayProps> = ({selectedRun, abortController, a
         borderRight={false}
         borderTop
       >
-        {/* {canUseShortcuts ? ( */}
+        {canUseShortcuts ? (
         <Box flexDirection="column">
           <Box flexDirection="row">
-          <Text>
-            {figures.pointerSmall}&nbsp;
-          </Text>
-          {(selectedRun.status === "success") ? (
-            <Text color="white" backgroundColor="green">
-              {selectedRun.status.toUpperCase()}
+            <Text>
+              {figures.pointerSmall}&nbsp;
             </Text>
-            ) : (
-            <Text color="white" backgroundColor="red">
-              {selectedRun.status.toUpperCase()}
+            {(selectedRun.status === "success") ? (
+              <Text color="white" backgroundColor="green">
+                {selectedRun.status.toUpperCase()}
+              </Text>
+              ) : (
+              <Text color="white" backgroundColor="red">
+                {selectedRun.status.toUpperCase()}
+              </Text>
+              )}
+            <Text>
+              &nbsp;| Watching for changes to {selectedRun.source}...
             </Text>
-            )}
-          <Text>
-            &nbsp;| Watching for changes to {selectedRun.source}...
-          </Text>
+          </Box>
+          <Box flexDirection="column">
+            <Text>{figures.pointerSmall} Instruction count delta: {delta}</Text>
           </Box>
           <Text>
             {figures.pointerSmall} Press <Text bold>d</Text> {figures.lineVertical} diff output with original
@@ -137,6 +217,11 @@ const Replay: FunctionComponent<ReplayProps> = ({selectedRun, abortController, a
             {figures.pointerSmall} Press <Text bold>q</Text> {figures.lineVertical} quit
           </Text>
         </Box>
+        ) : null}
+        <Box marginTop={canUseShortcuts ? 1 : 0}>
+            <Text>{statusMessage}</Text>
+          </Box>
+          {error ? <Text color="red">{error}</Text> : null}
       </Box>
     </>
   )
