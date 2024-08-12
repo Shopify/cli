40297851db6bb6c1b2aead68ae2c5a6b4090d336
Ariel Caplan <ariel.caplan@shopify.com>
2024-08-12 13:22:47 +0300

    Truncate version output content to just the version itself
    Use unadorned output so piping etc. works correctly

--- packages/cli/src/cli/services/commands/version.ts
+++ packages/cli/src/cli/services/commands/version.ts
@@ -1,6 +1,6 @@
 import {CLI_KIT_VERSION} from '@shopify/cli-kit/common/version'
-import {outputInfo, outputContent, outputToken} from '@shopify/cli-kit/node/output'
+import {outputInfo} from '@shopify/cli-kit/node/output'
 
 export async function versionService(): Promise<void> {
-  outputInfo(outputContent`Current Shopify CLI version: ${outputToken.yellow(CLI_KIT_VERSION)}`.value)
+  outputInfo(CLI_KIT_VERSION)
 }
