d1ae7cbcc19c6f9cbf857ce3ff031679aa830cc9
Fran Dios <fran.dios@shopify.com>
2024-08-12 15:24:05 +0200

    Use local files to replace assets in HTML instead of matching domain

--- packages/theme/src/cli/utilities/theme-environment/assets.ts
+++ packages/theme/src/cli/utilities/theme-environment/assets.ts
@@ -3,14 +3,25 @@ import {lookupMimeType} from '@shopify/cli-kit/node/mimes'
 import {defineEventHandler, serveStatic} from 'h3'
 import {joinPath} from '@shopify/cli-kit/node/path'
 import {stat} from 'fs/promises'
+import type {ThemeFileSystem} from '@shopify/cli-kit/node/themes/types'
 
 /** Forces assets to be served from local domain */
-export function replaceLocalAssets(html: string) {
-  // Matches assets domain in HTML link/script tags
-  return html.replace(
-    /<(?:link|script)\s?[^>]*\s(?:href|src)="(\/\/[^.]+\.myshopify\.com)\/cdn\/shop\/t\/\d+\/assets\//g,
-    (all, m1) => all.replaceAll(m1, ''),
-  )
+export function replaceLocalAssets(originalHtml: string, themeFileSystem: ThemeFileSystem) {
+  let html = originalHtml
+  const existingAssets = [...themeFileSystem.files.keys()].filter((key) => key.startsWith('assets'))
+
+  for (const [, matchedUrl] of html.matchAll(/<(?:link|script)\s?[^>]*\s(?:href|src)="([^"]+)"/g)) {
+    if (!matchedUrl) continue
+
+    const matchedAsset = existingAssets.find((asset) => matchedUrl.includes(asset))
+
+    if (matchedAsset) {
+      const querystring = matchedUrl.split('?')[1] ?? ''
+      html = html.replaceAll(matchedUrl, `/${matchedAsset}${querystring ? `?${querystring}` : ''}`)
+    }
+  }
+
+  return html
 }
 
 export function getAssetsHandler(rootDir: string) {
@@ -18,7 +29,7 @@ export function getAssetsHandler(rootDir: string) {
     if (event.method !== 'GET') return
 
     // Matches asset filenames in an HTTP Request URL path
-    const assetsFilename = event.path.match(/^\/cdn\/shop\/t\/\d+\/assets\/(.+)$/)?.[1]
+    const assetsFilename = event.path.match(/^\/assets\/(.+)$/)?.[1]
 
     if (assetsFilename) {
       const filepath = joinPath(rootDir, 'assets', assetsFilename)
