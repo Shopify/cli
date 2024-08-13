23c564e07f7f46f0ce8a94a91a66115dc492092c
Fran Dios <fran.dios@shopify.com>
2024-08-13 13:51:24 +0200

    Move more code to hot-reload file

--- packages/theme/src/cli/utilities/theme-environment/theme-environment.ts
+++ packages/theme/src/cli/utilities/theme-environment/theme-environment.ts
@@ -1,17 +1,9 @@
 import {reconcileAndPollThemeEditorChanges} from './remote-theme-watcher.js'
 import {DevServerContext} from './types.js'
 import {render} from './storefront-renderer.js'
-import {
-  getReplaceTemplates,
-  triggerHotReload,
-  injectHotReloadScript,
-  getHotReloadHandler,
-  setReplaceTemplate,
-  deleteReplaceTemplate,
-} from './hot-reload.js'
+import {setupTemplateWatcher, injectHotReloadScript, getHotReloadHandler} from './hot-reload.js'
 import {getAssetsHandler, replaceLocalAssets} from './assets.js'
 import {uploadTheme} from '../theme-uploader.js'
-import {THEME_DEFAULT_IGNORE_PATTERNS, THEME_DIRECTORY_PATTERNS} from '../theme-fs.js'
 import {
   createApp,
   defineEventHandler,
@@ -23,14 +15,10 @@ import {
   proxyRequest,
 } from 'h3'
 import {Theme} from '@shopify/cli-kit/node/themes/types'
-import {readFile} from '@shopify/cli-kit/node/fs'
-import {joinPath, relativePath, extname} from '@shopify/cli-kit/node/path'
-import {renderWarning} from '@shopify/cli-kit/node/ui'
 import {createServer} from 'node:http'
 
 export async function setupDevServer(theme: Theme, ctx: DevServerContext, onReady: () => void) {
   await ensureThemeEnvironmentSetup(theme, ctx)
-  await watchTemplates(ctx)
   await startDevelopmentServer(theme, ctx)
 
   onReady()
@@ -52,8 +40,10 @@ async function ensureThemeEnvironmentSetup(theme: Theme, ctx: DevServerContext) 
   })
 }
 
-function startDevelopmentServer(theme: Theme, ctx: DevServerContext) {
+async function startDevelopmentServer(theme: Theme, ctx: DevServerContext) {
   const app = createApp()
+
+  const {getInMemoryTemplates} = await setupTemplateWatcher(ctx)
 
   app.use(getHotReloadHandler(theme, ctx))
   app.use(getAssetsHandler(ctx.directory))
@@ -85,7 +75,7 @@ function startDevelopmentServer(theme: Theme, ctx: DevServerContext) {
         cookies: headers.get('cookie') || '',
         sectionId: '',
         headers: getProxyRequestHeaders(event),
-        replaceTemplates: getReplaceTemplates(),
+        replaceTemplates: getInMemoryTemplates(),
       })
 
       setResponseStatus(event, response.status, response.statusText)
@@ -100,48 +90,4 @@ function startDevelopmentServer(theme: Theme, ctx: DevServerContext) {
   )
 
   return new Promise((resolve) => createServer(toNodeListener(app)).listen(ctx.options.port, () => resolve(undefined)))
-}
-
-async function watchTemplates(ctx: DevServerContext) {
-  const {default: chokidar} = await import('chokidar')
-
-  const directoriesToWatch = new Set(
-    THEME_DIRECTORY_PATTERNS.filter((pattern) => !pattern.includes('assets')).map((pattern) =>
-      joinPath(ctx.directory, pattern.split('/').shift() ?? ''),
-    ),
-  )
-
-  let initialized = false
-  const getKey = (filePath: string) => relativePath(ctx.directory, filePath)
-
-  chokidar
-    .watch([...directoriesToWatch], {
-      ignored: [...THEME_DEFAULT_IGNORE_PATTERNS, '**/assets/**'],
-      persistent: true,
-      ignoreInitial: false,
-    })
-    .on('ready', () => (initialized = true))
-    .on('add', updateMemoryTemplate)
-    .on('change', updateMemoryTemplate)
-    .on('unlink', (filePath) => deleteReplaceTemplate(getKey(filePath)))
-
-  function updateMemoryTemplate(filePath: string) {
-    const extension = extname(filePath)
-    // console.debug('lala', filePath, Boolean(extension), {extension})
-    if (!['.liquid', '.json'].includes(extension)) return
-
-    // During initialization we only want to process
-    // JSON files to cache their contents early
-    if (!initialized && extension !== '.json') return
-
-    const key = getKey(filePath)
-
-    readFile(filePath)
-      .then((content) => setReplaceTemplate(key, content))
-      .catch((error) => renderWarning({headline: `Failed to read file ${filePath}: ${error.message}`}))
-      .then(() => triggerHotReload(key))
-      .catch((error) =>
-        renderWarning({headline: `Failed to fast refresh ${key}: ${error.message}\nPlease reload the page.`}),
-      )
-  }
 }
