d1c4d04bdf3582b3c3a8f47c98d16f9c459351cd
Alex Bradley <alexandre.bradley@shopify.com>
2024-08-14 15:13:20 -0400

    component test

--- packages/app/src/cli/services/function/ui/components/Replay.test.tsx
+++ packages/app/src/cli/services/function/ui/components/Replay.test.tsx
@@ -5,8 +5,23 @@ import {FunctionConfigType} from '../../../../models/extensions/specifications/f
 import {render} from '@shopify/cli-kit/node/testing/ui'
 import {AbortController} from '@shopify/cli-kit/node/abort'
 import React from 'react'
-import {beforeAll, describe, expect, test} from 'vitest'
+import {beforeAll, describe, expect, test, vi} from 'vitest'
 import {unstyled} from '@shopify/cli-kit/node/output'
+import { setupExtensionWatcherForReplay } from './hooks/extension-watcher.js'
+import { FunctionRunData } from '../../replay.js'
+
+vi.mock('./hooks/extension-watcher.js')
+
+interface FunctionRun {
+  type: 'functionRun'
+  input: string
+  output: string
+  logs: string
+  name: string
+  size: number
+  memory_usage: number
+  instructions: number
+}
 
 const defaultConfig = {
   name: 'MyFunction',
@@ -27,77 +42,107 @@ beforeAll(async () => {
   extension = await testFunctionExtension({config: defaultConfig})
 })
 
+const SELECTED_RUN = {
+  shopId: 69665030382,
+  apiClientId: 124042444801,
+  payload: {
+    export: 'run',
+    input: {
+      someInput: "someInput"
+    },
+    inputBytes: 136,
+    output: {
+      someOutput: "someOutput"
+    },
+    outputBytes: 195,
+    logs: 'First Log\nLog the second!,\n1,\nfourth line, length should be above!,\nFifth line!',
+    functionId: '34236fa9-42f5-4bb6-adaf-956e12fff0b0',
+    fuelConsumed: 532632,
+  },
+  logType: 'function_run',
+  cursor: '2024-08-02T17:45:27.683139Z',
+  status: 'success',
+  source: 'product-discount',
+  sourceNamespace: 'extensions',
+  logTimestamp: '2024-08-02T17:45:27.382075Z',
+  identifier: '123456',
+} as FunctionRunData
+
+const FUNCTION_RUN_FROM_SELECTED_RUN = {
+  type: 'functionRun',
+  input: SELECTED_RUN.payload.input,
+  output: SELECTED_RUN.payload.output,
+  logs: SELECTED_RUN.payload.logs,
+  name: SELECTED_RUN.source,
+  size: 0,
+  memory_usage: 0,
+  instructions: SELECTED_RUN.payload.fuelConsumed,
+} as FunctionRun
+
+const WATCHER_RETURN_VALUE = {
+  logs: [FUNCTION_RUN_FROM_SELECTED_RUN, FUNCTION_RUN_FROM_SELECTED_RUN],
+  isAborted: false,
+  canUseShortcuts: true,
+  statusMessage: `Watching for changes to ${SELECTED_RUN.source}...`,
+  recentFunctionRuns: [FUNCTION_RUN_FROM_SELECTED_RUN, FUNCTION_RUN_FROM_SELECTED_RUN]
+}
+
 describe('Replay', () => {
   test('renders a stream of lines from function-runner output, and shortcuts', async () => {
-    // Given
+    const mockedsetupExtensionWatcherForReplay = vi.fn().mockReturnValue(WATCHER_RETURN_VALUE)
+    vi.mocked(setupExtensionWatcherForReplay).mockImplementation(mockedsetupExtensionWatcherForReplay)
 
-    const selectedRun = {
-      shopId: 69665030382,
-      apiClientId: 124042444801,
-      payload: {
-        export: 'run',
-        input: {
-          cart: {
-            lines: [
-              {
-                quantity: 3,
-                merchandise: {
-                  typename: 'ProductVariant',
-                  id: 'gid://shopify/ProductVariant/45334064595182',
-                },
-              },
-            ],
-          },
-        },
-        inputBytes: 136,
-        output: {
-          discountApplicationStrategy: 'FIRST',
-          discounts: [
-            {
-              message: '10% off',
-              value: {
-                percentage: {
-                  value: 10,
-                },
-              },
-              targets: [
-                {
-                  productVariant: {
-                    id: 'gid://shopify/ProductVariant/45334064595182',
-                  },
-                },
-              ],
-            },
-          ],
-        },
-        outputBytes: 195,
-        logs: 'First Log\nLog the second!,\n1,\nfourth line, length should be above!,\nFifth line!',
-        functionId: '34236fa9-42f5-4bb6-adaf-956e12fff0b0',
-        fuelConsumed: 532632,
-      },
-      logType: 'function_run',
-      cursor: '2024-08-02T17:45:27.683139Z',
-      status: 'success',
-      source: 'product-discount',
-      sourceNamespace: 'extensions',
-      logTimestamp: '2024-08-02T17:45:27.382075Z',
-      identifier: '123456',
-    }
 
     const renderInstanceReplay = render(
       <Replay
-        selectedRun={selectedRun}
+        selectedRun={SELECTED_RUN}
         abortController={new AbortController()}
         app={testApp()}
         extension={extension}
       />,
     )
 
-    // await frontendPromise
-
     // Then
-    expect(unstyled(renderInstanceReplay.lastFrame()!.replace(/\d/g, '0'))).toMatchInlineSnapshot(`
-      "
+    expect(unstyled(renderInstanceReplay.lastFrame()!)).toMatchInlineSnapshot(`
+      "Input
+      {
+        \\"someInput\\": \\"someInput\\"
+      }
+      Logs
+      First Log
+      Log the second!,
+      1,
+      fourth line, length should be above!,
+      Fifth line!
+      Output
+      {
+        \\"someOutput\\": \\"someOutput\\"
+      }
+      Benchmark Results
+      Name: product-discount
+      Linear Memory Usage: 0KB
+      Instructions: 532.632K
+      Size: 0KB
+      Input
+      {
+        \\"someInput\\": \\"someInput\\"
+      }
+      Logs
+      First Log
+      Log the second!,
+      1,
+      fourth line, length should be above!,
+      Fifth line!
+      Output
+      {
+        \\"someOutput\\": \\"someOutput\\"
+      }
+      Benchmark Results
+      Name: product-discount
+      Linear Memory Usage: 0KB
+      Instructions: 532.632K
+      Size: 0KB
+
       ────────────────────────────────────────────────────────────────────────────────────────────────────
 
       › Watching for changes to product-discount...
