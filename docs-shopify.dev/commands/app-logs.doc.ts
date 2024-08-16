79c8923dbca576408fc475d7b599aa6e88491713
Alex Bradley <alexandre.bradley@shopify.com>
2024-08-16 18:22:19 -0400

    docs generate && build-docs

--- docs-shopify.dev/commands/app-logs.doc.ts
+++ docs-shopify.dev/commands/app-logs.doc.ts
@@ -4,11 +4,11 @@ import {ReferenceEntityTemplateSchema} from '@shopify/generate-docs'
 const data: ReferenceEntityTemplateSchema = {
   name: 'app logs',
   description: `
-  Opens a real-time stream of detailed log events from the selected app and store.
-  Use the \`--source\` argument to limit output to a particular function, such as a specific Shopify Function handle.
-  Use the \`--status\` argument to specify the type of status to retrieve, either \`success\` or \`failure\`.
+  Opens a real-time stream of detailed app logs from the selected app and store.
+  Use the \`--source\` argument to limit output to a particular log source, such as a specific Shopify Function handle. Use the \`shopify app logs sources\` command to view a list of sources.
+  Use the \`--status\` argument to filter on status, either \`success\` or \`failure\`.
   \`\`\`
-  shopify app logs --status=success --source=extension-handle
+  shopify app logs --status=success --source=extension.discount-function
   \`\`\`
   `,
   overviewPreviewDescription: `Stream detailed logs for your Shopify app.`,
