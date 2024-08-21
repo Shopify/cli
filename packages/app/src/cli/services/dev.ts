86f8fa8c9b66ebbab213f67a3b1ea9fe8d111a41
Isaac Roldán <isaac.roldan@shopify.com>
2024-08-21 10:26:52 +0200

    Use CilentName enum

--- packages/app/src/cli/services/dev.ts
+++ packages/app/src/cli/services/dev.ts
@@ -24,7 +24,11 @@ import {DeveloperPreviewController} from './dev/ui/components/Dev.js'
 import {DevProcessFunction} from './dev/processes/types.js'
 import {setCachedAppInfo} from './local-storage.js'
 import {canEnablePreviewMode} from './extensions/common.js'
-import {DeveloperPlatformClient, selectDeveloperPlatformClient} from '../utilities/developer-platform-client.js'
+import {
+  ClientName,
+  DeveloperPlatformClient,
+  selectDeveloperPlatformClient,
+} from '../utilities/developer-platform-client.js'
 import {Web, isCurrentAppSchema, getAppScopesArray, AppInterface} from '../models/app/app.js'
 import {OrganizationApp} from '../models/organization.js'
 import {getAnalyticsTunnelType} from '../utilities/analytics.js'
@@ -160,7 +164,7 @@ async function prepareForDev(commandOptions: DevOptions): Promise<DevConfig> {
     partnerUrlsUpdated,
     graphiqlPort,
     graphiqlKey,
-    consistentDev: developerPlatformClient.clientName === 'app-management',
+    consistentDev: developerPlatformClient.clientName === ClientName.AppManagement,
   }
 }
 
