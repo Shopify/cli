873ea2b695913f4938be0891064ba13d9ee280ad
Shaun Stanworth <shaun.stanworth@shopify.com>
2024-08-27 10:41:06 +0100

    Add `metafields` top level key

--- packages/app/src/cli/models/extensions/specifications/custom_data.ts
+++ packages/app/src/cli/models/extensions/specifications/custom_data.ts
@@ -2,6 +2,11 @@ import {createContractBasedConfigModuleSpecification} from '../specification.js'
 
 export const CustomDataSpecIdentifier = 'data'
 
-const customDataSpec = createContractBasedConfigModuleSpecification(CustomDataSpecIdentifier, 'product', 'metaobjects')
+const customDataSpec = createContractBasedConfigModuleSpecification(
+  CustomDataSpecIdentifier,
+  'product',
+  'metaobjects',
+  'metafields',
+)
 
 export default customDataSpec
