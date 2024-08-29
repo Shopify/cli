138b0a5f67ed17ce829cb8d1b1789f7f25a13886
Guilherme Carreiro <guilherme.carreiro@shopify.com>
2024-08-29 11:09:06 +0200

    Introduce the theme app extensions dev server infrastructure

---
+++ packages/theme/src/cli/utilities/theme-ext-environment/theme-ext-fs.test.ts
@@ -0,0 +1,161 @@
+import {mountThemeExtensionFileSystem} from './theme-ext-fs.js'
+import {test, describe, expect} from 'vitest'
+import type {Checksum, ThemeAsset} from '@shopify/cli-kit/node/themes/types'
+
+describe('theme-ext-fs', () => {
+  describe('mountThemeExtensionFileSystem', async () => {
+    test('mounts the local theme file system when the directory is valid', async () => {
+      // Given
+      const root = 'src/cli/utilities/fixtures/theme-ext'
+
+      // When
+      const themeFileSystem = mountThemeExtensionFileSystem(root)
+      await themeFileSystem.ready()
+
+      // Then
+      expect(themeFileSystem).toEqual({
+        root,
+        files: new Map([
+          fsEntry({checksum: 'd8ceb73ce5faa4ac22713071d2f0a6bd', key: 'blocks/star_rating.liquid'}),
+          fsEntry({checksum: 'dc9c03a6294d7fd8611ddf148e1f7e6e', key: 'locales/en.default.json'}),
+          fsEntry({checksum: '8a1dd937b2cfe9e669b26e41dc1de5e8', key: 'assets/thumbs-up.png'}),
+          fsEntry({checksum: '28fa42561b59f04fc32e98feb3b994ac', key: 'snippets/stars.liquid'}),
+        ]),
+        unsyncedFileKeys: new Set(),
+        ready: expect.any(Function),
+        delete: expect.any(Function),
+        write: expect.any(Function),
+        read: expect.any(Function),
+        stat: expect.any(Function),
+        addEventListener: expect.any(Function),
+        startWatcher: expect.any(Function),
+      })
+    })
+
+    test('mounts an empty file system when the directory is invalid', async () => {
+      // Given
+      const root = 'src/cli/utilities/invalid-directory'
+
+      // When
+      const themeFileSystem = mountThemeExtensionFileSystem(root)
+      await themeFileSystem.ready()
+
+      // Then
+      expect(themeFileSystem).toEqual({
+        root,
+        files: new Map(),
+        unsyncedFileKeys: new Set(),
+        ready: expect.any(Function),
+        delete: expect.any(Function),
+        write: expect.any(Function),
+        read: expect.any(Function),
+        stat: expect.any(Function),
+        addEventListener: expect.any(Function),
+        startWatcher: expect.any(Function),
+      })
+    })
+  })
+
+  describe('delete', () => {
+    test('"delete" removes the file from the file map', async () => {
+      // Given
+      const root = 'src/cli/utilities/fixtures/theme-ext'
+      const themeFileSystem = mountThemeExtensionFileSystem(root)
+      await themeFileSystem.ready()
+
+      // When
+      expect(themeFileSystem.files.has('snippets/stars.liquid')).toBe(true)
+      await themeFileSystem.delete('snippets/stars.liquid')
+
+      // Then
+      expect(themeFileSystem.files.has('snippets/stars.liquid')).toBe(false)
+    })
+
+    test('does nothing when the theme file does not exist', async () => {
+      // Given
+      const root = 'src/cli/utilities/fixtures/theme-ext'
+
+      // When
+      const themeFileSystem = mountThemeExtensionFileSystem(root)
+      await themeFileSystem.ready()
+      await themeFileSystem.delete('assets/nonexistent.css')
+
+      // Then
+      expect(themeFileSystem.files.has('assets/nonexistent.css')).toBe(false)
+    })
+  })
+
+  describe('write', () => {
+    test('"write" creates a file on the file map', async () => {
+      // Given
+      const root = 'src/cli/utilities/fixtures/theme-ext'
+      const themeFileSystem = mountThemeExtensionFileSystem(root)
+      await themeFileSystem.ready()
+
+      // When
+      expect(themeFileSystem.files.get('assets/new_file.css')).toBeUndefined()
+      await themeFileSystem.write({key: 'assets/new_file.css', checksum: '1010', value: 'content'})
+
+      // Then
+      expect(themeFileSystem.files.get('assets/new_file.css')).toEqual({
+        key: 'assets/new_file.css',
+        checksum: '1010',
+        value: 'content',
+      })
+    })
+
+    test('"write" creates an image file on the file map', async () => {
+      // Given
+      const root = 'src/cli/utilities/fixtures/theme-ext'
+      const attachment = '0x123!'
+      const themeFileSystem = mountThemeExtensionFileSystem(root)
+      await themeFileSystem.ready()
+
+      // When
+      expect(themeFileSystem.files.get('assets/new_image.gif')).toBeUndefined()
+
+      await themeFileSystem.write({key: 'assets/new_image.gif', checksum: '1010', attachment})
+
+      // Then
+      expect(themeFileSystem.files.get('assets/new_image.gif')).toEqual({
+        key: 'assets/new_image.gif',
+        checksum: '1010',
+        attachment,
+      })
+    })
+  })
+
+  describe('read', async () => {
+    test('"read" returns returns the content from the local disk and updates the file map', async () => {
+      // Given
+      const root = 'src/cli/utilities/fixtures/theme-ext'
+      const key = 'snippets/stars.liquid'
+      const themeFileSystem = mountThemeExtensionFileSystem(root)
+      await themeFileSystem.ready()
+      const file = themeFileSystem.files.get(key)
+
+      expect(file).toEqual({
+        key: 'snippets/stars.liquid',
+        checksum: '28fa42561b59f04fc32e98feb3b994ac',
+        value: expect.any(String),
+        attachment: '',
+      })
+
+      // When
+      delete file?.value
+      const content = await themeFileSystem.read(key)
+
+      // Then
+      expect(themeFileSystem.files.get(key)).toEqual({
+        key: 'snippets/stars.liquid',
+        checksum: '28fa42561b59f04fc32e98feb3b994ac',
+        value: content,
+        attachment: '',
+      })
+    })
+  })
+
+  function fsEntry({key, checksum}: Checksum): [string, ThemeAsset] {
+    return [key, {key, checksum, value: expect.any(String), attachment: expect.any(String)}]
+  }
+})
---
+++ packages/theme/src/cli/utilities/theme-ext-environment/theme-ext-fs.ts
@@ -0,0 +1,138 @@
+import {DEFAULT_IGNORE_PATTERNS, readThemeFile} from '../theme-fs.js'
+import {calculateChecksum} from '../asset-checksum.js'
+import {glob} from '@shopify/cli-kit/node/fs'
+import {joinPath, relativePath} from '@shopify/cli-kit/node/path'
+import {sleep} from '@shopify/cli-kit/node/system'
+import {buildThemeAsset} from '@shopify/cli-kit/node/themes/factories'
+import EventEmitter from 'node:events'
+import {stat} from 'fs/promises'
+import type {
+  ThemeAsset,
+  ThemeExtensionFileSystem,
+  ThemeFSEventName,
+  ThemeFSEventPayload,
+} from '@shopify/cli-kit/node/themes/types'
+
+const THEME_EXT_DIRECTORY_PATTERNS = [
+  'assets/**/*.*',
+  'locales/**/*.json',
+  'blocks/**/*.liquid',
+  'snippets/**/*.liquid',
+]
+
+export function mountThemeExtensionFileSystem(root: string): ThemeExtensionFileSystem {
+  const files = new Map<string, ThemeAsset>()
+  const unsyncedFileKeys = new Set<string>()
+  const eventEmitter = new EventEmitter()
+
+  const emitEvent = <T extends ThemeFSEventName>(eventName: T, payload: ThemeFSEventPayload<T>) => {
+    eventEmitter.emit(eventName, payload)
+  }
+
+  const read = async (key: string) => {
+    const fileContent = await readThemeFile(root, key)
+    const checksum = calculateChecksum(key, fileContent)
+    const value = typeof fileContent === 'string' ? fileContent : ''
+    const attachment = Buffer.isBuffer(fileContent) ? fileContent.toString('base64') : ''
+
+    files.set(key, buildThemeAsset({key, checksum, value, attachment}))
+
+    return fileContent
+  }
+
+  const initialFilesPromise = glob(THEME_EXT_DIRECTORY_PATTERNS, {
+    cwd: root,
+    deep: 3,
+    ignore: DEFAULT_IGNORE_PATTERNS,
+  }).then((filesPaths) => Promise.all(filesPaths.map(read)))
+
+  const handleFileUpdate = (eventName: 'add' | 'change', filePath: string) => {
+    const fileKey = relativePath(root, filePath)
+
+    const contentPromise = read(fileKey).then(() => {
+      const file = files.get(fileKey)!
+
+      unsyncedFileKeys.add(file.key)
+
+      return file.value || file.attachment || ''
+    })
+
+    sleep(5)
+      .then(() => {
+        unsyncedFileKeys.delete(fileKey)
+      })
+      .catch(() => {})
+
+    emitEvent(eventName, {
+      fileKey,
+      onContent: (fn) => {
+        contentPromise
+          .then((content) => {
+            if (content) fn(content)
+          })
+          .catch(() => {})
+      },
+      onSync: (_) => {},
+    })
+  }
+
+  const handleFileDelete = (filePath: string) => {
+    const fileKey = relativePath(root, filePath)
+
+    unsyncedFileKeys.delete(fileKey)
+
+    files.delete(fileKey)
+
+    emitEvent('unlink', {
+      fileKey,
+    })
+  }
+
+  const directoriesToWatch = new Set(
+    THEME_EXT_DIRECTORY_PATTERNS.map((pattern) => joinPath(root, pattern.split('/').shift() ?? '')),
+  )
+
+  return {
+    root,
+    files,
+    unsyncedFileKeys,
+    ready: async () => {
+      return initialFilesPromise.then(() => {})
+    },
+    delete: async (fileKey: string) => {
+      files.delete(fileKey)
+    },
+    write: async (asset: ThemeAsset) => {
+      files.set(asset.key, asset)
+    },
+    read,
+    stat: async (fileKey: string) => {
+      if (files.has(fileKey)) {
+        const absolutePath = joinPath(root, fileKey)
+        const stats = await stat(absolutePath)
+
+        if (stats.isFile()) {
+          const fileReducedStats = {size: stats.size, mtime: stats.mtime}
+          return fileReducedStats
+        }
+      }
+    },
+    addEventListener: (eventName, cb) => {
+      eventEmitter.on(eventName, cb)
+    },
+    startWatcher: async () => {
+      const {default: chokidar} = await import('chokidar')
+
+      const watcher = chokidar.watch([...directoriesToWatch], {
+        ignored: DEFAULT_IGNORE_PATTERNS,
+        persistent: !process.env.SHOPIFY_UNIT_TEST,
+        ignoreInitial: true,
+      })
+
+      watcher
+        .on('add', handleFileUpdate.bind(null, 'add'))
+        .on('change', handleFileUpdate.bind(null, 'change'))
+        .on('unlink', handleFileDelete.bind(null))
+    },
+  }
+}
