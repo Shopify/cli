3aef108853175edfa3c674a879b97ddbabc93312
James Meng <james.meng@shopify.com>
2024-09-05 15:53:37 -0700

    Implement URL notifier

--- packages/theme/src/cli/utilities/notifier.ts
+++ packages/theme/src/cli/utilities/notifier.ts
@@ -10,23 +10,28 @@ interface FileChangeInfo {
 export class Notifier {
   private notifyPath: string
   private initialized: boolean
+  private isValidUrl: boolean
+
   constructor(notifyPath: string) {
     this.notifyPath = notifyPath
     this.initialized = false
+    this.isValidUrl = this.validateUrl(notifyPath)
   }
 
   async notify(fileInfo: FileChangeInfo): Promise<void> {
-    if (!this.initialized) {
-      await this.init()
-    }
     try {
+      if (!this.initialized && !this.isValidUrl) {
+        await this.initFile()
+      }
+
       outputDebug(`Notifying filechange listener at ${this.notifyPath}...`)
-      if (!this.notifyPath) return
+      const content = JSON.stringify({files: [fileInfo]})
 
-      const content = JSON.stringify(fileInfo, null, 2)
-
-      if (this.isValidUrl(this.notifyPath)) {
-        await this.notifyUrl(content)
+      if (this.isValidUrl) {
+        const response = await this.notifyUrl(content)
+        if (!response.ok) {
+          outputWarn(`Failed to notify URL: ${response.statusText}`)
+        }
       } else {
         await this.notifyFile(content)
       }
@@ -36,19 +41,25 @@ export class Notifier {
     }
   }
 
-  private async init() {
+  private async initFile() {
     await fs.writeFile(this.notifyPath, '')
     this.initialized = true
   }
 
-  private async notifyUrl(_content: string): Promise<void> {}
+  private async notifyUrl(content: string): Promise<Response> {
+    return fetch(this.notifyPath, {
+      method: 'POST',
+      body: content,
+      headers: {'Content-Type': 'application/json'},
+    })
+  }
 
   private async notifyFile(content: string): Promise<void> {
     outputDebug(`Updating file timestamps at ${this.notifyPath}...`)
     await fs.appendFile(this.notifyPath, content)
   }
 
-  private isValidUrl(url: string): boolean {
+  private validateUrl(url: string): boolean {
     try {
       const parsedUrl = new URL(url)
       return parsedUrl.protocol === 'http:' || parsedUrl.protocol === 'https:'
