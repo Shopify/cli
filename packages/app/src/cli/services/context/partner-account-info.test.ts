4886ace5bcd74efa4b091a74a3c79b95384cdfe4
Isaac Roldán <isaac.roldan@shopify.com>
2024-09-12 16:27:14 +0200

    add tests for partner-account-info

--- packages/app/src/cli/services/context/partner-account-info.test.ts
+++ packages/app/src/cli/services/context/partner-account-info.test.ts
@@ -1,37 +1,69 @@
 import {AccountInfo, fetchCurrentAccountInformation} from './partner-account-info.js'
 import {testDeveloperPlatformClient} from '../../models/app/app.test-data.js'
 import {getCurrentAccountInfo} from '../../api/graphql/current_account_info.js'
-import {describe, expect, test, vi} from 'vitest'
+import {clearCachedAccountInfo, getCachedAccountInfo, setCachedAccountInfo} from '../../utilities/app-conf-store.js'
+import {beforeEach, describe, expect, test, vi} from 'vitest'
 import {AbortError} from '@shopify/cli-kit/node/error'
+import {outputDebug} from '@shopify/cli-kit/node/output'
 
 vi.mock('@shopify/cli-kit/node/session')
 vi.mock('../../api/graphql/current_account_info.js')
 vi.mock('@shopify/cli-kit/node/environment')
+vi.mock('@shopify/cli-kit/node/output')
+
+const userId = '1234-5678'
+const accountInfo: AccountInfo = {
+  type: 'UserAccount',
+  email: 'partner@shopify.com',
+}
 
 describe('fetchCurrentAccountInformation', () => {
-  test('returns complete user account info', async () => {
-    // Given
-    const userAccountInfo: AccountInfo = {
-      type: 'UserAccount',
-      email: 'partner@shopify.com',
-    }
-    vi.mocked(getCurrentAccountInfo).mockResolvedValue(userAccountInfo)
-
-    // When
-    const got = await fetchCurrentAccountInformation(testDeveloperPlatformClient())
-
-    // Then
-    expect(got).toEqual(userAccountInfo)
+  beforeEach(() => {
+    clearCachedAccountInfo()
   })
 
-  test('when error fetching account info returns unkonwn partner info', async () => {
+  test('returns cached account info if available', async () => {
     // Given
+    setCachedAccountInfo(userId, accountInfo)
+
+    // When
+    const got = await fetchCurrentAccountInformation(testDeveloperPlatformClient(), userId)
+
+    // Then
+    expect(got).toEqual(accountInfo)
+    expect(outputDebug).toHaveBeenCalledWith('Getting partner account info from cache')
+    expect(getCurrentAccountInfo).not.toHaveBeenCalled()
+  })
+
+  test('fetches and caches account info if not in cache', async () => {
+    // Given
+    vi.mocked(getCurrentAccountInfo).mockResolvedValue(accountInfo)
+
+    // When
+    const got = await fetchCurrentAccountInformation(testDeveloperPlatformClient(), userId)
+
+    // Then
+    expect(got).toEqual(accountInfo)
+
+    // Verify that the info was cached
+    const cachedInfo = getCachedAccountInfo(userId)
+    expect(cachedInfo).toEqual(accountInfo)
+  })
+
+  test('when error fetching account info returns unknown partner info', async () => {
+    // Given
+    clearCachedAccountInfo()
     vi.mocked(getCurrentAccountInfo).mockRejectedValue(new AbortError('Error'))
 
     // When
-    const got = await fetchCurrentAccountInformation(testDeveloperPlatformClient())
+    const got = await fetchCurrentAccountInformation(testDeveloperPlatformClient(), userId)
 
     // Then
     expect(got).toEqual({type: 'UnknownAccount'})
+    expect(outputDebug).toHaveBeenCalledWith('Error fetching user account info')
+
+    // Verify that no info was cached
+    const cachedInfo = getCachedAccountInfo(userId)
+    expect(cachedInfo).toBeUndefined()
   })
 })
