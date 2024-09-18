d7f7e8cf500cd41291fa575eb85467c93b7e0b5b
Shaun Stanworth <shaun.stanworth@shopify.com>
2024-09-18 16:36:43 +0100

    GraphQL query to pull in all API-managed webhook subscriptions

---
+++ packages/app/src/cli/api/graphql/admin/generated/list-webhook-subscriptions.ts
@@ -0,0 +1,96 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+import * as Types from './types.js'
+
+import {TypedDocumentNode as DocumentNode} from '@graphql-typed-document-node/core'
+
+export type ListWebhookSubscriptionsQueryVariables = Types.Exact<{[key: string]: never}>
+
+export type ListWebhookSubscriptionsQuery = {
+  webhookSubscriptions: {
+    edges: {
+      node: {
+        id: string
+        topic: Types.WebhookSubscriptionTopic
+        endpoint:
+          | {__typename: 'WebhookEventBridgeEndpoint'}
+          | {__typename: 'WebhookHttpEndpoint'; callbackUrl: unknown}
+          | {__typename: 'WebhookPubSubEndpoint'}
+      }
+    }[]
+  }
+}
+
+export const ListWebhookSubscriptions = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: {kind: 'Name', value: 'ListWebhookSubscriptions'},
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: {kind: 'Name', value: 'webhookSubscriptions'},
+            arguments: [
+              {kind: 'Argument', name: {kind: 'Name', value: 'first'}, value: {kind: 'IntValue', value: '100'}},
+            ],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {
+                  kind: 'Field',
+                  name: {kind: 'Name', value: 'edges'},
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      {
+                        kind: 'Field',
+                        name: {kind: 'Name', value: 'node'},
+                        selectionSet: {
+                          kind: 'SelectionSet',
+                          selections: [
+                            {kind: 'Field', name: {kind: 'Name', value: 'id'}},
+                            {kind: 'Field', name: {kind: 'Name', value: 'topic'}},
+                            {
+                              kind: 'Field',
+                              name: {kind: 'Name', value: 'endpoint'},
+                              selectionSet: {
+                                kind: 'SelectionSet',
+                                selections: [
+                                  {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
+                                  {
+                                    kind: 'InlineFragment',
+                                    typeCondition: {
+                                      kind: 'NamedType',
+                                      name: {kind: 'Name', value: 'WebhookHttpEndpoint'},
+                                    },
+                                    selectionSet: {
+                                      kind: 'SelectionSet',
+                                      selections: [
+                                        {kind: 'Field', name: {kind: 'Name', value: 'callbackUrl'}},
+                                        {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
+                                      ],
+                                    },
+                                  },
+                                ],
+                              },
+                            },
+                            {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
+                          ],
+                        },
+                      },
+                      {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
+                    ],
+                  },
+                },
+                {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<ListWebhookSubscriptionsQuery, ListWebhookSubscriptionsQueryVariables>
