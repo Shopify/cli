50db1e586f88b612b596f51cdbb9454bfe1d562d
Chris AtLee <chris.atlee@shopify.com>
2024-09-25 16:13:58 -0400

    Bump upload limit

--- packages/theme/src/cli/utilities/theme-uploader.ts
+++ packages/theme/src/cli/utilities/theme-uploader.ts
@@ -16,9 +16,9 @@ type ChecksumWithSize = Checksum & {size: number}
 type FileBatch = ChecksumWithSize[]
 
 // Limits for Bulk Requests
-export const MAX_BATCH_FILE_COUNT = 10
-// 100KB
-export const MAX_BATCH_BYTESIZE = 102400
+export const MAX_BATCH_FILE_COUNT = 50
+// 10MB
+export const MAX_BATCH_BYTESIZE = 1024000
 export const MAX_UPLOAD_RETRY_COUNT = 2
 
 export function uploadTheme(
