5268e095ef58584b56e843720b2c8432dacdad58
Isaac Roldán <isaac.roldan@shopify.com>
2024-10-10 10:59:28 +0200

    Use app-context in some basic commands

--- packages/app/src/cli/commands/app/config/link.ts
+++ packages/app/src/cli/commands/app/config/link.ts
@@ -1,6 +1,5 @@
 import {appFlags} from '../../../flags.js'
-import {loadApp} from '../../../models/app/loader.js'
-import {loadLocalExtensionsSpecifications} from '../../../models/extensions/load-specifications.js'
+import {linkedAppContext} from '../../../services/app-context.js'
 import link, {LinkOptions} from '../../../services/app/config/link.js'
 import AppCommand, {AppCommandOutput} from '../../../utilities/app-command.js'
 import {Flags} from '@oclif/core'
@@ -34,14 +33,15 @@ export default class ConfigLink extends AppCommand {
       apiKey: flags['client-id'],
     }
 
-    await link(options)
+    const result = await link(options)
 
-    const specifications = await loadLocalExtensionsSpecifications()
-
-    const app = await loadApp({
-      specifications,
+    const {app} = await linkedAppContext({
       directory: flags.path,
-      userProvidedConfigName: undefined,
+      clientId: undefined,
+      forceRelink: false,
+      userProvidedConfigName: result.state.configurationFileName,
+      // Using report because a bad extension config shouldn't prevent the app from being linked
+      mode: 'report',
     })
 
     return {app}
