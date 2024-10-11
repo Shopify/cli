fd0b49627839c71885af9b7d7d2d54beb41f7d7e
James Meng <james.meng@shopify.com>
2024-10-11 09:31:20 -0700

    Update tests

--- packages/theme/src/cli/utilities/cli-config.test.ts
+++ packages/theme/src/cli/utilities/cli-config.test.ts
@@ -1,4 +1,5 @@
 import {configureCLIEnvironment} from './cli-config.js'
+import {globalFlags} from '@shopify/cli-kit/node/cli'
 import {describe, expect, beforeEach, afterAll, test} from 'vitest'
 
 describe('configureCLIEnvironment', () => {
@@ -13,61 +14,50 @@ describe('configureCLIEnvironment', () => {
   })
 
   describe('verbose', () => {
-    test('sets DEBUG environment variable to * when verbose is true', () => {
+    test('sets verbose environment variable when verbose is true', () => {
       // Given
-      delete process.env.DEBUG
+      delete process.env[globalFlags.verbose.env!]
 
       // When
       configureCLIEnvironment({verbose: true})
 
       // Then
-      expect(process.env.DEBUG).toBe('*')
+      expect(process.env[globalFlags.verbose.env!]).toBe('true')
     })
 
-    test('does not overwrite existing DEBUG value when verbose is true', () => {
+    test('does not set verbose environment variable when verbose is false', () => {
       // Given
-      process.env.DEBUG = 'existing-value'
-
-      // When
-      configureCLIEnvironment({verbose: true})
-
-      // Then
-      expect(process.env.DEBUG).toBe('existing-value')
-    })
-
-    test('does not set DEBUG environment variable when verbose is false', () => {
-      // Given
-      delete process.env.DEBUG
+      delete process.env[globalFlags.verbose.env!]
 
       // When
       configureCLIEnvironment({verbose: false})
 
       // Then
-      expect(process.env.DEBUG).toBeUndefined()
+      expect(process.env[globalFlags.verbose.env!]).toBeUndefined()
     })
   })
 
   describe('noColor', () => {
-    test('sets FORCE_COLOR to 0 when noColor is true', () => {
+    test('sets no-color environment variable when noColor is true', () => {
       // Given
-      delete process.env.FORCE_COLOR
+      delete process.env[globalFlags['no-color'].env!]
 
       // When
       configureCLIEnvironment({noColor: true})
 
       // Then
-      expect(process.env.FORCE_COLOR).toBe('0')
+      expect(process.env[globalFlags['no-color'].env!]).toBe('true')
     })
 
-    test('does not set FORCE_COLOR when noColor is false', () => {
+    test('does not set no-color environment variable when noColor is false', () => {
       // Given
-      delete process.env.FORCE_COLOR
+      delete process.env[globalFlags['no-color'].env!]
 
       // When
       configureCLIEnvironment({noColor: false})
 
       // Then
-      expect(process.env.FORCE_COLOR).toBeUndefined()
+      expect(process.env[globalFlags['no-color'].env!]).toBeUndefined()
     })
   })
 })
