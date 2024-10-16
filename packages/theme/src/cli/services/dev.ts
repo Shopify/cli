98f7ab00071f91922068ca798fe59212154adf6b
James Meng <james.meng@shopify.com>
2024-10-16 11:17:57 -0700

    Remove legacy message

--- packages/theme/src/cli/services/dev.ts
+++ packages/theme/src/cli/services/dev.ts
@@ -6,7 +6,7 @@ import {isStorefrontPasswordProtected} from '../utilities/theme-environment/stor
 import {ensureValidPassword} from '../utilities/theme-environment/storefront-password-prompt.js'
 import {emptyThemeExtFileSystem} from '../utilities/theme-fs-empty.js'
 import {initializeDevServerSession} from '../utilities/theme-environment/dev-server-session.js'
-import {renderInfo, renderSuccess, renderWarning} from '@shopify/cli-kit/node/ui'
+import {renderSuccess, renderWarning} from '@shopify/cli-kit/node/ui'
 import {AdminSession, ensureAuthenticatedStorefront, ensureAuthenticatedThemes} from '@shopify/cli-kit/node/session'
 import {execCLI2} from '@shopify/cli-kit/node/ruby'
 import {outputDebug} from '@shopify/cli-kit/node/output'
@@ -49,8 +49,6 @@ export async function dev(options: DevOptions) {
     await legacyDev(options)
     return
   }
-
-  showNewVersionInfo()
 
   if (!(await hasRequiredThemeDirectories(options.directory)) && !(await currentDirectoryConfirmed(options.force))) {
     return
@@ -238,11 +236,4 @@ export async function refreshTokens(store: string, password: string | undefined,
   }
 
   return {adminSession, storefrontToken}
-}
-
-function showNewVersionInfo() {
-  renderInfo({
-    headline: [`You're using the new version of`, {command: 'shopify theme dev'}, {char: '.'}],
-    body: ['Run', {command: 'shopify theme dev --legacy'}, 'to switch back to the previous version.'],
-  })
 }
