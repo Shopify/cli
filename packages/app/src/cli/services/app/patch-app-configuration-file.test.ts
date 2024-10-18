33e2f6acac00affbc3d3a22bf72594d31200fdef
Isaac Roldán <isaac.roldan@shopify.com>
2024-10-18 11:22:08 +0200

    Update patch toml function to replace arrays instead of merging

--- packages/app/src/cli/services/app/patch-app-configuration-file.test.ts
+++ packages/app/src/cli/services/app/patch-app-configuration-file.test.ts
@@ -33,7 +33,7 @@ function writeDefaulToml(tmpDir: string) {
 }
 
 describe('patchAppConfigurationFile', () => {
-  test('updates existing configuration with new values and adds new top-levelfields', async () => {
+  test('updates existing configuration with new values and adds new top-levelfields, replaces arrays', async () => {
     await inTemporaryDirectory(async (tmpDir) => {
       const configPath = writeDefaulToml(tmpDir)
       const patch = {
@@ -41,6 +41,9 @@ describe('patchAppConfigurationFile', () => {
         application_url: 'https://example.com',
         access_scopes: {
           use_legacy_install_flow: false,
+        },
+        auth: {
+          redirect_urls: ['https://example.com/redirect3', 'https://example.com/redirect4'],
         },
       }
 
@@ -61,8 +64,8 @@ use_legacy_install_flow = false
 
 [auth]
 redirect_urls = [
-  "https://example.com/redirect",
-  "https://example.com/redirect2"
+  "https://example.com/redirect3",
+  "https://example.com/redirect4"
 ]
 
 [webhooks]
