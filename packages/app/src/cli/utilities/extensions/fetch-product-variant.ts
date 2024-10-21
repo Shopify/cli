6b063d17c1464c9fa36c4ec9ddeeab343f7069eb
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2024-10-21 10:36:40 -0400

    Fix the URL output to add a new product

--- packages/app/src/cli/utilities/extensions/fetch-product-variant.ts
+++ packages/app/src/cli/utilities/extensions/fetch-product-variant.ts
@@ -16,7 +16,7 @@ export async function fetchProductVariant(store: string) {
     throw new AbortError(
       'Could not find a product variant',
       `Your store needs to have at least one product to test a 'checkout_ui' extension\n
-You can add a new product here: https://${store}/admin/products/new`,
+You can add a new product here: ${store}/admin/products/new`,
     )
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const variantURL = result.products.edges[0]!.node.variants.edges[0]!.node.id
