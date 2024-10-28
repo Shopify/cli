4a70d5a78dc2259076f47b8cc38aa67a4733c416
James Meng <james.meng@shopify.com>
2024-10-28 15:59:48 -0700

    Remove cli-ruby from cli-kit

--- packages/cli-kit/assets/cli-ruby/test/test_helpers/shopifolk.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-module TestHelpers
-  module Shopifolk
-    def teardown
-      ShopifyCLI::Shopifolk.reset
-      super
-    end
-  end
-end
--- packages/cli-kit/assets/cli-ruby/vendor/deps/cli-kit/lib/cli/kit/support.rb
+++
@@ -1,9 +0,0 @@
-require 'cli/kit'
-
-module CLI
-  module Kit
-    module Support
-      autoload :TestHelper, 'cli/kit/support/test_helper'
-    end
-  end
-end
