dd35e0cee71903cb81a28f4032844adc9036ba9a
Isaac Roldán <isaac.roldan@shopify.com>
2024-11-11 11:22:53 +0100

    Print locale changes to debug

--- packages/app/src/cli/services/dev/extension/localization.ts
+++ packages/app/src/cli/services/dev/extension/localization.ts
@@ -57,7 +57,7 @@ export async function getLocalization(
       }),
     )
     localization.lastUpdated = Date.now()
-    outputDebug(`Parsed locales for extension ${extension.handle} at ${extension.directory}`)
+    outputDebug(`Parsed locales for extension ${extension.handle} at ${extension.directory}`, options.stdout)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any, no-catch-all/no-catch-all
   } catch (error: any) {
     status = 'error'
