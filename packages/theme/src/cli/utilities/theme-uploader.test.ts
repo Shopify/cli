07ff693af4599519f5a4990ee689afd5ea0ec801
James Meng <james.meng@shopify.com>
2024-12-05 11:14:43 -0800

    Theme Upload: Add theme blocks to dependant files

--- packages/theme/src/cli/utilities/theme-uploader.test.ts
+++ packages/theme/src/cli/utilities/theme-uploader.test.ts
@@ -296,6 +296,7 @@ describe('theme-uploader', () => {
         ['templates/product.json', {key: 'templates/product.json', checksum: '6'}],
         ['assets/image.png', {key: 'assets/image.png', checksum: '7'}],
         ['templates/product.context.uk.json', {key: 'templates/product.context.uk.json', checksum: '8'}],
+        ['blocks/block.liquid', {key: 'blocks/block.liquid', checksum: '9'}],
       ]),
     )
 
@@ -310,21 +311,17 @@ describe('theme-uploader', () => {
     await renderThemeSyncProgress()
 
     // Then
-    expect(bulkUploadThemeAssets).toHaveBeenCalledTimes(7)
-    // Mininum theme files start first
+    expect(bulkUploadThemeAssets).toHaveBeenCalledTimes(8)
+    // Minimum theme files start first
     expect(bulkUploadThemeAssets).toHaveBeenNthCalledWith(1, remoteTheme.id, MINIMUM_THEME_ASSETS, adminSession)
     // Dependent assets start second
     expect(bulkUploadThemeAssets).toHaveBeenNthCalledWith(
       2,
       remoteTheme.id,
-      [
-        {
-          key: 'sections/header.liquid',
-        },
-      ],
+      [{key: 'blocks/block.liquid'}],
       adminSession,
     )
-    // Independent assets start right before dependent assets start
+    // Independent assets start right after dependent assets start
     expect(bulkUploadThemeAssets).toHaveBeenNthCalledWith(
       3,
       remoteTheme.id,
@@ -347,17 +344,18 @@ describe('theme-uploader', () => {
       remoteTheme.id,
       [
         {
-          key: 'sections/header-group.json',
+          key: 'sections/header.liquid',
         },
       ],
       adminSession,
     )
+
     expect(bulkUploadThemeAssets).toHaveBeenNthCalledWith(
       5,
       remoteTheme.id,
       [
         {
-          key: 'templates/product.json',
+          key: 'sections/header-group.json',
         },
       ],
       adminSession,
@@ -367,13 +365,23 @@ describe('theme-uploader', () => {
       remoteTheme.id,
       [
         {
-          key: 'templates/product.context.uk.json',
+          key: 'templates/product.json',
         },
       ],
       adminSession,
     )
     expect(bulkUploadThemeAssets).toHaveBeenNthCalledWith(
       7,
+      remoteTheme.id,
+      [
+        {
+          key: 'templates/product.context.uk.json',
+        },
+      ],
+      adminSession,
+    )
+    expect(bulkUploadThemeAssets).toHaveBeenNthCalledWith(
+      8,
       remoteTheme.id,
       [
         {
