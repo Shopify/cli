b4eda0235dc469019e9514b6e5ad1219f99de677
Jeff Charles <jeff.charles@shopify.com>
2024-12-10 11:02:51 -0500

    Update Shopify Functions to version with plugin

--- packages/app/src/cli/services/function/binaries.ts
+++ packages/app/src/cli/services/function/binaries.ts
@@ -10,10 +10,10 @@ import fs from 'node:fs'
 import * as gzip from 'node:zlib'
 import {fileURLToPath} from 'node:url'
 
-const FUNCTION_RUNNER_VERSION = 'v6.3.0'
+const FUNCTION_RUNNER_VERSION = 'v6.5.0'
 const JAVY_VERSION = 'v4.0.0'
-// The Javy plugin version does not need to match the Javy version. It should
-// match the plugin version used in the function-runner version specified above.
+// The Javy plugin version should match the plugin version used in the
+// function-runner version specified above.
 const JAVY_PLUGIN_VERSION = 'v1'
 
 interface DownloadableBinary {
