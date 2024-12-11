e41c2ce7ba541dfb8cbd1832a6fd5ba80db9781e
Bret Little <bret.little@shopify.com>
2024-12-11 17:40:08 -0500

    Update

--- packages/cli/src/cli/commands/test/test.ts
+++ packages/cli/src/cli/commands/test/test.ts
@@ -2,7 +2,8 @@ import {globalFlags} from '@shopify/cli-kit/node/cli'
 import BaseCommand from '@shopify/cli-kit/node/base-command'
 import {Flags} from '@oclif/core'
 import {chromium, Cookie, devices, type Page} from 'playwright'
-import {outputInfo, outputContent, outputToken} from '@shopify/cli-kit/node/output'
+import {outputInfo} from '@shopify/cli-kit/node/output'
+import colors from '@shopify/cli-kit/node/colors'
 import assert from 'node:assert'
 
 const COLLECTION_LINK = '[data-shopify-collection-link]'
@@ -16,6 +17,15 @@ const SHOPIFY_CUSTOMER_PRIVACY = '#customer-privacy-api'
 
 const ANALYTICS_Y_COOKIE = '_shopify_y'
 const ANALYTICS_S_COOKIE = '_shopify_s'
+
+interface TestFlags {
+  url: string
+  'skip-cookies': boolean
+  'skip-collection': boolean
+  device: string
+  'no-color': boolean
+  headless: boolean
+}
 
 export default class Test extends BaseCommand {
   static summary = 'Test a Shopify storefront'
@@ -39,6 +49,25 @@ export default class Test extends BaseCommand {
       options: Object.keys(devices),
       env: 'SHOPIFY_FLAG_TEST_DEVICE',
     }),
+    'skip-cookies': Flags.boolean({
+      description: 'Skip validating cookies and consent',
+      required: false,
+      default: false,
+      env: 'SHOPIFY_FLAG_TEST_SKIP_COOKIES',
+    }),
+    'skip-collection': Flags.boolean({
+      description: 'Skip validating collection page',
+      required: false,
+      default: false,
+      env: 'SHOPIFY_FLAG_TEST_SKIP_COLLECTION',
+    }),
+    headless: Flags.boolean({
+      description: 'Run in headless mode',
+      required: false,
+      default: true,
+      allowNo: true,
+      env: 'SHOPIFY_FLAG_TEST_HEADLESS',
+    }),
   }
 
   async run() {
@@ -48,7 +77,7 @@ export default class Test extends BaseCommand {
       throw new Error(`Invalid device: ${flags.device}.\nValid devices are: ${Object.keys(devices).join(', ')}`)
     }
 
-    const browser = await chromium.launch({headless: false})
+    const browser = await chromium.launch({headless: flags.headless})
     const context = await browser.newContext(devices[flags.device])
 
     try {
@@ -58,7 +87,7 @@ export default class Test extends BaseCommand {
       // The actual interesting bit
       await context.route('**.jpg', (route) => route.abort())
 
-      await tryCollection(page, flags.url)
+      await fullSuite(page, flags)
     } finally {
       // Teardown
       await context.close()
@@ -67,24 +96,32 @@ export default class Test extends BaseCommand {
   }
 }
 
-async function tryCollection(page: Page, baseUrl: string) {
-  outputInfo('Full test')
+async function fullSuite(page: Page, flags: TestFlags) {
+  const {url: baseUrl} = flags
+
+  outputInfo(colors.blue`Full test`)
+
+  process.stdout.write(`    ├ ${pad(`Go to: ${baseUrl.substring(0, baseUrl.indexOf('?') ?? baseUrl.length)}`)}`)
 
   await page.goto(baseUrl)
 
-  outputInfo(outputContent`    └─── ${outputToken.link('Goto', baseUrl)}`)
+  printSuccess()
 
-  await page.context().clearCookies({
-    name: ANALYTICS_Y_COOKIE,
-  })
+  if (!flags['skip-cookies']) {
+    await page.context().clearCookies({
+      name: ANALYTICS_Y_COOKIE,
+    })
 
-  await page.context().clearCookies({
-    name: ANALYTICS_S_COOKIE,
-  })
+    await page.context().clearCookies({
+      name: ANALYTICS_S_COOKIE,
+    })
 
-  await acceptCookies(page)
+    await acceptCookies(page)
+  }
 
-  await loadCollection(page)
+  if (!flags['skip-collection']) {
+    await loadCollection(page)
+  }
 
   await loadProduct(page)
 
@@ -94,7 +131,7 @@ async function tryCollection(page: Page, baseUrl: string) {
 
   await checkout(page)
 
-  await verifyCheckout(page, {cookies})
+  await verifyCheckout(page, {cookies, flags})
 }
 
 async function loadCollection(page: Page) {
@@ -106,7 +143,7 @@ async function loadCollection(page: Page) {
 
   const href = await collectionLinks.first().getAttribute('href')
 
-  console.log(`Loading collection: ${href}`)
+  process.stdout.write(`    ├ ${pad(`Navigate: ${href}`)}`)
 
   if (!href) {
     throw new Error('Collection link href not found')
@@ -116,6 +153,8 @@ async function loadCollection(page: Page) {
   const url = new URL(page.url())
 
   await page.waitForURL(`${url.origin + href}*`)
+
+  printSuccess()
 }
 
 async function loadProduct(page: Page) {
@@ -131,60 +170,77 @@ async function loadProduct(page: Page) {
     throw new Error('Product link href not found')
   }
 
-  console.log(`Loading product: ${href}`)
+  process.stdout.write(`    ├ ${pad(`Navigate: ${href}`)}`)
 
   await productLinks.first().click()
   const url = new URL(page.url())
 
   await page.waitForURL(`${url.origin + href}*`, {})
+
+  printSuccess()
 }
 
 async function addToCart(page: Page) {
+  process.stdout.write(`    ├ ${pad(`Add to cart 🛒 `)}`)
+
   const addToCartButton = await page.locator(ADD_TO_CART_BUTTON)
 
   if (!addToCartButton) {
     throw new Error('Add to cart button not found')
   }
 
-  console.log('Adding to cart')
-
   await addToCartButton.first().click()
 
   await page.waitForSelector(CHECKOUT_LINK)
+
+  printSuccess()
 }
 
 async function checkout(page: Page) {
+  process.stdout.write(`    ├ ${pad(`Navigate: checkout 🧾 `)}`)
   const checkoutLink = await page.locator(CHECKOUT_LINK)
 
   if (!checkoutLink) {
     throw new Error('Checkout link not found')
   }
 
-  console.log('Checking out')
-
   await checkoutLink.first().click()
 
   await page.waitForNavigation()
+
+  printSuccess()
 }
 
 async function acceptCookies(page: Page) {
   if (!findCookie(await page.context().cookies(), ANALYTICS_Y_COOKIE)) {
-    outputInfo(outputContent`    └─── ${'Accept cookies'}`)
-
+    process.stdout.write(`    ├ ${pad('Accept cookies')}`)
     // there is no shopify analytics cookie, this means we need to accept cookies
     await page.locator(ACCEPT_COOKIES_BUTTON).click()
+
+    printSuccess()
   }
 }
 
-async function verifyCheckout(page: Page, {cookies}: {cookies: Cookie[]}) {
+async function verifyCheckout(page: Page, {cookies, flags}: {cookies: Cookie[]; flags: TestFlags}) {
+  process.stdout.write(`    ├ ${pad(`Validate analytics 📊 `)}`)
   const checkoutCookies = await page.context().cookies()
 
-  console.log('Verifying analytics cookies')
+  if (!flags['skip-cookies']) {
+    assert(findCookie(checkoutCookies, ANALYTICS_Y_COOKIE) === findCookie(cookies, ANALYTICS_Y_COOKIE))
+    assert(findCookie(checkoutCookies, ANALYTICS_S_COOKIE) === findCookie(cookies, ANALYTICS_S_COOKIE))
+  }
 
-  assert(findCookie(checkoutCookies, ANALYTICS_Y_COOKIE) === findCookie(cookies, ANALYTICS_Y_COOKIE))
-  assert(findCookie(checkoutCookies, ANALYTICS_S_COOKIE) === findCookie(cookies, ANALYTICS_S_COOKIE))
+  printSuccess()
 }
 
 function findCookie(cookies: Cookie[], name: string) {
   return cookies.find((cookie) => cookie.name === name)?.value
+}
+
+function pad(text: string) {
+  return text + colors.gray(text.padEnd(80, '.').substring(text.length))
+}
+
+function printSuccess() {
+  process.stdout.write(` ${colors.green`✔\n`}`)
 }
