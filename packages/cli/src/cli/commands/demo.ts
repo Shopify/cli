03b3d4711fff9b45d2feefd42f91f8d7074ef3e2
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2024-12-11 13:44:40 -0500

    Run shopify app init with execa

--- packages/cli/src/cli/commands/demo.ts
+++ packages/cli/src/cli/commands/demo.ts
@@ -1,7 +1,8 @@
-import {Flags} from '@oclif/core'
 import Command from '@shopify/cli-kit/node/base-command'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
-import {outputInfo} from '@shopify/cli-kit/node/output'
+import {cwd, joinPath} from '@shopify/cli-kit/node/path'
+import {renderInfo, renderTextPrompt} from '@shopify/cli-kit/node/ui'
+import {execa} from 'execa'
 
 export default class Demo extends Command {
   static summary = 'Demo command to showcase CLI functionality'
@@ -10,16 +11,33 @@ export default class Demo extends Command {
 
   static flags = {
     ...globalFlags,
-    name: Flags.string({
-      char: 'n',
-      description: 'Name to demo with',
-      env: 'SHOPIFY_FLAG_DEMO_NAME',
-    }),
   }
 
   async run(): Promise<void> {
-    const {flags} = await this.parse(Demo)
-    const name = flags.name ?? 'Shopify Developer'
-    outputInfo(`Hello ${name}! This is a demo command.`)
+    const currentDir = cwd()
+    // const {flags} = await this.parse(Demo)
+
+    renderInfo({
+      headline: 'Lets learn how to create and deploy a Shopify app!',
+      body: `This is a demo command that demonstrates how to use the CLI commands to create and deploy a Shopify app.`,
+    })
+
+    await renderTextPrompt({
+      message: 'Run the `shopify app init` command to get started:',
+      // defaultValue: 'expansive commerce app',
+      validate: (value) => {
+        if (value !== 'shopify app init') return 'Thats not the `shopify app init` command!'
+      },
+    })
+
+    const defaultOpts = {stdio: 'inherit' as const}
+
+    const template = 'remix'
+    const flavor = 'javascript'
+    const appName = 'demo-app'
+    const appPath = joinPath(currentDir, appName)
+
+    const initArgs = [`--template=${template}`, `--flavor=${flavor}`, `--name=${appName}`, `--path=${appPath}`]
+    await execa('shopify', ['app', 'init', ...initArgs], defaultOpts)
   }
 }
