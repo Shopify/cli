e07d791b4672b71f8580130ed78ac5356a6bb360
Bret Little <bret.little@shopify.com>
2024-12-12 12:16:09 -0500

    Update

--- packages/cli/src/cli/commands/test/test.ts
+++ packages/cli/src/cli/commands/test/test.ts
@@ -4,6 +4,7 @@ import {Flags} from '@oclif/core'
 import {chromium, Cookie, devices, type Page} from 'playwright'
 import {outputInfo} from '@shopify/cli-kit/node/output'
 import colors from '@shopify/cli-kit/node/colors'
+import {renderError, renderSuccess} from '@shopify/cli-kit/node/ui'
 import assert from 'node:assert'
 
 const COLLECTION_LINK = '[data-shopify-collection-link]'
@@ -25,6 +26,7 @@ interface TestFlags {
   device: string
   'no-color': boolean
   headless: boolean
+  verbose: boolean
 }
 
 export default class Test extends BaseCommand {
@@ -103,9 +105,17 @@ async function fullSuite(page: Page, flags: TestFlags) {
 
   process.stdout.write(`    ├ ${pad(`Go to: ${baseUrl.substring(0, baseUrl.indexOf('?') ?? baseUrl.length)}`)}`)
 
-  await page.goto(baseUrl)
+  try {
+    await page.goto(baseUrl)
+  } catch (error) {
+    process.stdout.write('\n')
+    renderError({
+      headline: 'Unable to load storefront.',
+    })
+    throw error
+  }
 
-  printSuccess()
+  printStepSuccess()
 
   if (!flags['skip-cookies']) {
     await page.context().clearCookies({
@@ -116,108 +126,186 @@ async function fullSuite(page: Page, flags: TestFlags) {
       name: ANALYTICS_S_COOKIE,
     })
 
-    await acceptCookies(page)
+    await acceptCookies(page, flags)
   }
 
   if (!flags['skip-collection']) {
-    await loadCollection(page)
+    await loadCollection(page, flags)
   }
 
-  await loadProduct(page)
+  await loadProduct(page, flags)
 
-  await addToCart(page)
+  await addToCart(page, flags)
 
   const cookies = await page.context().cookies()
 
-  await checkout(page)
+  await checkout(page, flags)
 
   await verifyCheckout(page, {cookies, flags})
+
+  renderSuccess({
+    headline: 'Validated to checkout.',
+  })
 }
 
-async function loadCollection(page: Page) {
-  const collectionLinks = await page.locator(COLLECTION_LINK)
+async function loadCollection(page: Page, flags: TestFlags) {
+  try {
+    process.stdout.write(`    ├ Navigate to collection: `)
 
-  if (!collectionLinks) {
-    throw new Error('Collection link not found')
+    const collectionLinks = await page.locator(COLLECTION_LINK)
+
+    if (!collectionLinks) {
+      throw new Error('Collection link not found')
+    }
+
+    const href = await collectionLinks.first().getAttribute('href')
+
+    process.stdout.write(pad(`${href}`, 56))
+
+    if (!href) {
+      throw new Error('Collection link href not found')
+    }
+
+    await collectionLinks.first().click()
+    const url = new URL(page.url())
+
+    await page.waitForURL(`${url.origin + href}*`)
+
+    printStepSuccess()
+  } catch (error) {
+    process.stdout.write('\n')
+    renderError({
+      headline: 'Unable to load a collection.',
+      body: 'Make sure a link on the homepage with the `data-shopify-collection-link` attribute exists.\n Alternatively, you can skip this step by passing the --skip-collection flag.',
+    })
+
+    if (flags.verbose) {
+      throw error
+    } else {
+      process.exit(1)
+    }
   }
-
-  const href = await collectionLinks.first().getAttribute('href')
-
-  process.stdout.write(`    ├ ${pad(`Navigate: ${href}`)}`)
-
-  if (!href) {
-    throw new Error('Collection link href not found')
-  }
-
-  await collectionLinks.first().click()
-  const url = new URL(page.url())
-
-  await page.waitForURL(`${url.origin + href}*`)
-
-  printSuccess()
 }
 
-async function loadProduct(page: Page) {
-  const productLinks = await page.locator(PRODUCT_LINK)
+async function loadProduct(page: Page, flags: TestFlags) {
+  try {
+    process.stdout.write(`    ├ Navigate to product: `)
 
-  if (!productLinks) {
-    throw new Error('Product link not found')
+    const productLinks = await page.locator(PRODUCT_LINK)
+
+    if (!productLinks) {
+      throw new Error('Product link not found')
+    }
+
+    const href = await productLinks.first().getAttribute('href')
+
+    if (!href) {
+      throw new Error('Product link href not found')
+    }
+
+    process.stdout.write(pad(href, 59))
+
+    await productLinks.first().click()
+    const url = new URL(page.url())
+
+    await page.waitForURL(`${url.origin + href}*`, {})
+
+    printStepSuccess()
+  } catch (error) {
+    process.stdout.write('\n')
+    renderError({
+      headline: 'Unable to load a product.',
+      body: 'Make sure your product links have the `data-shopify-product-link` attribute.',
+    })
+
+    if (flags.verbose) {
+      throw error
+    } else {
+      process.exit(1)
+    }
   }
-
-  const href = await productLinks.first().getAttribute('href')
-
-  if (!href) {
-    throw new Error('Product link href not found')
-  }
-
-  process.stdout.write(`    ├ ${pad(`Navigate: ${href}`)}`)
-
-  await productLinks.first().click()
-  const url = new URL(page.url())
-
-  await page.waitForURL(`${url.origin + href}*`, {})
-
-  printSuccess()
 }
 
-async function addToCart(page: Page) {
-  process.stdout.write(`    ├ ${pad(`Add to cart 🛒 `)}`)
+async function addToCart(page: Page, flags: TestFlags) {
+  try {
+    process.stdout.write(`    ├ ${pad(`Add to cart 🛒 `)}`)
 
-  const addToCartButton = await page.locator(ADD_TO_CART_BUTTON)
+    const addToCartButton = await page.locator(ADD_TO_CART_BUTTON)
 
-  if (!addToCartButton) {
-    throw new Error('Add to cart button not found')
+    if (!addToCartButton) {
+      throw new Error('Add to cart button not found')
+    }
+
+    await addToCartButton.first().click()
+
+    await page.waitForSelector(CHECKOUT_LINK)
+
+    printStepSuccess()
+  } catch (error) {
+    process.stdout.write('\n')
+    renderError({
+      headline: 'Unable to add to cart.',
+      body: 'Make sure your product add to cart button has the `data-shopify-product-add-to-cart` attribute.',
+    })
+
+    if (flags.verbose) {
+      throw error
+    } else {
+      process.exit(1)
+    }
   }
-
-  await addToCartButton.first().click()
-
-  await page.waitForSelector(CHECKOUT_LINK)
-
-  printSuccess()
 }
 
-async function checkout(page: Page) {
-  process.stdout.write(`    ├ ${pad(`Navigate: checkout 🧾 `)}`)
-  const checkoutLink = await page.locator(CHECKOUT_LINK)
+async function checkout(page: Page, flags: TestFlags) {
+  try {
+    process.stdout.write(`    ├ ${pad(`Navigate: checkout 🧾 `)}`)
+    const checkoutLink = await page.locator(CHECKOUT_LINK)
 
-  if (!checkoutLink) {
-    throw new Error('Checkout link not found')
+    if (!checkoutLink) {
+      throw new Error('Checkout link not found')
+    }
+
+    await checkoutLink.first().click()
+
+    await page.waitForNavigation()
+
+    printStepSuccess()
+  } catch (error) {
+    process.stdout.write('\n')
+    renderError({
+      headline: 'Unable to navigate to checkout.',
+      body: 'Make sure your checkout link has the `data-shopify-cart-checkout-link` attribute.',
+    })
+
+    if (flags.verbose) {
+      throw error
+    } else {
+      process.exit(1)
+    }
   }
-
-  await checkoutLink.first().click()
-
-  await page.waitForNavigation()
-
-  printSuccess()
 }
 
-async function acceptCookies(page: Page) {
-  if (!findCookie(await page.context().cookies(), ANALYTICS_Y_COOKIE)) {
-    process.stdout.write(`    ├ ${pad('Accept cookies')}`)
-    // there is no shopify analytics cookie, this means we need to accept cookies
-    await page.locator(ACCEPT_COOKIES_BUTTON).click()
+async function acceptCookies(page: Page, flags: TestFlags) {
+  try {
+    if (!findCookie(await page.context().cookies(), ANALYTICS_Y_COOKIE)) {
+      process.stdout.write(`    ├ ${pad('Cookie consent 🍪 ')}`)
+      // there is no shopify analytics cookie, this means we need to accept cookies
+      await page.locator(ACCEPT_COOKIES_BUTTON).click()
 
-    printSuccess()
+      printStepSuccess()
+    }
+  } catch (error) {
+    process.stdout.write('\n')
+    renderError({
+      headline: 'Unable to accept consent for cookies.',
+      body: 'If you are using custom consent, you can skip this step by passing the --skip-cookies flag.',
+    })
+
+    if (flags.verbose) {
+      throw error
+    } else {
+      process.exit(1)
+    }
   }
 }
 
@@ -230,17 +318,17 @@ async function verifyCheckout(page: Page, {cookies, flags}: {cookies: Cookie[]; 
     assert(findCookie(checkoutCookies, ANALYTICS_S_COOKIE) === findCookie(cookies, ANALYTICS_S_COOKIE))
   }
 
-  printSuccess()
+  printStepSuccess()
 }
 
 function findCookie(cookies: Cookie[], name: string) {
   return cookies.find((cookie) => cookie.name === name)?.value
 }
 
-function pad(text: string) {
-  return text + colors.gray(text.padEnd(80, '.').substring(text.length))
+function pad(text: string, length = 80) {
+  return text + colors.gray(text.padEnd(length, '.').substring(text.length))
 }
 
-function printSuccess() {
+function printStepSuccess() {
   process.stdout.write(` ${colors.green`✔\n`}`)
 }
