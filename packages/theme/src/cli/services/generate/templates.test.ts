bd1c151ba324129fcc1ec34bba1391633a58a8f0
James Meng <james.meng@shopify.com>
2024-12-12 15:22:51 -0800

    [Themes] Copy base template if it exists

--- packages/theme/src/cli/services/generate/templates.test.ts
+++ packages/theme/src/cli/services/generate/templates.test.ts
@@ -1,22 +1,25 @@
 import {generateTemplate} from './templates.js'
-import {inTemporaryDirectory, mkdir, readFile, writeFile} from '@shopify/cli-kit/node/fs'
+import {fileExists, inTemporaryDirectory, mkdir, readFile, writeFile} from '@shopify/cli-kit/node/fs'
 import {joinPath} from '@shopify/cli-kit/node/path'
-import {describe, expect, test, vi} from 'vitest'
-import {renderTextPrompt} from '@shopify/cli-kit/node/ui'
+import {beforeEach, describe, expect, test, vi} from 'vitest'
 
 vi.mock('@shopify/cli-kit/node/ui')
-
+vi.mock('@shopify/cli-kit/node/fs')
 describe('generateTemplate', () => {
-  test('generates a liquid template with provided name if provided', async () => {
+  beforeEach(() => {
+    vi.mocked(fileExists).mockResolvedValue(false)
+  })
+
+  test('generates new content for named template when base does not exist', async () => {
     await inTemporaryDirectory(async (tmpDir) => {
       // Given
       await mkdir(joinPath(tmpDir, 'templates'))
       const options = {
         name: 'test-template',
-        type: 'basic',
         path: tmpDir,
         fileType: 'liquid',
         resource: 'product',
+        type: 'basic',
       }
 
       // When
@@ -35,16 +38,31 @@ describe('generateTemplate', () => {
     })
   })
 
-  test('generates a base liquid template resource file if name is undefined', async () => {
+  test('generates a base liquid template file', async () => {
     await inTemporaryDirectory(async (tmpDir) => {
       // Given
       await mkdir(joinPath(tmpDir, 'templates'))
+      const baseContent = `{% schema %}
+{
+  "name": "product",
+  "type": "product",
+  "settings": [
+    {
+      "type": "text",
+      "id": "custom_setting",
+      "label": "Custom Setting"
+    }
+  ]
+}
+{% endschema %}`
+      await writeFile(joinPath(tmpDir, 'templates', 'product.liquid'), baseContent)
+
       const options = {
         name: undefined,
-        type: 'basic',
         path: tmpDir,
         fileType: 'liquid',
         resource: 'product',
+        type: 'basic',
       }
 
       // When
@@ -53,26 +71,20 @@ describe('generateTemplate', () => {
       // Then
       const templatePath = joinPath(tmpDir, 'templates', 'product.liquid')
       const content = await readFile(templatePath)
-      expect(content).toBe(`{% schema %}
-{
-  "name": "product",
-  "type": "product",
-  "settings": []
-}
-{% endschema %}`)
+      expect(content).toBe(baseContent)
     })
   })
 
-  test('generates a json template with provided name if provided', async () => {
+  test('generates new content for named json template when base does not exist', async () => {
     await inTemporaryDirectory(async (tmpDir) => {
       // Given
       await mkdir(joinPath(tmpDir, 'templates'))
       const options = {
         name: 'test-template',
-        type: 'basic',
         path: tmpDir,
         fileType: 'json',
         resource: 'collection',
+        type: 'basic',
       }
 
       // When
@@ -95,16 +107,29 @@ describe('generateTemplate', () => {
     })
   })
 
-  test('generates a base json template resource file if name is undefined', async () => {
+  test('generates a base json template file', async () => {
     await inTemporaryDirectory(async (tmpDir) => {
       // Given
       await mkdir(joinPath(tmpDir, 'templates'))
+      const baseContent = `{
+  "sections": {
+    "main": {
+      "type": "main-collection",
+      "settings": {
+        "custom_setting": "value"
+      }
+    }
+  },
+  "order": ["main"]
+}`
+      await writeFile(joinPath(tmpDir, 'templates', 'collection.json'), baseContent)
+
       const options = {
         name: undefined,
-        type: 'basic',
         path: tmpDir,
         fileType: 'json',
         resource: 'collection',
+        type: 'basic',
       }
 
       // When
@@ -113,42 +138,7 @@ describe('generateTemplate', () => {
       // Then
       const templatePath = joinPath(tmpDir, 'templates', 'collection.json')
       const content = await readFile(templatePath)
-      expect(content).toBe(`{
-  "sections": {
-    "main": {
-      "type": "main-collection",
-      "settings": {}
-    }
-  },
-  "order": [
-    "main"
-  ]
-}`)
-    })
-  })
-
-  test('resolves name conflicts by prompting for a new name', async () => {
-    await inTemporaryDirectory(async (tmpDir) => {
-      // Given
-      await mkdir(joinPath(tmpDir, 'templates'))
-      await writeFile(joinPath(tmpDir, 'templates', 'product.test-template.liquid'), 'test')
-      const options = {
-        name: 'test-template',
-        type: 'basic',
-        path: tmpDir,
-        fileType: 'liquid',
-        resource: 'product',
-      }
-      vi.mocked(renderTextPrompt).mockResolvedValueOnce('test-template').mockResolvedValueOnce('unique-template')
-
-      // When
-      await generateTemplate(options)
-
-      // Then`
-      expect(renderTextPrompt).toHaveBeenCalledTimes(2)
-      expect(renderTextPrompt).toHaveBeenCalledWith({
-        message: 'Template product.test-template.liquid already exists. Please provide a new name:',
-      })
+      expect(content).toBe(baseContent)
     })
   })
 })
