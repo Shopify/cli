96dec4f002175cd0e240623df021bef0138f6070
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2024-12-12 14:51:31 -0500

    Add step to view app in developer dashboard

--- packages/cli/src/cli/commands/demo.ts
+++ packages/cli/src/cli/commands/demo.ts
@@ -3,6 +3,7 @@ import Command from '@shopify/cli-kit/node/base-command'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
 import {AppInitCommand} from '@shopify/app'
 import {renderTextPrompt, renderInfo} from '@shopify/cli-kit/node/ui'
+import {developerDashboardFqdn} from '@shopify/cli-kit/node/context/fqdn'
 
 // just a chill demo
 //                                        .-+.   .-=:
@@ -110,6 +111,51 @@ export default class Demo extends Command {
     })
 
     const appInitCommand = AppInitCommand
-    await appInitCommand.run([])
+    const {app} = await appInitCommand.run([])
+
+    const appLink = await appDeepLink({
+      id: app?.configuration?.app_id,
+      organizationId: app?.configuration?.organization_id,
+    })
+
+    await renderInfo({
+      customSections: [
+        {
+          title: 'Good job you created an app!',
+          body: {
+            list: {
+              items: [
+                'When you chose an Organization, the app was created in that organization.',
+                {
+                  link: {
+                    label: 'Go to the link below to see your app in the Developer Dashboard.',
+                    url: appLink,
+                  },
+                },
+                'Come back here when you are done, and we can make a change to the app!',
+                {bold: 'Completion Time: 1 minute'},
+              ],
+            },
+          },
+        },
+      ],
+    })
   }
+}
+
+async function appDeepLink({
+  id,
+  organizationId,
+}: {
+  id: string | undefined
+  organizationId: string | undefined
+}): Promise<string> {
+  if (!id || !organizationId) throw new Error('App ID and Organization ID are required')
+  return `https://${await developerDashboardFqdn()}/dashboard/${organizationId}/apps/${numberFromGid(id)}`
+}
+
+function numberFromGid(gid: string): number {
+  const match = gid.match(/^gid.*\/(\d+)$/)
+  if (!match?.[1]) throw new Error('Invalid GID format')
+  return Number(match[1])
 }
