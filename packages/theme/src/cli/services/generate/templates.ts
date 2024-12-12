091088e357e37411183c7afbb3b7c84ffceb0dea
James Meng <james.meng@shopify.com>
2024-12-12 15:06:18 -0800

    [Themes] Consider base template existence when creating files

--- packages/theme/src/cli/services/generate/templates.ts
+++ packages/theme/src/cli/services/generate/templates.ts
@@ -1,22 +1,30 @@
-import {FileType, TemplateType} from '../../utilities/generator.js'
+import {FileType} from '../../utilities/generator.js'
 import {fileExists, writeFile} from '@shopify/cli-kit/node/fs'
 import {joinPath} from '@shopify/cli-kit/node/path'
 import {outputInfo} from '@shopify/cli-kit/node/output'
+import {renderTextPrompt} from '@shopify/cli-kit/node/ui'
 
 export interface TemplateGeneratorOptions {
-  name: string
-  type: TemplateType
+  name: string | undefined
   path: string
   fileType: FileType
   resource: string
 }
 
 export async function generateTemplate(options: TemplateGeneratorOptions) {
-  const templatePath = joinPath(options.path, 'templates', `${options.resource}.${options.name}.${options.fileType}`)
+  let templatePath =
+    options.name === undefined
+      ? joinPath(options.path, 'templates', `${options.resource}.${options.fileType}`)
+      : joinPath(options.path, 'templates', `${options.resource}.${options.name}.${options.fileType}`)
 
-  // Check if template already exists
-  if (await fileExists(templatePath)) {
-    throw new Error(`Template ${options.name} already exists at ${templatePath}`)
+  // eslint-disable-next-line no-await-in-loop
+  while (await fileExists(templatePath)) {
+    const filename = templatePath.split('/').pop()
+    // eslint-disable-next-line no-await-in-loop
+    const newName = await renderTextPrompt({
+      message: `Template ${filename} already exists. Please provide a new name:`,
+    })
+    templatePath = joinPath(options.path, 'templates', `${options.resource}.${newName}.${options.fileType}`)
   }
 
   // Write the file
@@ -29,8 +37,8 @@ export async function generateTemplate(options: TemplateGeneratorOptions) {
 
 function generateLiquidTemplateContent(options: TemplateGeneratorOptions): string {
   const baseSchema = {
-    name: options.name,
-    type: options.type,
+    name: options.resource,
+    type: options.resource,
     settings: [],
   }
 
@@ -43,7 +51,7 @@ function generateJsonTemplateContent(options: TemplateGeneratorOptions): string 
   const schema = {
     sections: {
       main: {
-        type: `main-${options.type}`,
+        type: `main-${options.resource}`,
         settings: {},
       },
     },
