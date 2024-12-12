925e454e61e88d6b329927b38b8b574e0c83f66d
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2024-12-12 16:04:15 -0500

    refactor of config file patch

--- packages/app/src/cli/services/generate/workflows/editor-extension-collection.ts
+++ packages/app/src/cli/services/generate/workflows/editor-extension-collection.ts
@@ -1,8 +1,7 @@
 import {Workflow} from './registry.js'
 import {joinPath} from '@shopify/cli-kit/node/path'
 import {renderTextPrompt} from '@shopify/cli-kit/node/ui'
-import {readFile, writeFile} from '@shopify/cli-kit/node/fs'
-import {decodeToml, encodeToml} from '@shopify/cli-kit/node/toml'
+import {patchConfigurationFile} from './patch-configuration-file.js'
 
 export const editorExtensionCollection: Workflow = {
   afterGenerate: async (options) => {
@@ -14,15 +13,13 @@ export const editorExtensionCollection: Workflow = {
     const extensions = await renderTextPrompt({
       message: `The extension handles to include in the collection, comma separated. Options: ${availableExtensions}`,
     })
-    const tomlPath = joinPath(options.generatedExtension.directory, 'shopify.extension.toml')
-    const tomlContents = await readFile(tomlPath)
-    const configuration = decodeToml(tomlContents)
-    if (configuration.extensions && Array.isArray(configuration.extensions) && configuration.extensions.length > 0) {
-      ;(configuration.extensions[0] as {includes: string[]}).includes = extensions
-        .split(',')
-        .map((handle) => handle.trim())
-    }
-    const encodedString = encodeToml(configuration)
-    await writeFile(tomlPath, encodedString)
+    await patchConfigurationFile({
+      path: joinPath(options.generatedExtension.directory, 'shopify.extension.toml'),
+      patch: {
+        extensions: [{
+          includes: extensions.split(',').map((handle) => handle.trim())
+        }],
+      },
+    })
   },
 }
