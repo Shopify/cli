6aba086f04f2e94dd6b964e1306d156e30b4025c
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2024-12-12 13:38:37 -0500

    some more work on editor collections

--- packages/app/src/cli/services/generate/workflows/editor-extension-collection.ts
+++ packages/app/src/cli/services/generate/workflows/editor-extension-collection.ts
@@ -1,7 +1,26 @@
 import {Workflow} from './registry.js'
+import {patchAppConfigurationFile} from '../../app/patch-app-configuration-file.js'
+import {joinPath} from '@shopify/cli-kit/node/path'
+import {renderTextPrompt} from '@shopify/cli-kit/node/ui'
 
 export const editorExtensionCollection: Workflow = {
   afterGenerate: async (options) => {
-    console.log('hello world')
+    const existingExtensions = options.generateOptions.app.extensionsForType({
+      identifier: 'ui_extension',
+      externalIdentifier: 'ui_extension',
+    })
+    const availableExtensions = existingExtensions.map((extension) => extension.handle).join(' ')
+    const extensions = await renderTextPrompt({
+      message: `The extension handles to include in the collection, comma separated. Options: ${availableExtensions}`,
+    })
+    await patchAppConfigurationFile({
+      path: joinPath(options.generatedExtension.directory, 'shopify.extension.toml'),
+      patch: {
+        extensions: {
+          includes: extensions.split(',').map((handle) => handle.trim()),
+        },
+      },
+      schema: options.generateOptions.app.configSchema,
+    })
   },
 }
