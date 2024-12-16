13471200e484139091d2c6fa7daa480be1ea0150
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2024-12-16 14:12:28 -0500

    Add token auth to theme profile command

--- packages/theme/src/cli/commands/theme/profile.ts
+++ packages/theme/src/cli/commands/theme/profile.ts
@@ -1,17 +1,24 @@
+import {themeFlags} from '../../flags.js'
 import ThemeCommand from '../../utilities/theme-command.js'
 import {profile} from '../../services/profile.js'
 import {Flags} from '@oclif/core'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
+import {ensureThemeStore} from '../../utilities/theme-store.js'
+import {ensureAuthenticatedThemes} from '@shopify/cli-kit/node/session'
 
 export default class Profile extends ThemeCommand {
   static summary = 'Profile the Liquid rendering of a theme page.'
 
-  static descriptionWithMarkdown = `TODO`
+  static usage = ['theme profile --url /products/classic-leather-jacket']
+
+  static descriptionWithMarkdown = `Profile the Shopify Liquid on a given page (URL).`
 
   static description = this.descriptionWithoutMarkdown()
 
   static flags = {
     ...globalFlags,
+    store: themeFlags.store,
+    password: themeFlags.password,
     url: Flags.string({
       char: 'u',
       description: 'URL to the theme page to profile.',
@@ -27,7 +34,9 @@ export default class Profile extends ThemeCommand {
 
   async run(): Promise<void> {
     const {flags} = await this.parse(Profile)
+    const store = ensureThemeStore(flags)
+    const adminSession = await ensureAuthenticatedThemes(store, flags.password, [], true)
 
-    await profile(flags.url, flags.json)
+    await profile(adminSession, flags.password, store, flags.url, flags.json)
   }
 }
