66ee3ec34f276dcd5f86951ce1d37076cb859532
Fran Dios <fran.dios@shopify.com>
2025-01-06 20:12:48 +0100

    Move hot reload decisions to frontend to increase client flexibility

--- packages/theme/src/cli/utilities/theme-fs.ts
+++ packages/theme/src/cli/utilities/theme-fs.ts
@@ -173,10 +173,11 @@ export function mountThemeFileSystem(root: string, options?: ThemeFileSystemOpti
           })
           .catch(() => {})
       },
-      onSync: (fn) => {
+      onSync: (onSuccess, onError) => {
         syncPromise
           .then((didSync) => {
-            if (didSync) fn()
+            if (didSync) onSuccess()
+            else onError?.()
           })
           .catch(() => {})
       },
