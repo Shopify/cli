c1062a91c0f357a7c8bbea09c456408bc4f64cea
Fran Dios <fran.dios@shopify.com>
2025-01-16 15:55:16 +0100

    Fix path in unbundled unit tests

--- packages/theme/src/cli/utilities/asset-path.ts
+++ packages/theme/src/cli/utilities/asset-path.ts
@@ -4,6 +4,10 @@ import {createRequire} from 'node:module'
 const require = createRequire(import.meta.url)
 
 export async function resolveAssetPath(...subpaths: string[]) {
+  if (process.env.SHOPIFY_UNIT_TEST) {
+    return joinPath(__dirname, '..', '..', '..', ...subpaths)
+  }
+
   const cliRootPath = dirname(require.resolve('@shopify/cli/package.json'))
   return joinPath(cliRootPath, 'dist', 'assets', ...subpaths)
 }
