ac51604a56118c395560199c9408701f056be9db
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2025-03-04 11:09:21 +0100

    Avoid throwing errors from background fetch

--- packages/cli/src/cli/commands/notifications/list.ts
+++ packages/cli/src/cli/commands/notifications/list.ts
@@ -1,11 +1,33 @@
 import {list} from '../../services/commands/notifications.js'
+import {Flags} from '@oclif/core'
 import Command from '@shopify/cli-kit/node/base-command'
+import {sendErrorToBugsnag} from '@shopify/cli-kit/node/error-handler'
 
 export default class List extends Command {
   static description = 'List current notifications configured for the CLI.'
   static hidden = true
 
+  static flags = {
+    'ignore-errors': Flags.boolean({
+      hidden: false,
+      description: "Don't fail if an error occurs.",
+      env: 'SHOPIFY_FLAG_IGNORE_ERRORS',
+    }),
+  }
+
   async run(): Promise<void> {
-    await list()
+    const {flags} = await this.parse(List)
+    try {
+      await list()
+    } catch (error) {
+      let message = `Error fetching notifications`
+      if (error instanceof Error) {
+        message = message.concat(`: ${error.message}`)
+      }
+      await sendErrorToBugsnag(message, 'expected_error')
+      if (!flags['ignore-errors']) {
+        throw error
+      }
+    }
   }
 }
