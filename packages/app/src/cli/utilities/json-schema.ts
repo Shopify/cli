9fcd22074fdd00a77c581fcf3253e3f47cd4bebe
Isaac Roldán <isaac.roldan@shopify.com>
2025-03-04 17:22:13 +0100

    Fix bug with contracts schemas

--- packages/app/src/cli/utilities/json-schema.ts
+++ packages/app/src/cli/utilities/json-schema.ts
@@ -1,6 +1,6 @@
 import {FlattenedRemoteSpecification} from '../api/graphql/extension_specifications.js'
 import {BaseConfigType} from '../models/extensions/schemas.js'
-import {configWithoutFirstClassFields, RemoteAwareExtensionSpecification} from '../models/extensions/specification.js'
+import {RemoteAwareExtensionSpecification} from '../models/extensions/specification.js'
 import {ParseConfigurationResult} from '@shopify/cli-kit/node/schema'
 import {
   HandleInvalidAdditionalProperties,
@@ -9,6 +9,22 @@ import {
 } from '@shopify/cli-kit/node/json-schema'
 import {isEmpty} from '@shopify/cli-kit/common/object'
 import {JsonMapType} from '@shopify/cli-kit/node/toml'
+
+/**
+ * The base properties that are added to all JSON Schema contracts.
+ *
+ * These are not part of the specific extension contract, but are present in all module tomls (with 'uuid' uidStrategy).
+ * They are optional properties, we just want to keep them if present in the config.
+ *
+ * They'll be stripped before deployment (see usage of `configWithoutFirstClassFields` in `specification.ts`)
+ */
+const JsonSchemaBaseProperties = {
+  type: {type: 'string'},
+  handle: {type: 'string'},
+  uid: {type: 'string'},
+  path: {type: 'string'},
+  extensions: {},
+}
 
 /**
  * Factory returning a function that can parse a configuration object against a locally defined zod schema, and a remotely defined JSON schema based contract
@@ -24,6 +40,7 @@ export async function unifiedConfigurationParserFactory(
     return merged.parseConfigurationObject
   }
   const contract = await normaliseJsonSchema(contractJsonSchema)
+  contract.properties = {...JsonSchemaBaseProperties, ...contract.properties}
   const extensionIdentifier = merged.identifier
 
   const parseConfigurationObject = (config: object): ParseConfigurationResult<BaseConfigType> => {
@@ -34,9 +51,8 @@ export async function unifiedConfigurationParserFactory(
     const zodValidatedData = zodParse.state === 'ok' ? zodParse.data : undefined
     const subjectForAjv = zodValidatedData ?? (config as JsonMapType)
 
-    const subjectForAjvWithoutFirstClassFields = configWithoutFirstClassFields(subjectForAjv)
     const jsonSchemaParse = jsonSchemaValidate(
-      subjectForAjvWithoutFirstClassFields,
+      subjectForAjv,
       contract,
       handleInvalidAdditionalProperties,
       extensionIdentifier,
@@ -63,6 +79,7 @@ export async function unifiedConfigurationParserFactory(
         errors,
       }
     }
+
     return {
       state: 'ok',
       data: jsonSchemaParse.data as BaseConfigType,
