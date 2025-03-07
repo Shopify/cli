5dee7f7ab1b02efdbb4ce12f0b4f30d1e0d00e53
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2025-03-07 11:08:30 -0500

    adjusted tests and added new ones

--- packages/app/src/cli/utilities/mkcert.test.ts
+++ packages/app/src/cli/utilities/mkcert.test.ts
@@ -6,6 +6,7 @@ import {joinPath} from '@shopify/cli-kit/node/path'
 import which from 'which'
 import {downloadGitHubRelease} from '@shopify/cli-kit/node/github'
 import {testWithTempDir} from '@shopify/cli-kit/node/testing/test-with-temp-dir'
+import {AbortError} from '@shopify/cli-kit/node/error'
 
 vi.mock('@shopify/cli-kit/node/system')
 vi.mock('which')
@@ -23,10 +24,10 @@ describe('mkcert', () => {
         await writeFile(joinPath(appDirectory, '.shopify', 'localhost.pem'), 'cert')
       })
 
-      const onRequiresDownloadConfirmation = vi.fn()
-      const {keyContent, certContent} = await generateCertificate({
+      const onRequiresConfirmation = vi.fn().mockReturnValue(true)
+      const {keyContent, certContent, certPath} = await generateCertificate({
         appDirectory,
-        onRequiresDownloadConfirmation,
+        onRequiresConfirmation,
         env: {
           SHOPIFY_CLI_MKCERT_BINARY: '/path/to/mkcert',
         },
@@ -35,6 +36,7 @@ describe('mkcert', () => {
 
       expect(keyContent).toBe('key')
       expect(certContent).toBe('cert')
+      expect(certPath).toBe(joinPath('.shopify', 'localhost.pem'))
     })
 
     testWithTempDir('generates a certificate using mkcert from default path', async ({tempDir}) => {
@@ -52,15 +54,16 @@ describe('mkcert', () => {
         await writeFile(joinPath(appDirectory, '.shopify', 'localhost.pem'), 'cert')
       })
 
-      const onRequiresDownloadConfirmation = vi.fn()
-      const {keyContent, certContent} = await generateCertificate({
+      const onRequiresConfirmation = vi.fn().mockReturnValue(true)
+      const {keyContent, certContent, certPath} = await generateCertificate({
         appDirectory,
-        onRequiresDownloadConfirmation,
+        onRequiresConfirmation,
         platform: 'linux',
       })
 
       expect(keyContent).toBe('key')
       expect(certContent).toBe('cert')
+      expect(certPath).toBe(joinPath('.shopify', 'localhost.pem'))
     })
 
     testWithTempDir('generates a certificate using mkcert from system PATH', async ({tempDir}) => {
@@ -76,15 +79,16 @@ describe('mkcert', () => {
         await writeFile(joinPath(appDirectory, '.shopify', 'localhost.pem'), 'cert')
       })
 
-      const onRequiresDownload = vi.fn()
-      const {keyContent, certContent} = await generateCertificate({
+      const onRequiresConfirmation = vi.fn().mockReturnValue(true)
+      const {keyContent, certContent, certPath} = await generateCertificate({
         appDirectory,
-        onRequiresDownloadConfirmation: onRequiresDownload,
+        onRequiresConfirmation,
         platform: 'linux',
       })
 
       expect(keyContent).toBe('key')
       expect(certContent).toBe('cert')
+      expect(certPath).toBe(joinPath('.shopify', 'localhost.pem'))
     })
 
     testWithTempDir('generates a certificate using a downloaded mkcert', async ({tempDir}) => {
@@ -98,16 +102,17 @@ describe('mkcert', () => {
         await writeFile(joinPath(appDirectory, '.shopify', 'localhost.pem'), 'cert')
       })
 
-      const onRequiresDownloadConfirmation = vi.fn().mockReturnValue(true)
-      const {keyContent, certContent} = await generateCertificate({
+      const onRequiresConfirmation = vi.fn().mockReturnValue(true)
+      const {keyContent, certContent, certPath} = await generateCertificate({
         appDirectory,
-        onRequiresDownloadConfirmation,
+        onRequiresConfirmation,
         platform: 'linux',
       })
 
       expect(keyContent).toBe('key')
       expect(certContent).toBe('cert')
-      expect(onRequiresDownloadConfirmation).toHaveBeenCalled()
+      expect(certPath).toBe(joinPath('.shopify', 'localhost.pem'))
+      expect(onRequiresConfirmation).toHaveBeenCalled()
       expect(downloadGitHubRelease).toHaveBeenCalledWith(
         'FiloSottile/mkcert',
         'v1.4.4',
@@ -127,22 +132,59 @@ describe('mkcert', () => {
         await writeFile(joinPath(appDirectory, '.shopify', 'localhost.pem'), 'cert')
       })
 
-      const onRequiresDownloadConfirmation = vi.fn().mockReturnValue(true)
-      const {keyContent, certContent} = await generateCertificate({
+      const onRequiresConfirmation = vi.fn().mockReturnValue(true)
+      const {keyContent, certContent, certPath} = await generateCertificate({
         appDirectory,
-        onRequiresDownloadConfirmation,
+        onRequiresConfirmation,
         platform: 'win32',
       })
 
       expect(keyContent).toBe('key')
       expect(certContent).toBe('cert')
-      expect(onRequiresDownloadConfirmation).toHaveBeenCalled()
+      expect(certPath).toBe(joinPath('.shopify', 'localhost.pem'))
+      expect(onRequiresConfirmation).toHaveBeenCalled()
       expect(downloadGitHubRelease).toHaveBeenCalledWith(
         'FiloSottile/mkcert',
         'v1.4.4',
         expect.any(String),
         mkcertDefaultPath,
       )
+    })
+
+    testWithTempDir('skips certificate generation if the user does not confirm', async ({tempDir}) => {
+      const appDirectory = tempDir
+      const onRequiresConfirmation = vi.fn().mockReturnValue(false)
+      const generatePromise = generateCertificate({
+        appDirectory,
+        onRequiresConfirmation,
+        platform: 'linux',
+      })
+
+      await expect(generatePromise).rejects.toThrow(AbortError)
+      expect(onRequiresConfirmation).toHaveBeenCalled()
+      expect(exec).not.toHaveBeenCalled()
+      expect(downloadGitHubRelease).not.toHaveBeenCalled()
+    })
+
+    testWithTempDir('skips certificate generation if the certificate already exists', async ({tempDir}) => {
+      const appDirectory = tempDir
+      await mkdir(joinPath(appDirectory, '.shopify'))
+      await writeFile(joinPath(appDirectory, '.shopify', 'localhost-key.pem'), 'key')
+      await writeFile(joinPath(appDirectory, '.shopify', 'localhost.pem'), 'cert')
+
+      const onRequiresConfirmation = vi.fn()
+      const {keyContent, certContent, certPath} = await generateCertificate({
+        appDirectory,
+        onRequiresConfirmation,
+        platform: 'linux',
+      })
+
+      expect(keyContent).toBe('key')
+      expect(certContent).toBe('cert')
+      expect(certPath).toBe(joinPath('.shopify', 'localhost.pem'))
+      expect(onRequiresConfirmation).not.toHaveBeenCalled()
+      expect(exec).not.toHaveBeenCalled()
+      expect(downloadGitHubRelease).not.toHaveBeenCalled()
     })
   })
 })
