423f231693e76caccba93e1b1ee5680a5d46efc4
Shaun Stanworth <shaun.stanworth@shopify.com>
2025-03-18 11:47:23 +0000

    Update DevServer vendor

---
+++ packages/cli-kit/src/public/node/vendor/dev_server/types.ts
@@ -0,0 +1,13 @@
+export interface DevServerCore {
+  url(prefix: string): string
+  host(prefix: string): string
+}
+
+export interface DevServer {
+  url(options?: HostOptions): string
+  host(options?: HostOptions): string
+}
+
+export interface HostOptions {
+  nonstandardHostPrefix?: string
+}
