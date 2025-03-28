e9c25a614e352f50fc28a0021d97ffe1c3264da9
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2025-03-28 14:41:19 +0100

    Exclude map files from the app bundle

--- packages/app/src/cli/services/bundle.ts
+++ packages/app/src/cli/services/bundle.ts
@@ -14,5 +14,6 @@ export async function compressBundle(inputPath: string, outputPath: string) {
   await zip({
     inputDirectory: inputPath,
     outputZipPath: outputPath,
+    matchFilePattern: ['**/*', '!**/*.js.map'],
   })
 }
