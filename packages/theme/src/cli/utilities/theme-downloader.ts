9551c9322039a4a17312138068a54cba49621728
James Meng <james.meng@shopify.com>
2025-05-15 16:59:09 -0700

    wip: https://github.com/Shopify/developer-tools-team/issues/457

--- packages/theme/src/cli/utilities/theme-downloader.ts
+++ packages/theme/src/cli/utilities/theme-downloader.ts
@@ -1,9 +1,10 @@
-import {batchedTasks, Task} from './batching.js'
+import {Task} from './batching.js'
 import {MAX_GRAPHQL_THEME_FILES} from '../constants.js'
 import {AdminSession} from '@shopify/cli-kit/node/session'
 import {fetchThemeAssets} from '@shopify/cli-kit/node/themes/api'
 import {ThemeFileSystem, Theme, Checksum, ThemeAsset} from '@shopify/cli-kit/node/themes/types'
 import {renderTasks} from '@shopify/cli-kit/node/ui'
+import {outputInfo} from '@shopify/cli-kit/node/output'
 
 interface DownloadOptions {
   nodelete: boolean
@@ -16,30 +17,112 @@ export async function downloadTheme(
   themeFileSystem: ThemeFileSystem,
   options: DownloadOptions,
 ) {
+  const getProgress = (params: {current: number; total: number}) =>
+    `[${Math.round((params.current / params.total) * 100)}%]`
   const deleteTasks = buildDeleteTasks(remoteChecksums, themeFileSystem, options)
   const downloadTasks = buildDownloadTasks(remoteChecksums, theme, themeFileSystem, session)
 
-  const tasks = [...deleteTasks, ...downloadTasks]
+  await renderTasks(
+    createIntervalTask({
+      promise: deleteTasks.promise,
+      titleGetter: () => `Cleaning your local directory ${getProgress(deleteTasks.progress)}`,
+      timeout: 1000,
+    }),
+  )
 
-  if (tasks.length > 0) {
-    await renderTasks(tasks)
-  }
+  outputInfo('!!! yooo')
+
+  await renderTasks(
+    createIntervalTask({
+      promise: downloadTasks.promise,
+      titleGetter: () => `Downloading files ${getProgress(downloadTasks.progress)}`,
+      timeout: 1000,
+    }),
+  )
+
+  await deleteTasks.promise
+  await downloadTasks.promise
+  outputInfo('!!! done')
+
+  // const tasks = [...deleteTasks, ...downloadTasks]
+
+  // if (tasks.length > 0) {
+  //   await renderTasks(
+  //     createIntervalTask({
+  //       promise: deleteTasks,
+  //       titleGetter: () => 'Downloading files',
+  //       timeout: 1000,
+  //     }),
+  //   )
+  // }
 }
 
-function buildDeleteTasks(remoteChecksums: Checksum[], themeFileSystem: ThemeFileSystem, options: DownloadOptions) {
-  if (options.nodelete) return []
+function buildDeleteTasks(
+  remoteChecksums: Checksum[],
+  themeFileSystem: ThemeFileSystem,
+  options: DownloadOptions,
+): SyncJob {
+  if (options.nodelete) return {progress: {current: 0, total: 0}, promise: Promise.resolve()}
 
   const remoteKeys = new Set(remoteChecksums.map((checksum) => checksum.key))
 
   const localKeys = themeFileSystem.applyIgnoreFilters([...themeFileSystem.files.values()]).map(({key}) => key)
   const localFilesToBeDeleted = localKeys.filter((key) => !remoteKeys.has(key))
 
-  return localFilesToBeDeleted.map((key) => {
-    return {
-      title: `Cleaning your local directory (removing ${key})`,
-      task: async () => themeFileSystem.delete(key),
-    }
+  const progress = {current: 0, total: localFilesToBeDeleted.length}
+  const deletionPromise = localFilesToBeDeleted.map((key) =>
+    themeFileSystem.delete(key).then(() => {
+      progress.current += 1
+    }),
+  )
+
+  const promise = Promise.all(deletionPromise).then(() => {
+    progress.current = progress.total
   })
+
+  // return localFilesToBeDeleted.map((key) => {
+  //   return {
+  //     title: `Cleaning your local directory (removing ${key})`,
+  //     task: async () => themeFileSystem.delete(key),
+  //   }
+  // })
+  return {progress, promise}
+}
+
+interface SyncJob {
+  progress: {current: number; total: number}
+  promise: Promise<void>
+}
+
+function createIntervalTask({
+  promise,
+  titleGetter,
+  timeout,
+}: {
+  promise: Promise<unknown>
+  titleGetter: () => string
+  timeout: number
+}) {
+  const tasks: Task[] = []
+
+  const addNextCheck = () => {
+    tasks.push({
+      title: titleGetter(),
+      task: async () => {
+        const result = await Promise.race([
+          promise,
+          new Promise((resolve) => setTimeout(() => resolve('timeout'), timeout)),
+        ])
+
+        if (result === 'timeout') {
+          addNextCheck()
+        }
+      },
+    })
+  }
+
+  addNextCheck()
+  return tasks
 }
 
 function buildDownloadTasks(
@@ -47,31 +130,40 @@ function buildDownloadTasks(
   theme: Theme,
   themeFileSystem: ThemeFileSystem,
   session: AdminSession,
-): Task[] {
-  let checksums = themeFileSystem.applyIgnoreFilters(remoteChecksums)
+): SyncJob {
+  const checksums = themeFileSystem.applyIgnoreFilters(remoteChecksums)
 
   // Filter out files we already have
-  checksums = checksums.filter((checksum) => {
-    const remoteChecksumValue = checksum.checksum
-    const localAsset = themeFileSystem.files.get(checksum.key)
+  const filesToDownload = checksums
+    .filter((checksum) => {
+      const remoteChecksumValue = checksum.checksum
+      const localAsset = themeFileSystem.files.get(checksum.key)
 
-    if (localAsset?.checksum === remoteChecksumValue) {
-      return false
-    } else {
-      return true
-    }
+      if (localAsset?.checksum === remoteChecksumValue) {
+        return false
+      } else {
+        return true
+      }
+    })
+    .map((checksum) => checksum.key)
+
+  const progress = {current: 0, total: filesToDownload.length}
+  const batches: string[][] = []
+  for (let i = 0; i < filesToDownload.length; i += MAX_GRAPHQL_THEME_FILES) {
+    batches.push(filesToDownload.slice(i, i + MAX_GRAPHQL_THEME_FILES))
+  }
+
+  const downloadPromises = batches.map((batch) =>
+    downloadFiles(theme, themeFileSystem, batch, session).then(() => {
+      progress.current += batch.length
+    }),
+  )
+
+  const promise = Promise.all(downloadPromises).then(() => {
+    progress.current = progress.total
   })
 
-  const filenames = checksums.map((checksum) => checksum.key)
-
-  const batches = batchedTasks(filenames, MAX_GRAPHQL_THEME_FILES, (batchedFilenames, i) => {
-    const title = `Downloading files ${i}..${i + batchedFilenames.length} / ${filenames.length} files`
-    return {
-      title,
-      task: async () => downloadFiles(theme, themeFileSystem, batchedFilenames, session),
-    }
-  })
-  return batches
+  return {progress, promise}
 }
 
 async function downloadFiles(theme: Theme, fileSystem: ThemeFileSystem, filenames: string[], session: AdminSession) {
