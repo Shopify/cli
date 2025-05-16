e18c9f6e20018eb421b0ee18813c9e6330c0c856
Alok Swamy <alok.swamy@shopify.com>
2025-05-16 16:33:39 -0400

    Set `Shopify/skeleton-theme` as the default theme

--- packages/theme/src/cli/services/init.ts
+++ packages/theme/src/cli/services/init.ts
@@ -1,9 +1,5 @@
 import {renderTasks} from '@shopify/cli-kit/node/ui'
-import {downloadGitRepository, getLatestTag} from '@shopify/cli-kit/node/git'
-import {inTemporaryDirectory} from '@shopify/cli-kit/node/fs'
-
-export const SKELETON_THEME_URL = 'https://github.com/Shopify/skeleton-theme.git'
-export const DAWN_URL = 'https://github.com/Shopify/dawn.git'
+import {downloadGitRepository} from '@shopify/cli-kit/node/git'
 
 export async function cloneRepo(repoUrl: string, destination: string) {
   await downloadRepository(repoUrl, destination)
@@ -26,15 +22,4 @@ async function downloadRepository(repoUrl: string, destination: string, latestTa
       },
     },
   ])
-}
-
-export async function getSkeletonThemeLatestTag() {
-  return inTemporaryDirectory(async (tempDir) => {
-    await downloadGitRepository({
-      repoUrl: SKELETON_THEME_URL,
-      destination: tempDir,
-    })
-
-    return getLatestTag(tempDir)
-  })
 }
