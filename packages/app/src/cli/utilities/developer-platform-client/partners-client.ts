db02e3f5ee7401e76f5e24689ab1758dd24b7fb2
Rod Mcnew <rod.mcnew@shopify.com>
2025-06-03 12:18:34 -0500

    test ci

--- packages/app/src/cli/utilities/developer-platform-client/partners-client.ts
+++ packages/app/src/cli/utilities/developer-platform-client/partners-client.ts
@@ -618,7 +618,7 @@ export class PartnersClient implements DeveloperPlatformClient {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async devSessionDelete(_input: unknown): Promise<any> {
-    // Dev Sessions are not supported in partners client.
+    // Dev Sessions are not supported in partners client. TESTCI
     return Promise.resolve()
   }
 
