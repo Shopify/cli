c2433d9ac7d1a8bbfc7ab81b8930440f4f502e16
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2025-06-13 13:16:41 +0200

    Do not require authentication for app function build

--- packages/app/src/cli/commands/app/function/build.ts
+++ packages/app/src/cli/commands/app/function/build.ts
@@ -1,11 +1,12 @@
-import {inFunctionContext, functionFlags} from '../../../services/function/common.js'
+import {chooseFunction, functionFlags} from '../../../services/function/common.js'
 import {buildFunctionExtension} from '../../../services/build/extension.js'
 import {appFlags} from '../../../flags.js'
-import AppLinkedCommand, {AppLinkedCommandOutput} from '../../../utilities/app-linked-command.js'
+import AppUnlinkedCommand, {AppUnlinkedCommandOutput} from '../../../utilities/app-unlinked-command.js'
+import {localAppContext} from '../../../services/app-context.js'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
 import {renderSuccess} from '@shopify/cli-kit/node/ui'
 
-export default class FunctionBuild extends AppLinkedCommand {
+export default class FunctionBuild extends AppUnlinkedCommand {
   static summary = 'Compile a function to wasm.'
 
   static descriptionWithMarkdown = `Compiles the function in your current directory to WebAssembly (Wasm) for testing purposes.`
@@ -18,26 +19,25 @@ export default class FunctionBuild extends AppLinkedCommand {
     ...functionFlags,
   }
 
-  public async run(): Promise<AppLinkedCommandOutput> {
+  public async run(): Promise<AppUnlinkedCommandOutput> {
     const {flags} = await this.parse(FunctionBuild)
 
-    const app = await inFunctionContext({
-      path: flags.path,
+    const app = await localAppContext({
+      directory: flags.path,
       userProvidedConfigName: flags.config,
-      apiKey: flags['client-id'],
-      reset: flags.reset,
-      callback: async (app, _, ourFunction) => {
-        await buildFunctionExtension(ourFunction, {
-          app,
-          stdout: process.stdout,
-          stderr: process.stderr,
-          useTasks: true,
-          environment: 'production',
-        })
-        renderSuccess({headline: 'Function built successfully.'})
-        return app
-      },
     })
+
+    const ourFunction = await chooseFunction(app, flags.path)
+
+    await buildFunctionExtension(ourFunction, {
+      app,
+      stdout: process.stdout,
+      stderr: process.stderr,
+      useTasks: true,
+      environment: 'production',
+    })
+    renderSuccess({headline: 'Function built successfully.'})
+
     return {app}
   }
 }
