da79dbeec3f3482c0808eff03b94e065922d44e4
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2025-06-13 13:16:41 +0200

    Do not require authentication for app function build

--- packages/app/src/cli/commands/app/function/build.ts
+++ packages/app/src/cli/commands/app/function/build.ts
@@ -1,7 +1,8 @@
-import {inFunctionContext, functionFlags} from '../../../services/function/common.js'
+import {chooseFunction, functionFlags} from '../../../services/function/common.js'
 import {buildFunctionExtension} from '../../../services/build/extension.js'
 import {appFlags} from '../../../flags.js'
 import AppCommand, {AppCommandOutput} from '../../../utilities/app-command.js'
+import {localAppContext} from '../../../services/app-context.js'
 import {globalFlags} from '@shopify/cli-kit/node/cli'
 import {renderSuccess} from '@shopify/cli-kit/node/ui'
 
@@ -21,23 +22,22 @@ export default class FunctionBuild extends AppCommand {
   public async run(): Promise<AppCommandOutput> {
     const {flags} = await this.parse(FunctionBuild)
 
-    const app = await inFunctionContext({
-      path: flags.path,
+    const app = await localAppContext({
+      directory: flags.path,
       userProvidedConfigName: flags.config,
-      apiKey: flags['client-id'],
-      reset: flags.reset,
-      callback: async (app, _, ourFunction) => {
-        await buildFunctionExtension(ourFunction, {
-          app,
-          stdout: process.stdout,
-          stderr: process.stderr,
-          useTasks: true,
-          environment: 'production',
-        })
-        renderSuccess({headline: 'Function built successfully.'})
-        return app
-      },
     })
+
+    const ourFunction = await chooseFunction(app, flags.path)
+
+    await buildFunctionExtension(ourFunction, {
+      app,
+      stdout: process.stdout,
+      stderr: process.stderr,
+      useTasks: true,
+      environment: 'production',
+    })
+    renderSuccess({headline: 'Function built successfully.'})
+
     return {app}
   }
 }
