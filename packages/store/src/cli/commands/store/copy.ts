b048396b4d3ad46c38c1ca71934c09237727b656
Pablo Mercado <pablo.mercado@shopify.com>
2025-06-26 11:18:11 -0400

    change text

--- packages/store/src/cli/commands/store/copy.ts
+++ packages/store/src/cli/commands/store/copy.ts
@@ -1,8 +1,8 @@
 import Command from '@shopify/cli-kit/node/base-command'
 
 export default class CopyCommand extends Command {
-  static summary = 'Copy a theme to another store.'
-  static description = 'This command allows you to copy a theme from one Shopify store to another.'
+  static summary = 'Copy data to another store.'
+  static description = 'This command allows you to copy data from one Shopify store to another.'
 
   async run() {
     console.log('This command is not yet implemented.')
