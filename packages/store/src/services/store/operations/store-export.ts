24f91851421ba1b654d56f13604dffcde5a653ae
Pablo Mercado <pablo.mercado@shopify.com>
2025-06-26 10:19:30 -0400

    new flag format

--- packages/store/src/services/store/operations/store-export.ts
+++ packages/store/src/services/store/operations/store-export.ts
@@ -3,15 +3,15 @@ import {FlagOptions} from '../../../lib/types.js'
 import {generateExportFilename} from '../utils/file-utils.js'
 
 export class StoreExportOperation implements StoreOperation {
-  async execute(from: string, to: string, flags: FlagOptions): Promise<void> {
+  async execute(fromStore: string, toFile: string, flags: FlagOptions): Promise<void> {
     // TODO: Implement actual export logic
     throw new Error('Store export functionality is not implemented yet')
   }
 
-  private determineOutputPath(from: string, to: string): string {
-    if (to === '<sqlite>') {
-      return generateExportFilename(from)
+  private determineOutputPath(fromStore: string, toFile: string): string {
+    if (toFile === '') {
+      return generateExportFilename(fromStore)
     }
-    return to
+    return toFile
   }
 }
