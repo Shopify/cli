f9a8cca03fdb4b25591b9482237d48b9df57962c
Pablo Mercado <pablo.mercado@shopify.com>
2025-07-07 20:21:34 -0400

    fix type gen

--- packages/store/src/cli/api/graphql/admin/generated/staged_uploads_create.ts
+++ packages/store/src/cli/api/graphql/admin/generated/staged_uploads_create.ts
@@ -1,103 +1,14 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-import * as Types from './types.js'
+/* eslint-disable @typescript-eslint/consistent-type-definitions, @typescript-eslint/naming-convention, @typescript-eslint/ban-types, @typescript-eslint/no-duplicate-type-constituents, @typescript-eslint/no-redundant-type-constituents, @nx/enforce-module-boundaries */
+import {JsonMapType} from '@shopify/cli-kit/node/toml'
+import * as Types from './types';
 
-import {TypedDocumentNode as DocumentNode} from '@graphql-typed-document-node/core'
-
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 export type StagedUploadsCreateMutationVariables = Types.Exact<{
-  input: Types.StagedUploadInput[] | Types.StagedUploadInput
-}>
+  input: Array<Types.StagedUploadInput> | Types.StagedUploadInput;
+}>;
 
-export type StagedUploadsCreateMutation = {
-  stagedUploadsCreate?: {
-    stagedTargets?:
-      | {
-          url?: string | null
-          resourceUrl?: string | null
-          parameters: {name: string; value: string}[]
-        }[]
-      | null
-    userErrors: {field?: string[] | null; message: string}[]
-  } | null
-}
 
-export const StagedUploadsCreate = {
-  kind: 'Document',
-  definitions: [
-    {
-      kind: 'OperationDefinition',
-      operation: 'mutation',
-      name: {kind: 'Name', value: 'StagedUploadsCreate'},
-      variableDefinitions: [
-        {
-          kind: 'VariableDefinition',
-          variable: {kind: 'Variable', name: {kind: 'Name', value: 'input'}},
-          type: {
-            kind: 'NonNullType',
-            type: {
-              kind: 'ListType',
-              type: {kind: 'NonNullType', type: {kind: 'NamedType', name: {kind: 'Name', value: 'StagedUploadInput'}}},
-            },
-          },
-        },
-      ],
-      selectionSet: {
-        kind: 'SelectionSet',
-        selections: [
-          {
-            kind: 'Field',
-            name: {kind: 'Name', value: 'stagedUploadsCreate'},
-            arguments: [
-              {
-                kind: 'Argument',
-                name: {kind: 'Name', value: 'input'},
-                value: {kind: 'Variable', name: {kind: 'Name', value: 'input'}},
-              },
-            ],
-            selectionSet: {
-              kind: 'SelectionSet',
-              selections: [
-                {
-                  kind: 'Field',
-                  name: {kind: 'Name', value: 'stagedTargets'},
-                  selectionSet: {
-                    kind: 'SelectionSet',
-                    selections: [
-                      {kind: 'Field', name: {kind: 'Name', value: 'url'}},
-                      {kind: 'Field', name: {kind: 'Name', value: 'resourceUrl'}},
-                      {
-                        kind: 'Field',
-                        name: {kind: 'Name', value: 'parameters'},
-                        selectionSet: {
-                          kind: 'SelectionSet',
-                          selections: [
-                            {kind: 'Field', name: {kind: 'Name', value: 'name'}},
-                            {kind: 'Field', name: {kind: 'Name', value: 'value'}},
-                            {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
-                          ],
-                        },
-                      },
-                      {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
-                    ],
-                  },
-                },
-                {
-                  kind: 'Field',
-                  name: {kind: 'Name', value: 'userErrors'},
-                  selectionSet: {
-                    kind: 'SelectionSet',
-                    selections: [
-                      {kind: 'Field', name: {kind: 'Name', value: 'field'}},
-                      {kind: 'Field', name: {kind: 'Name', value: 'message'}},
-                      {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
-                    ],
-                  },
-                },
-                {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
-              ],
-            },
-          },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode<StagedUploadsCreateMutation, StagedUploadsCreateMutationVariables>
+export type StagedUploadsCreateMutation = { stagedUploadsCreate?: { stagedTargets?: Array<{ url?: string | null, resourceUrl?: string | null, parameters: Array<{ name: string, value: string }> }> | null, userErrors: Array<{ field?: Array<string> | null, message: string }> } | null };
+
+
+export const StagedUploadsCreateDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"StagedUploadsCreate"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"input"}},"type":{"kind":"NonNullType","type":{"kind":"ListType","type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"StagedUploadInput"}}}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"stagedUploadsCreate"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"input"},"value":{"kind":"Variable","name":{"kind":"Name","value":"input"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"stagedTargets"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"url"}},{"kind":"Field","name":{"kind":"Name","value":"resourceUrl"}},{"kind":"Field","name":{"kind":"Name","value":"parameters"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"value"}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","name":{"kind":"Name","value":"userErrors"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"field"}},{"kind":"Field","name":{"kind":"Name","value":"message"}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}}]}}]} as unknown as DocumentNode<StagedUploadsCreateMutation, StagedUploadsCreateMutationVariables>;
