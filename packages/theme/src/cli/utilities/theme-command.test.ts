69f8bebbd6705c2b358869d02cd8596de500cb29
Julia Boutin <julia.boutin@shopify.com>
2025-07-30 14:49:35 -0600

    Ensure warning is displayed if multiple envs aren't supported

--- packages/theme/src/cli/utilities/theme-command.test.ts
+++ packages/theme/src/cli/utilities/theme-command.test.ts
@@ -4,8 +4,8 @@ import {describe, vi, expect, test, beforeEach} from 'vitest'
 import {Config, Flags} from '@oclif/core'
 import {AdminSession, ensureAuthenticatedThemes} from '@shopify/cli-kit/node/session'
 import {loadEnvironment} from '@shopify/cli-kit/node/environments'
-import {renderConcurrent, renderConfirmationPrompt, renderError} from '@shopify/cli-kit/node/ui'
 import {fileExistsSync} from '@shopify/cli-kit/node/fs'
+import {renderConcurrent, renderConfirmationPrompt, renderError, renderWarning} from '@shopify/cli-kit/node/ui'
 import type {Writable} from 'stream'
 
 vi.mock('@shopify/cli-kit/node/session')
@@ -111,6 +111,10 @@ class TestUnauthenticatedThemeCommand extends ThemeCommand {
   }
 }
 
+class TestNoMultiEnvThemeCommand extends TestThemeCommand {
+  static multiEnvironmentsFlags: RequiredFlags = null
+}
+
 describe('ThemeCommand', () => {
   let mockSession: AdminSession
 
@@ -204,6 +208,31 @@ describe('ThemeCommand', () => {
             expect.objectContaining({prefix: 'staging'}),
           ]),
           showTimestamps: true,
+        }),
+      )
+    })
+
+    test('multiple environments provided - displays warning if not allowed', async () => {
+      // Given
+      const environmentConfig = {store: 'store.myshopify.com'}
+      vi.mocked(loadEnvironment).mockResolvedValue(environmentConfig)
+      vi.mocked(ensureAuthenticatedThemes).mockResolvedValue(mockSession)
+
+      vi.mocked(renderConcurrent).mockResolvedValue(undefined)
+
+      await CommandConfig.load()
+      const command = new TestNoMultiEnvThemeCommand(
+        ['--environment', 'development', '--environment', 'staging'],
+        CommandConfig,
+      )
+
+      // When
+      await command.run()
+
+      // Then
+      expect(renderWarning).toHaveBeenCalledWith(
+        expect.objectContaining({
+          body: 'This command does not support multiple environments.',
         }),
       )
     })
