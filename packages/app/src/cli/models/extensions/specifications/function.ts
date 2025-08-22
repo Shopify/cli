e66f668aa94715dce4e8332c354b8e503e311a49
Isaac Roldán <isaac.roldan@shopify.com>
2025-08-22 13:04:01 +0200

    Remove bundling feature

--- packages/app/src/cli/models/extensions/specifications/function.ts
+++ packages/app/src/cli/models/extensions/specifications/function.ts
@@ -80,7 +80,7 @@ const functionSpec = createExtensionSpecification({
     'pickup_point_delivery_option_generator',
   ],
   schema: FunctionExtensionSchema,
-  appModuleFeatures: (_) => ['function', 'bundling'],
+  appModuleFeatures: (_) => ['function'],
   deployConfig: async (config, directory, apiKey) => {
     let inputQuery: string | undefined
     const moduleId = randomUUID()
