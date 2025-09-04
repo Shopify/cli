fcece685384f094a04d29140413716b8ad5a16d7
Sean McCarthy <sean.mccarthy@shopify.com>
2025-09-04 16:45:19 +1000

    Adding svg to channel specification file patterns

--- packages/app/src/cli/models/extensions/specifications/channel.ts
+++ packages/app/src/cli/models/extensions/specifications/channel.ts
@@ -1,16 +1,15 @@
 import {createExtensionSpecification} from '../specification.js'
 import {BaseSchemaWithHandle} from '../schemas.js'
 import {zod} from '@shopify/cli-kit/node/schema'
-import {joinPath, relativePath} from '@shopify/cli-kit/node/path'
+import {joinPath} from '@shopify/cli-kit/node/path'
 
 const ChannelSpecificationSchema = BaseSchemaWithHandle.extend({
   type: zod.literal('channel_config'),
   name: zod.string().optional(),
-  channel_requirements: zod.any(),
 })
 
 const SUBDIRECTORY_NAME = 'specifications'
-const FILE_EXTENSIONS = ['json', 'toml', 'yaml', 'yml']
+const FILE_EXTENSIONS = ['json', 'toml', 'yaml', 'yml', 'svg']
 
 // Generate glob patterns for all supported file types
 const getGlobPatterns = () => FILE_EXTENSIONS.map((ext) => joinPath(SUBDIRECTORY_NAME, '**', `*.${ext}`))
@@ -20,12 +19,10 @@ const channelSpecificationSpec = createExtensionSpecification({
   schema: ChannelSpecificationSchema,
   buildConfig: {mode: 'copy_files', filePatterns: getGlobPatterns()},
   appModuleFeatures: () => [],
-  deployConfig: async (config, directory) => {
+  deployConfig: async (config, _directory) => {
     return {
       handle: config.handle,
       name: config.name,
-      specifications,
-      channel_requirements: config.channel_requirements,
     }
   },
 })
