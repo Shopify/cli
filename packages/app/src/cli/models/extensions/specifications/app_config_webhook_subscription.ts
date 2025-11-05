b9cc4825ff9279cac46fb85fbf7fdda065a32ad1
George Li <george.li@shopify.com>
2025-11-05 16:39:44 -0500

    added max char validation

--- packages/app/src/cli/models/extensions/specifications/app_config_webhook_subscription.ts
+++ packages/app/src/cli/models/extensions/specifications/app_config_webhook_subscription.ts
@@ -29,7 +29,7 @@ export const SingleWebhookSubscriptionSchema = zod.object({
   include_fields: zod.array(zod.string({invalid_type_error: 'Value must be a string'})).optional(),
   filter: zod.string({invalid_type_error: 'Value must be a string'}).optional(),
   payload_query: zod.string({invalid_type_error: 'Value must be a string'}).trim().min(1).optional(),
-  name: zod.string({invalid_type_error: 'Value must be a string'}).min(1).optional(),
+  name: zod.string({invalid_type_error: 'Value must be a string'}).min(1).max(50).optional(),
 })
 
 /* this transforms webhooks remotely to be accepted by the TOML
