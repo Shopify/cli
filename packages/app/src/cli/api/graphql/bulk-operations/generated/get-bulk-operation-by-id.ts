d6d3dc6812feb201bda016b09d0899e03e096cce
Jordan Verasamy <jordan.verasamy@shopify.com>
2025-11-13 09:44:01 -0800

    introduce query to poll bulk operation status

---
+++ packages/app/src/cli/api/graphql/bulk-operations/generated/get-bulk-operation-by-id.ts
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-redundant-type-constituents */
+import * as Types from './types.js'
+
+import {TypedDocumentNode as DocumentNode} from '@graphql-typed-document-node/core'
+
+export type GetBulkOperationByIdQueryVariables = Types.Exact<{
+  id: Types.Scalars['ID']['input']
+}>
+
+export type GetBulkOperationByIdQuery = {
+  bulkOperation?: {
+    completedAt?: unknown | null
+    createdAt: unknown
+    errorCode?: Types.BulkOperationErrorCode | null
+    fileSize?: unknown | null
+    id: string
+    objectCount: unknown
+    partialDataUrl?: string | null
+    query: string
+    rootObjectCount: unknown
+    status: Types.BulkOperationStatus
+    type: Types.BulkOperationType
+    url?: string | null
+  } | null
+}
+
+export const GetBulkOperationById = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: {kind: 'Name', value: 'GetBulkOperationById'},
+      variableDefinitions: [
+        {
+          kind: 'VariableDefinition',
+          variable: {kind: 'Variable', name: {kind: 'Name', value: 'id'}},
+          type: {kind: 'NonNullType', type: {kind: 'NamedType', name: {kind: 'Name', value: 'ID'}}},
+        },
+      ],
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: {kind: 'Name', value: 'bulkOperation'},
+            arguments: [
+              {
+                kind: 'Argument',
+                name: {kind: 'Name', value: 'id'},
+                value: {kind: 'Variable', name: {kind: 'Name', value: 'id'}},
+              },
+            ],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {kind: 'Field', name: {kind: 'Name', value: 'completedAt'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'createdAt'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'errorCode'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'fileSize'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'id'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'objectCount'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'partialDataUrl'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'query'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'rootObjectCount'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'status'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'type'}},
+                {kind: 'Field', name: {kind: 'Name', value: 'url'}},
+                {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<GetBulkOperationByIdQuery, GetBulkOperationByIdQueryVariables>
