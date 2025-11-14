3793022915c03d00249628f4e837cc642de0c43f
Gray Gilmore <graygilmore@gmail.com>
2025-11-14 14:47:13 -0800

    Move tests to their proper location

--- packages/theme/src/cli/utilities/theme-ui.test.ts
+++ packages/theme/src/cli/utilities/theme-ui.test.ts
@@ -1,7 +1,9 @@
-import {themeComponent, themesComponent, ensureDirectoryConfirmed} from './theme-ui.js'
+import {themeComponent, themesComponent, ensureDirectoryConfirmed, ensureLiveThemeConfirmed} from './theme-ui.js'
 import {Theme} from '@shopify/cli-kit/node/themes/types'
 import {renderConfirmationPrompt, renderError, renderWarning} from '@shopify/cli-kit/node/ui'
-import {test, describe, expect, vi} from 'vitest'
+import {test, describe, expect, vi, beforeEach} from 'vitest'
+import {DEVELOPMENT_THEME_ROLE, LIVE_THEME_ROLE} from '@shopify/cli-kit/node/themes/utils'
+import {buildTheme} from '@shopify/cli-kit/node/themes/factories'
 
 vi.mock('@shopify/cli-kit/node/ui')
 
@@ -66,6 +68,47 @@ describe('ensureDirectoryConfirmed', () => {
       })
       expect(confirmed).toBe(false)
     })
+  })
+})
+
+describe('ensureLiveThemeConfirmed', () => {
+  const theme = buildTheme({id: 123, name: 'My Theme', role: DEVELOPMENT_THEME_ROLE})!
+  const liveTheme = buildTheme({id: 123, name: 'My Theme', role: LIVE_THEME_ROLE})!
+
+  beforeEach(() => {
+    vi.stubGlobal('process', {...process, stdout: {...process.stdout, isTTY: true}})
+  })
+
+  test('prompts for confirmation if acting on a live theme', async () => {
+    // Given
+    vi.mocked(renderConfirmationPrompt).mockResolvedValue(true)
+
+    const result = await ensureLiveThemeConfirmed(liveTheme, 'start development mode', false)
+
+    // Then
+    expect(renderConfirmationPrompt).toHaveBeenCalledWith({
+      message:
+        'You\'re about to start development mode on your live theme "My Theme". This will make changes visible to customers. Are you sure you want to proceed?',
+      confirmationMessage: 'Yes, proceed with live theme',
+      cancellationMessage: 'No, cancel',
+    })
+    expect(result).toBe(true)
+  })
+
+  test('does not prompt for confirmation if acting on a non-live theme', async () => {
+    // Given
+    await ensureLiveThemeConfirmed(theme, 'start development mode', false)
+
+    // Then
+    expect(renderConfirmationPrompt).not.toHaveBeenCalled()
+  })
+
+  test('does not prompt for confirmation if acting on a live theme and allowLive flag is true', async () => {
+    // Given
+    await ensureLiveThemeConfirmed(liveTheme, 'start development mode', true)
+
+    // Then
+    expect(renderConfirmationPrompt).not.toHaveBeenCalled()
   })
 })
 
