f492b21aba5336d8f76215f4ebc36cf39bef1f9a
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2025-12-04 13:05:04 -0500

    initial implementation of 'app execute'

--- packages/app/src/cli/index.ts
+++ packages/app/src/cli/index.ts
@@ -10,7 +10,8 @@ import Logs from './commands/app/logs.js'
 import Sources from './commands/app/app-logs/sources.js'
 import EnvPull from './commands/app/env/pull.js'
 import EnvShow from './commands/app/env/show.js'
-import Execute from './commands/app/bulk/execute.js'
+import BulkExecute from './commands/app/bulk/execute.js'
+import Execute from './commands/app/execute.js'
 import FunctionBuild from './commands/app/function/build.js'
 import FunctionReplay from './commands/app/function/replay.js'
 import FunctionRun from './commands/app/function/run.js'
@@ -53,7 +54,8 @@ export const commands: {[key: string]: typeof AppLinkedCommand | typeof AppUnlin
   'app:config:pull': ConfigPull,
   'app:env:pull': EnvPull,
   'app:env:show': EnvShow,
-  'app:bulk:execute': Execute,
+  'app:execute': Execute,
+  'app:bulk:execute': BulkExecute,
   'app:generate:schema': GenerateSchema,
   'app:function:build': FunctionBuild,
   'app:function:replay': FunctionReplay,
