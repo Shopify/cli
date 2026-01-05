6fd3249853900a77390eeca4f9c514d816478182
Matteo Depalo <matteo.depalo@shopify.com>
2026-01-05 17:11:27 +0000

    Remove leading semicolon

--- packages/app/src/cli/services/dev/app-events/app-event-watcher-handler.ts
+++ packages/app/src/cli/services/dev/app-events/app-event-watcher-handler.ts
@@ -135,8 +135,8 @@ async function reload(app: AppLinkedInterface): Promise<AppLinkedInterface> {
     return newApp
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
-    const err = new Error(`Error reloading app: ${error.message}`)
-    ;(err as Error & {cause: string}).cause = 'validation-error'
+    const err = new Error(`Error reloading app: ${error.message}`) as Error & {cause: string}
+    err.cause = 'validation-error'
     throw err
   }
 }
