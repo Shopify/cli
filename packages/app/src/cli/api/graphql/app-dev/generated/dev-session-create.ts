ab545de729655ce7604893a019c2088b948bfbca
Donald Merand <donald.merand@shopify.com>
2026-01-28 16:05:52 -0500

    Detect dev session takeover

--- packages/app/src/cli/api/graphql/app-dev/generated/dev-session-create.ts
+++ packages/app/src/cli/api/graphql/app-dev/generated/dev-session-create.ts
@@ -1,91 +1,16 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-import * as Types from './types.js'
+/* eslint-disable @typescript-eslint/consistent-type-definitions, @typescript-eslint/naming-convention, @typescript-eslint/ban-types, @typescript-eslint/no-duplicate-type-constituents, @typescript-eslint/no-redundant-type-constituents, @nx/enforce-module-boundaries */
 import {JsonMapType} from '@shopify/cli-kit/node/toml'
+import * as Types from './types.js'
 
-import {TypedDocumentNode as DocumentNode} from '@graphql-typed-document-node/core'
-
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 export type DevSessionCreateMutationVariables = Types.Exact<{
-  appId: Types.Scalars['String']['input']
-  assetsUrl: Types.Scalars['String']['input']
-  websocketUrl?: Types.InputMaybe<Types.Scalars['String']['input']>
-}>
+  appId: Types.Scalars['String']['input'];
+  assetsUrl: Types.Scalars['String']['input'];
+  websocketUrl?: Types.InputMaybe<Types.Scalars['String']['input']>;
+}>;
 
-export type DevSessionCreateMutation = {
-  devSessionCreate?: {
-    userErrors: {message: string; on: JsonMapType; field?: string[] | null; category: string}[]
-  } | null
-}
 
-export const DevSessionCreate = {
-  kind: 'Document',
-  definitions: [
-    {
-      kind: 'OperationDefinition',
-      operation: 'mutation',
-      name: {kind: 'Name', value: 'DevSessionCreate'},
-      variableDefinitions: [
-        {
-          kind: 'VariableDefinition',
-          variable: {kind: 'Variable', name: {kind: 'Name', value: 'appId'}},
-          type: {kind: 'NonNullType', type: {kind: 'NamedType', name: {kind: 'Name', value: 'String'}}},
-        },
-        {
-          kind: 'VariableDefinition',
-          variable: {kind: 'Variable', name: {kind: 'Name', value: 'assetsUrl'}},
-          type: {kind: 'NonNullType', type: {kind: 'NamedType', name: {kind: 'Name', value: 'String'}}},
-        },
-        {
-          kind: 'VariableDefinition',
-          variable: {kind: 'Variable', name: {kind: 'Name', value: 'websocketUrl'}},
-          type: {kind: 'NamedType', name: {kind: 'Name', value: 'String'}},
-        },
-      ],
-      selectionSet: {
-        kind: 'SelectionSet',
-        selections: [
-          {
-            kind: 'Field',
-            name: {kind: 'Name', value: 'devSessionCreate'},
-            arguments: [
-              {
-                kind: 'Argument',
-                name: {kind: 'Name', value: 'appId'},
-                value: {kind: 'Variable', name: {kind: 'Name', value: 'appId'}},
-              },
-              {
-                kind: 'Argument',
-                name: {kind: 'Name', value: 'assetsUrl'},
-                value: {kind: 'Variable', name: {kind: 'Name', value: 'assetsUrl'}},
-              },
-              {
-                kind: 'Argument',
-                name: {kind: 'Name', value: 'websocketUrl'},
-                value: {kind: 'Variable', name: {kind: 'Name', value: 'websocketUrl'}},
-              },
-            ],
-            selectionSet: {
-              kind: 'SelectionSet',
-              selections: [
-                {
-                  kind: 'Field',
-                  name: {kind: 'Name', value: 'userErrors'},
-                  selectionSet: {
-                    kind: 'SelectionSet',
-                    selections: [
-                      {kind: 'Field', name: {kind: 'Name', value: 'message'}},
-                      {kind: 'Field', name: {kind: 'Name', value: 'on'}},
-                      {kind: 'Field', name: {kind: 'Name', value: 'field'}},
-                      {kind: 'Field', name: {kind: 'Name', value: 'category'}},
-                      {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
-                    ],
-                  },
-                },
-                {kind: 'Field', name: {kind: 'Name', value: '__typename'}},
-              ],
-            },
-          },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode<DevSessionCreateMutation, DevSessionCreateMutationVariables>
+export type DevSessionCreateMutation = { devSessionCreate?: { devSession?: { websocketUrl?: string | null, updatedAt: string, user?: { id: string, email?: string | null } | null, app: { id: string, key: string } } | null, warnings?: Array<{ message: string, code: Types.DevSessionWarningCode }> | null, userErrors: Array<{ message: string, on: JsonMapType, field?: Array<string> | null, category: string }> } | null };
+
+
+export const DevSessionCreate = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"DevSessionCreate"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"appId"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"assetsUrl"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"websocketUrl"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"devSessionCreate"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"appId"},"value":{"kind":"Variable","name":{"kind":"Name","value":"appId"}}},{"kind":"Argument","name":{"kind":"Name","value":"assetsUrl"},"value":{"kind":"Variable","name":{"kind":"Name","value":"assetsUrl"}}},{"kind":"Argument","name":{"kind":"Name","value":"websocketUrl"},"value":{"kind":"Variable","name":{"kind":"Name","value":"websocketUrl"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"devSession"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"websocketUrl"}},{"kind":"Field","name":{"kind":"Name","value":"updatedAt"}},{"kind":"Field","name":{"kind":"Name","value":"user"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"email"}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","name":{"kind":"Name","value":"app"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"key"}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","name":{"kind":"Name","value":"warnings"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"message"}},{"kind":"Field","name":{"kind":"Name","value":"code"}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","name":{"kind":"Name","value":"userErrors"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"message"}},{"kind":"Field","name":{"kind":"Name","value":"on"}},{"kind":"Field","name":{"kind":"Name","value":"field"}},{"kind":"Field","name":{"kind":"Name","value":"category"}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}}]}}]} as unknown as DocumentNode<DevSessionCreateMutation, DevSessionCreateMutationVariables>;
