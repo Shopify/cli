6557375477cd20b42b1937d6d182ab9c80d6bc11
Josh White <Josh.White@shopify.com>
2026-01-29 16:22:04 -0500

    add HostedAppSpecIdentifier to CONFIG_EXTENSION_IDs to set experience to configuration

--- packages/app/src/cli/models/extensions/extension-instance.ts
+++ packages/app/src/cli/models/extensions/extension-instance.ts
@@ -12,6 +12,7 @@ import {PosSpecIdentifier} from './specifications/app_config_point_of_sale.js'
 import {PrivacyComplianceWebhooksSpecIdentifier} from './specifications/app_config_privacy_compliance_webhooks.js'
 import {WebhooksSpecIdentifier} from './specifications/app_config_webhook.js'
 import {WebhookSubscriptionSpecIdentifier} from './specifications/app_config_webhook_subscription.js'
+import {HostedAppHomeSpecIdentifier} from './specifications/app_config_hosted_app_home.js'
 import {
   ExtensionBuildOptions,
   buildFunctionExtension,
@@ -40,6 +41,7 @@ export const CONFIG_EXTENSION_IDS: string[] = [
   AppHomeSpecIdentifier,
   AppProxySpecIdentifier,
   BrandingSpecIdentifier,
+  HostedAppHomeSpecIdentifier,
   PosSpecIdentifier,
   PrivacyComplianceWebhooksSpecIdentifier,
   WebhookSubscriptionSpecIdentifier,
