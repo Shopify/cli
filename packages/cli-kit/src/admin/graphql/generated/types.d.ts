e637cdd895ecd5622efd06371968aceface127c9
Ryan Bahan <ryan.bahan@shopify.com>
2026-02-12 10:50:40 -0700

    update types

--- packages/cli-kit/src/admin/graphql/generated/types.d.ts
+++ packages/cli-kit/src/admin/graphql/generated/types.d.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions, @typescript-eslint/naming-convention, @typescript-eslint/no-explicit-any, tsdoc/syntax, @typescript-eslint/no-redundant-type-constituents, @nx/enforce-module-boundaries  */
+/* eslint-disable @typescript-eslint/consistent-type-definitions, @typescript-eslint/naming-convention, @typescript-eslint/no-explicit-any, tsdoc/syntax, @nx/enforce-module-boundaries  */
 import {JsonMapType} from '@shopify/cli-kit/shared/node/toml'
 
 export type Maybe<T> = T | null
