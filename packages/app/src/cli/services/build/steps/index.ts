1a2349bf1c80a2bf26bca110b46dc7215a8bf8cf
Alfonso Noriega <alfonso.noriega@shopify.com>
2026-03-10 09:32:41 +0100

    Add client steps implementation to support existing functionalities

--- packages/app/src/cli/services/build/steps/index.ts
+++ packages/app/src/cli/services/build/steps/index.ts
@@ -1,3 +1,10 @@
+import {executeIncludeAssetsStep} from './include_assets_step.js'
+import {executeBuildThemeStep} from './build-theme-step.js'
+import {executeBundleThemeStep} from './bundle-theme-step.js'
+import {executeBundleUIStep} from './bundle-ui-step.js'
+import {executeCopyStaticAssetsStep} from './copy-static-assets-step.js'
+import {executeBuildFunctionStep} from './build-function-step.js'
+import {executeCreateTaxStubStep} from './create-tax-stub-step.js'
 import type {LifecycleStep, BuildContext} from '../client-steps.js'
 
 /**
@@ -9,16 +16,30 @@ import type {LifecycleStep, BuildContext} from '../client-steps.js'
  * @returns The output from the step execution
  * @throws Error if the step type is not implemented or unknown
  */
-export async function executeStepByType(step: LifecycleStep, _context: BuildContext): Promise<unknown> {
+export async function executeStepByType(step: LifecycleStep, context: BuildContext): Promise<unknown> {
   switch (step.type) {
-    // Future step types (not implemented yet):
     case 'include_assets':
+      return executeIncludeAssetsStep(step, context)
+
     case 'build_theme':
+      return executeBuildThemeStep(step, context)
+
     case 'bundle_theme':
+      return executeBundleThemeStep(step, context)
+
     case 'bundle_ui':
+      return executeBundleUIStep(step, context)
+
     case 'copy_static_assets':
+      return executeCopyStaticAssetsStep(step, context)
+
     case 'build_function':
+      return executeBuildFunctionStep(step, context)
+
     case 'create_tax_stub':
+      return executeCreateTaxStubStep(step, context)
+
+    // Future step types (not implemented yet):
     case 'esbuild':
     case 'validate':
     case 'transform':
