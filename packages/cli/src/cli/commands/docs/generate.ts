15de7e0be8ac23475a0f366776b313e5da3237ac
Laurel Thorburn <codinglaurel@gmail.com>
2026-04-08 10:16:45 -0600

    do not change the clean description variable

--- packages/cli/src/cli/commands/docs/generate.ts
+++ packages/cli/src/cli/commands/docs/generate.ts
@@ -86,10 +86,7 @@ export async function writeCommandDocumentation(
   },`
 
   const description = command.descriptionWithMarkdown ?? command.description ?? command.summary ?? ''
-  const cleanDescription = description
-    ?.replace(/\\/g, '\\\\')
-    .replace(/`/g, '\\`')
-    .replace(/https:\/\/shopify\.dev/g, '')
+  const cleanDescription = description?.replace(/`/g, '\\`').replace(/https:\/\/shopify\.dev/g, '')
   const previewDescription = command.summary ?? description ?? ''
   const cleanPreview = previewDescription.replace(/`/g, '\\`').replace(/https:\/\/shopify\.dev/g, '')
 
