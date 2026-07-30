256a658e1cdd284f8dfcff012dcc1ab9ba333f54
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2026-07-30 13:12:31 +0200

    Make Oxlint the primary linter

---
+++ eslint.codegen.config.js
@@ -0,0 +1,4 @@
+import {eslintBaseConfig} from './eslint.config.js'
+
+// Generated GraphQL output relies on the full ESLint autofix behavior to remain deterministic.
+export default eslintBaseConfig
