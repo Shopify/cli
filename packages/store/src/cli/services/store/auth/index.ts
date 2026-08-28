d555d4cafc8f65793d060c80d5ec7b029746614c
Jeffrey Deng <jeffrey.deng@shopify.com>
2026-08-28 13:42:48 -0700

    Avoid printing sensitive store auth URLs

--- packages/store/src/cli/services/store/auth/index.ts
+++ packages/store/src/cli/services/store/auth/index.ts
@@ -76,7 +76,7 @@ export async function authenticateStoreWithApp(
     ...bootstrap.waitForAuthCodeOptions,
     onListening: async () => {
       const opened = await resolvedDependencies.openURL(authorizationUrl)
-      if (!opened) resolvedDependencies.presenter.manualAuthUrl(authorizationUrl)
+      if (!opened) resolvedDependencies.presenter.manualAuthUrl(authorizationUrl, {sensitive: Boolean(input.signup)})
     },
   })
   const tokenResponse = await bootstrap.exchangeCodeForToken(code)
