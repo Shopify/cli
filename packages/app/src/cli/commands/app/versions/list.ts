2ca5382be9f9b5719483cf7bc3263b5d370ffaba
Donald Merand <donald.merand@shopify.com>
2026-09-10 16:37:25 -0400

    Add JSON schema for app versions list
    Assisted-By: devx/f323f8f7-39c8-42ea-882b-5ae18595fcc9

--- packages/app/src/cli/commands/app/versions/list.ts
+++ packages/app/src/cli/commands/app/versions/list.ts
@@ -1,13 +1,20 @@
 import {appFlags} from '../../../flags.js'
-import versionList from '../../../services/versions-list.js'
+import {appVersionsListJsonOutputSchema, getAppVersions} from '../../../services/versions-list.js'
+import {renderAppVersionsList} from '../../../services/versions-list/presenter.js'
 import AppLinkedCommand, {AppLinkedCommandOutput} from '../../../utilities/app-linked-command.js'
 import {linkedAppContext} from '../../../services/app-context.js'
 import {globalFlags, jsonFlag} from '@shopify/cli-kit/node/cli'
+import {AbortError} from '@shopify/cli-kit/node/error'
+import {outputResult} from '@shopify/cli-kit/node/output'
 
 export default class VersionsList extends AppLinkedCommand {
   static summary = 'List deployed versions of your app.'
 
   static descriptionWithMarkdown = `Lists the deployed app versions. An app version is a snapshot of your app extensions.`
+
+  static get jsonOutputSchema() {
+    return appVersionsListJsonOutputSchema
+  }
 
   static description = this.descriptionForHelp()
 
@@ -27,13 +34,20 @@ export default class VersionsList extends AppLinkedCommand {
       userProvidedConfigName: flags.config,
     })
 
-    await versionList({
-      app,
-      remoteApp,
-      organization,
-      developerPlatformClient,
-      json: flags.json,
-    })
+    const result = await getAppVersions(developerPlatformClient, remoteApp)
+    if (!result) throw new AbortError(`Invalid API Key: ${remoteApp.apiKey}`)
+
+    if (flags.json) {
+      outputResult(appVersionsListJsonOutputSchema.encode(result.appVersions))
+    } else {
+      await renderAppVersionsList({
+        app,
+        remoteApp,
+        organization,
+        developerPlatformClient,
+        ...result,
+      })
+    }
 
     return {app}
   }
